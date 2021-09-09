6208f8cd5e9613ef5b5c8a8106f64f6dfcdaf27f
iknowhtml <iknowsmtp@gmail.com>
2021-09-09 16:32:42 -0400

    Use environment variables for credential

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,24 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-const serviceAccount = require('<PATH TO KEY>');
+
+require('dotenv').config();
+
+const credentialPath = (
+  process.env.NODE_ENV === 'development'
+    ? process.env.DEVELOPMENT_CREDENTIALS
+    : process.env.GOOGLE_APPLICATION_CREDENTIALS
+) as string;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(credentialPath),
 });
 
 export const torusAuth = functions.https.onRequest((request, response) => {
   functions.logger.info('Auth req sent!', { structuredData: true });
 
   const uid = '0xA494addbA803d75D3dA1b667C23ea596F31179b9';
+
+  console.log('aaa');
 
   admin
     .auth()
