fc60ee203df3b99cb329d055bbfd22bc111e037e
iknowhtml <iknowsmtp@gmail.com>
2021-09-09 16:54:56 -0400

    Start Firebase token authentication

--- client/utils/hooks/authentication.ts
+++ client/utils/hooks/authentication.ts
@@ -1,14 +1,20 @@
 import { useRouter } from 'next/router';
 import useWeb3 from './web3';
+import axios from 'axios';
 
 export const useLogin = () => {
   const { activate, connector } = useWeb3();
   const router = useRouter();
 
   return async () => {
-    if (connector) {
-      await activate(connector);
-    }
+    await activate(connector);
+
+    const response = await axios.get('http://localhost:5001/torus-tutorial/us-central1/torusAuth');
+
+    const token = response.data['Bearer Token'];
+
+    console.log(token);
+
     // needs to cast to string since redirect field is string | string[] | undefined
     router.push((router.query.redirect as string) ?? '/');
   };
