69b1336dac1d4107c0fd3cfe34b3e7a0ead7a3e6
iknowhtml <iknowsmtp@gmail.com>
2021-09-10 14:36:37 -0400

    Add logic to retrieve a Signer from a Connector

--- client/utils/hooks/authentication.ts
+++ client/utils/hooks/authentication.ts
@@ -1,21 +1,30 @@
 import { useRouter } from 'next/router';
 import useWeb3 from './web3';
 import axios from 'axios';
-import useFirebase from './firebase';
+import { authentication } from '../firebase';
 import { signInWithCustomToken } from '@firebase/auth';
+import { OpenLoginConnector } from 'utils/Connectors/OpenLoginConnector';
+import { Web3Provider } from '@ethersproject/providers';
+import { Wallet } from '@ethersproject/wallet';
 
 export const useLogin = () => {
   const { activate, connector } = useWeb3();
   const router = useRouter();
-  const { auth } = useFirebase();
 
   return async () => {
     await activate(connector);
 
+    const signer =
+      connector instanceof OpenLoginConnector
+        ? (connector.wallet as Wallet)
+        : new Web3Provider(await connector.getProvider()).getSigner();
+
+    console.log(signer.signMessage('Text'));
+
     const response = await axios.get('http://localhost:5001/torus-tutorial/us-central1/torusAuth');
 
     const token = response.data['Bearer Token'];
-    const userCredentials = await signInWithCustomToken(auth, token);
+    const userCredentials = await signInWithCustomToken(authentication, token);
 
     console.log(userCredentials);
 
