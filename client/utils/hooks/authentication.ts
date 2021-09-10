eb55495f828d218248ad0c3c7ba9bed4d5118136
iknowhtml <iknowsmtp@gmail.com>
2021-09-09 20:04:54 -0400

    Continue Firebase auth

--- client/utils/hooks/authentication.ts
+++ client/utils/hooks/authentication.ts
@@ -1,10 +1,13 @@
 import { useRouter } from 'next/router';
 import useWeb3 from './web3';
 import axios from 'axios';
+import useFirebase from './firebase';
+import { signInWithCustomToken } from '@firebase/auth';
 
 export const useLogin = () => {
   const { activate, connector } = useWeb3();
   const router = useRouter();
+  const { auth } = useFirebase();
 
   return async () => {
     await activate(connector);
@@ -12,8 +15,9 @@ export const useLogin = () => {
     const response = await axios.get('http://localhost:5001/torus-tutorial/us-central1/torusAuth');
 
     const token = response.data['Bearer Token'];
+    const userCredentials = await signInWithCustomToken(auth, token);
 
-    console.log(token);
+    console.log(userCredentials);
 
     // needs to cast to string since redirect field is string | string[] | undefined
     router.push((router.query.redirect as string) ?? '/');
---
+++ client/utils/hooks/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAgiH9P3HVDWmnFZvpgP8nvIpjB93LdBD0',
+  authDomain: 'torus-tutorial.firebaseapp.com',
+  projectId: 'torus-tutorial',
+  storageBucket: 'torus-tutorial.appspot.com',
+  messagingSenderId: '421605888175',
+  appId: '1:421605888175:web:f3571744c16e97bbbb045a',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const db = getFirestore();
+
+export default function useFirebase() {
+  return { auth, db };
+}
