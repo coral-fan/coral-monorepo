31e6c8649993cbc90173c4f55cb174a91d2938d7
iknowhtml <iknowsmtp@gmail.com>
2021-09-12 20:40:09 -0400

    Update Next configuration - Remove unnecessary Node Polyfill plugin - Add fallback configurations for Firebase Admin

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx'],
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      /*
-       fallback field needed due to Webpack 5 no longer auto-polyfilling node core modules
-      See https://stackoverflow.com/questions/64557638/how-to-polyfill-node-core-modules-in-webpack-5
-       */
-      config.plugins = [...config.plugins, new NodePolyfillPlugin()];
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        child_process: false,
+        net: false,
+        tls: false,
+        request: false,
+        worker_threads: false,
+        ['fast-crc32c']: false,
+      };
     }
-
     return config;
   },
 };
