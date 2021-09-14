1f9c7c57ceb1aa6938515918efa94e54f7fe4487
iknowhtml <iknowsmtp@gmail.com>
2021-09-14 17:59:39 -0400

    Fix check for if Firebase Admin is initialized

--- client/utils/firebase/getFirebaseAdmin.ts
+++ client/utils/firebase/getFirebaseAdmin.ts
@@ -2,7 +2,8 @@ export const getFirebaseAdmin = async () => {
   // needs async import or will throw error in console on client due to importing it client side
   const admin = await import('firebase-admin');
   // checks if admin has been initialized already
-  if (admin.app.length < 1) {
+  // debugger;
+  if (admin.apps.length < 1) {
     const credentialPath = (
       process.env.NODE_ENV === 'development'
         ? process.env.FIREBASE_ADMIN_CREDENTIALS
