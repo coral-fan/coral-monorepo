4a106d71747fb37a295461b3574735af4757b51f
iknowhtml <iknowsmtp@gmail.com>
2021-09-14 18:01:32 -0400

    Conditionally use connector & move logic to hook - Add getConnector function that will return correct connector in browser - Move connector and signer hook logic inside web3 hook file

--- client/utils/hooks/web3/useWeb3.ts
+++ client/utils/hooks/web3/useWeb3.ts
@@ -1,19 +1,32 @@
+import { useCallback, useMemo } from 'react';
 import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
-import useConnector from './useConnector';
-import useSigner from './useSigner';
+import { InjectedConnector } from '@web3-react/injected-connector';
+
+import { OpenLoginConnector } from 'utils/Connectors/OpenLoginConnector';
 
 export default function useWeb3() {
-  const { library, chainId, account, active, error, activate, deactivate, setError } = useWeb3React<
-    JsonRpcProvider | Web3Provider
-  >();
+  const { library, chainId, account, active, error, activate, deactivate, setError, connector } =
+    useWeb3React<JsonRpcProvider | Web3Provider>();
 
-  const signer = useSigner();
+  const injectedConnector = useMemo(
+    () => new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] }),
+    []
+  );
+  const openLoginConnector = useMemo(() => new OpenLoginConnector(), []);
 
-  const connector = useConnector();
+  const signer = useMemo(
+    () => (connector instanceof OpenLoginConnector ? connector.wallet : library?.getSigner()),
+    [connector, library]
+  );
+
+  const getConnector = useCallback(
+    () => (connector ?? window.ethereum ? injectedConnector : openLoginConnector),
+    [connector]
+  );
 
   return {
-    connector,
+    getConnector,
     library,
     chainId,
     account,
