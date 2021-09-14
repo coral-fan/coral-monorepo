20e51c3c28d8fef98cdd49804c0665204cf4ac01
iknowhtml <iknowsmtp@gmail.com>
2021-09-14 18:04:18 -0400

    Add CORS & set allowed headers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,59 +16,79 @@ admin.initializeApp({
 });
 
 export const getNonce = functions.https.onRequest(async (request, response) => {
-  // TODO: CORS!!!
   response.set('Access-Control-Allow-Origin', '*');
+  response.set('Access-Control-Allow-Methods', 'POST');
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
 
-  const address = request.body.address;
-  if (!isAddress(address)) {
-    response.status(401).send('');
-  }
-  const nonceRef = await admin.firestore().doc(`Nonce/${address}`).get();
-  try {
-    if (nonceRef.exists) {
-      const nonce = nonceRef.data();
-      response.send(nonce);
-    } else {
-      const newNonce = { nonce: 1 };
-      await admin.firestore().collection('Nonce').doc(address).set(newNonce);
-      response.send(newNonce);
+  if (request.method === 'OPTIONS') {
+    // stop preflight requests here
+    response.status(204).send('');
+    return;
+  } else {
+    const address = request.body.address;
+    if (!isAddress(address)) {
+      response.status(401).send('');
     }
-  } catch (error) {
-    response.status(401).send('');
+    const nonceRef = await admin.firestore().doc(`Nonce/${address}`).get();
+    try {
+      if (nonceRef.exists) {
+        const nonce = nonceRef.data();
+        response.send(nonce);
+      } else {
+        const newNonce = { nonce: 1 };
+        await admin.firestore().collection('Nonce').doc(address).set(newNonce);
+        response.send(newNonce);
+      }
+    } catch (error) {
+      response.status(401).send('');
+    }
   }
 });
 
 export const torusAuth = functions.https.onRequest(async (request, response) => {
   // TODO: CORS!!!
   response.set('Access-Control-Allow-Origin', '*');
+  response.set('Access-Control-Allow-Methods', 'POST');
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
 
-  const { address, signedMessage } = request.body;
-  if (!isAddress(address)) {
-    response.status(401).send('');
-  }
-  try {
-    const nonceRef = await admin.firestore().doc(`Nonce/${address}`).get();
-    if (nonceRef.exists) {
-      const nonceObj = nonceRef.data();
-      const nonce = nonceObj['nonce'] as number;
-      const derivedAddress = verifyTypedData(
-        {},
-        { Nonce: [{ name: 'nonce', type: 'uint256' }] },
-        { nonce: nonce },
-        signedMessage
-      );
-      if (address === derivedAddress) {
-        const nextNonce = nonce + 1;
-        await admin.firestore().doc(`Nonce/${derivedAddress}`).set({ nonce: nextNonce });
-        const customToken = await admin.auth().createCustomToken(derivedAddress);
-        response.send({ 'Bearer Token': customToken });
+  if (request.method === 'OPTIONS') {
+    // stop preflight requests here
+    response.status(204).send('');
+    return;
+  } else {
+    const { address, signedMessage } = request.body;
+    if (!isAddress(address)) {
+      response.status(401).send('');
+    }
+    try {
+      const nonceRef = await admin.firestore().doc(`Nonce/${address}`).get();
+      if (nonceRef.exists) {
+        const nonce = nonceRef.data()?.nonce;
+        const derivedAddress = verifyTypedData(
+          {},
+          { Nonce: [{ name: 'nonce', type: 'uint256' }] },
+          { nonce: nonce },
+          signedMessage
+        );
+        if (address === derivedAddress) {
+          const nextNonce = nonce + 1;
+          await admin.firestore().doc(`Nonce/${derivedAddress}`).set({ nonce: nextNonce });
+          const customToken = await admin.auth().createCustomToken(derivedAddress);
+          response.send({ 'Bearer Token': customToken });
+        } else {
+          response.status(401).send('');
+        }
       } else {
         response.status(401).send('');
       }
-    } else {
+    } catch (error) {
       response.status(401).send('');
     }
-  } catch (error) {
-    response.status(401).send('');
   }
 });
