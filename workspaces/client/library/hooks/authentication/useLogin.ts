46636502e78efa5166abc12cc557f7cb9d7440e5
iknowhtml <iknowsmtp@gmail.com>
2021-09-16 08:37:51 -0400

    Refactor hooks code organization

--- workspaces/client/library/hooks/authentication/useLogin.ts
+++ workspaces/client/library/hooks/authentication/useLogin.ts
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import useWeb3 from './web3';
+import useWeb3 from '../web3';
 import axios from 'axios';
 import { signInWithCustomToken, getAuth } from 'firebase/auth';
 import { Web3Provider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
-import { setCookie, destroyCookie, parseCookies } from 'nookies';
+import { setCookie } from 'nookies';
 import { OpenLoginConnector } from 'library/Connectors/OpenLoginConnector';
-import { LOGIN_ROUTE } from 'consts/routes';
 
 type LoginError = null | Record<string, string | number>;
 
@@ -74,25 +73,4 @@ export const useLogin = () => {
   };
 
   return { login, isLoggingIn, loginError };
-};
-
-export const useLogout = () => {
-  const { active, deactivate } = useWeb3();
-  const router = useRouter();
-  const { token } = parseCookies();
-  return async () => {
-    if (active) {
-      deactivate();
-    }
-    if (token) {
-      destroyCookie(undefined, 'token');
-      await getAuth().signOut();
-    }
-    /* check if current route is for login before performing Firebase & cookie authentication related clean up logic
-       this is necessary because the logout function is also used for MetaMask account/network change events
-    */
-    if (router.route !== LOGIN_ROUTE) {
-      router.push(LOGIN_ROUTE);
-    }
-  };
 };
---
+++ workspaces/client/library/hooks/authentication/useLogout.ts
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router';
+import useWeb3 from '../web3';
+import { getAuth } from 'firebase/auth';
+import { destroyCookie, parseCookies } from 'nookies';
+import { LOGIN_ROUTE } from 'consts/routes';
+
+export const useLogout = () => {
+  const { active, deactivate } = useWeb3();
+  const router = useRouter();
+  const { token } = parseCookies();
+  return async () => {
+    if (active) {
+      deactivate();
+    }
+    if (token) {
+      destroyCookie(undefined, 'token');
+      await getAuth().signOut();
+    }
+    /* check if current route is for login before performing Firebase & cookie authentication related clean up logic
+       this is necessary because the logout function is also used for MetaMask account/network change events
+    */
+    if (router.route !== LOGIN_ROUTE) {
+      router.push(LOGIN_ROUTE);
+    }
+  };
+};
