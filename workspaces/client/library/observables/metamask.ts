58374ff7c39f1fe89efafc0ff25ae3014798e48b
iknowhtml <iknowsmtp@gmail.com>
2021-09-25 09:35:15 -0400

    Remove logic for immediately emitting a value This is necessary for the auto-logout logic in AuthenticationManager

--- workspaces/client/library/observables/metamask.ts
+++ workspaces/client/library/observables/metamask.ts
@@ -10,10 +10,7 @@ export const getChainId$ = (): Observable<string> => {
   // checks if chainId$ are both not undefined
   if (!ref.chainId$) {
     // pipes share so subscriptions can be multi-casted
-    const chainId$ = fromEvent<string>(window.ethereum as any, 'chainChanged').pipe(
-      share(),
-      startWith(window.ethereum?.chainId)
-    );
+    const chainId$ = fromEvent<string>(window.ethereum as any, 'chainChanged').pipe(share());
 
     ref.chainId$ = chainId$;
   }
