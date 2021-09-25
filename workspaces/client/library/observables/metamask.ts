493bc830ef6a9e28067004fec5301aaaa567fee2
iknowhtml <iknowsmtp@gmail.com>
2021-09-25 09:07:20 -0400

    Add chainId observable utility

---
+++ workspaces/client/library/observables/metamask.ts
@@ -0,0 +1,22 @@
+import { fromEvent, Observable, share, startWith } from 'rxjs';
+
+// a ref is used to in order to memoize the chainId observable. Casted to any so properties can be modified
+const ref = {} as any;
+
+export const getChainId$ = (): Observable<string> => {
+  if (typeof window === 'undefined') {
+    throw new Error('getChainId$ should only be called client side.');
+  }
+  // checks if chainId$ are both not undefined
+  if (!ref.chainId$) {
+    // pipes share so subscriptions can be multi-casted
+    const chainId$ = fromEvent<string>(window.ethereum as any, 'chainChanged').pipe(
+      share(),
+      startWith(window.ethereum?.chainId)
+    );
+
+    ref.chainId$ = chainId$;
+  }
+
+  return ref.chainId$;
+};
