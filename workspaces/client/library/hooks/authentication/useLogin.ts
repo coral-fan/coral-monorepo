5aa2516df176167b744ed552d7105cc783834964
iknowhtml <iknowsmtp@gmail.com>
2021-10-04 06:55:22 -0400

    Refactor login to use promises & then syntax

--- workspaces/client/library/hooks/authentication/useLogin.ts
+++ workspaces/client/library/hooks/authentication/useLogin.ts
@@ -6,7 +6,6 @@ import { signInWithCustomToken, getAuth, UserCredential } from 'firebase/auth';
 import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
 import { setCookie } from 'nookies';
-import { map, mergeMap, of, pluck, withLatestFrom } from 'rxjs';
 
 import { OpenLoginConnector } from 'library/Connectors/OpenLoginConnector';
 import { IS_OPEN_LOGIN_PENDING } from 'consts';
@@ -17,10 +16,10 @@ const fetchNonce = (address: string) =>
     address: address,
   });
 
-const signAuthenticatedMessage = ([nonce, signer]: [number, Wallet | JsonRpcSigner]) =>
+const signAuthenticatedMessage = (signer: Wallet | JsonRpcSigner, nonce: number) =>
   signer.signMessage(getAuthenticationMessage(nonce));
 
-const fetchFirebaseAuthToken = ([signedMessage, address]: [string, string]) =>
+const fetchFirebaseAuthToken = (address: string) => (signedMessage: string) =>
   axios.post<{ ['Bearer Token']: string }>(
     'http://localhost:5001/torus-tutorial/us-central1/auth',
     {
@@ -42,47 +41,33 @@ export const useLogin = () => {
 
   const [loginError, setLoginError] = useState(null);
 
-  const login = () => {
+  const login = async () => {
     setIsLoggingIn(true);
 
-    const signer$ = of(getConnector()).pipe(
-      map((connector) => {
-        activate(connector);
-        return connector; // return connector to be used in next function
-      }),
-      mergeMap(async (connector) =>
-        connector instanceof OpenLoginConnector
-          ? (connector.wallet as Wallet)
-          : new Web3Provider(await connector.getProvider()).getSigner()
-      ) // grabs either a wallet or JSON RPC signer depending on if the connector is OpenLogin or Provider
-    );
+    const connector = getConnector();
 
-    const address$ = signer$.pipe(mergeMap((signer) => signer.getAddress()));
+    activate(connector);
 
-    const idToken$ = address$.pipe(
-      mergeMap(fetchNonce), // get nonce from back end
-      pluck('data', 'nonce'), // get nonce value from axios return value
-      withLatestFrom(signer$), // add signer into observable stream
-      mergeMap(signAuthenticatedMessage), //sign a message
-      withLatestFrom(address$), //add address into observable stream
-      mergeMap(fetchFirebaseAuthToken), // get Firebase token from back end
-      pluck('data', 'Bearer Token'), // get Bearer Token from axios return value
-      mergeMap((token) => signInWithCustomToken(getAuth(), token)),
-      mergeMap<UserCredential, Promise<string>>((userCredentials) =>
-        userCredentials.user.getIdToken()
-      ) // get user id token
-    );
+    const signer =
+      connector instanceof OpenLoginConnector
+        ? (connector.wallet as Wallet)
+        : new Web3Provider(await connector.getProvider()).getSigner();
 
-    idToken$.subscribe({
-      next: (idToken) => {
+    const address = await signer.getAddress();
+
+    fetchNonce(address)
+      .then(({ data: { nonce } }) => signAuthenticatedMessage(signer, nonce))
+      .then(fetchFirebaseAuthToken(address))
+      .then(({ data: { ['Bearer Token']: token } }) => signInWithCustomToken(getAuth(), token))
+      .then((userCredentials) => userCredentials.user.getIdToken())
+      .then((idToken) => {
         setCookie(undefined, 'token', idToken, { path: '/' });
         router.push((router.query.redirect as string) ?? '/');
-      },
-      error: (error) => {
+      })
+      .catch((error) => {
         setLoginError(error);
         setIsLoggingIn(false);
-      },
-    });
+      });
   };
 
   return { login, isLoggingIn, loginError };
