bcde81ff33763ad6b7a71dc456ccf9aa641aa025
iknowhtml <iknowsmtp@gmail.com>
2021-10-04 06:39:06 -0400

    Refactor login using RxJS

--- workspaces/client/library/hooks/authentication/useLogin.ts
+++ workspaces/client/library/hooks/authentication/useLogin.ts
@@ -2,15 +2,32 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useWeb3 from '../web3';
 import axios from 'axios';
-import { signInWithCustomToken, getAuth } from 'firebase/auth';
-import { Web3Provider } from '@ethersproject/providers';
+import { signInWithCustomToken, getAuth, UserCredential } from 'firebase/auth';
+import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
 import { setCookie } from 'nookies';
+import { map, mergeMap, of, pluck, withLatestFrom } from 'rxjs';
+
 import { OpenLoginConnector } from 'library/Connectors/OpenLoginConnector';
 import { IS_OPEN_LOGIN_PENDING } from 'consts';
 import { getAuthenticationMessage } from '@common/utils';
 
-type LoginError = null | Record<string, string | number>;
+const fetchNonce = (address: string) =>
+  axios.post<{ nonce: number }>('http://localhost:5001/torus-tutorial/us-central1/nonce', {
+    address: address,
+  });
+
+const signAuthenticatedMessage = ([nonce, signer]: [number, Wallet | JsonRpcSigner]) =>
+  signer.signMessage(getAuthenticationMessage(nonce));
+
+const fetchFirebaseAuthToken = ([signedMessage, address]: [string, string]) =>
+  axios.post<{ ['Bearer Token']: string }>(
+    'http://localhost:5001/torus-tutorial/us-central1/auth',
+    {
+      address,
+      signedMessage,
+    }
+  );
 
 export const useLogin = () => {
   const { activate, getConnector } = useWeb3();
@@ -23,51 +40,49 @@ export const useLogin = () => {
     }
   }, []);
 
-  const [loginError, setLoginError] = useState<LoginError>(null);
+  const [loginError, setLoginError] = useState(null);
 
-  const login = async () => {
+  const login = () => {
     setIsLoggingIn(true);
-    try {
-      const connector = getConnector();
-      await activate(connector);
 
-      const signer =
+    const signer$ = of(getConnector()).pipe(
+      map((connector) => {
+        activate(connector);
+        return connector; // return connector to be used in next function
+      }),
+      mergeMap(async (connector) =>
         connector instanceof OpenLoginConnector
           ? (connector.wallet as Wallet)
-          : new Web3Provider(await connector.getProvider()).getSigner();
+          : new Web3Provider(await connector.getProvider()).getSigner()
+      ) // grabs either a wallet or JSON RPC signer depending on if the connector is OpenLogin or Provider
+    );
 
-      const address = await signer.getAddress();
+    const address$ = signer$.pipe(mergeMap((signer) => signer.getAddress()));
 
-      const nonceResponse = await axios.post<{ nonce: number }>(
-        'http://localhost:5001/torus-tutorial/us-central1/nonce',
-        {
-          address: address,
-        }
-      );
+    const idToken$ = address$.pipe(
+      mergeMap(fetchNonce), // get nonce from back end
+      pluck('data', 'nonce'), // get nonce value from axios return value
+      withLatestFrom(signer$), // add signer into observable stream
+      mergeMap(signAuthenticatedMessage), //sign a message
+      withLatestFrom(address$), //add address into observable stream
+      mergeMap(fetchFirebaseAuthToken), // get Firebase token from back end
+      pluck('data', 'Bearer Token'), // get Bearer Token from axios return value
+      mergeMap((token) => signInWithCustomToken(getAuth(), token)),
+      mergeMap<UserCredential, Promise<string>>((userCredentials) =>
+        userCredentials.user.getIdToken()
+      ) // get user id token
+    );
 
-      const signedMessage = await signer.signMessage(
-        getAuthenticationMessage(nonceResponse.data.nonce)
-      );
-
-      const response = await axios.post('http://localhost:5001/torus-tutorial/us-central1/auth', {
-        address: address,
-        signedMessage: signedMessage,
-      });
-
-      const token = response.data['Bearer Token'];
-
-      const userCredentials = await signInWithCustomToken(getAuth(), token);
-
-      const idToken = await userCredentials.user.getIdToken();
-
-      setCookie(undefined, 'token', idToken, { path: '/' });
-
-      // needs to cast to string since redirect field is string | string[] | undefined
-      router.push((router.query.redirect as string) ?? '/');
-    } catch (error) {
-      setLoginError(error as LoginError);
-      setIsLoggingIn(false);
-    }
+    idToken$.subscribe({
+      next: (idToken) => {
+        setCookie(undefined, 'token', idToken, { path: '/' });
+        router.push((router.query.redirect as string) ?? '/');
+      },
+      error: (error) => {
+        setLoginError(error);
+        setIsLoggingIn(false);
+      },
+    });
   };
 
   return { login, isLoggingIn, loginError };
