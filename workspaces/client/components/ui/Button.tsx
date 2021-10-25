98dc6d3d3c36bc789d946b32d5ac8b46083dd848
iknowhtml <iknowsmtp@gmail.com>
2021-10-25 10:34:58 -0400

    Add hover state styling

--- workspaces/client/components/ui/Button.tsx
+++ workspaces/client/components/ui/Button.tsx
@@ -3,5 +3,10 @@ import tokens from 'styles/tokens';
 
 export const Button = styled.button`
   color: ${tokens.color.black};
+  border-color: ${tokens.color.black};
   background: ${tokens.color.white};
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
