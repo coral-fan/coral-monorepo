2a96768fefea884d60784d3150be81ba55a20bab
iknowhtml <iknowsmtp@gmail.com>
2021-10-27 10:21:01 -0400

    Add & refactor hooks - Add useIsNetworked supported hook - Refactor useGetChainId$ to useGetChainIdChanged$

--- workspaces/client/libraries/hooks/metamask.ts
+++ workspaces/client/libraries/hooks/metamask.ts
@@ -1,11 +1,37 @@
-import { fromEvent, share } from 'rxjs';
+import { SUPPORTED_CHAIN_IDS } from 'consts';
+import { useEffect, useState } from 'react';
+import { fromEvent, map, share, startWith } from 'rxjs';
 import { EventKeys } from 'types/ethereumish';
 import { useGetRefValue } from './utils';
 
-export const useGetChainId$ = () =>
+export const useGetChainIdChanged$ = () =>
   useGetRefValue(
     /* eslint @typescript-eslint/no-explicit-any: 'off' -- window.ethereum must be coerced as any so that fromEvent will accept the value as a EventEmitter*/
     () => fromEvent<EventKeys>(window.ethereum as any, 'chainChanged').pipe(share()),
-    'getChainId$',
+    'getChainIdChanged$',
     false
   );
+
+const getIsNetworkSupported = (chainId: string) => SUPPORTED_CHAIN_IDS.includes(parseInt(chainId));
+
+export const useIsNetworkSupported = () => {
+  const getChainIdChanged$ = useGetChainIdChanged$();
+
+  const [isNetworkSupported, setIsNetworkSupported] = useState(true);
+
+  useEffect(() => {
+    // only runs this if
+    if (window.ethereum) {
+      const isNetworkSupported$ = getChainIdChanged$().pipe(
+        map(getIsNetworkSupported),
+        startWith(getIsNetworkSupported(window.ethereum.chainId))
+      );
+
+      const subscription = isNetworkSupported$.subscribe(setIsNetworkSupported);
+
+      return () => subscription.unsubscribe();
+    }
+  }, [getChainIdChanged$, setIsNetworkSupported, isNetworkSupported]);
+
+  return isNetworkSupported;
+};
