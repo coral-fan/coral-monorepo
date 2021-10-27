07d3f9662ef6fb05ace0974b7d5b64247b12b4eb
iknowhtml <iknowsmtp@gmail.com>
2021-10-27 10:21:58 -0400

    Update import & variable name for chainIdChanged$

--- workspaces/client/components/managers/AuthenticationManager.tsx
+++ workspaces/client/components/managers/AuthenticationManager.tsx
@@ -2,11 +2,11 @@ import { getAuth } from '@firebase/auth';
 import { useEffect } from 'react';
 import { useLogout } from 'libraries/authentication/hooks';
 import { fromEvent, merge } from 'rxjs';
-import { useGetChainId$ } from 'libraries/hooks/metamask';
+import { useGetChainIdChanged$ } from 'libraries/hooks/metamask';
 
 export default function AuthenticationManager() {
   const logout = useLogout();
-  const getChainId$ = useGetChainId$();
+  const getChainIdChanged$ = useGetChainIdChanged$();
 
   useEffect(() => {
     let isLoggingOut = false;
@@ -22,14 +22,14 @@ export default function AuthenticationManager() {
   useEffect(() => {
     if (window.ethereum) {
       const subscription = merge(
-        getChainId$(),
+        getChainIdChanged$(),
         /* eslint @typescript-eslint/no-explicit-any: 'off' -- window.ethereum must be coerced as any so that fromEvent will accept the value as a EventEmitter. */
         fromEvent(window.ethereum as any, 'accountsChanged')
       ).subscribe(logout);
 
       return () => subscription.unsubscribe();
     }
-  }, [getChainId$, logout]);
+  }, [getChainIdChanged$, logout]);
 
   return <></>;
 }
