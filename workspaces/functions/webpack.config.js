cc6925e83089fff91f2c51d41ff65ff5774491a8
iknowhtml <iknowsmtp@gmail.com>
2021-10-28 08:10:59 -0400

    Add & format comments

--- workspaces/functions/webpack.config.js
+++ workspaces/functions/webpack.config.js
@@ -9,21 +9,22 @@ module.exports = (env, argv) => {
       path: path.resolve('dist'),
       filename: 'index.js',
       /*
-    need to specify to make bundle compatible with cloud functions
-    https://stackoverflow.com/questions/59118080/firebase-webpackbabel-functions-not-deploying
-    */
+        need to specify to make bundle compatible with cloud functions
+        https://stackoverflow.com/questions/59118080/firebase-webpackbabel-functions-not-deploying
+      */
       libraryTarget: 'commonjs',
     },
     /*
-  need to specify because Webpack 5 doesn't polyfill Node core modules
-  https://stackoverflow.com/questions/64557638/how-to-polyfill-node-core-modules-in-webpack-5
-  */
+      need to specify because Webpack 5 doesn't polyfill Node core modules
+      https://stackoverflow.com/questions/64557638/how-to-polyfill-node-core-modules-in-webpack-5
+    */
     target: 'node',
     /*
-  needed to suppress WARNING in ../../node_modules/express/lib/view.js 81:13-25 Critical dependency: the request of a dependency is an expression
-  https://stackoverflow.com/questions/41692643/webpack-and-express-critical-dependencies-warning
-  */
+      needed to suppress WARNING in ../../node_modules/express/lib/view.js 81:13-25 Critical dependency: the request of a dependency is an expression
+      https://stackoverflow.com/questions/41692643/webpack-and-express-critical-dependencies-warning
+    */
     externals: [{ express: { commonjs: 'express' } }],
+    // this is neccessary to allow compiling file outside of directory. https://github.com/dividab/tsconfig-paths-webpack-plugin
     resolve: {
       extensions: ['.ts', '.js'],
       plugins: [new TsconfigPathsPlugin()],
