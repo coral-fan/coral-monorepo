36c085c02d33ccb5bde0688914615c46fe2ec80f
iknowhtml <iknowsmtp@gmail.com>
2021-11-07 11:35:54 -0500

    Refactor for independence from React

--- workspaces/client/libraries/utils/hooks.ts
+++ workspaces/client/libraries/utils/hooks.ts
@@ -1,23 +1,25 @@
-import { useCallback, useRef } from 'react';
-
-type UseGetRefValueReturnFunction<T> = () => T;
-type UseGetRefValueReturnTuple<T> = [UseGetRefValueReturnFunction<T>, (value: T) => void];
+type getRefValueReturnFunction<T> = () => T;
+type getRefValueReturnTuple<T> = [getRefValueReturnFunction<T>, (value: T) => void];
 
 type InferUseGetRefReturnType<T, B> = B extends true
-  ? UseGetRefValueReturnTuple<T>
-  : UseGetRefValueReturnFunction<T>;
+  ? getRefValueReturnTuple<T>
+  : getRefValueReturnFunction<T>;
+
+type Ref<T> = {
+  current: undefined | T;
+};
 
 // implementation inspired by https://stackoverflow.com/questions/52817922/typescript-return-type-depending-on-parameter
-export function useGetRefValue<T, B extends boolean>(
+export function getRefValue<T, B extends boolean>(
   getValue: () => T,
   tag: string,
   // this is necessary to ensure type inference for B. Value of B must be passed as parameter or TypeScript won't be able to infer the type
   shouldReturnSetRefValue: B,
   shouldOnlyCallClientSide = true
 ): InferUseGetRefReturnType<T, typeof shouldReturnSetRefValue> {
-  const ref = useRef<undefined | T>(undefined);
+  const ref: Ref<T> = { current: undefined };
 
-  const getRefValue = useCallback(() => {
+  const getRefValue = () => {
     if (shouldOnlyCallClientSide && typeof window === 'undefined') {
       throw new Error(`${tag} should only be called client side.`);
     }
@@ -25,13 +27,13 @@ export function useGetRefValue<T, B extends boolean>(
       ref.current = getValue();
     }
     return ref.current;
-  }, [tag, getValue, shouldOnlyCallClientSide]);
+  };
 
-  const setRefValue = useCallback((value: T) => {
+  const setRefValue = (value: T) => {
     ref.current = value;
-  }, []);
+  };
 
   return (
-    shouldReturnSetRefValue ? <UseGetRefValueReturnTuple<T>>[getRefValue, setRefValue] : getRefValue
+    shouldReturnSetRefValue ? <getRefValueReturnTuple<T>>[getRefValue, setRefValue] : getRefValue
   ) as InferUseGetRefReturnType<T, B>;
 }
