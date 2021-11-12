2cf118671fba4cd8c0398ffe1dde4dbe63607871
iknowhtml <iknowsmtp@gmail.com>
2021-11-12 18:24:54 -0500

    Fix typos/remove unnecessary configurations

--- workspaces/functions/.eslintrc.js
+++ workspaces/functions/.eslintrc.js
@@ -1,32 +1,21 @@
 module.exports = {
-  root: true,
+  parser: '@typescript-eslint/parser',
   env: {
     es6: true,
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir './functions': 
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
+    'import/no-unresolved': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
