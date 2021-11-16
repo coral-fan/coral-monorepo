1db45151038a4049a9dac7acd5f6087dcc52fb26
iknowhtml <iknowsmtp@gmail.com>
2021-11-16 10:01:10 -0500

    Add implementation inspiration link comment

--- workspaces/client/next-server.js
+++ workspaces/client/next-server.js
@@ -1,3 +1,4 @@
+// implementation inspired by https://dev.to/rowaxl/what-i-struggled-with-next-js-using-firebase-hosting-and-enable-ssr-4e67
 // TODO: figure out how to ignore js files for typescript linting
 /* eslint-disable @typescript-eslint/no-var-requires -- this prevents the require statement from causing linting error */
 const { https } = require('firebase-functions');
