024ae121addc740b33f7ee3e239a4e1521041445
iknowhtml <iknowsmtp@gmail.com>
2021-11-24 06:07:43 -0500

    Check if environment variables are undefined

--- workspaces/client/consts/index.ts
+++ workspaces/client/consts/index.ts
@@ -1,17 +1,44 @@
+import { getEnvironmentVariableErrorMessage } from 'libraries/utils/errors';
+
+// application consts
 export const IS_OPEN_LOGIN_PENDING = 'is_open_login_pending';
-
-export const API_ENDPOINT =
-  process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'http://localhost:7070/torus-tutorial/us-central1';
-
-export const AVALANCHE = {
-  CHAIN_ID: process.env.NEXT_PUBLIC_AVALANCHE_CHAIN_ID ?? '0xa869',
-  CHAIN_NAME: process.env.NEXT_PUBLIC_AVALANCHE_CHAIN_NAME ?? 'Avalanche FUJI C-Chain',
-  RPC_URL: process.env.AVALANCHE_RPC_URL ?? 'https://api.avax-test.network/ext/bc/C/rpc',
-  BLOCK_EXPLORER_URL:
-    process.env.NEXT_PUBLIC_AVALANCHE_BLOCK_EXPLORER_URL ??
-    'https://cchain.explorer.avax-test.network/',
-};
 
 export const COOKIE_OPTIONS = {
   path: '/',
+};
+
+// environment variable const
+
+if (!process.env.NEXT_PUBLIC_ENV) {
+  throw Error(getEnvironmentVariableErrorMessage('ENV'));
+}
+
+export const CLIENT_ENVIRONMENT = process.env.NEXT_PUBLIC_ENV;
+
+// api consts
+if (!process.env.NEXT_PUBLIC_API_ENDPOINT) {
+  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_API_ENDPOINT'));
+}
+
+export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
+// avalanche consts
+if (!process.env.NEXT_PUBLIC_AVALANCHE_CHAIN_ID) {
+  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_AVALANCE_CHAIN_ID'));
+}
+if (!process.env.NEXT_PUBLIC_AVALANCHE_CHAIN_NAME) {
+  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_AVALANCHE_CHAIN_NAME'));
+}
+if (!process.env.NEXT_PUBLIC_AVALANCHE_RPC_URL) {
+  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_AVALANCHE_RPC_URL'));
+}
+if (!process.env.NEXT_PUBLIC_AVALANCHE_BLOCK_EXPLORER_URL) {
+  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_AVALANCHE_BLOCK_EXPLORER_URL'));
+}
+
+export const AVALANCHE = {
+  CHAIN_ID: process.env.NEXT_PUBLIC_AVALANCHE_CHAIN_ID,
+  CHAIN_NAME: process.env.NEXT_PUBLIC_AVALANCHE_CHAIN_NAME,
+  RPC_URL: process.env.NEXT_PUBLIC_AVALANCHE_RPC_URL,
+  BLOCK_EXPLORER_URL: process.env.NEXT_PUBLIC_AVALANCHE_BLOCK_EXPLORER_URL,
 };
