db1ea853ed042e4a9315b34d4f5543aa329c44c5
iknowhtml <iknowsmtp@gmail.com>
2021-12-07 17:10:58 -0500

    Add design tokens from Figma doc

--- workspaces/client/styles/tokens.ts
+++ workspaces/client/styles/tokens.ts
@@ -2,9 +2,42 @@ import { css } from '@emotion/react';
 import { createTheme } from 'theme-in-css';
 
 const tokens = createTheme({
+  breakpoint: {
+    desktop: '716px',
+  },
+  spacing: {
+    mobile: {
+      xs: '4px',
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '40px',
+    },
+    desktop: {
+      xs: '4px',
+      sm: '8px',
+      md: '18px',
+      lg: '36px',
+      xl: '60px',
+    },
+  },
   color: {
-    black: '#000',
-    white: '#FFF',
+    background: {
+      primary: '#171818',
+      secondary: '#322D36',
+      tertiary: '#3F3A43',
+    },
+    action: {
+      primary: '#6C28C7',
+      secondary: '#604F75',
+    },
+    gradient: {
+      primary: '',
+    },
+    pallete: {
+      gray: '#9D9D9D',
+      white: '#FFFFFF',
+    },
   },
 });
 
