b14ea7b382f94530e14405c1043acc75c33d9dd6
iknowhtml <iknowsmtp@gmail.com>
2021-12-08 15:09:25 -0500

    Move Button component to individual directory

--- workspaces/client/components/ui/Button.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import tokens from 'styles/tokens';
-
-export const Button = styled.button`
-  color: ${tokens.color.black};
-  border-color: ${tokens.color.black};
-  background: ${tokens.color.white};
-`;
---
+++ workspaces/client/components/ui/Button/Button.tsx
@@ -0,0 +1,4 @@
+import styled from '@emotion/styled';
+import tokens from 'styles/tokens';
+
+export const Button = styled.button``;
