cff6483ea86603ee434b8000a951d280a3077e26
iknowhtml <iknowsmtp@gmail.com>
2021-12-08 15:08:41 -0500

    Update configuration - Update stories blob - Add configuration to accomodate Emotion 11

--- workspaces/client/.storybook/main.js
+++ workspaces/client/.storybook/main.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-dark-mode'],
   framework: '@storybook/react',
   core: {
@@ -14,4 +16,19 @@ module.exports = {
   */
   // TODO: check back to see if this issue above becomes resolved
   typescript: { reactDocgen: false },
+  /*
+    https://stackoverflow.com/questions/65894711/module-not-found-error-cant-resolve-emotion-styled-base-when-running-story
+  */
+  webpackFinal: (config) => {
+    config.resolve.modules.unshift(path.resolve('src'));
+    // Sets up aliases to enable usage of Emotion 11
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@emotion/styled': path.resolve('../../node_modules', '@emotion/styled'),
+      '@emotion/core': path.resolve('../../node_modules', '@emotion/react'),
+      'emotion-theming': path.resolve('../../node_modules', '@emotion/react'),
+    };
+
+    return config;
+  },
 };
