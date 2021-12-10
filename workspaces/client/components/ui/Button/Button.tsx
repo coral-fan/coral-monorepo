f1df7e2251585e724d3acf572bde83095b1de62b
iknowhtml <iknowsmtp@gmail.com>
2021-12-10 16:52:11 -0500

    Extract reusable Button logic/types

--- workspaces/client/components/ui/Button/Button.tsx
+++ workspaces/client/components/ui/Button/Button.tsx
@@ -1,28 +1,10 @@
-import { css } from '@emotion/react';
 import { FC } from 'react';
-import tokens from 'styles/tokens';
-
-type ButtonVariant = 'primary' | 'secondary';
+import { ButtonVariant } from './types';
+import { getButtonStyle } from './utils';
 interface ButtonProps {
   variant?: ButtonVariant;
   icon?: boolean;
 }
-
-const getButtonStyle = (variant: ButtonVariant = 'primary') => css`
-  display: flex;
-  align-items: center;
-  gap: 13.13px;
-  padding: 16px;
-  border-radius: 31px;
-  border: none;
-  font-size: 14px;
-  line-height: 0;
-  text-transform: uppercase;
-  font-weight: 700;
-  height: 45px;
-  color: ${tokens.color.pallete.white};
-  background-color: ${tokens.color.action[variant]};
-`;
 
 const PlusIcon = () => (
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
