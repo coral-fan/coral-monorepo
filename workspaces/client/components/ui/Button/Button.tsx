2875d5f932b18f00280959a044184e3ed0d63c9a
iknowhtml <iknowsmtp@gmail.com>
2021-12-10 12:50:23 -0500

    Add Primary & Secondary Button Components

--- workspaces/client/components/ui/Button/Button.tsx
+++ workspaces/client/components/ui/Button/Button.tsx
@@ -1,4 +1,19 @@
 import styled from '@emotion/styled';
 import tokens from 'styles/tokens';
 
-export const Button = styled.button``;
+interface ButtonProps {
+  variant?: 'primary' | 'secondary';
+}
+
+export const Button = styled.button<ButtonProps>`
+  padding: 16px;
+  border-radius: 31px;
+  border: none;
+  font-size: 14px;
+  line-height: 0;
+  text-transform: uppercase;
+  font-weight: 700;
+  height: 45px;
+  color: ${tokens.color.pallete.white};
+  background-color: ${({ variant = 'primary' }) => tokens.color.action[variant]};
+`;
