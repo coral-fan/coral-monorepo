d43560e89c4fd45ad8d59eb55adb8fa01d981851
iknowhtml <iknowsmtp@gmail.com>
2021-12-10 16:48:24 -0500

    Add Button Component with plus icon

--- workspaces/client/components/ui/Button/Button.tsx
+++ workspaces/client/components/ui/Button/Button.tsx
@@ -1,11 +1,17 @@
-import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { FC } from 'react';
 import tokens from 'styles/tokens';
 
+type ButtonVariant = 'primary' | 'secondary';
 interface ButtonProps {
-  variant?: 'primary' | 'secondary';
+  variant?: ButtonVariant;
+  icon?: boolean;
 }
 
-export const Button = styled.button<ButtonProps>`
+const getButtonStyle = (variant: ButtonVariant = 'primary') => css`
+  display: flex;
+  align-items: center;
+  gap: 13.13px;
   padding: 16px;
   border-radius: 31px;
   border: none;
@@ -15,5 +21,31 @@ export const Button = styled.button<ButtonProps>`
   font-weight: 700;
   height: 45px;
   color: ${tokens.color.pallete.white};
-  background-color: ${({ variant = 'primary' }) => tokens.color.action[variant]};
+  background-color: ${tokens.color.action[variant]};
 `;
+
+const PlusIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3.125 10H16.875"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M10 3.125V16.875"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const Button: FC<ButtonProps> = ({ children, icon, variant }) => (
+  <button css={getButtonStyle(variant)}>
+    {icon && <PlusIcon />}
+    {children}
+  </button>
+);
