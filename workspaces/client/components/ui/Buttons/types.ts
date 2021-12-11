343e39d9703514d0408680e5ec21989079723f17
iknowhtml <iknowsmtp@gmail.com>
2021-12-11 12:56:00 -0500

    Add new line between exports

--- workspaces/client/components/ui/Buttons/types.ts
+++ workspaces/client/components/ui/Buttons/types.ts
@@ -1,6 +1,7 @@
 import { ComponentProps } from 'react';
 
 export type ButtonVariant = 'primary' | 'secondary';
+
 export interface BaseButtonProps extends ComponentProps<'button'> {
   variant?: ButtonVariant;
 }
