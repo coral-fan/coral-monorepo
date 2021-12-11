0daedfd6ee25b86fa6eb1a341343477477ad0b2c
iknowhtml <iknowsmtp@gmail.com>
2021-12-11 05:31:31 -0500

    Add Plus SVG Icon Component

---
+++ workspaces/client/components/ui/Buttons/ModalControlButton/Icons.tsx
@@ -0,0 +1,37 @@
+export const CloseIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M15.625 4.37503L4.375 15.625"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15.625 15.625L4.375 4.37503"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const LeftArrowIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M16.875 10H3.125"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.75 4.37503L3.125 10L8.75 15.625"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
