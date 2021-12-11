82b701be250a47547c3bcefc3b3ceb515844aac8
iknowhtml <iknowsmtp@gmail.com>
2021-12-11 05:32:33 -0500

    Add Modal Control Button Components Stories

---
+++ workspaces/client/components/ui/Buttons/ModalControlButton/ModalControlButton.stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react';
+import { ModalControlButton } from './ModalControlButton';
+
+export default {
+  title: 'Coral/Modal Control Button',
+  component: ModalControlButton,
+} as Meta;
+
+const Template: Story = ({ variant, ...args }) => (
+  <ModalControlButton variant={variant} {...args} />
+);
+
+export const Close = Template.bind({});
+Close.args = {
+  variant: 'close',
+};
+
+export const Previous = Template.bind({});
+Previous.args = {
+  variant: 'previous',
+};
