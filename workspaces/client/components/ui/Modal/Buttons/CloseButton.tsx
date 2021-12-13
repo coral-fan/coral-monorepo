28ad1b51965c33e48ddffc1f97a30590e04ec5e8
iknowhtml <iknowsmtp@gmail.com>
2021-12-13 11:32:45 -0500

    Refactor Control Button to separate Buttons

--- workspaces/client/components/ui/Modal/Buttons/CloseButton.tsx
+++ workspaces/client/components/ui/Modal/Buttons/CloseButton.tsx
@@ -1,3 +1,6 @@
+import { ComponentProps } from 'react';
+import { CONTROL_BUTTON_STYLE } from './consts';
+
 export const CloseIcon = () => (
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
@@ -17,21 +20,8 @@ export const CloseIcon = () => (
   </svg>
 );
 
-export const LeftArrowIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M16.875 10H3.125"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M8.75 4.37503L3.125 10L8.75 15.625"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
+export const CloseButton = (props: ComponentProps<'button'>) => (
+  <button css={CONTROL_BUTTON_STYLE} {...props}>
+    <CloseIcon />
+  </button>
 );
--- workspaces/client/components/ui/Modal/Buttons/consts.ts
+++ workspaces/client/components/ui/Modal/Buttons/consts.ts
@@ -1,14 +1,9 @@
 import { css } from '@emotion/react';
 import tokens from 'styles/tokens';
-import { CloseIcon, LeftArrowIcon } from './Icons';
-
-interface ControlButtonProps {
-  variant: 'close' | 'previous';
-}
 
 const SIZE = '30px';
 
-const controlButtonStyle = css`
+export const CONTROL_BUTTON_STYLE = css`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -20,9 +15,3 @@ const controlButtonStyle = css`
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
   border-radius: 30px;
 `;
-
-export const ControlButton = ({ variant }: ControlButtonProps) => (
-  <button css={[controlButtonStyle]}>
-    {variant === 'close' ? <CloseIcon /> : <LeftArrowIcon />}
-  </button>
-);
