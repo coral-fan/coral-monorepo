dde6b47e9e1fbaecba1339e95319024765b1576b
iknowhtml <iknowsmtp@gmail.com>
2021-12-12 20:26:55 -0500

    Fix gradient design token

--- workspaces/client/styles/tokens.ts
+++ workspaces/client/styles/tokens.ts
@@ -31,12 +31,10 @@ const tokens = createTheme({
       primary: '#6C28C7',
       secondary: '#604F75',
     },
-    gradient: {
-      primary: '',
-    },
     gray: '#9D9D9D',
     white: '#FFFFFF',
   },
+  gradient: 'linear-gradient(121.65deg, #3F2D4D 8.25%, #292030 98.53%)',
 });
 
 export default tokens;
