9dd5ef63d97a5e3cfff82bf42be434bc0743699a
iknowhtml <iknowsmtp@gmail.com>
2021-12-12 19:43:02 -0500

    Add loading arg & variant control configuration

--- workspaces/client/components/ui/Buttons/Button/Button.stories.tsx
+++ workspaces/client/components/ui/Buttons/Button/Button.stories.tsx
@@ -1,24 +1,33 @@
-import { Story, Meta } from '@storybook/react';
+import { Story, ComponentMeta } from '@storybook/react';
 import { Button } from './Button';
 
 export default {
   title: 'Coral/Buttons/Button',
   component: Button,
-} as Meta;
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      control: { type: 'select' },
+    },
+  },
+} as ComponentMeta<typeof Button>;
 
 const ButtonTemplate: Story = (args) => <Button {...args}>Button</Button>;
 
 export const Primary = ButtonTemplate.bind({});
 Primary.args = {
   variant: 'primary',
+  loading: false,
 };
 
 export const Secondary = ButtonTemplate.bind({});
 Secondary.args = {
   variant: 'secondary',
+  loading: false,
 };
 
 export const WithIcon = ButtonTemplate.bind({});
 WithIcon.args = {
-  icon: 'plus',
+  icon: true,
+  loading: false,
 };
