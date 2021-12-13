d8ebec5c68323cec866abc54dd49da859a2d49fd
iknowhtml <iknowsmtp@gmail.com>
2021-12-12 19:33:48 -0500

    Update to fix type error for theme-in-css package

--- workspaces/client/types/theme-in-css.d.ts
+++ workspaces/client/types/theme-in-css.d.ts
@@ -1,5 +1,20 @@
 declare module 'theme-in-css' {
   type ThemeValueMap = Record<string, string | number>;
   type ThemeConfig = Record<string, ThemeValueMap | ThemeConfig>;
+  interface ThemeCSSObject {
+    css: {
+      string: string;
+      properties: Array<[string, string]>;
+    };
+  }
+  declare type ThemeVariableValueMap<Map> = {
+    [key in keyof Map]: Map[key] extends Record<string, string>
+      ? ThemeVariableValueMap<Map[key]>
+      : string;
+  };
+  declare type ThemeVariables<Theme> = {
+    [token in keyof Theme]: ThemeVariableValueMap<Theme[token]>;
+  };
+  declare type Theme<Shape> = ThemeVariables<Shape> & ThemeCSSObject;
   function createTheme<Shape extends ThemeConfig>(config: Shape): Theme<Shape>;
 }
