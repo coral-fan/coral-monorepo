98da209ee5f701ecf80410d3d8402db6c71e992e
maks-p <mpazuniak@gmail.com>
2021-12-14 13:44:36 -0500

    Update ShareButton SVG

--- workspaces/client/pages/asset/components/Buttons/ShareButton/ShareButton.tsx
+++ workspaces/client/pages/asset/components/Buttons/ShareButton/ShareButton.tsx
@@ -2,23 +2,23 @@ import { Button } from 'components/ui/Buttons/Button';
 import { ComponentProps } from 'react';
 
 const ShareIcon = () => (
-  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      d="M10.75 7.24862L16 2L21.25 7.24862"
+      d="M7.21875 5.03039L10.5 1.75L13.7812 5.03039"
       stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
-      d="M16 16V2.00366"
+      d="M10.5 10.5V1.75232"
       stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
-      d="M22 12H25C25.2652 12 25.5196 12.1054 25.7071 12.2929C25.8946 12.4804 26 12.7348 26 13V26C26 26.2652 25.8946 26.5196 25.7071 26.7071C25.5196 26.8946 25.2652 27 25 27H7C6.73478 27 6.48043 26.8946 6.29289 26.7071C6.10536 26.5196 6 26.2652 6 26V13C6 12.7348 6.10536 12.4804 6.29289 12.2929C6.48043 12.1054 6.73478 12 7 12H10"
+      d="M14.25 8H16.125C16.2908 8 16.4497 8.06585 16.5669 8.18306C16.6842 8.30027 16.75 8.45924 16.75 8.625V16.75C16.75 16.9158 16.6842 17.0747 16.5669 17.1919C16.4497 17.3092 16.2908 17.375 16.125 17.375H4.875C4.70924 17.375 4.55027 17.3092 4.43306 17.1919C4.31585 17.0747 4.25 16.9158 4.25 16.75V8.625C4.25 8.45924 4.31585 8.30027 4.43306 8.18306C4.55027 8.06585 4.70924 8 4.875 8H6.75"
       stroke="white"
       stroke-width="2"
       stroke-linecap="round"
