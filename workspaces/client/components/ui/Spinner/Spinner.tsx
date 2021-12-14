2fc3c01b43a7fa546c534c7463db5d9051b9b4a0
iknowhtml <iknowsmtp@gmail.com>
2021-12-14 08:31:53 -0500

    Refactor prop value extraction logic to function

--- workspaces/client/components/ui/Spinner/Spinner.tsx
+++ workspaces/client/components/ui/Spinner/Spinner.tsx
@@ -5,6 +5,8 @@ import { SpinnerBaseProp as SpinnerProp } from './types';
 
 const SIZE = `20px`;
 
+const extractSize = ({ size }: SpinnerProp) => size;
+
 const spin = keyframes`
   0% {
     transform: rotate(0deg);
@@ -17,18 +19,18 @@ const spin = keyframes`
 const Wrapper = styled.div<SpinnerProp>`
   display: inline-block;
   position: relative;
-  width: ${({ size }) => size};
-  height: ${({ size }) => size}; ;
+  width: ${extractSize};
+  height: ${extractSize}; ;
 `;
 
 const Ring = styled.div<SpinnerProp>`
   box-sizing: border-box;
   display: block;
   position: absolute;
-  width: calc(${({ size }) => size} * 0.8);
-  height: calc(${({ size }) => size} * 0.8);
-  margin: calc(${({ size }) => size} / 10);
-  border: calc(${({ size }) => size} / 10) solid ${tokens.color.white};
+  width: calc(${extractSize} * 0.8);
+  height: calc(${extractSize} * 0.8);
+  margin: calc(${extractSize}} / 10);
+  border: calc(${extractSize} / 10) solid ${tokens.color.white};
   border-radius: 50%;
   animation: ${spin} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
   border-color: ${tokens.color.white} transparent transparent transparent;
