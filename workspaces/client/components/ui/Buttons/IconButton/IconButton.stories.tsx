9be45231ed1e3a9a15543cc2fbcf8d64d9e550c0
maks-p <mpazuniak@gmail.com>
2021-12-14 13:54:47 -0500

    Add Plus Icon as default to IconButton

--- workspaces/client/components/ui/Buttons/IconButton/IconButton.stories.tsx
+++ workspaces/client/components/ui/Buttons/IconButton/IconButton.stories.tsx
@@ -12,25 +12,6 @@ export default {
   },
 } as Meta;
 
-const PlusIcon = (
-  <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M5.1875 15.5H25.8125"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M15.5 5.18753V25.8125"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
 const Template: Story = (args) => <IconButton {...args} />;
 
 export const Primary = Template.bind({});
@@ -42,12 +23,5 @@ Primary.args = {
 export const Secondary = Template.bind({});
 Secondary.args = {
   variant: 'secondary',
-  loading: false,
-};
-
-export const PrimaryAddButton = Template.bind({});
-PrimaryAddButton.args = {
-  icon: PlusIcon,
-  variant: 'primary',
   loading: false,
 };
