35b634b7eaf735ee510b6bb6d34ab4baee411733
maks-p <mpazuniak@gmail.com>
2021-12-15 12:25:19 -0500

    Fix linting errors

--- workspaces/client/pages/asset/components/Buttons/ShareButton/ShareButton.tsx
+++ workspaces/client/pages/asset/components/Buttons/ShareButton/ShareButton.tsx
@@ -10,23 +10,23 @@ const SHARE_ICON = (
     <path
       d="M7.21875 5.03039L10.5 1.75L13.7812 5.03039"
       stroke="white"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M10.5 10.5V1.75232"
       stroke="white"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M14.25 8H16.125C16.2908 8 16.4497 8.06585 16.5669 8.18306C16.6842 8.30027 16.75 8.45924 16.75 8.625V16.75C16.75 16.9158 16.6842 17.0747 16.5669 17.1919C16.4497 17.3092 16.2908 17.375 16.125 17.375H4.875C4.70924 17.375 4.55027 17.3092 4.43306 17.1919C4.31585 17.0747 4.25 16.9158 4.25 16.75V8.625C4.25 8.45924 4.31585 8.30027 4.43306 8.18306C4.55027 8.06585 4.70924 8 4.875 8H6.75"
       stroke="white"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
