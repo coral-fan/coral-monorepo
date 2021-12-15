22f6295e57ba6603cb2d1eb3134b281bf465a89c
maks-p <mpazuniak@gmail.com>
2021-12-15 10:26:56 -0500

    Remove defaults, add placeholder icon to SB

--- workspaces/client/components/ui/Buttons/IconButton/IconButton.tsx
+++ workspaces/client/components/ui/Buttons/IconButton/IconButton.tsx
@@ -4,36 +4,11 @@ import { ConditionalSpinner } from '../../Spinner';
 import { BaseButtonProps as IconButtonProps } from '../types';
 import { getGlobalButtonStyle } from '../utils';
 
-// default icon
-const PlusIcon = (
-  <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M5.1875 15.5H25.8125"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M15.5 5.18753V25.8125"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
-
 const iconButtonStyle = css`
   width: ${BUTTON_SIZE};
 `;
 
-export const IconButton = ({
-  variant,
-  icon = PlusIcon,
-  loading = false,
-  ...props
-}: IconButtonProps) => (
+export const IconButton = ({ variant, icon, loading, ...props }: IconButtonProps) => (
   <button css={[getGlobalButtonStyle(variant), iconButtonStyle]} {...props}>
     <ConditionalSpinner loading={loading}>{icon}</ConditionalSpinner>
   </button>
