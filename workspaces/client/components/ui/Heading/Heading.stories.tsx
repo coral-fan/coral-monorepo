acdf404aaa69e42f7f38ce8c1f344410e089d01b
iknowhtml <iknowsmtp@gmail.com>
2021-12-15 09:20:56 -0500

    Add HeadingComponent Stories (level 1, 2 & 3)

--- workspaces/client/components/ui/Heading/Heading.stories.tsx
+++ workspaces/client/components/ui/Heading/Heading.stories.tsx
@@ -1,12 +1,30 @@
-import { Story, Meta } from '@storybook/react';
-import { Heading } from '.';
+import { Story, ComponentMeta } from '@storybook/react';
+import { Heading, HeadingProp } from './Heading';
 
 export default {
   title: 'Coral/Heading',
   component: Heading,
-} as Meta;
+  argTypes: {
+    level: {
+      options: [1, 2, 3],
+      control: { type: 'select' },
+    },
+  },
+} as ComponentMeta<typeof Heading>;
 
-const Template: Story = (args) => <Heading {...args}>Heading</Heading>;
+const Template: Story<HeadingProp> = ({ level }) => (
+  <Heading level={level}>{`Heading Level ${level}`}</Heading>
+);
 
-export const Level1 = Template.bind({});
-Level1.args = {};
+export const h1 = Template.bind({});
+h1.args = {
+  level: 1,
+};
+export const h2 = Template.bind({});
+h2.args = {
+  level: 2,
+};
+export const h3 = Template.bind({});
+h3.args = {
+  level: 3,
+};
