b70a1d0fc3dd2e4733d10381efda1a1c585e39ca
maks-p <mpazuniak@gmail.com>
2021-12-15 10:26:56 -0500

    Remove defaults, add placeholder icon to SB

--- workspaces/client/components/ui/Buttons/IconButton/IconButton.stories.tsx
+++ workspaces/client/components/ui/Buttons/IconButton/IconButton.stories.tsx
@@ -1,6 +1,26 @@
 import { Story, Meta } from '@storybook/react';
 import { IconButton } from './IconButton';
 
+// storybook icon
+const PlusIcon = (
+  <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M5.1875 15.5H25.8125"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15.5 5.18753V25.8125"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 export default {
   title: 'Coral/Buttons/Icon Button',
   component: IconButton,
@@ -8,6 +28,11 @@ export default {
     variant: {
       options: ['primary', 'secondary'],
       control: { type: 'select' },
+    },
+    icon: {
+      table: {
+        disable: true,
+      },
     },
   },
 } as Meta;
@@ -18,10 +43,12 @@ export const Primary = Template.bind({});
 Primary.args = {
   variant: 'primary',
   loading: false,
+  icon: PlusIcon,
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   variant: 'secondary',
   loading: false,
+  icon: PlusIcon,
 };
