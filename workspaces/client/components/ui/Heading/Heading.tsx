9fe8f83d2ecea5ccf8039d3ed8b04069b2a9440a
iknowhtml <iknowsmtp@gmail.com>
2021-12-15 09:20:28 -0500

    Complete Heading Component implementation

--- workspaces/client/components/ui/Heading/Heading.tsx
+++ workspaces/client/components/ui/Heading/Heading.tsx
@@ -1,3 +1,24 @@
+import { css, jsx } from '@emotion/react';
 import { FC } from 'react';
+import tokens from 'styles/tokens';
 
-export const Heading: FC = ({ children }) => <div>{children}</div>;
+const fontSizeDictionary = {
+  1: 24,
+  2: 18,
+  3: 14,
+};
+
+type Level = keyof typeof fontSizeDictionary;
+export interface HeadingProp {
+  level: Level;
+}
+
+const getHeadingStyle = (level: Level) => css`
+  font-weight: bold;
+  color: ${tokens.color.white};
+  font-size: ${`${fontSizeDictionary[level]}px`};
+  ${level === 3 ? 'text-transform: uppercase;' : null}
+`;
+
+export const Heading: FC<HeadingProp> = ({ children, level }) =>
+  jsx(`h${level}`, { css: getHeadingStyle(level) }, children);
