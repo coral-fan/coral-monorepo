163194649a2d1849835a9f3ec2a5691be83d6ce4
iknowhtml <iknowsmtp@gmail.com>
2021-12-19 13:16:08 -0500

    Update Component directories casing

---
+++ workspaces/client/pages/components/Modals/Modals.tsx
@@ -0,0 +1,11 @@
+import { WrongNetworkModal } from './WrongNetworkModal';
+import { SignUpModal } from './SignUpModal';
+import { PurchaseModal } from './PurchaseModal';
+
+export const Modals = () => (
+  <>
+    <WrongNetworkModal />
+    <SignUpModal />
+    <PurchaseModal />
+  </>
+);
