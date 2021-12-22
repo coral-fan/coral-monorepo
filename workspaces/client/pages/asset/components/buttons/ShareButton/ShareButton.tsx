f7c13eace30213a319e6f62787a9512de3f2dd41
maks-p <mpazuniak@gmail.com>
2021-12-22 12:46:30 -0500

    Modify Button to accept icon as prop

--- workspaces/client/pages/asset/components/buttons/ShareButton/ShareButton.tsx
+++ workspaces/client/pages/asset/components/buttons/ShareButton/ShareButton.tsx
@@ -3,9 +3,10 @@ import { ComponentProps } from 'react';
 import { Icon } from 'components/ui';
 import shareIconSVG from './shareIconSVG.svg';
 
+const shareIcon = <Icon svg={shareIconSVG} />;
+
 export const ShareButton = (props: ComponentProps<'button'>) => (
-  <Button variant={'secondary'} {...props}>
-    <Icon svg={shareIconSVG} />
+  <Button icon={shareIcon} variant={'secondary'} {...props}>
     Share
   </Button>
 );
