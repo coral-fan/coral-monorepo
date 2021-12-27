6b6bba6c116102411a869ec1f344a3af33a3cbee
maks-p <mpazuniak@gmail.com>
2021-12-27 14:41:03 -0500

    Add VideoBadge component

---
+++ workspaces/client/src/components/ui/badges/VideoBadge/VideoBadge.stories.tsx
@@ -0,0 +1,26 @@
+import { Story, Meta } from '@storybook/react';
+import { VideoBadge } from './VideoBadge';
+import { BadgeProps } from 'components/ui/Badge/types';
+
+export default {
+  title: 'Coral/Badges/Video Badge',
+  component: VideoBadge,
+  argTypes: {
+    size: {
+      options: ['small', 'medium', 'large', 'xtraLarge'],
+      control: { type: 'select' },
+    },
+    variant: {
+      options: ['primary', 'secondary'],
+      control: { type: 'select' },
+    },
+  },
+} as Meta;
+
+const Template: Story<BadgeProps> = (args) => <VideoBadge {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  size: 'large',
+  variant: 'primary',
+};
