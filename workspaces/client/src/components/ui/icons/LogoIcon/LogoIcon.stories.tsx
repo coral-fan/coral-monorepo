2192125b9a2b3c0ab51af619b331f2dfe8dd4772
iknowhtml <iknowsmtp@gmail.com>
2021-12-28 09:55:57 -0500

    Use Icon Stories configuration util function

--- workspaces/client/src/components/ui/icons/LogoIcon/LogoIcon.stories.tsx
+++ workspaces/client/src/components/ui/icons/LogoIcon/LogoIcon.stories.tsx
@@ -1,17 +1,8 @@
-import { Story, Meta } from '@storybook/react';
+import { getIconStoryConfigurations } from '../utils';
 import { LogoIcon } from './LogoIcon';
-import { IconProps } from 'components/ui/Icon';
 
-export default {
-  title: 'Coral/Icons/Logo Icon',
-  component: LogoIcon,
-  argTypes: {
-    size: {
-      control: { type: 'range' },
-    },
-  },
-} as Meta;
+const { meta, Default } = getIconStoryConfigurations(LogoIcon);
 
-const Template: Story<IconProps> = (args) => <LogoIcon {...args} />;
+export default meta;
 
-export const Default = Template.bind({});
+export { Default };
