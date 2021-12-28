5f05f64ae8f1d03afc226e8df2ecc1bcc29c8ca4
iknowhtml <iknowsmtp@gmail.com>
2021-12-28 15:34:53 -0500

    Extract button base style to constant

--- workspaces/client/src/components/ui/buttons/utils.ts
+++ workspaces/client/src/components/ui/buttons/utils.ts
@@ -1,14 +1,12 @@
 import { css } from '@emotion/react';
 import tokens from 'styles/tokens';
-import { BUTTON_SIZE } from './consts';
+import { BUTTON_BASE_STYLE, BUTTON_SIZE } from './consts';
 import { ButtonVariant } from './types';
 
 export const getButtonVariantStyle = (variant: ButtonVariant = 'primary') => css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  ${BUTTON_BASE_STYLE};
+
   border-radius: 31px;
-  border: none;
   height: ${BUTTON_SIZE};
   color: ${tokens.color.white};
   background-color: ${tokens.color.action[variant]};
