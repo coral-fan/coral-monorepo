57df5a908c9a67680a66b7621bb26a4e53b124c6
maks-p <mpazuniak@gmail.com>
2021-12-28 11:59:54 -0500

    Bug fix: Badge

--- workspaces/client/src/components/ui/Badge/Badge.tsx
+++ workspaces/client/src/components/ui/Badge/Badge.tsx
@@ -1,12 +1,12 @@
 import { getBadgeStyle } from './utils';
 import { Icon } from 'components/ui/Icon';
-import { iconSizeDictionary } from './consts';
+import { sizeDictionary } from './consts';
 import { BaseBadgeProps } from './types';
 
 export const Badge = ({ size, svg, variant = 'primary' }: BaseBadgeProps) => {
   return (
     <div css={getBadgeStyle(size, variant)}>
-      <Icon svg={svg} size={iconSizeDictionary[size]} />
+      <Icon svg={svg} size={sizeDictionary[size].icon} />
     </div>
   );
 };
