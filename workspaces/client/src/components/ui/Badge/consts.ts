b3f4f6b5fdd527405f3b924fdabc09b5fead18c4
maks-p <mpazuniak@gmail.com>
2021-12-27 20:58:53 -0500

    Renaming fix

--- workspaces/client/src/components/ui/Badge/consts.ts
+++ workspaces/client/src/components/ui/Badge/consts.ts
@@ -1,15 +1,15 @@
 export const badgeSizeDictionary = {
-  xtraSmall: '24px',
+  extraSmall: '24px',
   small: '27px',
   medium: '30px',
   large: '40px',
-  xtraLarge: '44.14px',
+  extraLarge: '44.14px',
 };
 
 export const iconSizeDictionary = {
-  xtraSmall: 14,
+  extraSmall: 14,
   small: 17,
   medium: 18,
   large: 22,
-  xtraLarge: 25,
+  extraLarge: 25,
 };
