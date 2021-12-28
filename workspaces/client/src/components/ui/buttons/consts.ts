69b29f5bf749859316912deac8330e6f297a8292
iknowhtml <iknowsmtp@gmail.com>
2021-12-28 15:34:53 -0500

    Extract button base style to constant

--- workspaces/client/src/components/ui/buttons/consts.ts
+++ workspaces/client/src/components/ui/buttons/consts.ts
@@ -1,1 +1,14 @@
+import { css } from '@emotion/react';
+
 export const BUTTON_SIZE = '45px';
+
+export const BUTTON_BASE_STYLE = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
