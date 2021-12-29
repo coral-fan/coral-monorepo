60c6d72046538d7507c700ecccc28362d1ad46ee
iknowhtml <iknowsmtp@gmail.com>
2021-12-29 14:05:27 -0500

    Add Menu Component

---
+++ workspaces/client/src/components/global/NavigationBar/Menu/Menu.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import tokens from 'styles/tokens';
+import { MenuItem } from './MenuItem';
+import { getIconComponent } from 'components/ui/icons/utils';
+import { creditCardSVG, userSVG, warningSVG } from './assets';
+import { Divider } from './Divider';
+import { NotificationMenuItemProp, NotificationMenuItem } from './NotificationMenuItem';
+
+const menuItems = [
+  ['manage credit card', 'CreditCardIcon', creditCardSVG],
+  ['profile', 'ProfileIcon', userSVG],
+  ['sign out', 'WarningIcon', warningSVG],
+].map(([itemText, componentName, iconSVG], index, array) => (
+  <>
+    <Divider isLast={index === array.length - 1} />
+    <MenuItem key={itemText} Icon={getIconComponent(componentName, iconSVG)}>
+      {itemText}
+    </MenuItem>
+  </>
+));
+
+const MenuContainer = styled.div`
+  width: 100%;
+  background-color: ${tokens.color.background.tertiary};
+  color: ${tokens.color.white};
+  padding: 8px 18px;
+  box-shadow: 0px 4px 18px rgba(0, 0, 0, 0.5);
+  border-radius: 12px;
+`;
+
+export type MenuProp = NotificationMenuItemProp;
+
+export const Menu = ({ notificationsCount }: MenuProp) => (
+  <MenuContainer>
+    <NotificationMenuItem notificationsCount={notificationsCount} />
+    {menuItems}
+  </MenuContainer>
+);
