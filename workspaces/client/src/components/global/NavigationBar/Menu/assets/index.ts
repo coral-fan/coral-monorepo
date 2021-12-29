ca3f6c6cebcc5f1c309aa218b55f008945ba179f
iknowhtml <iknowsmtp@gmail.com>
2021-12-29 14:05:05 -0500

    Export Menu Item SVGs Using Index File

---
+++ workspaces/client/src/components/global/NavigationBar/Menu/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as creditCardSVG } from './credit-card.svg';
+export { default as userSVG } from './user.svg';
+export { default as bellSVG } from './bell.svg';
+export { default as warningSVG } from './warning.svg';
