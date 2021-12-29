a9c926957881150452dc214a6987653b21308eb2
maks-p <mpazuniak@gmail.com>
2021-12-29 10:46:57 -0500

    Refactor to use Flex Container

--- workspaces/client/src/components/global/Footer/Footer.tsx
+++ workspaces/client/src/components/global/Footer/Footer.tsx
@@ -1,26 +1,20 @@
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
-import { Center } from 'components/layout';
+import { Flex, Center } from 'components/layout';
 import { LogoIcon } from 'components/ui';
 import tokens from 'styles/tokens';
 
-const footerStyle = css`
+const FOOTER_CONTENT = 'cryptomusic minting since 2021';
+
+const StyledFooter = styled(Center)`
   background: linear-gradient(121.65deg, #3f2d4d 8.25%, #292030 98.53%);
   position: absolute;
-  left: 0%;
-  bottom: 0%;
+  left: 0;
+  bottom: 0;
   width: 100%;
   height: 90px;
 `;
 
-const LogoWrapper = styled.div`
-  position: absolute;
-  top: 26px;
-`;
-
-const ContentWrapper = styled.div`
-  position: absolute;
-  top: 54px;
+const Content = styled.div`
   color: ${tokens.color.white};
   font-weight: 700;
   font-size: 10px;
@@ -30,15 +24,17 @@ const ContentWrapper = styled.div`
   text-transform: uppercase;
 `;
 
-const footerContent = 'cryptomusic minting since 2021';
+const Container = styled(Flex)`
+  gap: 8px;
+`;
 
 export const Footer = () => {
   return (
-    <Center css={footerStyle}>
-      <LogoWrapper>
-        <LogoIcon size={20} alt={'Coral Logo'} />
-      </LogoWrapper>
-      <ContentWrapper>{footerContent}</ContentWrapper>
-    </Center>
+    <StyledFooter>
+      <Container direction={'column'} alignItems={'center'}>
+        <LogoIcon size={20} />
+        <Content>{FOOTER_CONTENT}</Content>
+      </Container>
+    </StyledFooter>
   );
 };
