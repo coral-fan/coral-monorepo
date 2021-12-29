1722f83ee2fe7d5d6867bfc424d62e140dd3d957
maks-p <mpazuniak@gmail.com>
2021-12-29 09:44:06 -0500

    Add Footer Component

---
+++ workspaces/client/src/components/global/Footer/Footer.stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react';
+import { Footer } from './Footer';
+
+export default {
+  title: 'Coral/Footer',
+  component: Footer,
+} as Meta;
+
+const Template: Story = () => <Footer />;
+
+export const Default = Template.bind({});
+
+Default.parameters = {
+  controls: { hideNoControlsWarning: true },
+};
