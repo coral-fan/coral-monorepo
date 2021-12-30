ac73b39e92ac3b66805405631145bfe91f91e6cf
iknowhtml <iknowsmtp@gmail.com>
2021-12-30 13:29:40 -0500

    Refactor Menu Component to use Modal Component

--- workspaces/client/src/components/app/NavigationBar/Menu/Menu.tsx
+++ workspaces/client/src/components/app/NavigationBar/Menu/Menu.tsx
@@ -1,11 +1,10 @@
-import styled from '@emotion/styled';
-import tokens from 'styles/tokens';
+import { Fragment } from 'react';
+import { Modal } from 'components/ui';
 import { MenuItem } from './MenuItem';
 import { getIconComponent } from 'components/ui/icons/utils';
 import { creditCardSVG, userSVG, warningSVG } from './assets';
 import { Divider } from './Divider';
 import { NotificationMenuItemProp, NotificationMenuItem } from './NotificationMenuItem';
-import { Fragment } from 'react';
 
 const MENU_ITEM_CONFIGURATION = [
   ['manage credit card', 'CreditCardIcon', creditCardSVG],
@@ -22,20 +21,11 @@ const menuItems = MENU_ITEM_CONFIGURATION.map(
   )
 );
 
-const MenuContainer = styled.div`
-  width: 100%;
-  background-color: ${tokens.color.background.tertiary};
-  color: ${tokens.color.white};
-  padding: 8px 18px;
-  box-shadow: 0px 4px 18px rgba(0, 0, 0, 0.5);
-  border-radius: 12px;
-`;
-
 export type MenuProp = NotificationMenuItemProp;
 
 export const Menu = ({ notificationsCount }: MenuProp) => (
-  <MenuContainer>
+  <Modal>
     <NotificationMenuItem notificationsCount={notificationsCount} />
     {menuItems}
-  </MenuContainer>
+  </Modal>
 );
