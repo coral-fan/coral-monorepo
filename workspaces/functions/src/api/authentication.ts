e77d51cd939f1194a6ca537eda2608eee89c8cac
iknowhtml <iknowsmtp@gmail.com>
2021-12-30 13:42:07 -0500

    Update Firebase nonce collection name Updated collection for nonce but forgot to change in this file

--- workspaces/functions/src/api/authentication.ts
+++ workspaces/functions/src/api/authentication.ts
@@ -14,14 +14,14 @@ app.post('/api/auth', async (request, response) => {
     return response.status(401).send('');
   }
   try {
-    const nonceRef = await admin.firestore().doc(`Nonce/${address}`).get();
+    const nonceRef = await admin.firestore().doc(`nonce/${address}`).get();
     if (nonceRef.exists) {
       const nonce = nonceRef.data()?.nonce;
       const derivedAddress = verifyMessage(getAuthenticationMessage(nonce), signedMessage);
 
       if (address === derivedAddress) {
         const nextNonce = getNonce();
-        await admin.firestore().doc(`Nonce/${derivedAddress}`).set({ nonce: nextNonce });
+        await admin.firestore().doc(`nonce/${derivedAddress}`).set({ nonce: nextNonce });
         const customToken = await admin.auth().createCustomToken(derivedAddress);
         return response.send({ 'Bearer Token': customToken });
       }
