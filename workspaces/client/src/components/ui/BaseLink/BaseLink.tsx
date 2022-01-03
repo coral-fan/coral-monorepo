ca51d1250a2736e9ce293f968fca346e07be6d18
maks-p <mpazuniak@gmail.com>
2022-01-03 11:27:39 -0500

    Make ref forwardRef parameter, update typing

--- workspaces/client/src/components/ui/BaseLink/BaseLink.tsx
+++ workspaces/client/src/components/ui/BaseLink/BaseLink.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { ForwardedRef, ComponentProps, forwardRef } from 'react';
+import { ForwardedRef, ComponentPropsWithRef, forwardRef } from 'react';
 import { css } from '@emotion/react';
 
 const linkStyle = css`
@@ -9,17 +9,19 @@ const linkStyle = css`
   }
 `;
 
-export interface BaseLinkProps extends ComponentProps<'a'> {
+export interface BaseLinkProps extends ComponentPropsWithRef<'a'> {
   // string type is temporary, should use keys from paths file
   href: string;
-  ref: ForwardedRef<HTMLAnchorElement>;
 }
 
 /*
   Fix for displayName error when using forwardRef from:
   https://github.com/yannickcr/eslint-plugin-react/issues/2269
 */
-export const BaseLink = forwardRef(function BaseLink({ href, ref, ...props }: BaseLinkProps) {
+export const BaseLink = forwardRef(function BaseLink(
+  { href, ...props }: BaseLinkProps,
+  ref: ForwardedRef<HTMLAnchorElement>
+) {
   return (
     <Link href={href} passHref>
       <a css={linkStyle} ref={ref} {...props} />
