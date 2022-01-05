556bc603791f836f53cdaea459866f98ea22db50
maks-p <mpazuniak@gmail.com>
2022-01-05 12:54:13 -0500

    Update directory to Profile

--- workspaces/client/src/components/ui/User/Avatar/Avatar.tsx
+++
@@ -1,7 +0,0 @@
-import { getAvatarStyle } from './utils';
-import { AvatarProps } from './types';
-
-export const Avatar = ({ src, size, showBorder }: AvatarProps) => {
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img src={src} css={getAvatarStyle({ size, showBorder })} alt={''} />;
-};
