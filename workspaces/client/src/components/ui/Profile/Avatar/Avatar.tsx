5d4b80601e3b7d3a9e77c58173f879f76e4fbe55
maks-p <mpazuniak@gmail.com>
2022-01-06 10:39:30 -0500

    Update getImageStyle

--- workspaces/client/src/components/ui/Profile/Avatar/Avatar.tsx
+++ workspaces/client/src/components/ui/Profile/Avatar/Avatar.tsx
@@ -6,14 +6,11 @@ import tokens from 'styles/tokens';
 const DEFAULT_AVATAR =
   'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
 
-const getImageStyle = (hasBorder: boolean) => {
-  return css`
-    border-radius: 50%;
-    border: ${`solid 1px ${hasBorder ? `${tokens.color.white}` : 'transparent'}`};
-  `;
-};
+const getImageStyle = (hasBorder: boolean) => css`
+  border-radius: 50%;
+  border: ${`solid 1px ${hasBorder ? `${tokens.color.white}` : 'transparent'}`};
+`;
 
 export const Avatar = ({ src = DEFAULT_AVATAR, size, hasBorder }: AvatarProps) => {
-  // eslint-disable-next-line @next/next/no-img-element
   return <Image src={src} width={size} height={size} css={getImageStyle(hasBorder)} alt={''} />;
 };
