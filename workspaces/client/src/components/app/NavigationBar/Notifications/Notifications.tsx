7dfe6f532ba0a4b8cbe71ca6fa3448e7c7a3eb07
iknowhtml <iknowsmtp@gmail.com>
2022-01-07 17:50:11 -0500

    Add Notifications component

---
+++ workspaces/client/src/components/app/NavigationBar/Notifications/Notifications.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import { Modal } from 'components/ui';
+import { Notification } from 'libraries/models';
+import { Fragment } from 'react';
+import { Divider } from '../Divider';
+import { Bottom, Heading, NotificationProps } from '../../Notification';
+
+const NotificationContainer = styled.div`
+  padding: 14px 0;
+`;
+
+const NotificationItem = ({ heading, message, timestamp }: NotificationProps) => (
+  <NotificationContainer>
+    <Heading>{heading}</Heading>
+    <Bottom message={message} timestamp={timestamp}></Bottom>
+  </NotificationContainer>
+);
+
+export interface NotificationsProp {
+  notifications?: Notification[];
+}
+
+export const Notifications = ({ notifications }: NotificationsProp) => (
+  <Modal variant="previous" title="Notifications" onClick={() => console.log('clicked')}>
+    {notifications?.map((notification, i, array) => (
+      <Fragment key={i}>
+        <NotificationItem {...notification} />
+        {i === array.length - 1 ? null : <Divider />}
+      </Fragment>
+    ))}
+  </Modal>
+);
