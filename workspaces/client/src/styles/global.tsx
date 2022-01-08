7134fb4faac5a44566da9ad2b6e9b1c9b9895878
iknowhtml <iknowsmtp@gmail.com>
2022-01-08 16:52:17 -0500

    export global style serialized style for Storybook

--- workspaces/client/src/styles/global.tsx
+++ workspaces/client/src/styles/global.tsx
@@ -1,7 +1,7 @@
 import { css, Global } from '@emotion/react';
 import tokens, { globalTokens } from './tokens';
 
-const globalStyles = css`
+export const globalStyles = css`
   @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
   * {
     box-sizing: border-box;
