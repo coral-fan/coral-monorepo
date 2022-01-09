7f4373030e0807a20c20bfabcba2143149d5706d
iknowhtml <iknowsmtp@gmail.com>
2022-01-09 11:53:49 -0500

    Extract button style from consts & update name

--- workspaces/client/src/components/ui/buttons/consts.ts
+++ workspaces/client/src/components/ui/buttons/consts.ts
@@ -1,14 +1,1 @@
-import { css } from '@emotion/react';
-
 export const BUTTON_SIZE = '45px';
-
-export const BUTTON_BASE_STYLE = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: none;
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
