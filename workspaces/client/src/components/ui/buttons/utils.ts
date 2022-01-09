5dc61e4713ae67c77e0aadcb07f1bf129f4242a3
iknowhtml <iknowsmtp@gmail.com>
2022-01-09 11:54:15 -0500

    use new button style constant

--- workspaces/client/src/components/ui/buttons/utils.ts
+++ workspaces/client/src/components/ui/buttons/utils.ts
@@ -1,11 +1,11 @@
 import { css } from '@emotion/react';
 import tokens from 'styles/tokens';
-import { BUTTON_BASE_STYLE, BUTTON_SIZE } from './consts';
+import { BUTTON_SIZE } from './consts';
+import { buttonBaseStyle } from './styles';
 import { ButtonVariant } from './types';
 
 export const getButtonVariantStyle = (variant: ButtonVariant = 'primary') => css`
-  ${BUTTON_BASE_STYLE};
-
+  ${buttonBaseStyle};
   border-radius: 31px;
   height: ${BUTTON_SIZE};
   color: ${tokens.color.white};
