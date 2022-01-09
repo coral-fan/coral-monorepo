cfb40acd8e4a5b8ef8b039d3804381397506598b
iknowhtml <iknowsmtp@gmail.com>
2022-01-09 11:53:49 -0500

    Extract button style from consts & update name

---
+++ workspaces/client/src/components/ui/buttons/styles.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+
+export const buttonBaseStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
