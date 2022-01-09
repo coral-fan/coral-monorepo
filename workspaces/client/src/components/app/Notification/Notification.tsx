c878778c5a50e7ebd46a05f65376cca00dd1c89e
iknowhtml <iknowsmtp@gmail.com>
2022-01-09 09:07:35 -0500

    Add mount & unmount transition

--- workspaces/client/src/components/app/Notification/Notification.tsx
+++ workspaces/client/src/components/app/Notification/Notification.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { getIconComponent } from 'components/ui/icons/utils';
@@ -21,6 +22,7 @@ const closeButtonStyle = css`
   ${BUTTON_BASE_STYLE};
   background-color: transparent;
 `;
+
 const CloseButton = (props: ComponentProps<'button'>) => (
   <button css={closeButtonStyle} {...props}>
     <CloseIcon />
@@ -51,20 +53,34 @@ export const Bottom = ({ message, timestamp }: Omit<NotificationProps, 'heading'
   </BottomContainer>
 );
 
+const CLASS_NAME = 'notification';
+const TIMEOUT = 500;
+
 const NotificationContainer = styled(Card)`
   padding: 16px 18px;
   flex-direction: column;
   gap: 3px;
+  transition: transform ${TIMEOUT}ms;
+
+  &.${CLASS_NAME}-appear, &.${CLASS_NAME}-exit-active {
+    transform: translateX(9999px);
+  }
+
+  &.${CLASS_NAME}-appear-active, &.${CLASS_NAME}-exit {
+    transform: translateX(0px);
+  }
 `;
 
 export const Notification = ({ heading, message, timestamp }: NotificationProps) => (
-  <NotificationContainer>
-    <TopContainer>
-      <Heading>{heading}</Heading>
-      <CloseButton />
-    </TopContainer>
-    <Bottom message={message} timestamp={timestamp}></Bottom>
-  </NotificationContainer>
+  <CSSTransition in={true} appear={true} timeout={TIMEOUT} classNames={CLASS_NAME}>
+    <NotificationContainer>
+      <TopContainer>
+        <Heading>{heading}</Heading>
+        <CloseButton />
+      </TopContainer>
+      <Bottom message={message} timestamp={timestamp}></Bottom>
+    </NotificationContainer>
+  </CSSTransition>
 );
 
 export type { NotificationProps };
