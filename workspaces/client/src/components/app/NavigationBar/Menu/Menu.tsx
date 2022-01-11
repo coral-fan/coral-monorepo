8ca8541e3d6ebd92204b6285054aecb16cf71132
iknowhtml <iknowsmtp@gmail.com>
2022-01-10 20:16:28 -0500

    Remove logic using Divider component

--- workspaces/client/src/components/app/NavigationBar/Menu/Menu.tsx
+++ workspaces/client/src/components/app/NavigationBar/Menu/Menu.tsx
@@ -1,9 +1,7 @@
-import { Fragment } from 'react';
 import { Modal } from 'components/ui';
 import { MenuItem } from './MenuItem';
 import { getIconComponent } from 'components/ui/icons/utils';
 import { creditCardSVG, userSVG, warningSVG } from './assets';
-import { Divider } from '../Divider';
 import { NotificationMenuItemProp, NotificationMenuItem } from './NotificationMenuItem';
 
 const MENU_ITEM_CONFIGURATION = [
@@ -12,20 +10,15 @@ const MENU_ITEM_CONFIGURATION = [
   ['sign out', 'WarningIcon', warningSVG],
 ];
 
-const menuItems = MENU_ITEM_CONFIGURATION.map(
-  ([itemText, componentName, iconSVG], index, array) => (
-    <Fragment key={itemText}>
-      <Divider isLast={index === array.length - 1} />
-      <MenuItem Icon={getIconComponent(componentName, iconSVG)}>{itemText}</MenuItem>
-    </Fragment>
-  )
-);
-
 export type MenuProp = NotificationMenuItemProp;
 
 export const Menu = ({ notificationsCount }: MenuProp) => (
   <Modal>
     <NotificationMenuItem notificationsCount={notificationsCount} />
-    {menuItems}
+    {MENU_ITEM_CONFIGURATION.map(([itemText, componentName, iconSVG]) => (
+      <MenuItem key={itemText} Icon={getIconComponent(componentName, iconSVG)}>
+        {itemText}
+      </MenuItem>
+    ))}
   </Modal>
 );
