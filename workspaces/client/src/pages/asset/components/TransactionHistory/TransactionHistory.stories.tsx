c1e9a34b623a5be991004ac8ae97fe44276de47b
maks-p <mpazuniak@gmail.com>
2022-01-11 10:47:17 -0500

    Refactor Transaction History container

---
+++ workspaces/client/src/pages/asset/components/TransactionHistory/TransactionHistory.stories.tsx
@@ -0,0 +1,76 @@
+import { Story, Meta } from '@storybook/react';
+import { TransactionHistory, TransactionsProp } from './TransactionHistory';
+
+export default {
+  title: 'Coral/Pages/Asset/Transaction History',
+  component: TransactionHistory,
+} as Meta;
+
+const Template: Story<TransactionsProp> = (args) => <TransactionHistory {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  transactions: [
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+    {
+      src: 'https://www.stereofox.com/images/86513/resized.jpg',
+      description: '1 token claimed',
+      username: '@bonobooooos',
+      timestamp: new Date().toDateString(),
+    },
+  ],
+};
