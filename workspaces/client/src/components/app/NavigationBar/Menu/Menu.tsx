2c9efd570d0745068cdb7ef52d1d8e9993ad4f57
iknowhtml <iknowsmtp@gmail.com>
2022-01-11 13:12:29 -0500

    Simplify naming for local sub-component(s)

--- workspaces/client/src/components/app/NavigationBar/Menu/Menu.tsx
+++ workspaces/client/src/components/app/NavigationBar/Menu/Menu.tsx
@@ -1,8 +1,8 @@
 import { Modal } from 'components/ui';
-import { MenuItem } from './MenuItem';
 import { getIconComponent } from 'components/ui/icons/utils';
+import { Item } from './Item';
 import { creditCardSVG, userSVG, warningSVG } from './assets';
-import { NotificationMenuItemProp, NotificationMenuItem } from './NotificationMenuItem';
+import { NotificationItemProp, NotificationItem } from './NotificationItem';
 
 const MENU_ITEM_CONFIGURATION = [
   ['manage credit card', 'CreditCardIcon', creditCardSVG],
@@ -10,15 +10,15 @@ const MENU_ITEM_CONFIGURATION = [
   ['sign out', 'WarningIcon', warningSVG],
 ];
 
-export type MenuProp = NotificationMenuItemProp;
+export type MenuProp = NotificationItemProp;
 
 export const Menu = ({ notificationsCount }: MenuProp) => (
   <Modal>
-    <NotificationMenuItem notificationsCount={notificationsCount} />
+    <NotificationItem notificationsCount={notificationsCount} />
     {MENU_ITEM_CONFIGURATION.map(([itemText, componentName, iconSVG]) => (
-      <MenuItem key={itemText} Icon={getIconComponent(componentName, iconSVG)}>
+      <Item key={itemText} Icon={getIconComponent(componentName, iconSVG)}>
         {itemText}
-      </MenuItem>
+      </Item>
     ))}
   </Modal>
 );
