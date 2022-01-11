7ca7aa44672ece5b491775a26e563ed467adad20
iknowhtml <iknowsmtp@gmail.com>
2022-01-10 20:17:11 -0500

    Refactor Notifications to use CSS for dividers

--- workspaces/client/src/components/app/NavigationBar/Notifications/Notifications.tsx
+++ workspaces/client/src/components/app/NavigationBar/Notifications/Notifications.tsx
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
-import { Fragment } from 'react';
 import { Notification } from 'libraries/models';
 import { Modal } from 'components/ui';
-import { Divider } from '../Divider';
 import { Bottom, Heading, NotificationProps } from '../../Notification';
 
 const NotificationContainer = styled.div`
   padding: 14px 0;
+
+  &:not(:last-child) {
+    border-bottom: solid #4b4b4b 1px;
+  }
 `;
 
 const NotificationItem = ({ heading, message, timestamp }: NotificationProps) => (
@@ -22,11 +24,8 @@ export interface NotificationsProp {
 
 export const Notifications = ({ notifications }: NotificationsProp) => (
   <Modal variant="previous" title="Notifications" onClick={() => console.log('clicked')}>
-    {notifications?.map((notification, i, array) => (
-      <Fragment key={i}>
-        <NotificationItem {...notification} />
-        {i === array.length - 1 ? null : <Divider />}
-      </Fragment>
+    {notifications?.map((notification, i) => (
+      <NotificationItem key={i} {...notification} />
     ))}
   </Modal>
 );
