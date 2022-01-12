ae47446a3a991d076f6fe7a59f752d632db3d2f0
iknowhtml <iknowsmtp@gmail.com>
2022-01-12 12:33:32 -0500

    Add components/layout to match designs

--- workspaces/client/src/components/app/modals/SignUpModal.tsx
+++ workspaces/client/src/components/app/modals/SignUpModal.tsx
@@ -1,6 +1,15 @@
+import styled from '@emotion/styled';
 import { Modal, Button } from 'components/ui';
+import { Input } from 'components/ui/Input';
 import { useIsSigningUp } from 'libraries/authentication/hooks';
 import { useSignUpCompletedSubject } from 'libraries/authentication/hooks/signUpCompleteSubject';
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding-top: 24px;
+`;
 
 export const SignUpModal = () => {
   const [isSigningUp] = useIsSigningUp();
@@ -13,8 +22,14 @@ export const SignUpModal = () => {
   const completeSignUp = () => signUpCompletedSubject.next();
 
   return (
-    <Modal>
-      <Button onClick={completeSignUp}>Complete Sign Up</Button>
+    <Modal title="Sign up">
+      <Content>
+        <Input label="Pick a display name" placeholder="Enter a name" />
+        <Input label="Pick a username" placeholder="username" />
+        <Button onClick={completeSignUp} disabled={true}>
+          Create Account
+        </Button>
+      </Content>
     </Modal>
   );
 };
