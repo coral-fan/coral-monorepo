b34714ce8831811f79a640b5cbdf62fcaa1bcc39
iknowhtml <iknowsmtp@gmail.com>
2022-01-12 17:04:45 -0500

    Extract Profile Avatar Button to separate file

---
+++ workspaces/client/src/components/app/NavigationBar/components/ProfileAvatarButton.tsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+import { useLogout } from 'libraries/authentication/hooks';
+import { Avatar } from 'components/ui';
+import { buttonBaseStyle } from 'components/ui/buttons/styles';
+
+const profileAvatarButtonStyle = css`
+  ${buttonBaseStyle};
+  background-color: transparent;
+  margin-left: auto;
+`;
+
+export const ProfileAvatarButton = () => {
+  const logout = useLogout();
+  return (
+    <button css={profileAvatarButtonStyle} onClick={logout}>
+      <Avatar hasBorder={true} size={30} />
+    </button>
+  );
+};
