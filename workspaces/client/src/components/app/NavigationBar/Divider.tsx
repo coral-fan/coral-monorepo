b8ce32dd8c208a245403227480c84f5369f8f6d0
iknowhtml <iknowsmtp@gmail.com>
2022-01-12 16:49:19 -0500

    Remove unused Divider component

--- workspaces/client/src/components/app/NavigationBar/Divider.tsx
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled';
-import tokens from 'styles/tokens';
-
-export const Divider = styled.hr<{ isLast?: boolean }>`
-  border-color: ${({ isLast = false }) => (isLast ? tokens.color.white : '#4B4B4B')};
-  border-width: 0px 0px thin;
-`;
