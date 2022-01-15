77ef10f78488405aee64f3ad11bd9b8eff4123a8
iknowhtml <iknowsmtp@gmail.com>
2022-01-15 16:57:30 -0500

    Check if is signing up for authenticated state

--- workspaces/client/src/pages/index.page.tsx
+++ workspaces/client/src/pages/index.page.tsx
@@ -1,11 +1,14 @@
-import { useIsTokenAuthenticated } from 'libraries/authentication/hooks';
+import { useIsSigningUp, useIsTokenAuthenticated } from 'libraries/authentication/hooks';
 
 export default function Home() {
   const [isTokenAuthenticated] = useIsTokenAuthenticated();
+  const [isSigningUp] = useIsSigningUp();
 
   return (
     <>
-      <div>You are {`${isTokenAuthenticated ? 'authenticated' : 'not authenticated'}.`}</div>
+      <div>
+        You are {`${isTokenAuthenticated && !isSigningUp ? 'authenticated' : 'not authenticated'}.`}
+      </div>
     </>
   );
 }
