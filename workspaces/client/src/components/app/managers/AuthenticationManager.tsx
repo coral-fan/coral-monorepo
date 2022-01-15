553cb6392b7b4582e8de117f53c0d98e9da647d5
iknowhtml <iknowsmtp@gmail.com>
2022-01-15 17:18:49 -0500

    Remove logic to set is authenticated state

--- workspaces/client/src/components/app/managers/AuthenticationManager.tsx
+++ workspaces/client/src/components/app/managers/AuthenticationManager.tsx
@@ -1,25 +1,9 @@
 import { useEffect } from 'react';
 import { IS_OPEN_LOGIN_PENDING } from 'consts';
-import { useWeb3 } from 'libraries/blockchain/hooks';
-import {
-  useIsTokenAuthenticated,
-  useIsAuthenticated,
-  useIsLoggingIn,
-} from 'libraries/authentication/hooks';
+import { useIsLoggingIn } from 'libraries/authentication/hooks';
 
-const getIsAuthenticated = (isTokenAuthenticated: boolean, isConnectorActive: boolean) =>
-  isTokenAuthenticated && isConnectorActive;
-
+// checks if token is authenticated, user is not signing up and that their wallet is active and updates the redux store state accordingly
 export const AuthenticationManager = () => {
-  const [isTokenAuthenticated] = useIsTokenAuthenticated();
-  const { active } = useWeb3();
-
-  const [, setIsAuthenticated] = useIsAuthenticated();
-
-  useEffect(() => {
-    setIsAuthenticated(getIsAuthenticated(isTokenAuthenticated, active));
-  }, [setIsAuthenticated, isTokenAuthenticated, active]);
-
   const [, setIsLoggingIn] = useIsLoggingIn();
 
   useEffect(() => {
