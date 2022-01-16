1b6fdde334ca48b37fe050cbd720cacce6ccd847
iknowhtml <iknowsmtp@gmail.com>
2022-01-16 07:39:32 -0500

    Update action creator name to avoid confusion - When using state hooks, convention for setter function is set<STATE> - Name was the same for action creators which was confusing

--- workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
+++ workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
@@ -1,11 +1,11 @@
 import { RootState } from 'libraries/state/types';
 import { useDispatch, useSelector } from 'react-redux';
-import { setIsSigningUp } from '../slice';
+import { updateIsSigningUp } from '../slice';
 
 export const useIsSigningUp = (): [boolean, (isSigningUp: boolean) => void] => {
   const dispatch = useDispatch();
   return [
     useSelector((state: RootState) => state.authentication.isSigningUp),
-    (isSigningUp: boolean) => dispatch(setIsSigningUp(isSigningUp)),
+    (isSigningUp: boolean) => dispatch(updateIsSigningUp(isSigningUp)),
   ];
 };
