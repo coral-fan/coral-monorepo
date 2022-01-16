888d2718c5b5acbbc098e5c40e2f24eb143f47cf
iknowhtml <iknowsmtp@gmail.com>
2022-01-16 07:14:33 -0500

    Update authentication message copy

--- workspaces/common/utils/src/index.ts
+++ workspaces/common/utils/src/index.ts
@@ -1,4 +1,4 @@
 export const getAuthenticationMessage = (nonce: number) =>
-  `Hi from Crypto Music (Tentative)!\n
+  `Hi from Coral!\n
    Sign this message to prove you own this wallet to login. This won't cost any AVAX.\n
    As a security measure, here's a nonce: ${nonce}`;
