70c826cd42bbf4e0215beac7f845632fe10614da
iknowhtml <iknowsmtp@gmail.com>
2022-01-16 06:14:47 -0500

    Uncomment epic & add placeholder logic

--- workspaces/client/src/libraries/authentication/epics.ts
+++ workspaces/client/src/libraries/authentication/epics.ts
@@ -1,21 +1,13 @@
-export {};
-// import { createAction } from '@reduxjs/toolkit';
-// import { Action } from 'redux';
-// import { filter, map, Observable, tap } from 'rxjs';
-// import { setIsSigningUp } from './slice';
+import { createAction } from '@reduxjs/toolkit';
+import { Action } from 'redux';
+import { filter, map, Observable } from 'rxjs';
+import { setIsLoggingIn } from './slice';
 
-// const terminateActionStream = createAction('terminate_action_stream');
+const terminateActionStream = () => createAction('terminate_action_stream');
 
-// const IS_SIGNING_UP = 'isSigningUp';
-
-// // typing from https://stackoverflow.com/questions/64320308/react-observable-epic-with-redux-toolkit-and-typescript
-// export const signUpEpic = (action$: Observable<Action>) =>
-//   action$.pipe(
-//     filter(setIsSigningUp.match),
-//     tap(({ payload: isSigningUp }) =>
-//       isSigningUp
-//         ? localStorage.setItem(IS_SIGNING_UP, JSON.stringify(isSigningUp))
-//         : localStorage.removeItem(IS_SIGNING_UP)
-//     ),
-//     map(() => terminateActionStream())
-//   );
+// typing from https://stackoverflow.com/questions/64320308/react-observable-epic-with-redux-toolkit-and-typescript
+export const signUpEpic = (action$: Observable<Action>) =>
+  action$.pipe(
+    // filter((action) => setIsLoggingIn.match(action) && action.payload === false),
+    map(terminateActionStream)
+  );
