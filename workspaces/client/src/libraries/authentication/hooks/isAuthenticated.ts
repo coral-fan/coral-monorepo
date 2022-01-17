7fc095b80bc8a058b6f798a5bc5a6b5c03482acf
iknowhtml <iknowsmtp@gmail.com>
2022-01-17 11:25:24 -0500

    Remove unnecessary array deconstruction logic

--- workspaces/client/src/libraries/authentication/hooks/isAuthenticated.ts
+++ workspaces/client/src/libraries/authentication/hooks/isAuthenticated.ts
@@ -3,7 +3,7 @@ import { useIsSigningUp, useIsTokenAuthenticated } from '.';
 
 export const useIsAuthenticated = () => {
   const { active } = useWeb3();
-  const [isTokenAuthenticated] = useIsTokenAuthenticated();
+  const isTokenAuthenticated = useIsTokenAuthenticated();
   const [isSigningUp] = useIsSigningUp();
 
   return active && isTokenAuthenticated && !isSigningUp;
