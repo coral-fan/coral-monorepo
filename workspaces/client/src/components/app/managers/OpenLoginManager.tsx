601cdd91e0f3f41ce6998c4663faf65939f3addb
iknowhtml <iknowsmtp@gmail.com>
2022-01-17 08:02:45 -0500

    Rename & update logic for Authentication Manager

--- workspaces/client/src/components/app/managers/OpenLoginManager.tsx
+++ workspaces/client/src/components/app/managers/OpenLoginManager.tsx
@@ -1,16 +1,18 @@
 import { useEffect } from 'react';
 import { IS_OPEN_LOGIN_PENDING } from 'consts';
-import { useIsLoggingIn } from 'libraries/authentication/hooks';
+import { useIsLoggingIn, useLogin } from 'libraries/authentication/hooks';
 
 // checks if token is authenticated, user is not signing up and that their wallet is active and updates the redux store state accordingly
-export const AuthenticationManager = () => {
+export const OpenLoginManager = () => {
+  const { login } = useLogin();
   const [, setIsLoggingIn] = useIsLoggingIn();
 
   useEffect(() => {
     if (sessionStorage.getItem(IS_OPEN_LOGIN_PENDING)) {
       setIsLoggingIn(true);
+      login();
     }
-  }, [setIsLoggingIn]);
+  }, [setIsLoggingIn, login]);
 
   return <></>;
 };
