31a3b8065a58f7ea79a4ba72248dc312e4323916
iknowhtml <iknowsmtp@gmail.com>
2022-01-18 06:04:04 -0500

    Refactor useIsNetworkSupported hook Update to use observable utility hook

--- workspaces/client/src/libraries/blockchain/hooks/metamask.ts
+++ workspaces/client/src/libraries/blockchain/hooks/metamask.ts
@@ -1,31 +1,20 @@
-import { useEffect, useState } from 'react';
 import { delay, map, retryWhen, startWith } from 'rxjs';
 import { AVALANCHE } from 'consts';
 import { getChainIdChanged$ } from '../observables';
+import { useObservable } from 'libraries/utils/hooks';
 
-const getIsNetworkSupported = (chainId: string) => chainId.toLowerCase() === AVALANCHE.CHAIN_ID;
+const getIsNetworkSupported$ = () =>
+  getChainIdChanged$().pipe(
+    startWith(window.ethereum.chainId),
+    map((chainId: string) => chainId?.toLowerCase() === AVALANCHE.CHAIN_ID),
+    /*
+      retry is necessary because in production, chainId can be null.
+      likely a race condition where metamask doesn't initialize before react
+    */
+    retryWhen((error) => error.pipe(delay(1000)))
+  );
 
-export const useIsNetworkSupported = () => {
-  const [isNetworkSupported, setIsNetworkSupported] = useState(false);
+const isNetworkSupportedInvariant = () => window.ethereum && window.ethereum.addListener;
 
-  useEffect(() => {
-    // only runs this ethereum provider is injected by metamask & not brave
-    if (window.ethereum && window.ethereum.addListener) {
-      const isNetworkSupported$ = getChainIdChanged$().pipe(
-        startWith(window.ethereum.chainId),
-        map(getIsNetworkSupported),
-        /*
-        retry is neccessary because in production, chainId can be null.
-        likely a race condition where metamask doesn't initialize before react
-         */
-        retryWhen((error) => error.pipe(delay(1000)))
-      );
-
-      const subscription = isNetworkSupported$.subscribe(setIsNetworkSupported);
-
-      return () => subscription.unsubscribe();
-    }
-  }, []);
-
-  return isNetworkSupported;
-};
+export const useIsNetworkSupported = () =>
+  useObservable(getIsNetworkSupported$, false, isNetworkSupportedInvariant);
