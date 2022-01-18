297fa5477eee109984f6659c21c95d980b814939
iknowhtml <iknowsmtp@gmail.com>
2022-01-18 16:52:48 -0500

    Refactor is token authenticated hook - Use observable hook - Add get token observable that polls cookie every 500 ms - Add get cookie helper function

--- workspaces/client/src/libraries/authentication/hooks/isTokenAuthenticated.ts
+++ workspaces/client/src/libraries/authentication/hooks/isTokenAuthenticated.ts
@@ -1,3 +1,22 @@
+import { useObservable } from 'libraries/utils/hooks';
 import { parseCookies } from 'nookies';
+import { filter, interval, map, startWith, tap } from 'rxjs';
 
-export const useIsTokenAuthenticated = () => parseCookies().hasOwnProperty('token');
+export const getToken = () => parseCookies().token;
+
+export const getToken$ = (initialToken?: string) => {
+  const tokenRef = { current: initialToken };
+  return interval(500).pipe(
+    startWith(getToken()),
+    map(getToken),
+    filter((token) => token !== tokenRef.current),
+    tap((token) => (tokenRef.current = token))
+  );
+};
+
+export const useToken = () => useObservable<string | undefined>(getToken$, getToken());
+
+export const useIsTokenAuthenticated = () => {
+  const token = useToken();
+  return token !== undefined;
+};
