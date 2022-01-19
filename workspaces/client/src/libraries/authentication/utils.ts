5774a0455d6a2721b5d38936b983ca811aa1df42
iknowhtml <iknowsmtp@gmail.com>
2022-01-19 08:44:07 -0500

    Move get message code from common to client

--- workspaces/client/src/libraries/authentication/utils.ts
+++ workspaces/client/src/libraries/authentication/utils.ts
@@ -1,3 +1,8 @@
 import { parseCookies } from 'nookies';
 
 export const getToken = () => parseCookies().token;
+
+export const getAuthenticationMessage = (nonce: number) =>
+  `Hi from Coral!\n
+   Sign this message to prove you own this wallet to login. This won't cost any AVAX.\n
+   As a security measure, here's a nonce: ${nonce}`;
