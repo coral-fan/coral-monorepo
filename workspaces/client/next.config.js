40156f846b58a2522d9aabfd85134150551271aa
iknowhtml <iknowsmtp@gmail.com>
2022-01-19 11:08:22 -0500

    Remove unnecessary Next configurations

--- workspaces/client/next.config.js
+++ workspaces/client/next.config.js
@@ -9,16 +9,6 @@ module.exports = withImages({
   images: {
     domains: ['upload.wikimedia.org'], // Placeholder Avatar Image
   },
-  /*
-    neccessary to ensure Next can compile file outside of project directory.
-    https://github.com/vercel/next.js/blob/canary/packages/next/build/webpack-config.ts (line 927, search config.experimental.externalDir)
-    https://stackoverflow.com/questions/63450928/nextjs-import-external-components-from-parent-directory
-    https://github.com/vercel/next.js/issues/9474#issuecomment-810212174
-  */
-
-  experimental: {
-    externalDir: true,
-  },
   webpack: (config, { isServer }) => {
     // resolves issues with Firebase Admin
     if (!isServer) {
@@ -31,19 +21,13 @@ module.exports = withImages({
         request: false,
         worker_threads: false,
         ['fast-crc32c']: false,
+        http2: false,
+        dns: false,
       };
     }
+
+    config.optimization.providedExports = true;
+
     return config;
   },
-  // async redirects() {
-  //   return process.env.NODE_ENV === 'development'
-  //     ? [
-  //         {
-  //           source: '/api/:path*',
-  //           destination: 'http://localhost:7070/api/:path*',
-  //           permanent: false,
-  //         },
-  //       ]
-  //     : [];
-  // },
 });
