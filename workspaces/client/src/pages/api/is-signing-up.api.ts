81b89a68424905a832a71117d9990c2a8610c22d
iknowhtml <iknowsmtp@gmail.com>
2022-01-19 10:58:34 -0500

    Remove unnecessary documentation comment

--- workspaces/client/src/pages/api/is-signing-up.api.ts
+++ workspaces/client/src/pages/api/is-signing-up.api.ts
@@ -2,7 +2,6 @@ import { getFirebaseAdmin } from 'libraries/firebase';
 import { Handler } from './types';
 import { getHandler } from './utils';
 
-// this route format is necessary becasue rewrite done remove source: https://stackoverflow.com/questions/64916582/firebase-functions-return-404
 const post: Handler = async (req, res) => {
   const { idToken } = req.body;
   if (idToken) {
