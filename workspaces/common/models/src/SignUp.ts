f776e65f5e5492d33a809f289142c99b124a4d6b
iknowhtml <iknowsmtp@gmail.com>
2022-01-19 08:42:34 -0500

    Remove common workspace from monorepo

--- workspaces/common/models/src/SignUp.ts
+++
@@ -1,3 +0,0 @@
-export interface SignUp {
-  isSigningUp: boolean;
-}
--- workspaces/common/utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export const getAuthenticationMessage = (nonce: number) =>
-  `Hi from Coral!\n
-   Sign this message to prove you own this wallet to login. This won't cost any AVAX.\n
-   As a security measure, here's a nonce: ${nonce}`;
