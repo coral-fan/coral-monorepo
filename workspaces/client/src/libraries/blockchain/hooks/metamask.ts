39e46a89f26b64bfc4023d1d3810063d6f518f22
iknowhtml <iknowsmtp@gmail.com>
2022-01-20 07:50:04 -0500

    Add logic to handle null case for chain id

--- workspaces/client/src/libraries/blockchain/hooks/metamask.ts
+++ workspaces/client/src/libraries/blockchain/hooks/metamask.ts
@@ -1,4 +1,4 @@
-import { delay, map, retryWhen, startWith, tap } from 'rxjs';
+import { delay, map, retryWhen, startWith, tap, throwError } from 'rxjs';
 import { AVALANCHE } from 'consts';
 import { getChainIdChanged$ } from '../observables';
 import { useObservable } from 'libraries/utils/hooks';
@@ -7,7 +7,12 @@ const getIsNetworkSupported$ = () =>
   getChainIdChanged$().pipe(
     startWith(window.ethereum.chainId),
     tap(console.log),
-    map((chainId: string) => chainId?.toLowerCase() === AVALANCHE.CHAIN_ID),
+    map((chainId: null | string) => {
+      if (chainId === null) {
+        return throwError(() => new Error());
+      }
+      return chainId.toLowerCase() === AVALANCHE.CHAIN_ID;
+    }),
     /*
       retry is necessary because in production, chainId can be null.
       likely a race condition where metamask doesn't initialize before react
