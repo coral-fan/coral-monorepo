ae8e0014ade76a26a9f7c43c642712b4862a1f94
maks-p <mpazuniak@gmail.com>
2022-01-21 13:48:41 -0500

    Delete Flex, Center components

--- workspaces/client/src/components/layout/Center.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { Flex } from './Flex';
-
-export const Center: FC<{ className?: string }> = ({ children, className }) => (
-  <Flex className={className} justifyContent={'center'} alignItems={'center'}>
-    {children}
-  </Flex>
-);
