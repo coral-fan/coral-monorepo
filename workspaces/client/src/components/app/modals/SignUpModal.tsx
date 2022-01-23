d85e51337512ad38c64d57631ee00b3d9a97b4f4
iknowhtml <iknowsmtp@gmail.com>
2022-01-23 08:22:03 -0500

    Update layout & add form validation logic

--- workspaces/client/src/components/app/modals/SignUpModal.tsx
+++ workspaces/client/src/components/app/modals/SignUpModal.tsx
@@ -4,13 +4,21 @@ import { Input } from 'components/ui/Input';
 
 import { useIsSigningUp } from 'libraries/authentication';
 import { useIsNetworkSupported } from 'libraries/blockchain';
+import { useForm } from 'react-hook-form';
+import { boolean, object, string } from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-const Content = styled.div`
+const SignUpForm = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 24px;
-  padding-top: 24px;
+  padding-top: 18px;
   align-items: center;
+`;
+
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
 
 const LegalAgreementContainer = styled.div`
@@ -18,15 +26,39 @@ const LegalAgreementContainer = styled.div`
   gap: 10px;
   align-items: center;
   width: fit-content;
+  padding-bottom: 20px;
 `;
 
 const LegalAgreementCopy = styled.div`
   font-size: 14px;
 `;
 
+const signUpSchema = object({
+  username: string().required().min(3),
+  email: string()
+    .email()
+    .optional()
+    .transform((value) => (value === '' ? undefined : value)),
+  doesAgree: boolean().required().default(false).isTrue(),
+});
+
 export const SignUpModal = () => {
   const [isSigningUp, setIsSigningUp] = useIsSigningUp();
   const isNetworkSupported = useIsNetworkSupported();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(signUpSchema),
+    mode: 'all',
+  });
+
+  const completeSignUp = handleSubmit((data) => {
+    console.log(data);
+    setIsSigningUp(false);
+  });
 
   if (!isSigningUp || !isNetworkSupported) {
     return null;
@@ -34,12 +66,23 @@ export const SignUpModal = () => {
 
   return (
     <Modal title="Sign up">
-      <Content>
-        <Input label="Pick a display name" placeholder="Enter a name" />
-        <Input label="Pick a username" placeholder="username" />
-        <Input label="Email address" placeholder="example@email.com" />
+      <SignUpForm onSubmit={completeSignUp}>
+        <InputsContainer>
+          <Input
+            label="Pick a username"
+            placeholder="username"
+            {...register('username')}
+            error={errors?.username?.message}
+          />
+          <Input
+            label="Email address"
+            placeholder="example@email.com"
+            {...register('email')}
+            error={errors?.email?.message}
+          />
+        </InputsContainer>
         <LegalAgreementContainer>
-          <Toggle />
+          <Toggle {...register('doesAgree')} />
           <LegalAgreementCopy>
             <div>
               I agree to Coral&apos;s <u>privacy policy</u>
@@ -49,8 +92,10 @@ export const SignUpModal = () => {
             </div>
           </LegalAgreementCopy>
         </LegalAgreementContainer>
-        <Button onClick={() => setIsSigningUp(false)}>Create Account</Button>
-      </Content>
+        <Button type="submit" disabled={!isValid}>
+          Create Account
+        </Button>
+      </SignUpForm>
     </Modal>
   );
 };
