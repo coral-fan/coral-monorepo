2b8c9d5621accefcd68d402a83ceed441461354c
iknowhtml <iknowsmtp@gmail.com>
2022-01-23 10:14:20 -0500

    Update Handler type

--- workspaces/client/src/pages/api/types.ts
+++ workspaces/client/src/pages/api/types.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export type Handler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
+export type Handler = (req: NextApiRequest, res: NextApiResponse) => Promise<void> | void;
 
 export interface MethodHandlers {
   get?: Handler;
