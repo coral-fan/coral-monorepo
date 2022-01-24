0df9ef7790a9953245d25f4e0480ebdcd93f6ea9
iknowhtml <iknowsmtp@gmail.com>
2022-01-24 12:54:04 -0500

    Use latest Firebase Admin API for API handlers

--- workspaces/client/src/pages/api/auth.api.ts
+++ workspaces/client/src/pages/api/auth.api.ts
@@ -1,7 +1,9 @@
 import { isAddress } from '@ethersproject/address';
 import { verifyMessage } from '@ethersproject/wallet';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
 import { getAuthenticationMessage } from 'libraries/authentication';
-import { getFirebaseAdmin } from 'libraries/firebase';
+import { initializeFirebaseAdmin } from 'libraries/firebase';
 import { Handler } from './types';
 import { getHandler } from './utils';
 import { getNonce } from './utils/nonce';
@@ -12,16 +14,17 @@ const post: Handler = async (req, res) => {
     return res.status(400).send('Address is not valid.');
   }
   try {
-    const admin = await getFirebaseAdmin();
-    const nonceRef = await admin.firestore().doc(`nonce/${address}`).get();
+    await initializeFirebaseAdmin();
+    const firestore = getFirestore();
+    const nonceRef = await firestore.doc(`nonce/${address}`).get();
     if (nonceRef.exists) {
       const nonce = nonceRef.data()?.nonce;
       const derivedAddress = verifyMessage(getAuthenticationMessage(nonce), signedMessage);
 
       if (address === derivedAddress) {
         const nextNonce = getNonce();
-        await admin.firestore().doc(`nonce/${derivedAddress}`).set({ nonce: nextNonce });
-        const token = await admin.auth().createCustomToken(derivedAddress);
+        await firestore.doc(`nonce/${derivedAddress}`).set({ nonce: nextNonce });
+        const token = await getAuth().createCustomToken(derivedAddress);
         return res.status(200).send({ token });
       }
     }
--- workspaces/client/src/pages/api/nonce.api.ts
+++ workspaces/client/src/pages/api/nonce.api.ts
@@ -1,5 +1,6 @@
 import { isAddress } from '@ethersproject/address';
-import { getFirebaseAdmin } from 'libraries/firebase';
+import { getFirestore } from 'firebase-admin/firestore';
+import { initializeFirebaseAdmin } from 'libraries/firebase';
 import { Handler } from './types';
 import { getHandler } from './utils';
 import { getNonce } from './utils/nonce';
@@ -9,16 +10,18 @@ const post: Handler = async (req, res) => {
   if (!isAddress(address)) {
     return res.status(400).send('Address is not valid.');
   }
-  const admin = await getFirebaseAdmin();
-  const nonceRef = await admin.firestore().doc(`nonce/${address}`).get();
+
+  await initializeFirebaseAdmin();
+  const firestore = getFirestore();
+  const nonceRef = await firestore.doc(`nonce/${address}`).get();
   try {
     if (nonceRef.exists) {
       const nonce = nonceRef.data();
       return res.send(nonce);
     } else {
       const nonce = getNonce();
-      await admin.firestore().collection('nonce').doc(address).set({ nonce });
-      await admin.firestore().collection('is-signing-up').doc(address).set({ isSigningUp: true });
+      await firestore.collection('nonce').doc(address).set({ nonce });
+      await firestore.collection('is-signing-up').doc(address).set({ isSigningUp: true });
       return res.send(nonce);
     }
   } catch (error) {
