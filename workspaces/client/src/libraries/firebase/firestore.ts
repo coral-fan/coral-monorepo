e3ffd4792295f9dec7e1c93dcb73336b568e23b5
iknowhtml <iknowsmtp@gmail.com>
2022-01-24 12:57:50 -0500

    Add helpers & Firebase snapshot/reference utils

--- workspaces/client/src/libraries/firebase/firestore.ts
+++ workspaces/client/src/libraries/firebase/firestore.ts
@@ -1,8 +1,44 @@
 import { getApp } from 'firebase/app';
-import { getFirestore, doc } from 'firebase/firestore';
+import {
+  getFirestore as getFirestoreClientSide,
+  doc,
+  getDoc,
+  DocumentReference,
+} from 'firebase/firestore';
+import { initializeFirebaseAdmin, initializeFirebaseApp } from '.';
 
-export const getDocRef = (collection: string, id: string) => {
-  const firebaseApp = getApp();
-  const firestoreDb = getFirestore(firebaseApp);
-  return doc(firestoreDb, collection, id);
+const isServerSide = () => typeof window === 'undefined';
+
+const getFirestoreServerSide = async () => {
+  initializeFirebaseAdmin();
+  const { getApp } = await import('firebase-admin/app');
+  const app = getApp();
+  const { getFirestore } = await import('firebase-admin/firestore');
+  return getFirestore(app);
+};
+
+const getDocumentReferenceServerSide = async (collection: string, id: string) => {
+  const firestore = await getFirestoreServerSide();
+  return firestore.doc(`${collection}/${id}`);
+};
+
+const getDocumentReferenceClientSide = async (collection: string, id: string) => {
+  initializeFirebaseApp();
+  const app = getApp();
+  const firestore = getFirestoreClientSide(app);
+  return doc(firestore, collection, id);
+};
+
+export const getDocumentReference = (collection: string, id: string) =>
+  (isServerSide() ? getDocumentReferenceServerSide : getDocumentReferenceClientSide)(
+    collection,
+    id
+  );
+
+export const getDocumentSnapshot = async (collection: string, id: string) => {
+  const documentReference = await getDocumentReference(collection, id);
+  // checks if documentReference is instance of DocumentReference which is from firebase/firestore (client side only)
+  return documentReference instanceof DocumentReference
+    ? getDoc(documentReference)
+    : documentReference.get();
 };
