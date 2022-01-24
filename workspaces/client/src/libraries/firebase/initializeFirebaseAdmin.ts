77e820c05f112894cde9e2d0094095a3bbd72106
iknowhtml <iknowsmtp@gmail.com>
2022-01-24 15:11:35 -0500

    Simplify logic & rename credential helper function

--- workspaces/client/src/libraries/firebase/initializeFirebaseAdmin.ts
+++ workspaces/client/src/libraries/firebase/initializeFirebaseAdmin.ts
@@ -1,17 +1,14 @@
 import { SERVER_ENVIRONMENT } from 'consts';
 import { getEnvironmentVariableErrorMessage } from 'libraries/utils/errors';
 
-const getServiceAccount = () => {
+const getCredential = (): string | Record<string, string> => {
+  if (!process.env.FIREBASE_ADMIN_CREDENTIAL) {
+    throw Error(getEnvironmentVariableErrorMessage('FIREBASE_ADMIN_CREDENTIAL'));
+  }
   if (SERVER_ENVIRONMENT === 'production') {
-    if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
-      throw Error(getEnvironmentVariableErrorMessage('FIREBASE_SERVICE_ACCOUNT_JSON'));
-    }
-    return JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+    return JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIAL);
   } else {
-    if (!process.env.FIREBASE_ADMIN_CREDENTIALS) {
-      throw Error(getEnvironmentVariableErrorMessage('FIREBASE_ADMIN_CREDENTIALS'));
-    }
-    return process.env.FIREBASE_ADMIN_CREDENTIALS;
+    return process.env.FIREBASE_ADMIN_CREDENTIAL;
   }
 };
 
@@ -20,7 +17,7 @@ export const initializeFirebaseAdmin = async () => {
   // checks if admin has been initialized already
   if (getApps().length < 1) {
     const { initializeApp, cert } = await import('firebase-admin/app');
-    const serviceAccount = getServiceAccount();
+    const serviceAccount = getCredential();
     initializeApp({
       credential: cert(serviceAccount),
     });
