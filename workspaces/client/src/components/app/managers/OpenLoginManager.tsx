131705fb32f8aee64cbdd92fa90f3384d8a59abc
iknowhtml <iknowsmtp@gmail.com>
2022-01-24 12:52:16 -0500

    Remove unnecessary comment

--- workspaces/client/src/components/app/managers/OpenLoginManager.tsx
+++ workspaces/client/src/components/app/managers/OpenLoginManager.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { IS_OPEN_LOGIN_PENDING } from 'consts';
 import { useIsLoggingIn, useLogin } from 'libraries/authentication';
 
-// checks if token is authenticated, user is not signing up and that their wallet is active and updates the redux store state accordingly
 export const OpenLoginManager = () => {
   const { login } = useLogin();
   const [, setIsLoggingIn] = useIsLoggingIn();
