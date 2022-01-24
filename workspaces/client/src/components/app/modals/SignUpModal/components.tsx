4073f561966fd2f21feadb2cc41b3e6bffe533e8
iknowhtml <iknowsmtp@gmail.com>
2022-01-24 15:36:39 -0500

    Extract styled components to separate file

---
+++ workspaces/client/src/components/app/modals/SignUpModal/components.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const SignUpForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding-top: 24px;
+  align-items: center;
+`;
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 4px;
+`;
+export const LegalAgreementContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  width: fit-content;
+  padding-bottom: 24px;
+`;
+export const LegalAgreementCopy = styled.div`
+  font-size: 14px;
+`;
