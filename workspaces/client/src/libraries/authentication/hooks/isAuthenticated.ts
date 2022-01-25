1053e61e89cfc4b0b56b4c25d73fccb771900879
iknowhtml <iknowsmtp@gmail.com>
2022-01-25 15:41:18 -0500

    Update is signing up hook usage

--- workspaces/client/src/libraries/authentication/hooks/isAuthenticated.ts
+++ workspaces/client/src/libraries/authentication/hooks/isAuthenticated.ts
@@ -4,7 +4,7 @@ import { useIsLoggingIn, useIsSigningUp, useIsTokenAuthenticated } from '.';
 export const useIsAuthenticated = () => {
   const { active } = useWeb3();
   const isTokenAuthenticated = useIsTokenAuthenticated();
-  const [isSigningUp] = useIsSigningUp();
+  const isSigningUp = useIsSigningUp();
   const [isLoggingIn] = useIsLoggingIn();
 
   return active && isTokenAuthenticated && !isSigningUp && !isLoggingIn;
