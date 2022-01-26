7abeab49445d0c99555866c5355dc5ac7765f612
iknowhtml <iknowsmtp@gmail.com>
2022-01-26 05:45:54 -0500

    Add Is Signing Up State Manager component

--- workspaces/client/src/components/app/managers/Managers.tsx
+++ workspaces/client/src/components/app/managers/Managers.tsx
@@ -1,11 +1,13 @@
 import { OpenLoginManager } from './OpenLoginManager';
 import { ConnectorActivationManager } from './ConnectorActivationManager';
 import { LogoutManager } from './LogoutManager';
+import { IsSigningUpStateManager } from './IsSigningUpStateManager';
 
 export const Managers = () => (
   <>
     <OpenLoginManager />
     <ConnectorActivationManager />
     <LogoutManager />
+    <IsSigningUpStateManager />
   </>
 );
