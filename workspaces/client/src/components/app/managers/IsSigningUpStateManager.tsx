48b785b2055a1d5ef930ca6b0452467faebdfea1
iknowhtml <iknowsmtp@gmail.com>
2022-01-26 16:35:33 -0500

    Refactor is signing up pipe to use docData

--- workspaces/client/src/components/app/managers/IsSigningUpStateManager.tsx
+++ workspaces/client/src/components/app/managers/IsSigningUpStateManager.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { filter, map, mergeMap } from 'rxjs';
-import { doc } from 'rxfire/firestore';
+import { doc, docData } from 'rxfire/firestore';
 import { useDispatch } from 'react-redux';
 
 import { useIsSigningUp } from 'libraries/authentication';
@@ -9,7 +9,7 @@ import { getDocumentReferenceClientSide, getUserUid$ } from 'libraries/firebase'
 import { updateIsSigningUp } from 'libraries/authentication/slice';
 
 export const IsSigningUpStateManager = () => {
-  const isSigningUp = useIsSigningUp();
+  const isSigningUpFromRedux = useIsSigningUp();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,14 +18,14 @@ export const IsSigningUpStateManager = () => {
         // need type guard: https://stackoverflow.com/questions/57206909/typescript-with-rxjs-filter-typing-problem
         filter((uid): uid is string => uid !== undefined),
         mergeMap((uid) => getDocumentReferenceClientSide('is-signing-up', uid)),
-        mergeMap((docRef) => doc(docRef)),
-        map((docRef) => docRef?.data()?.isSigningUp),
-        filter((currentIsSigningUp: boolean) => currentIsSigningUp !== isSigningUp)
+        mergeMap((docRef) => docData(docRef)),
+        map((data) => data.isSigningUp),
+        filter((isSigningUp: boolean) => isSigningUp !== isSigningUpFromRedux)
       )
       .subscribe((isSigningUp) => dispatch(updateIsSigningUp(isSigningUp)));
 
     return () => subscription.unsubscribe();
-  }, [dispatch, isSigningUp]);
+  }, [dispatch, isSigningUpFromRedux]);
 
   return <></>;
 };
