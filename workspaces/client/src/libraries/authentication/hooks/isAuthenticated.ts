2cd916fcbb087d19a9821e5c5573b4640a6274e0
iknowhtml <iknowsmtp@gmail.com>
2022-01-26 05:43:02 -0500

    Update logic & interval for get token$ util

--- workspaces/client/src/libraries/authentication/hooks/isAuthenticated.ts
+++ workspaces/client/src/libraries/authentication/hooks/isAuthenticated.ts
@@ -1,11 +1,9 @@
-import { useWeb3 } from 'libraries/blockchain';
 import { useIsLoggingIn, useIsSigningUp, useIsTokenAuthenticated } from '.';
 
 export const useIsAuthenticated = () => {
-  const { active } = useWeb3();
   const isTokenAuthenticated = useIsTokenAuthenticated();
   const isSigningUp = useIsSigningUp();
   const [isLoggingIn] = useIsLoggingIn();
 
-  return active && isTokenAuthenticated && !isSigningUp && !isLoggingIn;
+  return isTokenAuthenticated && !isSigningUp && !isLoggingIn;
 };
