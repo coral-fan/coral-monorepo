292bd135015c4cd64e15d501da43b37f0b734d20
iknowhtml <iknowsmtp@gmail.com>
2022-01-27 14:37:18 -0500

    Fix re-render issue by memoizing set state

--- workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
+++ workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
@@ -1,11 +1,14 @@
 import { RootState } from 'libraries/state/types';
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateIsSigningUp } from '../slice';
 
 export const useIsSigningUp = (): [boolean, (isSigningUp: boolean) => void] => {
   const dispatch = useDispatch();
-  return [
-    useSelector((state: RootState) => state.authentication.isSigningUp),
+  const isSigningUp = useSelector((state: RootState) => state.authentication.isSigningUp);
+  const setIsSigningUp = useCallback(
     (isSigningUp: boolean) => dispatch(updateIsSigningUp(isSigningUp)),
-  ];
+    [dispatch]
+  );
+  return [isSigningUp, setIsSigningUp];
 };
