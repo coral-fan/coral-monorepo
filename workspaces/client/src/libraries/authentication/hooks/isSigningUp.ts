e44b58ae2a083bd5fc9a9f5fb5f231a1596f9f7a
iknowhtml <iknowsmtp@gmail.com>
2022-01-27 07:36:59 -0500

    Revert is signing up hook to original API

--- workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
+++ workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
@@ -1,5 +1,11 @@
 import { RootState } from 'libraries/state/types';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateIsSigningUp } from '../slice';
 
-export const useIsSigningUp = () =>
-  useSelector((state: RootState) => state.authentication.isSigningUp);
+export const useIsSigningUp = (): [boolean, (isSigningUp: boolean) => void] => {
+  const dispatch = useDispatch();
+  return [
+    useSelector((state: RootState) => state.authentication.isSigningUp),
+    (isSigningUp: boolean) => dispatch(updateIsSigningUp(isSigningUp)),
+  ];
+};
