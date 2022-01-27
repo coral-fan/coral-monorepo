1627255d3b67297090084419ff79d7960dbed0eb
iknowhtml <iknowsmtp@gmail.com>
2022-01-27 14:41:08 -0500

    Memoize set state function in is logging in hook

--- workspaces/client/src/libraries/authentication/hooks/isLoggingIn.ts
+++ workspaces/client/src/libraries/authentication/hooks/isLoggingIn.ts
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'libraries/state/types';
 import { updateIsLoggingIn } from '../slice';
+import { useCallback } from 'react';
 
 export const useIsLoggingIn = (): [boolean, (isLoggingIn: boolean) => void] => {
   const dispatch = useDispatch();
-  return [
-    useSelector((state: RootState) => state.authentication.isLoggingIn),
+  const isLoggingIn = useSelector((state: RootState) => state.authentication.isLoggingIn);
+  const setIsLoggingIn = useCallback(
     (isLoggingIn: boolean) => dispatch(updateIsLoggingIn(isLoggingIn)),
-  ];
+    [dispatch]
+  );
+
+  return [isLoggingIn, setIsLoggingIn];
 };
