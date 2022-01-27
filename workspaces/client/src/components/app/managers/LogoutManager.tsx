dee323d20b76e51bb81f7a7253526db0656e82b8
iknowhtml <iknowsmtp@gmail.com>
2022-01-27 07:40:34 -0500

    Update Logout Manager component - Add logic to logout when token stored in cookies is destroyed - Refactor authentication token change logout to use token hook

--- workspaces/client/src/components/app/managers/LogoutManager.tsx
+++ workspaces/client/src/components/app/managers/LogoutManager.tsx
@@ -1,24 +1,26 @@
 import { getAuth } from '@firebase/auth';
 import { useEffect } from 'react';
-import { getToken, useLogout } from 'libraries/authentication';
+import { useLogout, useToken } from 'libraries/authentication';
 import { filter, fromEvent, map } from 'rxjs';
 import { idToken } from 'rxfire/auth';
 import { getIsMetaMaskInjected } from 'libraries/blockchain';
 
 export const LogoutManager = () => {
   const logout = useLogout();
+  const tokenFromCookies = useToken();
+
   // logic to log user out when the authentication token changes
   useEffect(() => {
-    const tokenFromCookies = getToken();
     const subscription = idToken(getAuth())
       .pipe(
+        // need to map to undefined as the token from cookies will either be a string or undefined
         map((token) => token ?? undefined),
-        filter((token) => token !== tokenFromCookies)
+        filter((token) => tokenFromCookies === undefined || token !== tokenFromCookies)
       )
       .subscribe(logout);
 
     return () => subscription.unsubscribe();
-  }, [logout]);
+  }, [tokenFromCookies, logout]);
 
   // logic to ensure the user is logged out when the account changes on metamask
   useEffect(() => {
