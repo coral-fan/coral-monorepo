d19843714ab736021ca00c350762a178458589bd
iknowhtml <iknowsmtp@gmail.com>
2022-01-27 14:34:47 -0500

    Fix sign up state issue on MetaMask account change

--- workspaces/client/src/libraries/authentication/hooks/logout.ts
+++ workspaces/client/src/libraries/authentication/hooks/logout.ts
@@ -2,12 +2,13 @@ import { useWeb3 } from 'libraries/blockchain/hooks';
 import { getAuth } from 'firebase/auth';
 import { destroyCookie } from 'nookies';
 import { COOKIE_OPTIONS } from 'consts';
-import { useToken } from '.';
+import { useIsSigningUp, useToken } from '.';
 import { useCallback } from 'react';
 
 export const useLogout = () => {
   const { active, deactivate } = useWeb3();
   const token = useToken();
+  const [isSigningUp, setIsSigningUp] = useIsSigningUp();
 
   const logout = useCallback(async () => {
     if (active) {
@@ -17,7 +18,11 @@ export const useLogout = () => {
       destroyCookie(undefined, 'token', COOKIE_OPTIONS);
       await getAuth().signOut();
     }
-  }, [active, deactivate, token]);
+
+    if (isSigningUp) {
+      setIsSigningUp(false);
+    }
+  }, [active, deactivate, token, isSigningUp, setIsSigningUp]);
 
   return logout;
 };
