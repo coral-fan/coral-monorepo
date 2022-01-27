d9ac177146d4a34872f971e1a18729bcb92fb307
maks-p <mpazuniak@gmail.com>
2022-01-27 15:55:07 -0500

    Use getDocumentReferenceServerSide - auth

--- workspaces/client/src/pages/api/auth.api.ts
+++ workspaces/client/src/pages/api/auth.api.ts
@@ -3,7 +3,11 @@ import { verifyMessage } from '@ethersproject/wallet';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuthenticationMessage } from 'libraries/authentication';
-import { initializeFirebaseAdmin } from 'libraries/firebase';
+import {
+  getDocumentReferenceServerSide,
+  getFirestoreServerSide,
+  initializeFirebaseAdmin,
+} from 'libraries/firebase';
 import { Handler } from './types';
 import { getHandler } from './utils';
 import { getNonce } from './utils/nonce';
@@ -14,15 +18,14 @@ const post: Handler = async (req, res) => {
     return res.status(400).send('Address is not valid.');
   }
   try {
-    await initializeFirebaseAdmin();
-    const firestore = getFirestore();
-    const nonceRef = await firestore.doc(`nonce/${address}`).get();
-    if (nonceRef.exists) {
-      const nonce = nonceRef.data()?.nonce;
+    const nonceRef = (await getDocumentReferenceServerSide('nonce', `${address}`)).get();
+    if (nonceRef) {
+      const nonce = await (await nonceRef)?.data()?.nonce;
       const derivedAddress = verifyMessage(getAuthenticationMessage(nonce), signedMessage);
 
       if (address === derivedAddress) {
         const nextNonce = getNonce();
+        const firestore = await getFirestoreServerSide();
         await firestore.doc(`nonce/${derivedAddress}`).set({ nonce: nextNonce });
         const token = await getAuth().createCustomToken(derivedAddress);
         return res.status(200).send({ token });
