7bfef70da783d404990287ad02bfeea83ae84b6a
iknowhtml <iknowsmtp@gmail.com>
2022-01-28 20:52:08 -0500

    Add Kunst Grotesk font face to global stylesheet

--- workspaces/client/src/styles/global.tsx
+++ workspaces/client/src/styles/global.tsx
@@ -2,13 +2,60 @@ import { css, Global } from '@emotion/react';
 import tokens, { globalTokens } from './tokens';
 
 export const globalStyles = css`
-  @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'normal';
+    font-weight: 400;
+    font-display: swap;
+    src: './fonts/FTKunstGrotesk-Regular.woff2' format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'normal';
+    font-weight: 500;
+    font-display: swap;
+    src: './fonts/FTKunstGrotesk-Medium.woff2' format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'normal';
+    font-weight: 700;
+    font-display: swap;
+    src: './fonts/FTKunstGrotesk-Bold.woff2' format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'italic';
+    font-weight: 400;
+    font-display: swap;
+    src: './fonts/FTKunstGrotesk-RegularItalic.woff2' format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'italic';
+    font-weight: 500;
+    font-display: swap;
+    src: './fonts/FTKunstGrotesk-MediumItalic.woff2' format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'italic';
+    font-weight: 700;
+    font-display: swap;
+    src: './fonts/FTKunstGrotesk-BoldItalic.woff2' format('woff2');
+  }
+
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family: 'DM Sans', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-      Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Kunst Grotesk', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     color: ${tokens.color.white};
   }
 
