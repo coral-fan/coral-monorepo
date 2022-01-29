25129c87bdb679bb64307333af0a386966f7ba91
iknowhtml <iknowsmtp@gmail.com>
2022-01-28 21:04:32 -0500

    Memoize initial token value

--- workspaces/client/src/libraries/authentication/hooks/isTokenAuthenticated.ts
+++ workspaces/client/src/libraries/authentication/hooks/isTokenAuthenticated.ts
@@ -1,4 +1,5 @@
 import { useObservable } from 'libraries/utils/hooks';
+import { useMemo } from 'react';
 import { distinctUntilChanged, interval, map, startWith } from 'rxjs';
 import { getToken } from '../utils';
 
@@ -6,7 +7,10 @@ const getToken$ = () => {
   return interval(250).pipe(startWith(getToken()), map(getToken), distinctUntilChanged());
 };
 
-export const useToken = () => useObservable<string | undefined>(getToken$, getToken());
+export const useToken = () => {
+  const initialToken = useMemo(() => getToken(), []);
+  return useObservable<string | undefined>(getToken$, initialToken);
+};
 
 export const useIsTokenAuthenticated = () => {
   const token = useToken();
