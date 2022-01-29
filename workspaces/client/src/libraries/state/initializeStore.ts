3da5ca5b8447298478059ba1fe43fcea91ad5df9
iknowhtml <iknowsmtp@gmail.com>
2022-01-28 21:02:16 -0500

    Remove Redux Observable usage

--- workspaces/client/src/libraries/state/initializeStore.ts
+++ workspaces/client/src/libraries/state/initializeStore.ts
@@ -1,22 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { ServerSideData } from 'pages/_app.page';
-import { combineEpics, createEpicMiddleware } from 'redux-observable';
 
 import authenticationReducer, {
   initialState as initialAuthenticationState,
 } from '../authentication/slice';
 
-const rootEpic = combineEpics();
-
 type InitialState = ServerSideData['initialState'];
 
 export const initializeStore = (initialState: InitialState) => {
-  const epicMiddleware = createEpicMiddleware();
   const store = configureStore({
     reducer: {
       authentication: authenticationReducer,
     },
-    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), epicMiddleware],
     preloadedState: {
       authentication: {
         ...initialAuthenticationState,
@@ -24,8 +19,6 @@ export const initializeStore = (initialState: InitialState) => {
       },
     },
   });
-
-  epicMiddleware.run(rootEpic);
 
   return store;
 };
