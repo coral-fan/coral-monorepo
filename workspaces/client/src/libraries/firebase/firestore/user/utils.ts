f51821b0bee5f6f91621f437fee10ce555af9ecf
maks-p <mpazuniak@gmail.com>
2022-02-02 17:44:39 -0500

    Make uid 2nd param for upsertUser

--- workspaces/client/src/libraries/firebase/firestore/user/utils.ts
+++ workspaces/client/src/libraries/firebase/firestore/user/utils.ts
@@ -2,21 +2,13 @@ import { getDoc, setDoc } from '@firebase/firestore';
 import { getDocumentReferenceClientSide } from 'libraries/firebase';
 import { User } from './types';
 
-interface UserWithUid extends User {
-  uid: string;
-}
-
 /*
 At Least One Type implementation from:
 https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-requires-a-single-property-to-be-set/48244432
 */
-
 type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
-type PartialUserData = AtLeastOne<UserWithUid>;
 
-export const upsertUser = async (incomingUserData: PartialUserData) => {
-  const { uid } = incomingUserData;
-
+export const upsertUser = async (incomingUserData: AtLeastOne<User>, uid: string) => {
   if (uid === undefined) {
     return;
   }
