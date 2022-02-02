5e18ec6cf7af7d01edb95f7a32b69c98d4ddc5fe
iknowhtml <iknowsmtp@gmail.com>
2022-02-02 13:43:58 -0500

    Use token value directly from cookie

--- workspaces/client/src/components/app/managers/LogoutManager.tsx
+++ workspaces/client/src/components/app/managers/LogoutManager.tsx
@@ -1,26 +1,25 @@
 import { getAuth } from '@firebase/auth';
 import { useEffect } from 'react';
-import { useLogout, useToken } from 'libraries/authentication';
+import { getToken, useLogout } from 'libraries/authentication';
 import { filter, fromEvent, map } from 'rxjs';
 import { idToken } from 'rxfire/auth';
 import { isMetaMaskInjected } from 'libraries/blockchain';
 
 export const LogoutManager = () => {
   const logout = useLogout();
-  const tokenFromCookies = useToken();
-
   // logic to log user out when the authentication token changes
   useEffect(() => {
     const subscription = idToken(getAuth())
       .pipe(
-        // need to map to undefined as the token from cookies will either be a string or undefined
+        // need to map to undefined as the token from cookies will either be a string or null
         map((token) => token ?? undefined),
-        filter((token) => token !== tokenFromCookies)
+        // check if token from cookies isn't undefined since
+        filter((token) => token !== getToken())
       )
       .subscribe(logout);
 
     return () => subscription.unsubscribe();
-  }, [tokenFromCookies, logout]);
+  }, [logout]);
 
   // logic to ensure the user is logged out when the account changes on metamask
   useEffect(() => {
