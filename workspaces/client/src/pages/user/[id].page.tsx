ef1688b8f0110f1e5f4a7b1c73187f50d86708ec
iknowhtml <iknowsmtp@gmail.com>
2022-02-03 08:29:40 -0500

    Refactor logic to use SSR instead of SSG SSR = server-side render, SSG = static-site generation

--- workspaces/client/src/pages/user/[id].page.tsx
+++ workspaces/client/src/pages/user/[id].page.tsx
@@ -1,34 +1,13 @@
 import styled from '@emotion/styled';
-
-interface User {
-  id: string;
-  username: string;
-}
+import { User } from 'libraries/firebase';
+import { getServerSidePropsFactory } from '../utils/ssr';
 
 const Container = styled.div`
   display: flex;
 `;
 
-export default function User({ username }: User) {
-  return <Container>{`${username}'s Profile`}</Container>;
+export default function UserPage({ user }: { user: User }) {
+  return <Container>{`${user.username}'s Profile`}</Container>;
 }
 
-interface UserRouteParam {
-  params: {
-    id: string;
-  };
-}
-
-const userIds = [1, 2, 3, 4, 5];
-const users: User[] = userIds.map((id) => ({ id: `${id}`, username: `username_${id}` }));
-const paths = users.map(({ id }) => ({ params: { id: `${id}` } }));
-
-export const getStaticPaths = async () => {
-  return { paths, fallback: false };
-};
-
-export const getStaticProps = async ({ params }: UserRouteParam) => {
-  return {
-    props: { id: params.id, username: users.find((user) => user.id === params.id)?.username },
-  };
-};
+export const getServerSideProps = getServerSidePropsFactory('id', 'users');
