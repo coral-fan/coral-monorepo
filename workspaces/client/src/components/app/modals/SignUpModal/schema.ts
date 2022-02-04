40a8e802d3d321f41c53ede66d8d3880686ec323
maks-p <mpazuniak@gmail.com>
2022-02-04 14:28:03 -0500

    Extract common schema logic

--- workspaces/client/src/components/app/modals/SignUpModal/schema.ts
+++ workspaces/client/src/components/app/modals/SignUpModal/schema.ts
@@ -1,21 +1,11 @@
-import { boolean, object, string, InferType } from 'yup';
+import { object, InferType } from 'yup';
+import { getUsernameSchema, email, doesAgree } from 'libraries/utils/schemas';
 
 export const getSignUpSchema = (usernames: Set<string>) =>
   object({
-    username: string()
-      .required()
-      .min(3)
-      .matches(/^([a-zA-Z\d_])+$/g, 'Only alphanumeric characters and _ are allowed')
-      .test({
-        name: 'is-username-unique',
-        test: (username) => username !== undefined && !usernames.has(username.toLowerCase()),
-        message: 'Username is taken',
-      }),
-    email: string()
-      .email()
-      .transform((value: string) => (value === '' ? null : value))
-      .nullable(),
-    doesAgree: boolean().required().default(false).isTrue(),
+    username: getUsernameSchema(usernames),
+    email,
+    doesAgree
   });
 
 export type SignUpSchema = InferType<ReturnType<typeof getSignUpSchema>>;
