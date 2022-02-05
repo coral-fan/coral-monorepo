dd1c655e62e970b6afb7c17e95cc6f0a4b3a41b5
iknowhtml <iknowsmtp@gmail.com>
2022-02-05 08:28:17 -0500

    Refactor upsert user util to handle private data

--- workspaces/client/src/libraries/models/user/utils.ts
+++ workspaces/client/src/libraries/models/user/utils.ts
@@ -2,6 +2,7 @@ import { getDoc, setDoc } from '@firebase/firestore';
 import { getDocumentReferenceClientSide } from 'libraries/firebase';
 import { User } from './types';
 import { getDocumentData } from 'libraries/firebase';
+import { PrivateUserData } from '.';
 
 export const getIsUserSigningUp = async (uid: string) => {
   const isSigningUpData = await getDocumentData('is-signing-up', uid);
@@ -21,26 +22,68 @@ https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-r
 */
 type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
 
-export const upsertUser = async (incomingUserData: AtLeastOne<User>, uid: string) => {
+type IncomingUserData = AtLeastOne<User & PrivateUserData>;
+
+const extractData = <T, U>(data: T, keys: (keyof U)[]): Partial<U> =>
+  Object.entries(data).reduce(
+    (extractedData, [key, value]) =>
+      key in keys ? { ...extractedData, [key]: value } : extractedData,
+    {}
+  );
+
+const USER_PROPERTIES: (keyof User)[] = ['assets', 'notifications', 'profilePhoto', 'username'];
+const PRIVATE_USER_DATA_PROPERTIES: (keyof PrivateUserData)[] = ['creditCardInformation', 'email'];
+
+const isObjectEmpty = (object: Record<string, unknown>) => Object.keys(object).length === 0;
+
+export const upsertUser = async (uid: string, incomingUserData: IncomingUserData) => {
   if (uid === undefined) {
-    return;
+    throw Error('uid for user should not be undefined.');
   }
 
-  const userDocumentReference = await getDocumentReferenceClientSide('users', uid);
-  const userDocumentSnapshot = await getDoc(userDocumentReference);
+  const userData = extractData<IncomingUserData, User>(incomingUserData, USER_PROPERTIES);
 
-  await setDoc(
-    userDocumentReference,
-    userDocumentSnapshot.exists()
-      ? incomingUserData
-      : {
-          email: null,
-          profilePhoto: null,
-          creditCardInformation: null,
-          notifications: [],
-          assets: [],
-          ...incomingUserData,
-        },
-    { merge: true }
+  if (!isObjectEmpty(userData)) {
+    const userDocumentReference = getDocumentReferenceClientSide('users', uid);
+    const userDocumentSnapshot = await getDoc(userDocumentReference);
+    await setDoc(
+      userDocumentReference,
+      userDocumentSnapshot.exists()
+        ? userData
+        : {
+            profilePhoto: null,
+            notifications: [],
+            assets: [],
+            ...userData,
+          },
+      { merge: true }
+    );
+  }
+
+  const privateUserData = extractData<IncomingUserData, PrivateUserData>(
+    incomingUserData,
+    PRIVATE_USER_DATA_PROPERTIES
   );
+
+  if (!isObjectEmpty(privateUserData)) {
+    const privateUserDataDocumentReference = getDocumentReferenceClientSide(
+      'users',
+      uid,
+      'private',
+      'data'
+    );
+    const privateUserDataDocumentSnapshot = await getDoc(privateUserDataDocumentReference);
+
+    await setDoc(
+      privateUserDataDocumentReference,
+      privateUserDataDocumentSnapshot.exists()
+        ? privateUserData
+        : {
+            email: null,
+            creditCardInformation: null,
+            ...privateUserData,
+          },
+      { merge: true }
+    );
+  }
 };
