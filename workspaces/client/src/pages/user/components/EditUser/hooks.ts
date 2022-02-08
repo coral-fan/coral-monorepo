0ae4eb2850b68f38f1eb224c0c7584af8b02e520
maks-p <mpazuniak@gmail.com>
2022-02-08 15:38:28 -0500

    Create EditUser hook

---
+++ workspaces/client/src/pages/user/components/EditUser/hooks.ts
@@ -0,0 +1,37 @@
+import { Dispatch, SetStateAction, useMemo, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { upsertUser, useUsernames, useUserUid } from 'libraries/firebase';
+import { getEditUserSchema, EditUserSchema } from './schemas';
+
+export const useEditUser = (showModal: Dispatch<SetStateAction<boolean>>) => {
+  const usernames = useUsernames();
+  const editUserSchema = getEditUserSchema(usernames);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isValid },
+  } = useForm<EditUserSchema>({
+    resolver: yupResolver(editUserSchema),
+    mode: 'all',
+  });
+
+  const [isEditUserSubmitting, setIsEditUserSubmitting] = useState(false);
+  const uid = useUserUid();
+
+  const handleSubmitEditUser = useMemo(
+    () =>
+      handleSubmit(async ({ username, email }) => {
+        setIsEditUserSubmitting(true);
+        if (uid !== undefined) {
+          await upsertUser({ username, email }, uid);
+        }
+        setIsEditUserSubmitting(false);
+        showModal(false);
+      }),
+    [handleSubmit, showModal, uid]
+  );
+  return { register, setValue, errors, isValid, isEditUserSubmitting, handleSubmitEditUser };
+};
