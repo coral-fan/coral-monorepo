7120b6e01b653b49fa877e1b09b0a1beac24c97a
maks-p <mpazuniak@gmail.com>
2022-02-08 16:03:57 -0500

    Update imports

--- workspaces/client/src/pages/user/components/EditUser/types.ts
+++ workspaces/client/src/pages/user/components/EditUser/types.ts
@@ -1,3 +1,3 @@
-import { User } from 'libraries/firebase';
+import { User } from 'libraries/models';
 
-export type EditUserProps = Omit<User, 'creditCardInformation' | 'notifications' | 'assets'>
+export type EditUserProps = Omit<User, 'creditCardInformation' | 'notifications' | 'assets'>;
