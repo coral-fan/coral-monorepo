1ad56157af4c7d817ba9bb88d316e4c917dd571c
maks-p <mpazuniak@gmail.com>
2022-02-08 15:38:46 -0500

    Add Modal to user page & handle Modal state

--- workspaces/client/src/pages/user/[id].page.tsx
+++ workspaces/client/src/pages/user/[id].page.tsx
@@ -1,13 +1,33 @@
 import styled from '@emotion/styled';
 import { User } from 'libraries/models';
 import { getServerSidePropsFactory } from '../utils/ssr';
+import { Button } from 'components/ui';
+import { EditUserModal } from './components/EditUser/EditUserModal';
+import { useState } from 'react';
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 
 export default function UserPage({ user }: { user: User }) {
-  return <Container>{`${user.username}'s Profile`}</Container>;
+  const [isEditingUser, setIsEditingUser] = useState(false);
+  const { email, username, profilePhoto } = user;
+
+  return (
+    <Container>
+      {`${user.username}'s Profile`}
+      <Button onClick={() => setIsEditingUser(true)}>Update Profile</Button>
+      {isEditingUser ? (
+        <EditUserModal
+          email={email}
+          username={username}
+          profilePhoto={profilePhoto}
+          showModal={setIsEditingUser}
+        />
+      ) : null}
+    </Container>
+  );
 }
 
 export const getServerSideProps = getServerSidePropsFactory('id', 'users');
