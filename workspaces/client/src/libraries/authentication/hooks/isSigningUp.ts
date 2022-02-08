f0d3f37d2232e4379716dbde8e2c4fd0f754ec23
iknowhtml <iknowsmtp@gmail.com>
2022-02-08 16:41:41 -0500

    Revert hook to only update local state

--- workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
+++ workspaces/client/src/libraries/authentication/hooks/isSigningUp.ts
@@ -1,19 +1,13 @@
 import { RootState } from 'libraries/state/types';
-import { getCoralAPIAxios } from 'libraries/utils/api';
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateIsSigningUp } from '../slice';
 
-const axios = getCoralAPIAxios();
-
-export const useIsSigningUp = (): [boolean, (isSigningUp: boolean) => Promise<void>] => {
+export const useIsSigningUp = (): [boolean, (isSigningUp: boolean) => void] => {
   const dispatch = useDispatch();
   const isSigningUp = useSelector((state: RootState) => state.authentication.isSigningUp);
   const setIsSigningUp = useCallback(
-    async (isSigningUp: boolean) => {
-      await axios.post('is-signing-up', { isSigningUp: false });
-      dispatch(updateIsSigningUp(isSigningUp));
-    },
+    (isSigningUp: boolean) => dispatch(updateIsSigningUp(isSigningUp)),
     [dispatch]
   );
   return [isSigningUp, setIsSigningUp];
