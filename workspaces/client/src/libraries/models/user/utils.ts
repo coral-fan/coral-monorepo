bbf20412d151fc174bf0e97c9d176341e5f12829
iknowhtml <iknowsmtp@gmail.com>
2022-02-08 16:55:36 -0500

    Fix bug with when user data isn't in Firestore

--- workspaces/client/src/libraries/models/user/utils.ts
+++ workspaces/client/src/libraries/models/user/utils.ts
@@ -3,11 +3,7 @@ import { getDocumentData } from 'libraries/firebase';
 export const getIsUserSigningUp = async (uid: string) => {
   const isSigningUpData = await getDocumentData('is-signing-up', uid);
 
-  if (isSigningUpData === undefined) {
-    throw Error(
-      'Data object for is signing up should not be undefined. Please that the uid you passed is valid.'
-    );
-  }
+  const isSigningUp = isSigningUpData?.isSigningUp;
 
-  return isSigningUpData.isSigningUp as Promise<boolean>;
+  return typeof isSigningUp === 'boolean' ? isSigningUp : false;
 };
