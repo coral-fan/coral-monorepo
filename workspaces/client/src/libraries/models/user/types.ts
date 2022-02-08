92502e276404d55f7349d732faa553f03e72686f
iknowhtml <iknowsmtp@gmail.com>
2022-02-08 15:45:17 -0500

    Export/add additional types - Export CreditCardInformation - Add AtLeastOne utility type - Add IncomingUserData type

--- workspaces/client/src/libraries/models/user/types.ts
+++ workspaces/client/src/libraries/models/user/types.ts
@@ -2,7 +2,7 @@ import { Asset } from '../asset';
 import { Notification } from '../notification';
 import { NullableString } from '../types';
 
-interface CreditCardInformation {
+export interface CreditCardInformation {
   firstName: string;
   lastName: string;
   cardNumber: number;
@@ -27,3 +27,11 @@ export interface PrivateUserData {
   email: NullableString;
   creditCardInformation: CreditCardInformation | null;
 }
+
+/*
+At Least One Type implementation from:
+https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-requires-a-single-property-to-be-set/48244432
+*/
+type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
+
+export type IncomingUserData = AtLeastOne<User & PrivateUserData>;
