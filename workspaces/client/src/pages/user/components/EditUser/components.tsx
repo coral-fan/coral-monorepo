d72f41dadafbb9b424de477c654c3a7ecec2f2c6
maks-p <mpazuniak@gmail.com>
2022-02-08 15:37:38 -0500

    Add general style to modal

---
+++ workspaces/client/src/pages/user/components/EditUser/components.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const EditUserForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 4px;
+`
+
+export const AvatarWrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  margin: auto;
+`
+
+export const AvatarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 40px 0px;
+`
