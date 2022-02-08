47b14fadaf7da9058cfdae7d1aca8b360ffc6766
iknowhtml <iknowsmtp@gmail.com>
2022-02-08 16:56:07 -0500

    Remove unnecessary logic to update sign up state

--- workspaces/client/src/libraries/authentication/hooks/logout.ts
+++ workspaces/client/src/libraries/authentication/hooks/logout.ts
@@ -2,13 +2,12 @@ import { useWeb3 } from 'libraries/blockchain/hooks';
 import { getAuth } from 'firebase/auth';
 import { destroyCookie } from 'nookies';
 import { COOKIE_OPTIONS } from 'consts';
-import { useIsSigningUp, useToken } from '.';
+import { useToken } from '.';
 import { useCallback } from 'react';
 
 export const useLogout = () => {
   const { active, deactivate } = useWeb3();
   const token = useToken();
-  const [isSigningUp, setIsSigningUp] = useIsSigningUp();
 
   const logout = useCallback(async () => {
     if (active) {
@@ -18,11 +17,7 @@ export const useLogout = () => {
       destroyCookie(undefined, 'token', COOKIE_OPTIONS);
       await getAuth().signOut();
     }
-
-    if (isSigningUp) {
-      setIsSigningUp(false);
-    }
-  }, [active, deactivate, token, isSigningUp, setIsSigningUp]);
+  }, [active, deactivate, token]);
 
   return logout;
 };
