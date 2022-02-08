a91c10e1e57b8d510d2e8d13168141a4294ddd7c
maks-p <mpazuniak@gmail.com>
2022-02-08 15:38:07 -0500

    Create Edit User Modal component

---
+++ workspaces/client/src/pages/user/components/EditUser/EditUserModal.tsx
@@ -0,0 +1,66 @@
+import { useIsNetworkSupported } from 'libraries/blockchain';
+import { useIsAuthenticated } from 'libraries/authentication';
+import { Modal, Button } from 'components/ui';
+import { Input } from 'components/ui';
+import { Avatar } from 'components/ui';
+
+import { AvatarContainer, AvatarWrapper, EditUserForm, InputsContainer } from './components';
+
+import { useEditUser } from './hooks';
+import { Dispatch, SetStateAction, useEffect } from 'react';
+import { EditUserProps } from './types';
+
+interface EditUserModalProps extends EditUserProps {
+  showModal: Dispatch<SetStateAction<boolean>>
+}
+
+export const EditUserModal = ({ username, email, profilePhoto, showModal }: EditUserModalProps) => {
+  const isAuthenticated = useIsAuthenticated();
+  const isNetworkSupported = useIsNetworkSupported();
+
+  const { register, setValue, errors, isValid, isEditUserSubmitting, handleSubmitEditUser } =
+    useEditUser(showModal);
+
+  useEffect(() => {
+    setValue('username', username);
+    setValue('email', email);
+  }, []);
+
+  if (!isAuthenticated || !isNetworkSupported) {
+    return null;
+  }
+
+  return (
+    <Modal onClick={() => showModal(false)} variant={'close'}>
+      <EditUserForm onSubmit={handleSubmitEditUser}>
+        <AvatarContainer>
+          <AvatarWrapper>
+            <Avatar size={200} hasBorder={false} />
+          </AvatarWrapper>
+          <Button>Change Photo</Button>
+        </AvatarContainer>
+        <InputsContainer>
+          <Input
+            label="Username"
+            placeholder="username"
+            {...register('username')}
+            error={errors?.username?.message}
+          />
+          <Input
+            label="Email"
+            placeholder="example@email.com"
+            {...register('email')}
+            error={errors?.email?.message}
+          />
+        </InputsContainer>
+        <Button
+          type="submit"
+          disabled={!isValid}
+          loading={isEditUserSubmitting}
+        >
+          Update Account
+        </Button>
+      </EditUserForm>
+    </Modal>
+  );
+};
