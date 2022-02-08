b0baf543f0425a2091d8df76273a8d99bffc6419
iknowhtml <iknowsmtp@gmail.com>
2022-02-08 15:45:29 -0500

    Add User API endpoint

---
+++ workspaces/client/src/pages/api/user/index.api.ts
@@ -0,0 +1,34 @@
+import { IncomingUserData } from 'libraries/models';
+import { ERROR_RESPONSE } from '../consts';
+import { Handler } from '../types';
+import { getHandler } from '../utils';
+import { PRIVATE_USER_DATA_PROPERTIES, USER_PROPERTIES } from './consts';
+import { PrivateUserDataProperties, UserProperties } from './types';
+import { upsertUser } from './utils';
+
+const isIncomingUserDataValid = (
+  incomingUserData: Record<string, unknown>
+): incomingUserData is IncomingUserData =>
+  Object.keys(incomingUserData).every(
+    (key) =>
+      USER_PROPERTIES.has(key as UserProperties) ||
+      PRIVATE_USER_DATA_PROPERTIES.has(key as PrivateUserDataProperties)
+  );
+
+const post: Handler = async (req, res) => {
+  const { uid, incomingUserData } = req.body;
+
+  if (typeof uid !== 'string' || !isIncomingUserDataValid(incomingUserData)) {
+    return res.status(400).json({ error: 'Invalid data was provided.' });
+  }
+
+  try {
+    await upsertUser(uid, incomingUserData);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(ERROR_RESPONSE);
+  }
+  return res.status(200).send('');
+};
+
+export default getHandler({ post });
