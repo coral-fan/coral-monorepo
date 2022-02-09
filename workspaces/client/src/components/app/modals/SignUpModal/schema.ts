b38b1f9f99058129bcfc09d8fdfc14cd70aa78f9
maks-p <mpazuniak@gmail.com>
2022-02-09 13:24:58 -0500

    Refactor schema

--- workspaces/client/src/components/app/modals/SignUpModal/schema.ts
+++ workspaces/client/src/components/app/modals/SignUpModal/schema.ts
@@ -1,11 +1,11 @@
 import { object, InferType } from 'yup';
-import { getUsernameSchema, email, doesAgree } from 'libraries/utils/schemas';
+import { getUsernameSchema, EMAIL_SCHEMA, DOES_AGREE_SCHEMA } from 'libraries/utils/schemas';
 
 export const getSignUpSchema = (usernames: Set<string>) =>
   object({
     username: getUsernameSchema(usernames),
-    email,
-    doesAgree
+    email: EMAIL_SCHEMA,
+    doesAgree: DOES_AGREE_SCHEMA,
   });
 
 export type SignUpSchema = InferType<ReturnType<typeof getSignUpSchema>>;
