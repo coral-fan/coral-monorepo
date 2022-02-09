6a352166d05e76f49e27c76ee9ae5ee27ff5bf62
iknowhtml <iknowsmtp@gmail.com>
2022-02-09 14:55:00 -0500

    Use updated get username schema util function

--- workspaces/client/src/pages/user/components/EditUserModal/schemas.ts
+++ workspaces/client/src/pages/user/components/EditUserModal/schemas.ts
@@ -1,9 +1,9 @@
 import { object, InferType } from 'yup';
 import { getUsernameSchema, EMAIL_SCHEMA } from 'libraries/utils/schemas';
 
-export const getEditUserSchema = (usernames: Set<string>) =>
+export const getEditUserSchema = (usernames: Set<string>, currentUsername: string) =>
   object({
-    username: getUsernameSchema(usernames),
+    username: getUsernameSchema(usernames, currentUsername),
     email: EMAIL_SCHEMA,
   });
 
