2dbd8ad3a1f4d96956574407065e29aa2d0b0108
iknowhtml <iknowsmtp@gmail.com>
2022-02-09 14:54:32 -0500

    Add check for username same for uniqueness test

--- workspaces/client/src/libraries/utils/schemas.ts
+++ workspaces/client/src/libraries/utils/schemas.ts
@@ -1,15 +1,24 @@
 import { boolean, string } from 'yup';
 
-export const getUsernameSchema = (usernames: Set<string>) =>
-  string()
+export const getUsernameSchema = (usernames: Set<string>, currentUsername?: string) => {
+  return string()
     .required()
     .min(3)
     .matches(/^([a-zA-Z\d_])+$/g, 'Only alphanumeric characters and _ are allowed')
     .test({
       name: 'is-username-unique',
-      test: (username) => username !== undefined && !usernames.has(username.toLowerCase()),
+      test: (username) => {
+        if (username === undefined) {
+          return false;
+        }
+        username = username.toLowerCase();
+        const isUsernameTaken = usernames.has(username);
+        const isUsernameSame = username === currentUsername?.toLowerCase();
+        return !isUsernameTaken || isUsernameSame;
+      },
       message: 'Username is taken',
     });
+};
 
 export const EMAIL_SCHEMA = string()
   .email()
