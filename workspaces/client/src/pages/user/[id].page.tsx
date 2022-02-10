9f69d54ad54559b872fd95ab4c3b6337df9616b4
iknowhtml <iknowsmtp@gmail.com>
2022-02-10 13:02:12 -0500

    Update convention to Update User

--- workspaces/client/src/pages/user/[id].page.tsx
+++ workspaces/client/src/pages/user/[id].page.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 import { PrivateUserData, PublicUserData, User, useUserUid } from 'libraries/models';
 import { Button } from 'components/ui';
-import { EditUserModal } from './components/EditUserModal/EditUserModal';
+import { UpdateUserModal } from './components/EditUserModal/UpdateProfileModal';
 import { useState } from 'react';
-import { EditUserProps } from './components/EditUserModal/types';
+import { UpdateUserProps } from './components/EditUserModal/types';
 import { GetServerSideProps } from 'next';
 import { getDocumentData } from 'libraries/firebase';
 import { getUidServerSide } from 'pages/utils';
@@ -13,13 +13,13 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const EditUser = (props: EditUserProps) => {
+const UpdateUser = (props: UpdateUserProps) => {
   const [isEditUserModalOpen, setIsEditUserModalOpen] = useState(false);
   return (
     <>
       <Button onClick={() => setIsEditUserModalOpen(true)}>Update Profile</Button>
       {isEditUserModalOpen ? (
-        <EditUserModal {...props} setIsModalOpen={setIsEditUserModalOpen} />
+        <UpdateUserModal {...props} setIsModalOpen={setIsEditUserModalOpen} />
       ) : null}
     </>
   );
@@ -39,7 +39,7 @@ export default function UserPage({ uid, user }: UserPageProps) {
     <Container>
       {`${user.username}'s Profile`}
       {uid === userUid && email !== undefined && (
-        <EditUser {...{ email, username, profilePhoto }} />
+        <UpdateUser {...{ email, username, profilePhoto }} />
       )}
     </Container>
   );
