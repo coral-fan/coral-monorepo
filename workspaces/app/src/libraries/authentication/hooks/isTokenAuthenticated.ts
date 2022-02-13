7841086ba5c3a922d43172599d57bb8f654384ac
iknowhtml <iknowsmtp@gmail.com>
2022-02-13 11:51:09 -0500

    Update token utils - Refactor get token$ util to use RxFire - Update type for token for hook

--- workspaces/app/src/libraries/authentication/hooks/isTokenAuthenticated.ts
+++ workspaces/app/src/libraries/authentication/hooks/isTokenAuthenticated.ts
@@ -1,18 +1,20 @@
+import { getAuth } from 'firebase/auth';
 import { useObservable } from 'libraries/utils/hooks';
 import { useMemo } from 'react';
-import { distinctUntilChanged, interval, map } from 'rxjs';
+import { idToken } from 'rxfire/auth';
 import { getToken } from '../utils';
 
-const getToken$ = () => {
-  return interval(250).pipe(map(getToken), distinctUntilChanged());
+export const getToken$ = () => {
+  const auth = getAuth();
+  return idToken(auth);
 };
 
 export const useToken = () => {
   const initialToken = useMemo(() => getToken(), []);
-  return useObservable<string | undefined>(getToken$, initialToken);
+  return useObservable<string | null>(getToken$, initialToken);
 };
 
 export const useIsTokenAuthenticated = () => {
   const token = useToken();
-  return token !== undefined;
+  return token !== null;
 };
