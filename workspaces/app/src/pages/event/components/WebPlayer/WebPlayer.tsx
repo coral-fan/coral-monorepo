57df03d1aa05a2ab24b7dae8a9b834470650b916
maks-p <mpazuniak@gmail.com>
2022-02-14 11:37:56 -0500

    Create event page and working web player prototype

---
+++ workspaces/app/src/pages/event/components/WebPlayer/WebPlayer.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import Script from 'next/script';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+interface WebPlayerProps {
+  url: string;
+}
+
+const JWPLAYER_LIBRARY_URL = 'https://cdn.jwplayer.com/libraries/lja4xTpS.js';
+
+export const WebPlayer = ({ url }: WebPlayerProps) => {
+  return (
+    <>
+      <Script
+        id="myElement"
+        src={JWPLAYER_LIBRARY_URL}
+        onLoad={() => {
+          jwplayer('myElement').setup({
+            playlist: [
+              {
+                file: `${url}`,
+              },
+            ],
+          });
+        }}
+      />
+      <Container>
+        <div id="myElement"></div>
+      </Container>
+    </>
+  );
+};
