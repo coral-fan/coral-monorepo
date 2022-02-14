3270907cbdc5b164fc4fbbeb19b76b107cdc7a6a
maks-p <mpazuniak@gmail.com>
2022-02-14 15:34:24 -0500

    Add jwplayer typing

--- workspaces/app/src/types/global.d.ts
+++ workspaces/app/src/types/global.d.ts
@@ -1,7 +1,9 @@
 import type { MetamaskInpageProvider } from '@metamask/providers';
+import type { JWPlayer } from '@types/jwplayer';
 // prevent type error when accessing the ethereum property on the window object
 declare global {
   interface Window {
     ethereum: MetamaskInpageProvider;
+    jwplayer: JWPlayer;
   }
 }
