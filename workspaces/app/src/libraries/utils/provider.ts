325f9a963cf54eea9a435029ba8b060c1e56e100
iknowhtml <iknowsmtp@gmail.com>
2022-02-15 08:22:12 -0500

    Remove unnecessary provider module

--- workspaces/app/src/libraries/utils/provider.ts
+++
@@ -1,8 +0,0 @@
-import { ExternalProvider, JsonRpcProvider, Web3Provider } from '@ethersproject/providers';
-
-export const getLibrary = (provider: ExternalProvider | JsonRpcProvider | undefined) => {
-  if (provider) {
-    return provider instanceof JsonRpcProvider ? provider : new Web3Provider(provider);
-  }
-  return undefined;
-};
