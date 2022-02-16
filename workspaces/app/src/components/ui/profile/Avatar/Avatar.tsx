dc8d260738fcfd4363b426f9486bef94fb4e6137
iknowhtml <iknowsmtp@gmail.com>
2022-02-16 12:18:56 -0500

    Fix Avatar component size issue

--- workspaces/app/src/components/ui/profile/Avatar/Avatar.tsx
+++ workspaces/app/src/components/ui/profile/Avatar/Avatar.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { AvatarProps } from './types';
 import tokens from 'styles/tokens';
@@ -6,11 +7,20 @@ import tokens from 'styles/tokens';
 const DEFAULT_AVATAR =
   'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
 
+type WrapperProps = Omit<AvatarProps, 'src'>;
+
+const Wrapper = styled.div<WrapperProps>`
+  --size: ${({ size }) => size}px;
+  height: var(--size);
+  width: var(--size);
+`;
+
 const getImageStyle = (hasBorder: boolean) => css`
   border-radius: 50%;
   border: ${`solid 1px ${hasBorder ? `${tokens.border.color.primary}` : 'transparent'}`};
 `;
-
 export const Avatar = ({ src = DEFAULT_AVATAR, size, hasBorder }: AvatarProps) => (
-  <Image src={src} width={size} height={size} css={getImageStyle(hasBorder)} alt={''} />
+  <Wrapper hasBorder={hasBorder} size={size}>
+    <Image src={src} width={size} height={size} css={getImageStyle(hasBorder)} alt={''} />
+  </Wrapper>
 );
