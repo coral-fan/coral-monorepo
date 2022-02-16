23377911ce2ecc2fcfb2212a34246917ad0fab80
iknowhtml <iknowsmtp@gmail.com>
2022-02-16 14:53:40 -0500

    Update modal imports

--- workspaces/app/src/pages/user/components/UpdateProfile/UpdateProfile.tsx
+++ workspaces/app/src/pages/user/components/UpdateProfile/UpdateProfile.tsx
@@ -1,12 +1,22 @@
 import { Button } from 'components/ui';
-import { useIsUpdateProfileInfoModalOpen } from 'pages/user/hooks';
-import { UpdateProfileInfoModal } from './UpdateProfileInfoModal/UpdateProfileInfoModal';
+import {
+  useIsUpdateProfileInfoModalOpen,
+  useIsUpdateProfilePhotoModalOpen,
+} from 'pages/user/hooks';
+import { UpdateProfilePhotoModal, UpdateProfileInfoModal } from './components';
 
 export const UpdateProfile = () => {
   const [isUpdateProfileInfoModalOpen, setIsProfileInfoModalOpen] =
     useIsUpdateProfileInfoModalOpen();
+
+  const [isUpdateProfilePhotoModalOpen, setIsUpdateProfilePhotoOpen] =
+    useIsUpdateProfilePhotoModalOpen();
   return (
     <>
+      <div>
+        <Button onClick={() => setIsUpdateProfilePhotoOpen(true)}>Update Profile Photo</Button>
+        {isUpdateProfilePhotoModalOpen ? <UpdateProfilePhotoModal /> : null}
+      </div>
       <div>
         <Button onClick={() => setIsProfileInfoModalOpen(true)}>Update Profile</Button>
         {isUpdateProfileInfoModalOpen ? <UpdateProfileInfoModal /> : null}
