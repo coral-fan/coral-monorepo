b07b0ea2bc5b8a28a5971581dba48cde226019ff
maks-p <mpazuniak@gmail.com>
2022-02-21 14:09:08 -0500

    Update styled components, add font-weight

--- workspaces/app/src/components/ui/buttons/variants/CtaButton/CtaButton.tsx
+++ workspaces/app/src/components/ui/buttons/variants/CtaButton/CtaButton.tsx
@@ -1,36 +1,37 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { FC } from 'react';
-import { BaseButton, BaseButtonProps as ButtonProps } from 'components/ui';
+import { BaseButton, BaseButtonProps as ButtonProps } from '../../BaseButton';
 import rightArrowSvg from './rightArrow.svg';
 import tokens from 'styles/tokens';
 import { getIconComponent } from 'components/ui/icons/utils';
 
-const ButtonStyle = css`
+const ButtonWrapper = styled(BaseButton)`
   padding: 28px 20px;
   border-radius: ${tokens.border.radius.sm};
   background-color: ${tokens.background.color.brand};
   border: ${tokens.background.color.brand};
 `;
 
-const Wrapper = styled.div`
+const Container = styled.div`
   display: flex;
   justify-content: space-between;
   text-transform: uppercase;
   font-size: ${tokens.font.size.lg};
+  font-weight: ${tokens.font.weight.bold};
   line-height: ${tokens.font.line_height.lg};
   letter-spacing: ${tokens.font.letter_spacing.lg};
-  color: ${tokens.font.color.button};
+  color: ${tokens.font.color.contrast};
 `;
 
+const RightArrowIcon = getIconComponent('rightArrow', rightArrowSvg);
+
 export const CtaButton: FC<ButtonProps> = ({ children, loading, ...props }) => {
-  const RightArrowIcon = getIconComponent('rightArrow', rightArrowSvg);
   return (
-    <BaseButton css={ButtonStyle} loading={loading} {...props}>
-      <Wrapper>
+    <ButtonWrapper loading={loading} {...props}>
+      <Container>
         {children}
         <RightArrowIcon size={24} />
-      </Wrapper>
-    </BaseButton>
+      </Container>
+    </ButtonWrapper>
   );
 };
