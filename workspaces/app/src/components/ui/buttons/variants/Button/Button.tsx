b5e904f05a9ffd21b82809c7a299e2ad84813ce9
maks-p <mpazuniak@gmail.com>
2022-02-21 14:09:08 -0500

    Update styled components, add font-weight

--- workspaces/app/src/components/ui/buttons/variants/Button/Button.tsx
+++ workspaces/app/src/components/ui/buttons/variants/Button/Button.tsx
@@ -1,22 +1,23 @@
-import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { FC } from 'react';
-import { BaseButton, BaseButtonProps as ButtonProps } from 'components/ui';
+import { BaseButton, BaseButtonProps as ButtonProps } from '../../BaseButton';
 import tokens from 'styles/tokens';
 
-const ButtonStyle = css`
+const ButtonWrapper = styled(BaseButton)`
   padding: 15px;
   text-transform: uppercase;
   font-size: ${tokens.font.size.sm};
+  font-weight: ${tokens.font.weight.bold};
   line-height: ${tokens.font.line_height.sm};
   letter-spacing: ${tokens.font.letter_spacing.sm};
-  color: ${tokens.font.color.button};
+  color: ${tokens.font.color.contrast};
   border-radius: ${tokens.border.radius.sm};
   background-color: ${tokens.background.color.brand};
   border: ${tokens.background.color.brand};
 `;
 
 export const Button: FC<ButtonProps> = ({ children, loading, ...props }) => (
-  <BaseButton css={ButtonStyle} loading={loading} {...props}>
+  <ButtonWrapper loading={loading} {...props}>
     {children}
-  </BaseButton>
+  </ButtonWrapper>
 );
