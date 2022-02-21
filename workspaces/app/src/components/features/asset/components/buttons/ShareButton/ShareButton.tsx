fe47ba7fa585ff3c0e7e4fa18ecdbfefc4132a6b
maks-p <mpazuniak@gmail.com>
2022-02-21 11:48:49 -0500

    Update ShareButton

--- workspaces/app/src/components/features/asset/components/buttons/ShareButton/ShareButton.tsx
+++ workspaces/app/src/components/features/asset/components/buttons/ShareButton/ShareButton.tsx
@@ -1,12 +1,6 @@
 import { Button } from 'components/ui/buttons';
-import { getIconComponent } from 'components/ui/icons/utils';
 import { ComponentProps } from 'react';
-import shareIconSVG from './share.svg';
-
-const ShareIcon = getIconComponent('ShareIcon', shareIconSVG);
 
 export const ShareButton = (props: ComponentProps<'button'>) => (
-  <Button icon={<ShareIcon />} variant={'secondary'} {...props}>
-    Share
-  </Button>
+  <Button {...props}>share this nft</Button>
 );
