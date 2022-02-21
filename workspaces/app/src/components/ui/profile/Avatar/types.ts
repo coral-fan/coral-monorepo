a9883c54dbf71e7765524b6e10c850ca24d19fce
iknowhtml <iknowsmtp@gmail.com>
2022-02-21 12:11:22 -0500

    Move AvatarProps to Avatar component file

--- workspaces/app/src/components/ui/profile/Avatar/types.ts
+++ workspaces/app/src/components/ui/profile/Avatar/types.ts
@@ -5,8 +5,3 @@ export interface Draggable {
   xOffset?: number;
   yOffset?: number;
 }
-export interface AvatarProps extends Omit<ImageProps, 'src'>, Draggable {
-  src?: ImageProps['src'];
-  size: number;
-  hasBorder: boolean;
-}
