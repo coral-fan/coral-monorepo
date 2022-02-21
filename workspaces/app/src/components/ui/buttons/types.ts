f3e450dde4826bd09d2e6acfc463c36be25d2649
maks-p <mpazuniak@gmail.com>
2022-02-21 11:49:08 -0500

    Delete unnecessary files

--- workspaces/app/src/components/ui/buttons/types.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentProps } from 'react';
-
-export type ButtonVariant = 'primary' | 'secondary';
-
-export interface BaseButtonProps extends ComponentProps<'button'> {
-  variant?: ButtonVariant;
-  loading?: boolean;
-}
