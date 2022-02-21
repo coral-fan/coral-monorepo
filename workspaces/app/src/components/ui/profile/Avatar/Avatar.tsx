7a4cafe68b7aa1e10183498c9a3e71751a16b4bb
iknowhtml <iknowsmtp@gmail.com>
2022-02-21 09:13:43 -0500

    Add logic to position image inside Avatar

--- workspaces/app/src/components/ui/profile/Avatar/Avatar.tsx
+++ workspaces/app/src/components/ui/profile/Avatar/Avatar.tsx
@@ -13,14 +13,43 @@ const Wrapper = styled.div<WrapperProps>`
   --size: ${({ size }) => size}px;
   height: var(--size);
   width: var(--size);
+  position: relative;
 `;
 
+const getOffsetPosition = (x?: number, y?: number) => {
+  if (x === undefined && y === undefined) {
+    return undefined;
+  }
+
+  const xOffset = x === undefined ? '' : `${x}px `;
+  const yOffset = y === undefined ? '' : `${y}px`;
+
+  return `${xOffset}${yOffset}`;
+};
 const getImageStyle = (hasBorder: boolean) => css`
   border-radius: 50%;
   border: ${`solid 1px ${hasBorder ? `${tokens.border.color.primary}` : 'transparent'}`};
 `;
-export const Avatar = ({ src = DEFAULT_AVATAR, size, hasBorder }: AvatarProps) => (
-  <Wrapper hasBorder={hasBorder} size={size}>
-    <Image src={src} width={size} height={size} css={getImageStyle(hasBorder)} alt={''} />
-  </Wrapper>
-);
+
+export const Avatar = ({
+  src = DEFAULT_AVATAR,
+  size,
+  hasBorder = false,
+  xOffset,
+  yOffset,
+}: AvatarProps) => {
+  const objectPosition = getOffsetPosition(xOffset, yOffset);
+  return (
+    <Wrapper hasBorder={hasBorder} size={size}>
+      <Image
+        draggable={false}
+        alt={''}
+        layout={'fill'}
+        priority={true}
+        objectPosition={objectPosition}
+        src={src}
+        css={getImageStyle(hasBorder)}
+      />
+    </Wrapper>
+  );
+};
