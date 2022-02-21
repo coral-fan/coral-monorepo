ae3e2a1b3178dc82f77a9dae41a74b0a1a3a5b68
iknowhtml <iknowsmtp@gmail.com>
2022-02-21 09:13:16 -0500

    Add Draggable type

--- workspaces/app/src/components/ui/profile/Avatar/types.ts
+++ workspaces/app/src/components/ui/profile/Avatar/types.ts
@@ -1,6 +1,11 @@
+import { NoEthereumProviderError } from '@web3-react/injected-connector';
 import { ImageProps } from 'next/image';
 
-export interface AvatarProps extends Omit<ImageProps, 'src'> {
+export interface Draggable {
+  xOffset?: number;
+  yOffset?: number;
+}
+export interface AvatarProps extends Omit<ImageProps, 'src'>, Draggable {
   src?: ImageProps['src'];
   size: number;
   hasBorder: boolean;
