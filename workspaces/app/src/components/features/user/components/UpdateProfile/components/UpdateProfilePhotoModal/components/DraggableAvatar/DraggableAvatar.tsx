a00319b2e665c4382aad11f018e7630100df9795
iknowhtml <iknowsmtp@gmail.com>
2022-02-21 12:59:49 -0500

    Add logic to drag photo position

--- workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/components/DraggableAvatar/DraggableAvatar.tsx
+++ workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/components/DraggableAvatar/DraggableAvatar.tsx
@@ -1,16 +1,18 @@
 import { Avatar } from 'components/ui';
-import { useEffect, useRef } from 'react';
-import { filter, fromEvent, map, pipe, zip } from 'rxjs';
+import { useEffect, useRef, useState } from 'react';
+import { filter, fromEvent, map, mergeMapTo, pairwise, pipe, scan, takeUntil } from 'rxjs';
 import { Draggable } from 'components/ui/profile/Avatar/types';
 import styled from '@emotion/styled';
 
 type DraggableAvatarProps = Draggable;
+
 const Wrapper = styled.div`
   &:hover {
     cursor: move;
   }
 `;
-const coordinatesFromMouseEvent = (element: HTMLElement, mouseEventName: 'mousedown' | 'mouseup') =>
+
+const coordinatesFromMouseEvent = (element: HTMLElement) => (mouseEventName: string) =>
   fromEvent(element, mouseEventName).pipe(
     filter((event: Event): event is MouseEvent => event instanceof MouseEvent),
     pipe(
@@ -20,21 +22,60 @@ const coordinatesFromMouseEvent = (element: HTMLElement, mouseEventName: 'moused
       }))
     )
   );
+
+type Coordinates = [number, number];
+
+const getOffsetCoordinates = (
+  element: HTMLElement,
+  [initialX, initialY]: Coordinates,
+  imageElement: HTMLElement
+) => {
+  const [mouseDownCoordinates$, mouseMoveCoordinates$] = ['mousedown', 'mousemove'].map(
+    coordinatesFromMouseEvent(element)
+  );
+  const mouseUp$ = fromEvent(element, 'mouseup');
+
+  return mouseDownCoordinates$.pipe(
+    mergeMapTo(
+      mouseMoveCoordinates$.pipe(
+        pairwise(),
+        map(
+          ([previousCoordinates, currentCoordinates]): Coordinates => [
+            currentCoordinates.x - previousCoordinates.x,
+            currentCoordinates.y - previousCoordinates.y,
+          ]
+        ),
+        takeUntil(mouseUp$)
+      )
+    ),
+    scan<Coordinates, Coordinates>(
+      ([xOffset, yOffset], [xDiff, yDiff]) => [xOffset + xDiff, yOffset + yDiff],
+      [initialX, initialY]
+    )
+  );
+};
+
 export const DraggableAvatar = ({ xOffset = 0, yOffset = 0 }: Draggable = {}) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [[x, y], setCoordinates] = useState<Coordinates>([xOffset, yOffset]);
 
   useEffect(() => {
-    if (ref.current) {
-      const mouseDownCoordinates$ = coordinatesFromMouseEvent(ref.current, 'mousedown');
-      const mouseUpCoordinates$ = coordinatesFromMouseEvent(ref.current, 'mouseup');
+    if (wrapperRef.current) {
+      const imageElement = wrapperRef.current.querySelector('img');
 
-      zip(mouseDownCoordinates$, mouseUpCoordinates$).subscribe(console.log);
+      if (imageElement === null) {
+        throw Error('An imageElement should not be null.');
+      }
+
+      getOffsetCoordinates(wrapperRef.current, [xOffset, yOffset], imageElement).subscribe(
+        setCoordinates
+      );
     }
   }, []);
 
   return (
-    <Wrapper ref={ref}>
-      <Avatar size={200} hasBorder={true} />
+    <Wrapper ref={wrapperRef}>
+      <Avatar size={200} hasBorder={true} xOffset={x} yOffset={y} />
     </Wrapper>
   );
 };
