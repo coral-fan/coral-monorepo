119335ca15a9310e2c2e8e2dd110776196da816c
iknowhtml <iknowsmtp@gmail.com>
2022-02-22 13:39:44 -0500

    Refactor Avatar - Add logic to add hover styling if used in Avatar Editor - Add logic to scale element without affecting layout - Add ref to component to enable dragging image - Use updated object position util function

--- workspaces/app/src/components/ui/profile/Avatar/Avatar.tsx
+++ workspaces/app/src/components/ui/profile/Avatar/Avatar.tsx
@@ -1,25 +1,36 @@
-import { ReactEventHandler } from 'react';
+import { ForwardedRef, forwardRef, ReactEventHandler } from 'react';
 
 import Image, { ImageProps } from 'next/image';
 import styled from '@emotion/styled';
 
 import { DEFAULT_AVATAR } from './consts';
 import { Draggable } from './types';
-import { getOffsetPosition } from './utils';
+import { formatObjectPosition } from './utils';
 
 import tokens from 'styles/tokens';
+import { css } from '@emotion/react';
+import { AVATAR_SIZE } from 'components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/components';
 
-type WrapperProps = Omit<AvatarProps, 'src'>;
+type WrapperProps = Omit<AvatarProps, 'src'> & { isDraggable: boolean };
+
+const draggableHoverStyle = css`
+  &:hover {
+    cursor: move;
+  }
+`;
 
 const Wrapper = styled.div<WrapperProps>`
-  --size: ${({ size }) => size}px;
+  --size: ${AVATAR_SIZE}px;
   height: var(--size);
   width: var(--size);
+  transform: scale(${({ size }) => size / AVATAR_SIZE});
+  margin: -${({ size }) => (AVATAR_SIZE - size) / 2}px;
   position: relative;
   border-radius: 50%;
   overflow: hidden;
   border: ${({ hasBorder }) =>
     `solid 1px ${hasBorder ? `${tokens.border.color.primary}` : 'transparent'}`};
+  ${({ isDraggable }) => (isDraggable ? draggableHoverStyle : null)}
 `;
 
 interface AvatarProps extends Omit<ImageProps, 'src'>, Draggable {
@@ -28,25 +39,24 @@ interface AvatarProps extends Omit<ImageProps, 'src'>, Draggable {
   hasBorder: boolean;
 }
 
-export const Avatar = ({
-  src = DEFAULT_AVATAR,
-  size,
-  hasBorder = false,
-  xOffset,
-  yOffset,
-}: AvatarProps) => {
-  const objectPosition = getOffsetPosition(xOffset, yOffset);
-  return (
-    <Wrapper hasBorder={hasBorder} size={size}>
-      <Image
-        draggable={false}
-        alt={''}
-        layout={'fill'}
-        priority={true}
-        objectPosition={objectPosition}
-        objectFit={'none'}
-        src={src}
-      />
-    </Wrapper>
-  );
-};
+export const Avatar = forwardRef(
+  (
+    { src = DEFAULT_AVATAR, size, hasBorder = false, xOffset, yOffset }: AvatarProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    const objectPosition = formatObjectPosition(xOffset, yOffset);
+    return (
+      <Wrapper hasBorder={hasBorder} size={size} isDraggable={ref !== null} ref={ref}>
+        <Image
+          draggable={false}
+          alt={''}
+          layout={'fill'}
+          priority={true}
+          objectPosition={objectPosition}
+          objectFit={'none'}
+          src={src}
+        />
+      </Wrapper>
+    );
+  }
+);
