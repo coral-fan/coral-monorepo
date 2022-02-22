0e7e60833c62c32ce271b9907d150bd080308e3c
iknowhtml <iknowsmtp@gmail.com>
2022-02-22 13:36:07 -0500

    Rename format position util function

--- workspaces/app/src/components/ui/profile/Avatar/utils.ts
+++ workspaces/app/src/components/ui/profile/Avatar/utils.ts
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import tokens from 'styles/tokens';
 
-export const getOffsetPosition = (x?: number, y?: number) => {
+export const formatObjectPosition = (x?: number, y?: number) => {
   const xOffset = `${x ?? 0}px `;
   const yOffset = `${y ?? 0}px`;
 
