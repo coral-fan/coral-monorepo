fa85cc77bb3d42b85df0489c0004dbc4a2840a10
iknowhtml <iknowsmtp@gmail.com>
2022-02-24 08:52:17 -0500

    Remove Profile Avatar Button component

--- workspaces/app/src/components/app/components/NavigationBar/components/index.ts
+++ workspaces/app/src/components/app/components/NavigationBar/components/index.ts
@@ -1,6 +1,5 @@
 export * from './HamburgerMenuButton';
 export * from './Menu';
 export * from './Notifications';
-export * from './ProfileAvatarButton';
 export * from './LoginButton';
 export * from './LogoHomeLink';
