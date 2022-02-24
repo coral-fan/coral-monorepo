85d97261a0f133650f76975dd9b7f96c6545168e
iknowhtml <iknowsmtp@gmail.com>
2022-02-24 14:14:11 -0500

    Format file & update User Page props

--- workspaces/app/src/components/features/user/User.tsx
+++ workspaces/app/src/components/features/user/User.tsx
@@ -14,56 +14,54 @@ interface UserPageProps {
 
 export const UserPage = ({ userData }: UserPageProps) => {
   return (
-    <UserPageProvider initialUserData={userData}>
+    <UserPageProvider userData={userData}>
       <UserProfile />
     </UserPageProvider>
   );
 };
 
-export const getServerSideProps: GetServerSideProps<
-  UserPageProps,
-  { userProfileId: string }
-> = async (context) => {
-  const { params } = context;
+export const getServerSideProps: GetServerSideProps<UserPageProps, { userProfileId: string }> =
+  async (context) => {
+    const { params } = context;
 
-  if (params === undefined) {
+    if (params === undefined) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const { userProfileId } = params;
+    const publicUserData = await getDocumentData<PublicUserData>('users', userProfileId);
+
+    if (publicUserData === undefined) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const auth = await getAuthenticationServerSide();
+
+    const token = getIdToken(context);
+
+    const authenticatedUserUid =
+      token === null
+        ? undefined
+        : (
+            await auth.verifyIdToken(token).catch(() => {
+              destroyCookie(context, ID_TOKEN_KEY);
+            })
+          )?.uid;
+
+    const privateUserData =
+      authenticatedUserUid === userProfileId
+        ? await getDocumentData<PrivateUserData>('users', userProfileId, 'private', 'data')
+        : undefined;
+
+    const userData: User = { ...publicUserData, ...privateUserData };
+
     return {
-      notFound: true,
+      props: {
+        userData,
+      },
     };
-  }
-
-  const { userProfileId } = params;
-  const publicUserData = await getDocumentData<PublicUserData>('users', userProfileId);
-
-  if (publicUserData === undefined) {
-    return {
-      notFound: true,
-    };
-  }
-
-  const auth = await getAuthenticationServerSide();
-
-  const token = getIdToken(context);
-
-  const authenticatedUserUid =
-    token === null
-      ? undefined
-      : (
-          await auth.verifyIdToken(token).catch(() => {
-            destroyCookie(context, ID_TOKEN_KEY);
-          })
-        )?.uid;
-
-  const privateUserData =
-    authenticatedUserUid === userProfileId
-      ? await getDocumentData<PrivateUserData>('users', userProfileId, 'private', 'data')
-      : undefined;
-
-  const userData: User = { ...publicUserData, ...privateUserData };
-
-  return {
-    props: {
-      userData,
-    },
   };
-};
