d562d1870bf1875c991831ba65d44d7abce29fc4
iknowhtml <iknowsmtp@gmail.com>
2022-02-24 13:42:42 -0500

    Update Update Profile Photo Modal component - Add EditableAvatar component with hover styling - Refactor ref to use callback ref - Add upload photo logic

--- workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/UpdateProfilePhotoModal.tsx
+++ workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/UpdateProfilePhotoModal.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, useEffect, useRef, useState } from 'react';
+import { ChangeEventHandler, useCallback, useState } from 'react';
 import styled from '@emotion/styled';
 import { useIsNetworkSupported } from 'libraries/blockchain';
 import { useIsAuthenticated } from 'libraries/authentication';
@@ -7,10 +7,17 @@ import { Avatar, Button, Modal, OffsetPercentages } from 'components/ui';
 import { useIsUpdateProfilePhotoModalOpen, useUser } from 'components/features/user/hooks';
 import { getObjectPosition } from './observables';
 import { PREVIEW_SIZE } from './consts';
+import { useUpdateProfilePhoto } from './hooks';
 
 const Slider = styled.input`
   &:hover {
     cursor: pointer;
+  }
+`;
+
+const EditableAvatar = styled(Avatar)`
+  &:hover {
+    cursor: move;
   }
 `;
 
@@ -21,39 +28,62 @@ export const UpdateProfilePhotoModal = () => {
 
   const [{ profilePhoto }] = useUser();
 
-  const avatarRef = useRef<HTMLDivElement>(null);
-  const [offsetPercentages, setOffsetPercentages] = useState<OffsetPercentages>(
-    profilePhoto.offsetPercentages
-  );
-
-  useEffect(() => {
-    if (avatarRef.current) {
-      getObjectPosition(avatarRef.current, offsetPercentages).subscribe(setOffsetPercentages);
-    }
-  }, []);
-
   const [scale, setScale] = useState(profilePhoto.scale);
+
+  const isScaleSame = scale === profilePhoto.scale;
 
   const handleRangeValueChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setScale(parseFloat(event.target.value));
   };
 
+  const {
+    src,
+    handleImageFileChange,
+    updateProfilePhoto,
+    isProfilePhotoUpdating,
+    isProfilePhotoSame,
+  } = useUpdateProfilePhoto(profilePhoto.src);
+
+  const [offsetPercentages, setOffsetPercentages] = useState<OffsetPercentages>(
+    profilePhoto.offsetPercentages
+  );
+
+  const isOffsetPercentagesSame = offsetPercentages.every(
+    (offsetPercentage, i) => offsetPercentage === profilePhoto.offsetPercentages[i]
+  );
+
+  const avatarRef = useCallback((avatarWrapperElement) => {
+    if (avatarWrapperElement !== null) {
+      getObjectPosition(avatarWrapperElement, offsetPercentages).subscribe(setOffsetPercentages);
+    }
+  }, []);
+
   if (!isAuthenticated || !isNetworkSupported) {
     return null;
   }
 
+  const handleUpdateProfilePhoto = () => {
+    updateProfilePhoto(offsetPercentages, scale);
+  };
+
   return (
     <Modal onClick={() => setIsModalOpen(false)} variant={'close'}>
-      <Avatar
+      <EditableAvatar
         size={PREVIEW_SIZE}
         offsetPercentages={offsetPercentages}
         scale={scale}
-        src={profilePhoto.src}
+        src={src}
         ref={avatarRef}
       />
-      <Button>Upload Photo</Button>
+      <input type="file" accept="image/*" onChange={handleImageFileChange} />
       <Slider type="range" min={1} max={1.5} onChange={handleRangeValueChange} step={0.01} />
-      <Button>Update Photo</Button>
+      <Button
+        onClick={handleUpdateProfilePhoto}
+        loading={isProfilePhotoUpdating}
+        disabled={isScaleSame && isOffsetPercentagesSame && isProfilePhotoSame}
+      >
+        Update Photo
+      </Button>
     </Modal>
   );
 };
