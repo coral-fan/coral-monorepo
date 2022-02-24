e7eec5cde5595fe87f696f2b6af601be85d61167
iknowhtml <iknowsmtp@gmail.com>
2022-02-24 08:50:29 -0500

    Set profile photo state from user data

--- workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/UpdateProfilePhotoModal.tsx
+++ workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/UpdateProfilePhotoModal.tsx
@@ -2,9 +2,9 @@ import { ChangeEventHandler, useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { useIsNetworkSupported } from 'libraries/blockchain';
 import { useIsAuthenticated } from 'libraries/authentication';
-import { Avatar, Button, Modal, PercentageOffsets } from 'components/ui';
+import { Avatar, Button, Modal, OffsetPercentages } from 'components/ui';
 
-import { useIsUpdateProfilePhotoModalOpen } from 'components/features/user/hooks';
+import { useIsUpdateProfilePhotoModalOpen, useUser } from 'components/features/user/hooks';
 import { getObjectPosition } from './observables';
 import { PREVIEW_SIZE } from './consts';
 
@@ -19,16 +19,20 @@ export const UpdateProfilePhotoModal = () => {
   const isNetworkSupported = useIsNetworkSupported();
   const [, setIsModalOpen] = useIsUpdateProfilePhotoModalOpen();
 
+  const [{ profilePhoto }] = useUser();
+
   const avatarRef = useRef<HTMLDivElement>(null);
-  const [[x, y], objectPosition] = useState<PercentageOffsets>([0, 0]);
+  const [offsetPercentages, setOffsetPercentages] = useState<OffsetPercentages>(
+    profilePhoto.offsetPercentages
+  );
 
   useEffect(() => {
     if (avatarRef.current) {
-      getObjectPosition(avatarRef.current, [x, y]).subscribe(objectPosition);
+      getObjectPosition(avatarRef.current, offsetPercentages).subscribe(setOffsetPercentages);
     }
   }, []);
 
-  const [scale, setScale] = useState(1);
+  const [scale, setScale] = useState(profilePhoto.scale);
 
   const handleRangeValueChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setScale(parseFloat(event.target.value));
@@ -40,7 +44,13 @@ export const UpdateProfilePhotoModal = () => {
 
   return (
     <Modal onClick={() => setIsModalOpen(false)} variant={'close'}>
-      <Avatar size={PREVIEW_SIZE} percentageOffsets={[x, y]} scale={scale} ref={avatarRef} />
+      <Avatar
+        size={PREVIEW_SIZE}
+        offsetPercentages={offsetPercentages}
+        scale={scale}
+        src={profilePhoto.src}
+        ref={avatarRef}
+      />
       <Button>Upload Photo</Button>
       <Slider type="range" min={1} max={1.5} onChange={handleRangeValueChange} step={0.01} />
       <Button>Update Photo</Button>
