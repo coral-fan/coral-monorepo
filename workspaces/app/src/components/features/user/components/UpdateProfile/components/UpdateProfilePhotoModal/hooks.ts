265743339c6966ff2b77b4c2f1d5c5e0cec0713a
iknowhtml <iknowsmtp@gmail.com>
2022-02-24 13:45:54 -0500

    Update image upload hook - Rename hook - Expose various image upload states to hook consumers - Add logic to upload photo only when image is uploaded - Add logic to update User data when image is uploaded

--- workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/hooks.ts
+++ workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/hooks.ts
@@ -1,43 +1,71 @@
+import { useIsUpdateProfilePhotoModalOpen } from 'components/features/user/hooks';
+import { OffsetPercentages } from 'components/ui';
 import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
-import { useUserUid } from 'libraries/models';
+import { upsertUser, useUserUid } from 'libraries/models';
+import { useRefetchPageData } from 'libraries/utils';
 import { ChangeEvent, useCallback, useState } from 'react';
 
-export const useImageUpload = () => {
+export const useUpdateProfilePhoto = (initialSrc: string) => {
   const uid = useUserUid();
 
-  const [localImageUrl, setLocalImageUrl] = useState<string>();
+  const [src, setSrc] = useState<string>(initialSrc);
   const [imageFile, setImageFile] = useState<File>();
-  const [isImageUploading, setIsImageUploading] = useState(false);
+  const [isProfilePhotoUpdating, setIsProfilePhotoUpdating] = useState(false);
+  const [isProfilePhotoSame, setIsProfilePhotoSame] = useState(true);
+
   const handleImageFileChange = useCallback(
     ({ target: { files } }: ChangeEvent<HTMLInputElement>) => {
       if (files instanceof FileList) {
         const image = files[0];
         setImageFile(image);
-        setLocalImageUrl(URL.createObjectURL(image));
+        setSrc(URL.createObjectURL(image));
+        setIsProfilePhotoSame(false);
       }
     },
     []
   );
+  const refetchPageData = useRefetchPageData();
+  const [, setIsModalOpen] = useIsUpdateProfilePhotoModalOpen();
 
-  const handleImageUpload = useCallback(async () => {
-    setIsImageUploading(true);
-    if (uid === undefined) {
-      throw Error(
-        'useImageUpload hook handleImageUpload function should not be called if the user is not logged in.'
-      );
-    } else {
-      try {
-        const storage = getStorage();
-        const imageRef = ref(storage, `${uid}/profile-photo`);
-        const result = await uploadBytes(imageRef, imageFile as File);
-        const downloadUrl = await getDownloadURL(result.ref);
-        console.log(downloadUrl);
-      } catch (error) {
-      } finally {
-        setIsImageUploading(false);
+  const updateProfilePhoto = useCallback(
+    async (offsetPercentages: OffsetPercentages, scale: number) => {
+      setIsProfilePhotoUpdating(true);
+      if (uid === undefined) {
+        throw Error(
+          'useImageUpload hook handleImageUpload function should not be called if the user is not logged in.'
+        );
+      } else {
+        try {
+          if (imageFile === undefined) {
+            await upsertUser(uid, {
+              profilePhoto: { src: initialSrc, offsetPercentages, scale },
+            });
+          } else {
+            const storage = getStorage();
+            const imageRef = ref(storage, `${uid}/profile-photo`);
+            const result = await uploadBytes(imageRef, imageFile);
+            const downloadUrl = await getDownloadURL(result.ref);
+            await upsertUser(uid, {
+              profilePhoto: { src: downloadUrl, offsetPercentages, scale },
+            });
+          }
+
+          await refetchPageData();
+          setIsModalOpen(false);
+        } catch (error) {
+        } finally {
+          setIsProfilePhotoUpdating(false);
+        }
       }
-    }
-  }, [uid, imageFile]);
+    },
+    [uid, imageFile]
+  );
 
-  return { localImageUrl, isImageUploading, handleImageFileChange, handleImageUpload };
+  return {
+    src,
+    handleImageFileChange,
+    isProfilePhotoSame,
+    updateProfilePhoto,
+    isProfilePhotoUpdating,
+  };
 };
