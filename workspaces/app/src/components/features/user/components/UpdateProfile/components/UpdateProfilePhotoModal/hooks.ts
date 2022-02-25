5ddb00f561ba1ef1970f5924899554d8d0185f21
iknowhtml <iknowsmtp@gmail.com>
2022-02-24 19:42:28 -0500

    Add missing dependencies to useCallback

--- workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/hooks.ts
+++ workspaces/app/src/components/features/user/components/UpdateProfile/components/UpdateProfilePhotoModal/hooks.ts
@@ -58,7 +58,7 @@ export const useUpdateProfilePhoto = (initialSrc: string) => {
         }
       }
     },
-    [uid, imageFile]
+    [imageFile, initialSrc, refetchPageData, setIsModalOpen, uid]
   );
 
   return {
