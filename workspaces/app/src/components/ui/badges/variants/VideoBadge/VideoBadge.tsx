feea9fb340f874914a70bc524c12235aa549f00f
maks-p <mpazuniak@gmail.com>
2022-02-25 15:02:10 -0500

    Update casing

--- workspaces/app/src/components/ui/badges/variants/VideoBadge/VideoBadge.tsx
+++ workspaces/app/src/components/ui/badges/variants/VideoBadge/VideoBadge.tsx
@@ -1,4 +1,4 @@
 import { BaseBadge } from '../../BaseBadge';
 import videoSVG from './video.svg';
 
-export const VideoBadge = () => <BaseBadge svg={videoSVG}>video call</BaseBadge>;
+export const VideoBadge = () => <BaseBadge svg={videoSVG}>Video Call</BaseBadge>;
