955cef4330493015a621991bdf86a701780f550b
maks-p <mpazuniak@gmail.com>
2022-02-24 19:31:50 -0500

    Delete old badges

--- workspaces/app/src/components/ui/badges/MusicBadge/MusicBadge.tsx
+++
@@ -1,7 +0,0 @@
-import { Badge } from '../Badge';
-import musicSVG from './music.svg';
-import { BadgeProps } from 'components/ui/badges/types';
-
-export const MusicBadge = ({ size, variant }: BadgeProps) => {
-  return <Badge iconComponent="MusicIcon" svg={musicSVG} size={size} variant={variant} />;
-};
--- workspaces/app/src/components/ui/badges/VideoBadge/VideoBadge.tsx
+++
@@ -1,7 +0,0 @@
-import { Badge } from '../Badge';
-import videoSVG from './video.svg';
-import { BadgeProps } from 'components/ui/badges/types';
-
-export const VideoBadge = ({ size, variant }: BadgeProps) => {
-  return <Badge iconComponent="VideoIcon" svg={videoSVG} size={size} variant={variant} />;
-};
