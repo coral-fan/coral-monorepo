24ff2edf6070a8ea3679f8ff5ce8eab524403c86
maks-p <mpazuniak@gmail.com>
2022-02-24 19:31:50 -0500

    Delete old badges

--- workspaces/app/src/components/ui/badges/DoorBadge/DoorBadge.tsx
+++
@@ -1,7 +0,0 @@
-import { Badge } from '../Badge';
-import doorSVG from './door.svg';
-import { BadgeProps } from 'components/ui/badges/types';
-
-export const DoorBadge = ({ size, variant }: BadgeProps) => {
-  return <Badge iconComponent="DoorIcon" svg={doorSVG} size={size} variant={variant} />;
-};
