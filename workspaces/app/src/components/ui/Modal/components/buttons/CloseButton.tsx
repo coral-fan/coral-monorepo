cbf4883e4353867624433a7378fa04f8955fed7b
iknowhtml <iknowsmtp@gmail.com>
2022-02-26 15:01:27 -0500

    Extract Close Button & remove unnecessary buttons

--- workspaces/app/src/components/ui/Modal/components/buttons/CloseButton.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentProps } from 'react';
-import { CONTROL_BUTTON_STYLE } from './consts';
-import { CloseIcon } from 'components/ui';
-
-export const CloseButton = (props: ComponentProps<'button'>) => (
-  <button css={CONTROL_BUTTON_STYLE} {...props}>
-    <CloseIcon size={22.5} />
-  </button>
-);
