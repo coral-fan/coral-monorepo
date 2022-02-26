a3e55973636140eea30490bb750c4b63cc766f56
iknowhtml <iknowsmtp@gmail.com>
2022-02-26 09:05:22 -0500

    Extract Overlay component & move to components

---
+++ workspaces/app/src/components/ui/Modal/components/Overlay.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Overlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+  padding: 0 12px;
+`;
