b826dec043cfc836928b1db5270157ccf9b43bfd
iknowhtml <iknowsmtp@gmail.com>
2022-02-26 09:04:55 -0500

    Move buttons to components directory

--- workspaces/app/src/components/ui/Modal/components/buttons/CloseButton.tsx
+++ workspaces/app/src/components/ui/Modal/components/buttons/CloseButton.tsx
@@ -4,6 +4,6 @@ import { CloseIcon } from 'components/ui';
 
 export const CloseButton = (props: ComponentProps<'button'>) => (
   <button css={CONTROL_BUTTON_STYLE} {...props}>
-    <CloseIcon size={20} />
+    <CloseIcon size={22.5} />
   </button>
 );
--- workspaces/app/src/components/ui/Modal/components/buttons/consts.ts
+++ workspaces/app/src/components/ui/Modal/components/buttons/consts.ts
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import tokens from 'styles/tokens';
 
-const SIZE = '30px';
+const SIZE = '45px';
 
 export const CONTROL_BUTTON_STYLE = css`
   display: flex;
@@ -11,7 +11,7 @@ export const CONTROL_BUTTON_STYLE = css`
   height: ${SIZE};
   color: ${tokens.font.color.primary};
   background-color: ${tokens.background.color.secondary};
-  border: 0.5px solid #534b58;
+  border: none;
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
   border-radius: 30px;
 
