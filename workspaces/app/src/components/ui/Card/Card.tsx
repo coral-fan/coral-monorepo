06b6861fcfa711c979755a13981dabdcd4a9a424
iknowhtml <iknowsmtp@gmail.com>
2022-02-26 09:06:51 -0500

    Update Card background color to match designs

--- workspaces/app/src/components/ui/Card/Card.tsx
+++ workspaces/app/src/components/ui/Card/Card.tsx
@@ -3,7 +3,7 @@ import tokens from 'styles/tokens';
 
 export const Card = styled.div`
   display: flex;
-  background-color: ${tokens.background.color.tertiary};
+  background-color: ${tokens.background.color.secondary};
   border-radius: 12px;
   width: 100%;
 `;
