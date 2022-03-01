319bd93d3a7b3f48a7dcc5063e85c1bbd7665e85
iknowhtml <iknowsmtp@gmail.com>
2022-03-01 12:27:06 -0500

    Update Collection type

--- workspaces/app/src/libraries/models/collection/types.ts
+++ workspaces/app/src/libraries/models/collection/types.ts
@@ -1,13 +1,16 @@
+export type CollectionType = 'music' | 'event' | 'merch';
+
 export interface Collection {
   // id = nft smart contract address
   id: string;
   /* blockchain data index cache */
   artistId: string;
+  imageUrl: string;
   maxMintable: number;
   /* */
-  type: 'access' | 'event';
+  type: CollectionType;
   price: number;
   dropDate: string;
   description: string;
-  details: string[];
+  details: string[] | null;
 }
