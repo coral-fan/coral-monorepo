b71e7d0e2201246c5221c5953e8d41c9eed477d1
maks-p <mpazuniak@gmail.com>
2022-02-28 21:33:39 -0500

    Delete icons

--- workspaces/app/src/components/app/components/NavigationBar/components/Menu/assets/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as creditCardSVG } from './credit-card.svg';
-export { default as userSVG } from './user.svg';
-export { default as bellSVG } from './bell.svg';
-export { default as warningSVG } from './warning.svg';
