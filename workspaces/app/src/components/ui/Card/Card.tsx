a8463e95556677514caa92ff7bd534fea0818c30
maks-p <mpazuniak@gmail.com>
2022-02-28 21:32:39 -0500

    Update border radius on card using design tokens

--- workspaces/app/src/components/ui/Card/Card.tsx
+++ workspaces/app/src/components/ui/Card/Card.tsx
@@ -4,6 +4,6 @@ import tokens from 'styles/tokens';
 export const Card = styled.div`
   display: flex;
   background-color: ${tokens.background.color.secondary};
-  border-radius: 12px;
+  border-radius: ${tokens.border.radius.lg};
   width: 100%;
 `;
