2f961e3f975ca6896536093b5a80f807e95dd9de
iknowhtml <iknowsmtp@gmail.com>
2022-03-01 12:27:14 -0500

    Update Asset type

--- workspaces/app/src/libraries/models/asset/types.ts
+++ workspaces/app/src/libraries/models/asset/types.ts
@@ -1,8 +1,11 @@
-// TODO: need to think about whether we need meta data stored off chain
-export interface Asset {
+export interface AssetData {
   id: number;
-  collectionId: string;
+  userId: number;
 }
+
+export type Asset = AssetData & {
+  collectionId: string;
+};
 /* not tracking the following because writes can happen frequently and outside of our platform
    - user id
    - transaction history
