071ae4d9be41562cec9306081c15adba061180c5
maks-p <mpazuniak@gmail.com>
2022-03-01 21:00:55 -0500

    Create ExperienceDetail component

---
+++ workspaces/app/src/components/features/asset/components/ExperienceDetails/ExperienceDetails.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react';
+import { ExperienceDetailProp, ExperienceDetails } from './ExperienceDetails';
+
+export default {
+  title: 'Coral/Asset/Experience Details',
+  component: ExperienceDetails,
+} as Meta;
+
+// eslint-disable-next-line jsx-a11y/alt-text
+const Template: Story<ExperienceDetailProp> = ({ ...args }) => <ExperienceDetails {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  details: [
+    'A personal call between just you and Bonobo',
+    'Available any time before March 1st, 2022',
+    "Accessible by Zoom after you've torn the ticket",
+  ],
+};
---
+++ workspaces/app/src/components/features/asset/components/ExperienceDetails/ExperienceDetails.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { Detail } from './components';
+import tokens from 'styles/tokens';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Heading = styled.h4`
+  font-size: ${tokens.font.size.md};
+  line-height: ${tokens.font.line_height.md};
+  letter-spacing: ${tokens.font.letter_spacing.md};
+  color: ${tokens.font.color.primary};
+  font-weight: ${tokens.font.weight.bold};
+  text-transform: uppercase;
+  padding-bottom: 12px;
+`;
+
+export interface ExperienceDetailProp {
+  details: string[];
+}
+
+export const ExperienceDetails = ({ details }: ExperienceDetailProp) => {
+  return (
+    <Container>
+      <Heading>Experience Details</Heading>
+      {details.map((detail: string) => (
+        <Detail key={detail}>{detail}</Detail>
+      ))}
+    </Container>
+  );
+};
