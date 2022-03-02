139bafa3c6ce9a911bda7def5dbf0e961965ffd8
iknowhtml <iknowsmtp@gmail.com>
2022-03-02 10:34:52 -0500

    Update Share Button copy text casing

--- workspaces/app/src/components/ui/nft/buttons/ShareButton/ShareButton.tsx
+++ workspaces/app/src/components/ui/nft/buttons/ShareButton/ShareButton.tsx
@@ -2,5 +2,5 @@ import { Button } from 'components/ui/buttons';
 import { ComponentProps } from 'react';
 
 export const ShareButton = (props: ComponentProps<'button'>) => (
-  <Button {...props}>share this nft</Button>
+  <Button {...props}>Share This NFT</Button>
 );
