3b90430099df2ecfb2fa08fc15aabe2cf306e46e
maks-p <mpazuniak@gmail.com>
2022-03-03 13:29:28 -0500

    Move uid functions to utils directory

--- workspaces/app/src/libraries/models/user/index.ts
+++ workspaces/app/src/libraries/models/user/index.ts
@@ -3,4 +3,3 @@ export * from './observables';
 export * from './types';
 export * from './utils';
 export * from './api';
-export * from './uid';
--- workspaces/app/src/libraries/models/user/utils.ts
+++ workspaces/app/src/libraries/models/user/utils.ts
@@ -1,5 +1,29 @@
 import { getDocumentData } from 'libraries/firebase';
 import { IsUserSigningUpData } from '.';
+import { destroyCookie, parseCookies } from 'nookies';
+import { COOKIE_OPTIONS } from 'consts';
+import { initializeFirebaseAdmin } from 'libraries/firebase';
+import { getAuth } from 'firebase/auth';
+import { getApp } from 'firebase/app';
+
+type Context = Parameters<typeof parseCookies>[0] & Parameters<typeof destroyCookie>[0];
+
+export const getUidServerSide = async (ctx: Context) => {
+  await initializeFirebaseAdmin();
+  try {
+    const { getApp } = await import('firebase-admin/app');
+    const app = getApp();
+    const { getAuth } = await import('firebase-admin/auth');
+    const cookies = parseCookies(ctx);
+    const { uid } = await getAuth(app).verifyIdToken(cookies.token);
+    return uid;
+  } catch (_) {
+    destroyCookie(ctx, 'token', COOKIE_OPTIONS);
+    return undefined;
+  }
+};
+
+export const getUidClientSide = () => getAuth(getApp()).currentUser?.uid;
 
 export const getIsUserSigningUp = async (uid: string) => {
   const isUserSigningUpData = await getDocumentData<IsUserSigningUpData>('is-signing-up', uid);
