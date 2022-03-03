d716c8c39716d18e8395cc4446a0beeb05b0fe8f
maks-p <mpazuniak@gmail.com>
2022-03-03 11:33:54 -0500

    Revert initializeStore

--- workspaces/app/src/libraries/state/utils.ts
+++ workspaces/app/src/libraries/state/utils.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { ServerSideData } from 'components/app';
 
 import authenticationReducer, {
   initialState as initialAuthenticationState,
 } from '../authentication/slice';
 
-export const initializeStore = () => {
+type InitialState = ServerSideData['initialState'];
+
+export const initializeStore = (initialState: InitialState) => {
   const store = configureStore({
     reducer: {
       authentication: authenticationReducer,
@@ -12,6 +15,7 @@ export const initializeStore = () => {
     preloadedState: {
       authentication: {
         ...initialAuthenticationState,
+        ...initialState,
       },
     },
   });
