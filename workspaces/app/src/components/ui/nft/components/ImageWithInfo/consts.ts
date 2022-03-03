49766c066c2f1928b43336f95277a98e9329ecc1
iknowhtml <iknowsmtp@gmail.com>
2022-03-03 13:28:37 -0500

    Add Image With Info default args constant

---
+++ workspaces/app/src/components/ui/nft/components/ImageWithInfo/consts.ts
@@ -0,0 +1,9 @@
+export const IMAGE_WITH_INFO_DEFAULT_ARGS = {
+  src: 'https://www.stereofox.com/images/86513/resized.jpg',
+  profilePhoto: {
+    src: 'https://www.stereofox.com/images/86513/resized.jpg',
+    offsetPercentages: [0, 0],
+    scale: 1,
+  },
+  artist: 'Bonobo',
+};
