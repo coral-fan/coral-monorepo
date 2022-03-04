24f8ed2f18da162497bfa81c22d48a4cef10d9b5
iknowhtml <iknowsmtp@gmail.com>
2022-03-04 15:20:45 -0500

    Extract Base Info default Story args to const file

--- workspaces/app/src/components/ui/nft/components/BaseInfo/BaseInfo.stories.tsx
+++ workspaces/app/src/components/ui/nft/components/BaseInfo/BaseInfo.stories.tsx
@@ -1,8 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { VideoBadge } from '../../../badges';
 
 import { BaseInfo } from './BaseInfo';
-import { IMAGE_WITH_INFO_DEFAULT_ARGS } from '../ImageWithInfo/consts';
+import { BASE_INFO_DEFAULT_ARGS } from './consts';
 
 export default {
   title: 'Coral/UI/NFT/Components/Info Base',
@@ -14,10 +13,4 @@ const Template: ComponentStory<typeof BaseInfo> = (args) => <BaseInfo {...args} 
 
 export const Default = Template.bind({});
 
-Default.args = {
-  name: 'Behind the Scenes Studio Tour',
-  Badge: VideoBadge,
-  description:
-    'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-  ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-};
+Default.args = BASE_INFO_DEFAULT_ARGS;
---
+++ workspaces/app/src/components/ui/nft/components/BaseInfo/consts.ts
@@ -0,0 +1,8 @@
+import { IMAGE_WITH_INFO_DEFAULT_ARGS } from '../ImageWithInfo/consts';
+
+export const BASE_INFO_DEFAULT_ARGS = {
+  name: 'Behind the Scenes Studio Tour',
+  description:
+    'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+  ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+};
