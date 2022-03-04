83742b40d3e7e717795093e7cc047a0525410e74
iknowhtml <iknowsmtp@gmail.com>
2022-03-04 15:18:55 -0500

    Move flex direction styling to Card component

--- workspaces/app/src/components/ui/Card/Card.tsx
+++ workspaces/app/src/components/ui/Card/Card.tsx
@@ -3,6 +3,7 @@ import tokens from 'styles/tokens';
 
 export const Card = styled.div`
   display: flex;
+  flex-direction: column;
   background-color: ${tokens.background.color.secondary};
   border-radius: ${tokens.border.radius.lg};
   width: 100%;
