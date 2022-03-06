63e5dc18df3e51f4dd91a565c9c141e2ab525357
iknowhtml <iknowsmtp@gmail.com>
2022-03-06 10:36:28 -0500

    Update Drop Card Story default args

--- workspaces/app/src/components/ui/nft/DropCard/DropCard.stories.tsx
+++ workspaces/app/src/components/ui/nft/DropCard/DropCard.stories.tsx
@@ -13,9 +13,11 @@ const Template: ComponentStory<typeof DropCard> = (args) => <DropCard {...args} 
 
 export const Default = Template.bind({});
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- using destructuring to remove property without mutating object
+const { nameHeadingLevel, ...baseDefaultArgs } = BASE_INFO_DEFAULT_ARGS;
+
 Default.args = {
-  ...BASE_INFO_DEFAULT_ARGS,
-  nameHeadingLevel: 3,
+  ...baseDefaultArgs,
   Badge: EventBadge,
   dropDateTimestamp: new Date().getTime(),
 };
