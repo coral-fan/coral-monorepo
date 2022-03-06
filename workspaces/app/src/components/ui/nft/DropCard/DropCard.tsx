f35e31b66e000005461497d8e0b68886b9847264
iknowhtml <iknowsmtp@gmail.com>
2022-03-06 08:46:51 -0500

    Complete Drop Card component

--- workspaces/app/src/components/ui/nft/DropCard/DropCard.tsx
+++ workspaces/app/src/components/ui/nft/DropCard/DropCard.tsx
@@ -1,3 +1,55 @@
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
+import styled from '@emotion/styled';
 import { Card } from 'components/ui/Card';
+import tokens from 'styles/tokens';
+import { BaseInfo, BaseInfoProps } from '../components';
 
-export const DropCard = () => <Card></Card>;
+interface DropCardProps extends BaseInfoProps {
+  Badge: () => EmotionJSX.Element;
+  dropDateTimestamp: string;
+}
+
+const DropCardContainer = styled(Card)`
+  overflow: hidden;
+  width: 300px;
+`;
+
+const BadgeAndDropDateContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const DropDateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  font-size: ${tokens.font.size.xs};
+  font-weight: ${tokens.font.weight.bold};
+`;
+
+const formatDropCardDate = (timestamp: string) => {
+  const dropDate = new Date(timestamp);
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    timeZoneName: 'short',
+  }).format(dropDate);
+
+  return formattedDate;
+};
+export const DropCard = ({ Badge, dropDateTimestamp, ...baseInfoProps }: DropCardProps) => (
+  <DropCardContainer>
+    <BaseInfo {...baseInfoProps}>
+      <BadgeAndDropDateContainer>
+        <Badge />
+        <DropDateContainer>
+          <span>Drops On</span>
+          <span>{formatDropCardDate(dropDateTimestamp)}</span>
+        </DropDateContainer>
+      </BadgeAndDropDateContainer>
+    </BaseInfo>
+  </DropCardContainer>
+);
