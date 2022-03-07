49eb9fdb87cc6f05144a745dcb2754fb25a316ee
iknowhtml <iknowsmtp@gmail.com>
2022-03-07 14:27:26 -0500

    Update Artist type

--- workspaces/app/src/libraries/models/artist/types.ts
+++ workspaces/app/src/libraries/models/artist/types.ts
@@ -1,11 +1,12 @@
 import { NullableString } from '../types';
 import { Collection } from '../collection';
+import { Photo } from '../user';
 // id = coral wallet address
 export interface Artist {
   // id = coral wallet address, not artist's personal wallet address
   id: string;
   name: string;
-
+  profilePhoto: Photo;
   socialMedia: {
     twitter: NullableString;
     facebook: NullableString;
