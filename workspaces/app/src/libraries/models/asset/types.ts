a113f1025b9117547e7852e18d2fafdda325491d
iknowhtml <iknowsmtp@gmail.com>
2022-03-07 14:27:16 -0500

    Update Asset type

--- workspaces/app/src/libraries/models/asset/types.ts
+++ workspaces/app/src/libraries/models/asset/types.ts
@@ -1,12 +1,15 @@
+import { Collection } from '../collection';
+import { User } from '../user';
+
 // AssetData is data returned from db, aka off chain
 export interface AssetData {
   id: number;
   collectionId: string;
 }
 
-// userId is derived from on cnain data
-export type Asset = AssetData & {
-  userId: number;
-};
+// userId is derived from on chain data
+export type Asset = Pick<AssetData, 'id'> &
+  Pick<Collection, 'imageUrl' | 'type' | 'description' | 'details'> &
+  Pick<User, 'profilePhoto' | 'username'>;
 
 // TODO: we will eventually need to index transaction history
