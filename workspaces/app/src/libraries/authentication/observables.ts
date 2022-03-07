077fc4d8b2eb0534b2e7dd9d0609bbe971490ca6
maks-p <mpazuniak@gmail.com>
2022-03-07 13:57:52 -0500

    Create getLoggedIn$ observable

--- workspaces/app/src/libraries/authentication/observables.ts
+++ workspaces/app/src/libraries/authentication/observables.ts
@@ -1,7 +1,13 @@
 import { getAuth } from 'firebase/auth';
-import { idToken } from 'rxfire/auth';
+import { authState, idToken } from 'rxfire/auth';
+import { filter } from 'rxjs';
 
 export const getIdToken$ = () => {
   const auth = getAuth();
   return idToken(auth);
+};
+
+export const getLoggedIn$ = () => {
+  const auth = getAuth();
+  return authState(auth).pipe(filter((user) => !!user));
 };
