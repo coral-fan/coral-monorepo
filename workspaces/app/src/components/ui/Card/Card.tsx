ab5096761489578dbe9cb0069653830995f1d9bd
iknowhtml <iknowsmtp@gmail.com>
2022-03-08 06:23:10 -0500

    Add overflow styling & move width styling

--- workspaces/app/src/components/ui/Card/Card.tsx
+++ workspaces/app/src/components/ui/Card/Card.tsx
@@ -4,7 +4,8 @@ import tokens from 'styles/tokens';
 export const Card = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  overflow: hidden;
   background-color: ${tokens.background.color.secondary};
   border-radius: ${tokens.border.radius.lg};
-  width: 100%;
 `;
