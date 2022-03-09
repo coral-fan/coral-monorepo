6932791fcd2347bfe1b27bd68dc6c9e244b0fc02
maks-p <mpazuniak@gmail.com>
2022-03-08 19:00:16 -0500

    Create getUser observable

--- workspaces/app/src/libraries/models/user/observables.ts
+++ workspaces/app/src/libraries/models/user/observables.ts
@@ -1,8 +1,9 @@
 import { getAuth } from 'firebase/auth';
-import { getCollectionReferenceClientSide } from 'libraries/firebase';
+import { getCollectionReferenceClientSide, getDocumentData } from 'libraries/firebase';
 import { user } from 'rxfire/auth';
 import { collectionData } from 'rxfire/firestore';
-import { map } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs';
+import { User } from './types';
 
 // client side only
 const getFirebaseUser$ = () => {
@@ -18,5 +19,15 @@ export const getUsernames$ = () => {
   const usersCollectionReference = getCollectionReferenceClientSide('users');
   return collectionData(usersCollectionReference).pipe(
     map((users) => new Set(users.map((user) => user.username.toLowerCase())))
+  );
+};
+
+//client side only
+export const getUser$ = () => {
+  return getUserUid$().pipe(
+    filter((uid): uid is string => uid !== undefined),
+    mergeMap((uid) => getDocumentData<User>('users', uid)),
+    filter((user): user is User => user !== undefined),
+    map((user) => user)
   );
 };
