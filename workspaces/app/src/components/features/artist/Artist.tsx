e716ec124ad6396c848b3e570cf57c3af7355c35
maks-p <mpazuniak@gmail.com>
2022-03-10 17:45:40 -0500

    Update Artist page with updated data

--- workspaces/app/src/components/features/artist/Artist.tsx
+++ workspaces/app/src/components/features/artist/Artist.tsx
@@ -31,10 +31,15 @@ export const getServerSideProps: GetServerSideProps<ArtistPageProps, { artistId:
   const artistData: Artist = {
     id: '1',
     name: 'Bonobo',
+    imageUrl: 'https://www.stereofox.com/images/86513/resized.jpg',
+    description:
+      'Simon Green, known by his stage name Bonobo, is a British musician, producer, and DJ based in Los Angeles. He debuted with a trip hop aesthetic, and has since explored more upbeat approaches as well as jazz and world music influences.',
+    quote:
+      'I’m beyond honored to have you all as my fans and I can’t wait to show you how I create this music',
     socialMedia: {
-      twitter: null,
-      facebook: null,
-      instagram: null,
+      twitter: 'si_bonobo',
+      facebook: 'bonoboofficial',
+      instagram: 'si_bonobo',
     },
     collections: [
       {
