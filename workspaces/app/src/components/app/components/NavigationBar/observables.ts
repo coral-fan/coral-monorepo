661944526e633cb308a68bf601347608ca8237db
maks-p <mpazuniak@gmail.com>
2022-03-10 10:28:27 -0500

    Rename UserProfileData to UserProfile

--- workspaces/app/src/components/app/components/NavigationBar/observables.ts
+++ workspaces/app/src/components/app/components/NavigationBar/observables.ts
@@ -2,7 +2,7 @@ import { getDocumentData } from 'libraries/firebase';
 import { getUserUid$, User } from 'libraries/models';
 import { filter, map, mergeMap } from 'rxjs/operators';
 
-export const getUserProfileData$ = () =>
+export const getUserProfile$ = () =>
   getUserUid$().pipe(
     filter((uid): uid is string => uid !== undefined),
     mergeMap((uid) => getDocumentData<User>('users', uid)),
