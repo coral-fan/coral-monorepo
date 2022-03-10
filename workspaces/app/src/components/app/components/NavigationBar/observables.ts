d5950f9a6a5392d740d98836ea5f79e5de5a0d16
maks-p <mpazuniak@gmail.com>
2022-03-10 08:31:21 -0500

    Make implicit return

--- workspaces/app/src/components/app/components/NavigationBar/observables.ts
+++ workspaces/app/src/components/app/components/NavigationBar/observables.ts
@@ -2,11 +2,10 @@ import { getDocumentData } from 'libraries/firebase';
 import { getUserUid$, User } from 'libraries/models';
 import { filter, map, mergeMap } from 'rxjs/operators';
 
-export const getUserProfileData$ = () => {
-  return getUserUid$().pipe(
+export const getUserProfileData$ = () =>
+  getUserUid$().pipe(
     filter((uid): uid is string => uid !== undefined),
     mergeMap((uid) => getDocumentData<User>('users', uid)),
     filter((user): user is User => user !== undefined),
     map(({ username, profilePhoto }) => ({ username, profilePhoto }))
   );
-};
