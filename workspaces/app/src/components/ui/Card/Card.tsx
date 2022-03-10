765e4ac96148199fad9fb2ef7e730d3f8210dc47
iknowhtml <iknowsmtp@gmail.com>
2022-03-10 08:33:55 -0500

    Add logic to change background color based on prop

--- workspaces/app/src/components/ui/Card/Card.tsx
+++ workspaces/app/src/components/ui/Card/Card.tsx
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled';
 import tokens from 'styles/tokens';
 
-export const Card = styled.div`
+interface CardProps {
+  variant?: 'primary' | 'secondary';
+}
+export const Card = styled.div<CardProps>`
   display: flex;
   flex-direction: column;
   width: 100%;
   overflow: hidden;
-  background-color: ${tokens.background.color.secondary};
+  background-color: ${({ variant }) =>
+    variant === 'secondary' ? tokens.background.color.tertiary : tokens.background.color.secondary};
   border-radius: ${tokens.border.radius.lg};
 `;
