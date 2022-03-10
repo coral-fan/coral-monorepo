8c32c52d71203d2bf14d3c5c4ea9af99f40d16f0
maks-p <mpazuniak@gmail.com>
2022-03-10 10:23:27 -0500

    Rename onItemClick

--- workspaces/app/src/components/app/components/NavigationBar/components/Menu/NotificationItem.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/Menu/NotificationItem.tsx
@@ -5,7 +5,7 @@ import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
 
 export interface NotificationItemProp {
   notificationsCount: number;
-  onItemClick: () => void;
+  handleCloseMenu: () => void;
 }
 const Badge = styled.div`
   height: 22px;
@@ -35,8 +35,8 @@ const Content = styled.div`
 const getFontSizeStyle = (count: number) => css`
   font-size: ${count > 99 ? 8 : count > 9 ? 11 : 14}px;
 `;
-export const NotificationItem = ({ notificationsCount, onItemClick }: NotificationItemProp) => (
-  <Item onItemClick={onItemClick}>
+export const NotificationItem = ({ notificationsCount, handleCloseMenu }: NotificationItemProp) => (
+  <Item handleCloseMenu={handleCloseMenu}>
     <Content>
       notifications
       {notificationsCount > 0 && (
