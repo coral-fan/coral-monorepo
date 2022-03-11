58945603b0460ec5ade3c8b5249ec118a750ef53
iknowhtml <iknowsmtp@gmail.com>
2022-03-11 06:54:12 -0500

    Extract photo args to const file

---
+++ workspaces/app/src/components/ui/profile/Avatar/consts.ts
@@ -0,0 +1,7 @@
+import { Photo } from 'libraries/models/user/types';
+
+export const DEFAULT_PROFILE_PHOTO: Photo = {
+  src: 'https://www.stereofox.com/images/86513/resized.jpg',
+  offsetPercentages: [0, 0],
+  scale: 1,
+};
