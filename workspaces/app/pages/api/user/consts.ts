fb69df80e77a1e479a826acc516b9417f3a3f610
iknowhtml <iknowsmtp@gmail.com>
2022-03-11 06:44:23 -0500

    Add type field as User property

--- workspaces/app/pages/api/user/consts.ts
+++ workspaces/app/pages/api/user/consts.ts
@@ -4,6 +4,7 @@ export const USER_PROPERTIES = new Set<UserProperties>([
   'assets',
   'notifications',
   'profilePhoto',
+  'type',
   'username',
 ]);
 export const PRIVATE_USER_DATA_PROPERTIES = new Set<PrivateUserDataProperties>([
