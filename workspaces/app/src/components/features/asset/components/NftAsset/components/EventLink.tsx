9f0fa2e5c5a92657f71967016b9752890a501cfc
iknowhtml <iknowsmtp@gmail.com>
2022-03-12 15:05:56 -0500

    Rename & updated Event Link component

--- workspaces/app/src/components/features/asset/components/NftAsset/components/EventLink.tsx
+++ workspaces/app/src/components/features/asset/components/NftAsset/components/EventLink.tsx
@@ -1,20 +1,22 @@
 import styled from '@emotion/styled';
 import { Link } from 'components/ui';
 import { CtaContent, CtaWrapperStyle } from 'components/ui/buttons/variants/CtaButton';
-import { Event } from 'libraries/models';
+import { GatedEvent } from 'libraries/models';
 import tokens from 'styles/tokens';
 
 const Wrapper = styled(Link)`
   ${CtaWrapperStyle};
   display: block;
+  width: 100%;
   padding: 36px 20px;
   border-top: dashed ${tokens.background.color.primary} 3px;
 `;
 
-interface AccessEventLinkProps {
-  eventId: Event['id'];
+interface EventLinkProps {
+  eventId: GatedEvent['id'];
 }
-export const AccessEventLink = ({ eventId }: AccessEventLinkProps) => (
+
+export const EventLink = ({ eventId }: EventLinkProps) => (
   <Wrapper href={`events/${eventId}`}>
     <CtaContent>Access Event</CtaContent>
   </Wrapper>
