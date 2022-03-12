cdbdca32a8ba17499af573caaa50e128189f89da
iknowhtml <iknowsmtp@gmail.com>
2022-03-12 14:57:46 -0500

    Added Gated Content types

--- workspaces/app/src/libraries/models/collection/types.ts
+++ workspaces/app/src/libraries/models/collection/types.ts
@@ -2,6 +2,17 @@ import { Artist } from '../artist';
 
 export type CollectionType = 'music' | 'event' | 'merch';
 
+export interface GatedEvent {
+  type: 'event';
+  id: string;
+}
+export interface GatedUrl {
+  type: 'url';
+  url: string;
+}
+
+export type GatedContent = GatedUrl | GatedEvent | null;
+
 export interface CollectionData {
   // id = nft smart contract address
   id: string;
@@ -16,6 +27,7 @@ export interface CollectionData {
   dropDate: string;
   description: string;
   details: string[] | null;
+  gatedContent: GatedContent;
 }
 
 export interface Collection extends Omit<CollectionData, 'artistId'> {
