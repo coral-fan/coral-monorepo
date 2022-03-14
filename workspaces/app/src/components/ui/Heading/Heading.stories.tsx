a1bd321d35fe3c8ae7fc27f4c506782321d85f21
maks-p <mpazuniak@gmail.com>
2022-03-14 13:24:03 -0400

    Refactor Heading to split semantic, style levels

--- workspaces/app/src/components/ui/Heading/Heading.stories.tsx
+++ workspaces/app/src/components/ui/Heading/Heading.stories.tsx
@@ -1,5 +1,5 @@
 import { Story, ComponentMeta } from '@storybook/react';
-import { Heading, HeadingProp } from './Heading';
+import { Heading, HeadingProps } from './Heading';
 
 export default {
   title: 'Coral/UI/Heading',
@@ -9,20 +9,29 @@ export default {
       options: [1, 2, 3],
       control: { type: 'select' },
     },
+    styleVariant: {
+      options: ['h1', 'h2', 'h3'],
+      control: { type: 'select' },
+    },
   },
 } as ComponentMeta<typeof Heading>;
 
-const Template: Story<HeadingProp> = ({ level }) => <Heading level={level}>{`H${level}`}</Heading>;
+const Template: Story<HeadingProps> = ({ level, styleVariant }) => (
+  <Heading level={level} styleVariant={styleVariant}>{`H${level}`}</Heading>
+);
 
 export const level1 = Template.bind({});
 level1.args = {
   level: 1,
+  styleVariant: 'h1',
 };
 export const level2 = Template.bind({});
 level2.args = {
   level: 2,
+  styleVariant: 'h2',
 };
 export const level3 = Template.bind({});
 level3.args = {
   level: 3,
+  styleVariant: 'h3',
 };
--- workspaces/app/src/components/ui/Heading/Heading.tsx
+++ workspaces/app/src/components/ui/Heading/Heading.tsx
@@ -2,24 +2,44 @@ import { css, jsx } from '@emotion/react';
 import { FC } from 'react';
 import tokens from 'styles/tokens';
 
-const { font } = tokens;
+const { color, weight, size, line_height, letter_spacing } = tokens.font;
 
 const fontSizeDictionary = {
-  1: font.size.xl,
-  2: font.size.lg,
-  3: font.size.md,
+  h1: size.xxl,
+  h2: size.xl,
+  h3: size.lg,
+  h4: size.md,
 };
 
-export type HeadingLevel = keyof typeof fontSizeDictionary;
-export interface HeadingProp {
+const lineHeightDictionary = {
+  h1: line_height.xxl,
+  h2: line_height.xl,
+  h3: line_height.lg,
+  h4: line_height.md,
+};
+
+const letterSpacingDictionary = {
+  h1: letter_spacing.xxl,
+  h2: letter_spacing.xl,
+  h3: letter_spacing.lg,
+  h4: letter_spacing.md,
+};
+
+export type HeadingLevel = 1 | 2 | 3 | 4;
+export type HeadingStyleVariant = keyof typeof fontSizeDictionary;
+
+export interface HeadingProps {
   level: HeadingLevel;
+  styleVariant: HeadingStyleVariant;
 }
 
-const getHeadingStyle = (level: HeadingLevel) => css`
-  font-weight: bold;
-  color: ${font.color.primary};
-  font-size: ${fontSizeDictionary[level]};
+const getHeadingStyle = (styleVariant: HeadingStyleVariant) => css`
+  font-weight: ${weight.bold};
+  color: ${color.primary};
+  font-size: ${fontSizeDictionary[styleVariant]};
+  line-height: ${lineHeightDictionary[styleVariant]};
+  letter-spacing: ${letterSpacingDictionary[styleVariant]};
 `;
 
-export const Heading: FC<HeadingProp> = ({ children, level, ...props }) =>
-  jsx(`h${level}`, { css: getHeadingStyle(level), ...props }, children);
+export const Heading: FC<HeadingProps> = ({ children, level, styleVariant, ...props }) =>
+  jsx(`h${level}`, { css: getHeadingStyle(styleVariant), ...props }, children);
