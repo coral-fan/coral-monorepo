ef57594fe597e192e0a39221a46ef54f3908e414
iknowhtml <iknowsmtp@gmail.com>
2022-03-14 12:59:25 -0400

    Add documentation comments

--- workspaces/app/src/libraries/blockchain/utils.ts
+++ workspaces/app/src/libraries/blockchain/utils.ts
@@ -2,4 +2,9 @@ import { MetaMaskInpageProvider } from '@metamask/providers';
 
 export const isMetaMaskInjected = () =>
   window.ethereum !== undefined &&
+  /* 
+  casting due windowing typing collision from @metamask/detect-provider
+  unable to define custom global declaration of window for some reason
+  see node_modules/@metamask/detect-provider/dist/index.d.ts
+  */
   (window.ethereum as MetaMaskInpageProvider).addListener !== undefined;
