073aeafd98e91a66028abc308ee0d9392d8ad64e
iknowhtml <iknowsmtp@gmail.com>
2022-03-14 12:59:25 -0400

    Add documentation comments

--- workspaces/app/src/libraries/blockchain/observables/metamask.ts
+++ workspaces/app/src/libraries/blockchain/observables/metamask.ts
@@ -3,12 +3,18 @@ import { AVALANCHE } from 'consts';
 import { delay, fromEvent, map, Observable, retryWhen, startWith, throwError } from 'rxjs';
 
 export const fromEthereumEvent = <T>(eventName: string) =>
+  /* 
+  casting due windowing typing collision from @metamask/detect-provider
+  unable to define custom global declaration of window for some reason
+  see node_modules/@metamask/detect-provider/dist/index.d.ts
+  */
   fromEvent(window.ethereum as MetaMaskInpageProvider, eventName) as Observable<T>;
 
 export const getChainIdChanged$ = () => fromEthereumEvent<string | null>('chainChanged');
 
 export const getIsNetworkSupported$ = () =>
   getChainIdChanged$().pipe(
+    // see above comment regarding casting
     startWith((window.ethereum as MetaMaskInpageProvider).chainId),
     map((chainId) => {
       if (chainId === null) {
