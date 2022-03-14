93d7bd858bec562fc769138f1022c2779183ba9c
iknowhtml <iknowsmtp@gmail.com>
2022-03-14 12:53:00 -0400

    Remove unnecessary global typing

--- workspaces/app/src/types/global.d.ts
+++ workspaces/app/src/types/global.d.ts
@@ -1,9 +1,7 @@
-import type { MetamaskInpageProvider } from '@metamask/providers';
 import type { JWPlayer } from '@types/jwplayer';
 // prevent type error when accessing the ethereum property on the window object
 declare global {
   interface Window {
-    ethereum: MetamaskInpageProvider;
     jwplayer: JWPlayer;
   }
 }
