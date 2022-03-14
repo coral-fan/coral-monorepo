cb63940198b7f00fe3877fae658eb68aa3a19b3e
iknowhtml <iknowsmtp@gmail.com>
2022-03-14 12:54:05 -0400

    Add work around to window object typing error

--- workspaces/app/src/libraries/blockchain/hooks/metamask.ts
+++ workspaces/app/src/libraries/blockchain/hooks/metamask.ts
@@ -1,24 +1,6 @@
-import { delay, map, retryWhen, startWith, throwError } from 'rxjs';
-import { AVALANCHE } from 'consts';
-import { getChainIdChanged$ } from '../observables';
+import { getIsNetworkSupported$ } from '../observables';
 import { useObservable } from 'libraries/utils/hooks';
 import { isMetaMaskInjected } from '../utils';
-
-const getIsNetworkSupported$ = () =>
-  getChainIdChanged$().pipe(
-    startWith(window.ethereum.chainId),
-    map((chainId: null | string) => {
-      if (chainId === null) {
-        return throwError(() => new Error());
-      }
-      return chainId.toLowerCase() === AVALANCHE.CHAIN_ID;
-    }),
-    /*
-      retry is necessary because in production, chainId can be null.
-      likely a race condition where metamask doesn't initialize before react
-    */
-    retryWhen((error) => error.pipe(delay(1000)))
-  );
 
 export const useIsNetworkSupported = () =>
   useObservable(getIsNetworkSupported$, true, isMetaMaskInjected);
--- workspaces/app/src/libraries/blockchain/observables/metamask.ts
+++ workspaces/app/src/libraries/blockchain/observables/metamask.ts
@@ -1,3 +1,24 @@
-import { fromEvent } from 'rxjs';
+import { MetaMaskInpageProvider } from '@metamask/providers';
+import { AVALANCHE } from 'consts';
+import { delay, fromEvent, map, Observable, retryWhen, startWith, throwError } from 'rxjs';
 
-export const getChainIdChanged$ = () => fromEvent(window.ethereum, 'chainChanged');
+export const fromEthereumEvent = <T>(eventName: string) =>
+  fromEvent(window.ethereum as MetaMaskInpageProvider, eventName) as Observable<T>;
+
+export const getChainIdChanged$ = () => fromEthereumEvent<string | null>('chainChanged');
+
+export const getIsNetworkSupported$ = () =>
+  getChainIdChanged$().pipe(
+    startWith((window.ethereum as MetaMaskInpageProvider).chainId),
+    map((chainId) => {
+      if (chainId === null) {
+        return throwError(() => new Error());
+      }
+      return chainId.toLowerCase() === AVALANCHE.CHAIN_ID;
+    }),
+    /*
+      retry is necessary because in production, chainId can be null.
+      likely a race condition where metamask doesn't initialize before react
+    */
+    retryWhen((error) => error.pipe(delay(1000)))
+  );
