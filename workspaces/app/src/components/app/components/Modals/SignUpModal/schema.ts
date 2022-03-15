57e2cf803bc114a718ea8aaee8962d04f7990ec4
iknowhtml <iknowsmtp@gmail.com>
2022-03-15 08:31:51 -0400

    Move does agree schema to sign up for co-location

--- workspaces/app/src/components/app/components/Modals/SignUpModal/schema.ts
+++ workspaces/app/src/components/app/components/Modals/SignUpModal/schema.ts
@@ -1,11 +1,12 @@
-import { object, InferType } from 'yup';
-import { getUsernameSchema, EMAIL_SCHEMA, DOES_AGREE_SCHEMA } from 'libraries/utils/schemas';
+import { object, InferType, boolean } from 'yup';
+import { getUsernameSchema, EMAIL_SCHEMA } from 'libraries/utils/schemas';
 
 export const getSignUpSchema = (usernames: Set<string>) =>
   object({
     username: getUsernameSchema(usernames),
     email: EMAIL_SCHEMA,
-    doesAgree: DOES_AGREE_SCHEMA,
+    doesAgree: boolean().required().default(false).isTrue(),
+    doesAgreeToMarketing: boolean().required().default(false),
   });
 
 export type SignUpSchema = InferType<ReturnType<typeof getSignUpSchema>>;
