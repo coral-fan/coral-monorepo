81f7240aedbfd7512561365baf7fc8b02ad1805f
maks-p <mpazuniak@gmail.com>
2022-03-14 22:52:53 -0400

    Update Story to ComponentStory

--- workspaces/app/src/components/ui/Heading/Heading.stories.tsx
+++ workspaces/app/src/components/ui/Heading/Heading.stories.tsx
@@ -1,5 +1,5 @@
-import { Story, ComponentMeta } from '@storybook/react';
-import { Heading, HeadingProps } from './Heading';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Heading } from './Heading';
 
 export default {
   title: 'Coral/UI/Heading',
@@ -16,30 +16,30 @@ export default {
   },
 } as ComponentMeta<typeof Heading>;
 
-const Template: Story<HeadingProps> = ({ level, styleVariant }) => (
+const Template: ComponentStory<typeof Heading> = ({ level, styleVariant }) => (
   <Heading level={level} styleVariant={styleVariant}>{`Heading ${level}`}</Heading>
 );
 
-export const level1 = Template.bind({});
-level1.args = {
+export const Level1 = Template.bind({});
+Level1.args = {
   level: 1,
   styleVariant: 'h1',
 };
 
-export const level2 = Template.bind({});
-level2.args = {
+export const Level2 = Template.bind({});
+Level2.args = {
   level: 2,
   styleVariant: 'h2',
 };
 
-export const level3 = Template.bind({});
-level3.args = {
+export const Level3 = Template.bind({});
+Level3.args = {
   level: 3,
   styleVariant: 'h3',
 };
 
-export const level4 = Template.bind({});
-level4.args = {
+export const Level4 = Template.bind({});
+Level4.args = {
   level: 4,
   styleVariant: 'h4',
 };
