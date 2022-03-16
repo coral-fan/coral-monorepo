52424ed1fde865aa582c759d6a7bd1e35d07840b
iknowhtml <iknowsmtp@gmail.com>
2022-03-16 13:30:39 -0400

    Add missing Facebook property to artist data

--- workspaces/app/src/components/features/artist/Artist.tsx
+++ workspaces/app/src/components/features/artist/Artist.tsx
@@ -38,6 +38,7 @@ export const getServerSideProps: GetServerSideProps<ArtistPageProps, { artistId:
       scale: 1,
     },
     socialHandles: {
+      facebook: null,
       twitter: null,
       instagram: null,
       tiktok: null,
