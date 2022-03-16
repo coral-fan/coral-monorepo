d57878ebc4211e051f93156c39c790a9ab1a9bae
maks-p <mpazuniak@gmail.com>
2022-03-16 13:59:41 -0400

    Update NavigationBar and MenuButton

--- workspaces/app/src/components/app/components/NavigationBar/components/HamburgerMenuButton/HamburgerMenuButton.stories.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/HamburgerMenuButton/HamburgerMenuButton.stories.tsx
@@ -1,14 +1,12 @@
-import { Story, Meta } from '@storybook/react';
-import { HamburgerMenuButton, HamburgerMenuButtonProps } from './HamburgerMenuButton';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { HamburgerMenuButton } from './HamburgerMenuButton';
 
 export default {
   title: 'Coral/App/Navigation Bar/Hamburger Icon Button',
   component: HamburgerMenuButton,
-} as Meta;
+} as ComponentMeta<typeof HamburgerMenuButton>;
 
-const Template: Story<HamburgerMenuButtonProps> = ({ hasNotifications }) => (
-  <HamburgerMenuButton hasNotifications={hasNotifications} />
-);
+const Template: ComponentStory<typeof HamburgerMenuButton> = () => <HamburgerMenuButton />;
 
 export const Default = Template.bind({});
 
