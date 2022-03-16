6e8724fd906606998937f3d985ff10c33091e273
maks-p <mpazuniak@gmail.com>
2022-03-16 19:02:37 -0400

    Refactor anonymous function in map

--- workspaces/app/src/libraries/window/hooks.ts
+++ workspaces/app/src/libraries/window/hooks.ts
@@ -1,6 +1,6 @@
 import { useObservable } from 'libraries/utils';
 import { fromEvent, map } from 'rxjs';
-import { DESKTOP_BREAKPOINT } from 'styles';
+import { DESKTOP_BREAKPOINT_INT } from 'styles';
 
 const getWindowDimensions = () => {
   const { innerWidth: width, innerHeight: height } = window;
@@ -9,12 +9,12 @@ const getWindowDimensions = () => {
 
 export const useWindowDimensions = () => {
   const resizeWindow$ = fromEvent(window, 'resize');
-  const getWindowDimensions$ = () => resizeWindow$.pipe(map(() => getWindowDimensions()));
+  const getWindowDimensions$ = () => resizeWindow$.pipe(map(getWindowDimensions));
 
   return useObservable(getWindowDimensions$, getWindowDimensions());
 };
 
 export const useIsDesktop = () => {
   const { width } = useWindowDimensions();
-  return width >= parseInt(DESKTOP_BREAKPOINT.slice(0, -2));
+  return width >= DESKTOP_BREAKPOINT_INT;
 };
