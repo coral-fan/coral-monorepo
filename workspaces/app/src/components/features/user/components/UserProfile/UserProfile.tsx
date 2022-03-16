22dd1aee2be627624144ad5d3b8bbe8b4c51743f
maks-p <mpazuniak@gmail.com>
2022-03-16 14:26:42 -0400

    Create UserProfile component

---
+++ workspaces/app/src/components/features/user/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,133 @@
+import styled from '@emotion/styled';
+import { Avatar, LinkButton } from 'components/ui';
+import {
+  useIsUpdateProfilePhotoModalOpen,
+  useIsUpdateProfileInfoModalOpen,
+  useUser,
+} from 'components/features/user/hooks';
+import { EditAvatarButton as EditAvatarButtonComponent } from '../EditAvatarButton';
+import { useCallback, useEffect } from 'react';
+import { UpdateProfileInfoModal } from '../UpdateProfile/components/UpdateProfileInfoModal';
+import { UpdateProfilePhotoModal } from '../UpdateProfile/components/UpdateProfilePhotoModal';
+import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
+import { useRouter } from 'next/router';
+import { useIsDesktop } from 'libraries/window';
+import { useUserUid } from 'libraries/models';
+
+const { size, line_height, letter_spacing, weight } = tokens.font;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    flex-direction: row;
+    gap: 30px;
+  }
+`;
+
+const AvatarContainer = styled.div`
+  width: fit-content;
+  position: relative;
+`;
+
+const EditAvatarButton = styled(EditAvatarButtonComponent)`
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  right: 0;
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    right: 15px;
+  }
+`;
+
+const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    align-items: flex-start;
+  }
+`;
+
+const EditProfileLinkButton = styled(LinkButton)`
+  font-size: ${size.xs};
+  letter-spacing: ${letter_spacing.xs};
+  line-height: ${line_height.xs};
+  text-transform: uppercase;
+  text-decoration: underline;
+`;
+
+const Username = styled.span`
+  font-size: ${size.xl};
+  letter-spacing: ${letter_spacing.xl};
+  line-height: ${line_height.xl};
+  font-weight: ${weight.bold};
+`;
+
+export const UserProfile = () => {
+  const { userProfileId } = useRouter().query;
+
+  if (typeof userProfileId !== 'string') {
+    throw Error('userProfileId must be of type string');
+  }
+
+  const isDesktop = useIsDesktop();
+  const [{ username, profilePhoto }] = useUser();
+  const currentUserUid = useUserUid();
+
+  const [isUpdateProfilePhotoModalOpen, setIsUpdateProfilePhotoOpen] =
+    useIsUpdateProfilePhotoModalOpen();
+  const [isUpdateProfileInfoModalOpen, setIsProfileInfoModalOpen] =
+    useIsUpdateProfileInfoModalOpen();
+
+  useEffect(() => {
+    /*
+    Without this useEffect, the modal opens on refresh / reload;
+    */
+    setIsUpdateProfilePhotoOpen(false);
+  }, []);
+
+  const openUpdateProfilePhotoModal = useCallback(
+    () => setIsUpdateProfilePhotoOpen(true),
+    [setIsUpdateProfilePhotoOpen]
+  );
+
+  const openUpdateProfileInfoModal = useCallback(
+    () => setIsProfileInfoModalOpen(true),
+    [setIsProfileInfoModalOpen]
+  );
+
+  const avatarSize = isDesktop ? 200 : 125;
+  const isCurrentUser = currentUserUid === userProfileId;
+
+  return (
+    <ProfileContainer>
+      <AvatarContainer>
+        <Avatar size={avatarSize} {...profilePhoto} />
+        {isCurrentUser && (
+          <>
+            <EditAvatarButton onClick={openUpdateProfilePhotoModal} />
+            {isUpdateProfilePhotoModalOpen ? <UpdateProfilePhotoModal /> : null}
+          </>
+        )}
+      </AvatarContainer>
+      <TextContent>
+        <Username>{username}</Username>
+        {isCurrentUser && (
+          <>
+            <EditProfileLinkButton onClick={openUpdateProfileInfoModal}>
+              Update Profile
+            </EditProfileLinkButton>
+            {isUpdateProfileInfoModalOpen ? <UpdateProfileInfoModal /> : null}
+          </>
+        )}
+      </TextContent>
+    </ProfileContainer>
+  );
+};
