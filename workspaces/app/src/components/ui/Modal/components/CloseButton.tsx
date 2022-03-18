7f0d145552b411808049701d21f0d97abcc547ba
maks-p <mpazuniak@gmail.com>
2022-03-18 18:35:44 -0400

    Update buttons using design token

--- workspaces/app/src/components/ui/Modal/components/CloseButton.tsx
+++ workspaces/app/src/components/ui/Modal/components/CloseButton.tsx
@@ -1,16 +1,14 @@
 import { ComponentProps } from 'react';
 import { CloseIcon } from 'components/ui';
 import styled from '@emotion/styled';
-import tokens from 'styles/tokens';
-
-const SIZE = '45px';
+import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
 
 const Wrapper = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: ${SIZE};
-  height: ${SIZE};
+  width: ${tokens.buttons.size.mobile};
+  height: ${tokens.buttons.size.mobile};
   color: ${tokens.font.color.primary};
   background-color: ${tokens.background.color.secondary};
   border: none;
@@ -20,10 +18,15 @@ const Wrapper = styled.button`
   &:hover {
     cursor: pointer;
   }
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    width: ${tokens.buttons.size.desktop};
+    height: ${tokens.buttons.size.desktop};
+  }
 `;
 
 export const CloseButton = (props: ComponentProps<'button'>) => (
   <Wrapper {...props}>
-    <CloseIcon size={22.5} />
+    <CloseIcon size={26} />
   </Wrapper>
 );
