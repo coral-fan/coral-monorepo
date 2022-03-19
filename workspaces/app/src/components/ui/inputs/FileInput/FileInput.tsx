5972e2cfe55b2be4005a76fb8a3701e61b92a063
maks-p <mpazuniak@gmail.com>
2022-03-18 20:46:55 -0400

    Add custom file input component

---
+++ workspaces/app/src/components/ui/inputs/FileInput/FileInput.tsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+import { ComponentProps, ForwardedRef, forwardRef } from 'react';
+import tokens from 'styles/tokens';
+import { Error } from '../components';
+
+interface InputProps extends ComponentProps<'input'> {
+  error?: string;
+}
+
+const Input = styled.input`
+  display: none;
+`;
+
+const Label = styled.label`
+  text-transform: uppercase;
+  font-size: ${tokens.font.size.xs};
+  letter-spacing: ${tokens.font.letter_spacing.xs};
+  line-height: ${tokens.font.line_height.xs};
+  font-weight: ${tokens.font.weight.bold};
+  text-decoration: underline;
+  padding: 12px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const FileInput = forwardRef(function FileInput(
+  { error, ...props }: InputProps,
+  ref: ForwardedRef<HTMLInputElement>
+) {
+  return (
+    <>
+      <Label htmlFor="file-upload">
+        <Input id="file-upload" ref={ref} type="file" accept="image/*" {...props} />
+        {error && <Error>{error}</Error>}
+        Choose Photo
+      </Label>
+    </>
+  );
+});
