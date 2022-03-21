f4314881f84f47a81c87b49e68eab4655014cbc9
maks-p <mpazuniak@gmail.com>
2022-03-21 08:32:27 -0400

    Refactor LogoHomeLink with generalized style

--- workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
@@ -1,20 +1,13 @@
 import styled from '@emotion/styled';
 import { LogoIcon, Link as LinkComponent } from 'components/ui';
-import { DESKTOP_BREAKPOINT } from 'styles';
-import tokens from 'styles/tokens';
+import { navigationButtonStyle } from 'components/ui/buttons/styles';
 
 const Link = styled(LinkComponent)`
-  width: ${tokens.buttons.size.mobile};
-  height: ${tokens.buttons.size.mobile};
-
-  @media (min-width: ${DESKTOP_BREAKPOINT}) {
-    width: ${tokens.buttons.size.desktop};
-    height: ${tokens.buttons.size.desktop};
-  }
+  ${navigationButtonStyle}
 `;
 
 export const LogoHomeLink = () => (
-  <Link href="/">
+  <Link transparent={true} href="/">
     <LogoIcon />
   </Link>
 );
