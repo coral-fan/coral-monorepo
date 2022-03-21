de3478efa786a78e74f72a9da49f3fd60d17bc52
iknowhtml <iknowsmtp@gmail.com>
2022-03-21 17:41:47 -0400

    Add Private Event Modal component for Event page

--- workspaces/app/src/components/features/event/components/PrivateEventModal/PrivateEventModal.tsx
+++ workspaces/app/src/components/features/event/components/PrivateEventModal/PrivateEventModal.tsx
@@ -1,10 +1,16 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { Button, Modal } from 'components/ui';
 import { getIconComponent } from 'components/ui/icons/utils';
-import { useIsDesktop } from 'libraries/window';
 import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
+import { Button, ButtonLink, Modal } from 'components/ui';
+import { useIsDesktop } from 'libraries/window';
 import privateEventSVG from './lock.svg';
+import { useIsAuthenticated, useLogin } from 'libraries/authentication';
+
+const mainContainerStyle = css`
+  align-items: center;
+  gap: 16px;
+`;
 
 const PrivateEventIcon = getIconComponent('PrivateEventIcon', privateEventSVG);
 
@@ -17,18 +23,29 @@ const Message = styled.span`
   font-size: var(--font-size);
 `;
 
-const mainContainerStyle = css`
-  align-items: center;
-  gap: 16px;
-`;
+interface PrivateEventModalProps {
+  collectionId: string;
+}
 
-export const PrivateEventModal = () => {
+export const PrivateEventModal = ({ collectionId }: PrivateEventModalProps) => {
   const isDesktop = useIsDesktop();
+  const isAuthenticated = useIsAuthenticated();
+
+  const { login } = useLogin();
+
   return (
     <Modal title="This Is A Private Event" mainContainerStyle={mainContainerStyle}>
       <PrivateEventIcon size={isDesktop ? 275 : 175} />
-      <Message>Please log in so we can check your wallet.</Message>
-      <Button>Login</Button>
+      <Message>
+        {isAuthenticated
+          ? 'This event is for members and ticket holders only. Buy a ticket now for this special event and exclusive perks.'
+          : 'Please log in so we can check your wallet.'}
+      </Message>
+      {isAuthenticated ? (
+        <ButtonLink href={`/collection/${collectionId}`}>Buy Ticket</ButtonLink>
+      ) : (
+        <Button onClick={login}>Login</Button>
+      )}
     </Modal>
   );
 };
--- workspaces/app/src/components/features/event/components/PrivateEventModal/index.ts
+++ workspaces/app/src/components/features/event/components/PrivateEventModal/index.ts
@@ -1,2 +1,1 @@
-export * from './WebPlayer';
 export * from './PrivateEventModal';
