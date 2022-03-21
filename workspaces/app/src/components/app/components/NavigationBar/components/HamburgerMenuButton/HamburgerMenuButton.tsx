9d1b3d913a4491730ab9da195b8034688d056d57
maks-p <mpazuniak@gmail.com>
2022-03-21 08:31:55 -0400

    Refactor navigation buttons to use new component

--- workspaces/app/src/components/app/components/NavigationBar/components/HamburgerMenuButton/HamburgerMenuButton.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/HamburgerMenuButton/HamburgerMenuButton.tsx
@@ -1,24 +1,11 @@
 import { getIconComponent } from 'components/ui/icons/utils';
 import hamburgerIconSVG from './hamburger.svg';
-import { BaseButton } from 'components/ui/buttons/BaseButton';
-import styled from '@emotion/styled';
-import { DESKTOP_BREAKPOINT } from 'styles';
-import tokens from 'styles/tokens';
+import { NavigationButton } from 'components/ui';
 
 const HamburgerIcon = getIconComponent('HamburgerIcon', hamburgerIconSVG);
 
-const MenuButton = styled(BaseButton)`
-  width: ${tokens.buttons.size.mobile};
-  height: ${tokens.buttons.size.mobile};
-
-  @media (min-width: ${DESKTOP_BREAKPOINT}) {
-    width: ${tokens.buttons.size.desktop};
-    height: ${tokens.buttons.size.desktop};
-  }
-`;
-
 export const HamburgerMenuButton = ({ ...props }) => (
-  <MenuButton {...props}>
+  <NavigationButton {...props}>
     <HamburgerIcon />
-  </MenuButton>
+  </NavigationButton>
 );
--- workspaces/app/src/components/ui/Modal/components/CloseButton.tsx
+++ workspaces/app/src/components/ui/Modal/components/CloseButton.tsx
@@ -1,32 +1,9 @@
 import { ComponentProps } from 'react';
 import { CloseIcon } from 'components/ui';
-import styled from '@emotion/styled';
-import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
-
-const Wrapper = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: ${tokens.buttons.size.mobile};
-  height: ${tokens.buttons.size.mobile};
-  color: ${tokens.font.color.primary};
-  background-color: ${tokens.background.color.secondary};
-  border: none;
-  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
-  border-radius: 30px;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  @media (min-width: ${DESKTOP_BREAKPOINT}) {
-    width: ${tokens.buttons.size.desktop};
-    height: ${tokens.buttons.size.desktop};
-  }
-`;
+import { NavigationButton } from 'components/ui';
 
 export const CloseButton = (props: ComponentProps<'button'>) => (
-  <Wrapper {...props}>
-    <CloseIcon size={26} />
-  </Wrapper>
+  <NavigationButton {...props}>
+    <CloseIcon />
+  </NavigationButton>
 );
