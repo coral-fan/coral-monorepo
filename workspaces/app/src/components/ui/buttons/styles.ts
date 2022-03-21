5cea3f123eb6ef7c9321ceec19422594951860fe
maks-p <mpazuniak@gmail.com>
2022-03-21 08:29:56 -0400

    Create generalized navigation button style

--- workspaces/app/src/components/ui/buttons/styles.ts
+++ workspaces/app/src/components/ui/buttons/styles.ts
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import tokens, { colors, DESKTOP_BREAKPOINT } from 'styles/tokens';
 
 export const buttonBaseStyle = css`
   border: none;
@@ -12,4 +13,23 @@ export const buttonBaseStyle = css`
     filter: opacity(0.5);
     cursor: not-allowed;
   }
+`;
+
+interface NavigationButtonStyle {
+  transparent: boolean;
+}
+export const navigationButtonStyle = ({ transparent }: NavigationButtonStyle) => css`
+  --button-size: ${tokens.buttons.size.mobile};
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    --button-size: ${tokens.buttons.size.desktop};
+  }
+
+  width: var(--button-size);
+  height: var(--button-size);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${transparent ? transparent : colors.gray[11]};
 `;
