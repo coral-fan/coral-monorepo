36d3895af255aef399eb33190bc82b969e33b925
iknowhtml <iknowsmtp@gmail.com>
2022-03-21 09:31:40 -0400

    Add Button Link component

---
+++ workspaces/app/src/components/ui/links/ButtonLink/ButtonLink.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { buttonStyle } from 'components/ui/buttons';
+import { Link } from '../Link';
+
+export const ButtonLink = styled(Link)`
+  ${buttonStyle}
+  text-align: center;
+`;
