6ca9caaa6c01492159dd88ea9074e7621e9a97ed
iknowhtml <iknowsmtp@gmail.com>
2022-03-23 08:00:47 -0400

    Reorganize blockchain library directory

--- workspaces/app/src/libraries/blockchain/metamask/hooks.ts
+++ workspaces/app/src/libraries/blockchain/metamask/hooks.ts
@@ -1,6 +1,6 @@
-import { getIsNetworkSupported$ } from '../observables';
+import { getIsNetworkSupported$ } from './observables';
 import { useObservable } from 'libraries/utils/hooks';
-import { isMetaMaskInjected } from '../utils';
+import { isMetaMaskInjected } from './utils';
 
 export const useIsNetworkSupported = () =>
   useObservable(getIsNetworkSupported$, true, isMetaMaskInjected);
