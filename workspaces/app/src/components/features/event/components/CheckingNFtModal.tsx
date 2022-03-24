7304c23f15d0bdb63878ea0f7cc2d9cf7105a742
iknowhtml <iknowsmtp@gmail.com>
2022-03-24 18:06:55 -0400

    Add Checking NFT Modal component

---
+++ workspaces/app/src/components/features/event/components/CheckingNFtModal.tsx
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+import { LogoSpinner } from 'components/app/components/Modals/SignInModal/LogoSpinner';
+import { Modal } from 'components/ui';
+import { useIconSize } from '../hooks';
+
+const mainContainerStyle = css`
+  align-items: center;
+`;
+
+export const CheckingNftModal = () => {
+  const iconSize = useIconSize();
+
+  return (
+    <Modal title="Checking Your Wallet" mainContainerStyle={mainContainerStyle}>
+      <LogoSpinner size={iconSize} />
+    </Modal>
+  );
+};
