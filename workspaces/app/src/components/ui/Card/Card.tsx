36ec2e712c88b5271c7534c6046c85aaaffc6f05
iknowhtml <iknowsmtp@gmail.com>
2022-03-24 18:07:30 -0400

    Add contrast variant for Card component

--- workspaces/app/src/components/ui/Card/Card.tsx
+++ workspaces/app/src/components/ui/Card/Card.tsx
@@ -1,15 +1,23 @@
 import styled from '@emotion/styled';
+import { toNestError } from '@hookform/resolvers';
 import tokens from 'styles/tokens';
 
+type Variant = 'primary' | 'secondary' | 'contrast';
 interface CardProps {
-  variant?: 'primary' | 'secondary';
+  variant?: Variant;
 }
+
+const backgroundColorMap: Record<Variant, string> = {
+  primary: tokens.background.color.secondary,
+  secondary: tokens.background.color.tertiary,
+  contrast: tokens.background.color.contrast,
+};
+
 export const Card = styled.div<CardProps>`
   display: flex;
   flex-direction: column;
   width: 100%;
   overflow: hidden;
-  background-color: ${({ variant }) =>
-    variant === 'secondary' ? tokens.background.color.tertiary : tokens.background.color.secondary};
+  background-color: ${({ variant }) => backgroundColorMap[variant ?? 'primary']};
   border-radius: ${tokens.border.radius.lg};
 `;
