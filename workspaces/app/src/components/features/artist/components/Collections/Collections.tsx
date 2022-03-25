77065af1080b78c690db4c0da739c2efcd724468
maks-p <mpazuniak@gmail.com>
2022-03-25 18:44:12 -0400

    Rename CardsContainer to ProfileItems

--- workspaces/app/src/components/features/artist/components/Collections/Collections.tsx
+++ workspaces/app/src/components/features/artist/components/Collections/Collections.tsx
@@ -2,33 +2,31 @@ import { getBadge } from 'components/ui/badges/utils';
 import { DropCard } from 'components/ui';
 import { Collection } from 'libraries/models';
 import { sortCollectionByDropDateDesc } from 'components/ui/profile';
-import { CardsContainer } from 'components/ui';
-import { AssetCardWrapper } from 'components/ui/profile';
+import { ProfileItems } from 'components/ui';
+import { ProfileItemWrapper } from 'components/ui/profile';
 
 interface CollectionProps {
   collections: Collection[];
 }
 
-export const Collections = ({ collections }: CollectionProps) => {
-  return (
-    <CardsContainer>
-      {sortCollectionByDropDateDesc(collections).map((collection) => {
-        const { name, artistName, artistProfilePhoto, imageUrl, type, id, dropDate } = collection;
-        const badge = getBadge(type);
-        return (
-          <AssetCardWrapper key={name}>
-            <DropCard
-              id={id}
-              title={name}
-              Badge={badge}
-              imageUrl={imageUrl}
-              artistName={artistName}
-              artistProfilePhoto={artistProfilePhoto}
-              dropDateTimestamp={dropDate}
-            />
-          </AssetCardWrapper>
-        );
-      })}
-    </CardsContainer>
-  );
-};
+export const Collections = ({ collections }: CollectionProps) => (
+  <ProfileItems>
+    {sortCollectionByDropDateDesc(collections).map((collection) => {
+      const { name, artistName, artistProfilePhoto, imageUrl, type, id, dropDate } = collection;
+      const badge = getBadge(type);
+      return (
+        <ProfileItemWrapper key={name}>
+          <DropCard
+            id={id}
+            title={name}
+            Badge={badge}
+            imageUrl={imageUrl}
+            artistName={artistName}
+            artistProfilePhoto={artistProfilePhoto}
+            dropDateTimestamp={dropDate}
+          />
+        </ProfileItemWrapper>
+      );
+    })}
+  </ProfileItems>
+);
--- workspaces/app/src/components/ui/profile/Profile/components/CardsContainer/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './CardsContainer';
-export * from './components';
