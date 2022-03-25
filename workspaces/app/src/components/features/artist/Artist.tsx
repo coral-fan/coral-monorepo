5302665b9d17ffeae5a36bfc60b8186befdada59
maks-p <mpazuniak@gmail.com>
2022-03-25 12:23:30 -0400

    Update Artist, User pages with dummy data

--- workspaces/app/src/components/features/artist/Artist.tsx
+++ workspaces/app/src/components/features/artist/Artist.tsx
@@ -1,18 +1,18 @@
-import styled from '@emotion/styled';
 import { Artist } from 'libraries/models';
 import { GetServerSideProps } from 'next';
 import { IMAGE_WITH_INFO_DEFAULT_ARGS } from 'components/ui/nft/components/ImageWithInfo/consts';
-
-const Container = styled.div`
-  display: flex;
-`;
+import { ArtistProfile } from './components/ArtistProfile';
 
 interface ArtistPageProps {
   artistData: Artist;
 }
 
 export const ArtistPage = ({ artistData }: ArtistPageProps) => {
-  return <Container>{`${artistData.name}`}</Container>;
+  return (
+    <>
+      <ArtistProfile artistData={artistData} />
+    </>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps<ArtistPageProps, { artistId: string }> = async (
@@ -33,18 +33,19 @@ export const getServerSideProps: GetServerSideProps<ArtistPageProps, { artistId:
     id: '1',
     name: 'Bonobo',
     bio: 'Simon Green, known by his stage name Bonobo, is a British musician, producer, and DJ based in Los Angeles. He debuted with a trip hop aesthetic, and has since explored more upbeat approaches as well as jazz and world music influences.',
-    quote: null,
+    quote:
+      'I’m beyond honored to have you all as my fans and I can’t wait to show you how I create this music',
     profilePhoto: {
       src: 'https://www.stereofox.com/images/86513/resized.jpg',
       offsetPercentages: [0, 0],
       scale: 1,
     },
     socialHandles: {
-      facebook: null,
-      twitter: null,
-      instagram: null,
+      facebook: 'bonoboofficial',
+      twitter: 'si_bonobo',
+      instagram: 'si_bonobo',
       tiktok: null,
-      soundcloud: null,
+      soundcloud: 'bonobo',
       discogs: null,
       spotify: null,
     },
@@ -60,8 +61,57 @@ export const getServerSideProps: GetServerSideProps<ArtistPageProps, { artistId:
         maxMintable: 5000,
         type: 'music',
         price: 1000,
+        dropDate: '2023-09-01',
+        description:
+          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+        details: null,
+        ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+      },
+      {
+        id: '2',
+        name: 'Access Tour: This is an Extra Long Title',
+        gatedContent: {
+          type: 'url',
+          url: '/',
+        },
+        maxMintable: 5000,
+        type: 'music',
+        price: 1000,
         dropDate: '2022-04-01',
-        description: 'Bonobo Collection',
+        description:
+          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+        details: null,
+        ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+      },
+      {
+        id: '3',
+        name: 'This Is An Event',
+        gatedContent: {
+          type: 'url',
+          url: '/',
+        },
+        maxMintable: 5000,
+        type: 'event',
+        price: 1000,
+        dropDate: '2022-04-01',
+        description:
+          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+        details: null,
+        ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+      },
+      {
+        id: '3',
+        name: 'MERCH',
+        gatedContent: {
+          type: 'url',
+          url: '/',
+        },
+        maxMintable: 5000,
+        type: 'merch',
+        price: 1000,
+        dropDate: '2022-03-01',
+        description:
+          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
         details: null,
         ...IMAGE_WITH_INFO_DEFAULT_ARGS,
       },
--- workspaces/app/src/components/features/user/User.tsx
+++ workspaces/app/src/components/features/user/User.tsx
@@ -1,4 +1,4 @@
-import { PrivateUserData, PublicUserData, User } from 'libraries/models';
+import { Asset, PrivateUserData, PublicUserData, User } from 'libraries/models';
 import { GetServerSideProps } from 'next';
 import { getDocumentData } from 'libraries/firebase';
 import { getIdToken } from 'libraries/authentication';
@@ -7,15 +7,17 @@ import { ID_TOKEN_KEY } from 'consts';
 import { getAuthenticationServerSide } from 'libraries/firebase/authentication';
 import { UserPageProvider } from './provider';
 import { UserProfile } from './components/UserProfile';
+import { IMAGE_WITH_INFO_DEFAULT_ARGS } from 'components/ui/nft/components/ImageWithInfo/consts';
 
 interface UserPageProps {
   userData: User;
 }
 
 export const UserPage = ({ userData }: UserPageProps) => {
+  const { assets } = userData;
   return (
     <UserPageProvider userData={userData}>
-      <UserProfile />
+      <UserProfile assets={assets} />
     </UserPageProvider>
   );
 };
@@ -59,7 +61,82 @@ export const getServerSideProps: GetServerSideProps<
       ? await getDocumentData<PrivateUserData>('users', userProfileId, 'private', 'data')
       : undefined;
 
-  const userData: User = { ...publicUserData, ...privateUserData };
+  const assets: Asset[] = [
+    {
+      id: 3671,
+      collectionId: '1',
+      collectionName: 'Bonobo',
+      ownerUsername: publicUserData.username,
+      ownerAddress: userProfileId,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'music',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+    {
+      id: 9,
+      collectionId: '1',
+      collectionName: 'Behind the Scenes Studio Tour',
+      ownerUsername: publicUserData.username,
+      ownerAddress: userProfileId,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'music',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+    {
+      id: 9732,
+      collectionId: '2',
+      collectionName: 'Access Tour: This is an Extra Long Title',
+      ownerUsername: publicUserData.username,
+      ownerAddress: userProfileId,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'merch',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+    {
+      id: 234,
+      collectionId: '3',
+      collectionName: 'This Is An Event',
+      ownerUsername: publicUserData.username,
+      ownerAddress: userProfileId,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'event',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+  ];
+
+  const userData: User = { ...publicUserData, ...privateUserData, assets };
 
   return {
     props: {
