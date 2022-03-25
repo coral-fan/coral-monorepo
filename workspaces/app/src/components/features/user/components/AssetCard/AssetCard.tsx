baf5a42716731a696e57381c83a8ba290b66f10e
maks-p <mpazuniak@gmail.com>
2022-03-25 12:16:36 -0400

    Create AssetCard for user profile

---
+++ workspaces/app/src/components/features/user/components/AssetCard/AssetCard.tsx
@@ -0,0 +1,36 @@
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
+import styled from '@emotion/styled';
+import { Link } from 'components/ui';
+import { Card } from 'components/ui/Card';
+import { BaseInfo, BaseInfoProps } from 'components/ui';
+
+interface AssetCardProps extends Omit<BaseInfoProps, 'titleHeadingLevel' | 'titleStyleVariant'> {
+  Badge: () => EmotionJSX.Element;
+  collectionId: string;
+  id: number;
+}
+
+const AssetCardContainer = styled(Card)`
+  &:hover {
+    opacity: 85%;
+  }
+`;
+
+const BadgeAndTokenIdContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const AssetCard = ({ Badge, collectionId, id, ...baseInfoProps }: AssetCardProps) => (
+  <Link href={`/collection/${collectionId}/asset/${id}`}>
+    <AssetCardContainer>
+      <BaseInfo titleHeadingLevel={3} titleStyleVariant={'h3'} {...baseInfoProps}>
+        <BadgeAndTokenIdContainer>
+          <Badge />
+          {id}
+        </BadgeAndTokenIdContainer>
+      </BaseInfo>
+    </AssetCardContainer>
+  </Link>
+);
