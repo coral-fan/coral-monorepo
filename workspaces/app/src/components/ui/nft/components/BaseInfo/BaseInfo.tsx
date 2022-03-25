43e1210fd5721a4ca9ed6329ae23957652f01ec6
maks-p <mpazuniak@gmail.com>
2022-03-25 12:13:30 -0400

    Refactor BaseInfo so cards have consistent height

--- workspaces/app/src/components/ui/nft/components/BaseInfo/BaseInfo.tsx
+++ workspaces/app/src/components/ui/nft/components/BaseInfo/BaseInfo.tsx
@@ -2,7 +2,7 @@ import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
 import styled from '@emotion/styled';
 import { Heading, HeadingLevel, HeadingStyleVariant } from 'components/ui';
 import { FC } from 'react';
-import tokens from 'styles/tokens';
+import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
 import { ImageWithInfo, ImageWithInfoProps } from '..';
 
 export interface BaseInfoProps extends ImageWithInfoProps {
@@ -10,13 +10,18 @@ export interface BaseInfoProps extends ImageWithInfoProps {
   titleHeadingLevel: HeadingLevel;
   titleStyleVariant: HeadingStyleVariant;
   Badge?: () => EmotionJSX.Element;
-  description?: string;
+  description?: string | false;
 }
 
 const BaseInfoContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  gap: ${tokens.spacing.mobile.xs};
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    gap: ${tokens.spacing.desktop.xs};
+  }
 `;
 
 const Description = styled.p`
@@ -27,8 +32,15 @@ const Description = styled.p`
 const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 16px;
-  padding: 10px 16px;
+  padding: 10px 16px 14px 16px;
+  gap: ${tokens.spacing.mobile.lg};
+
+  // Min-height creates space for long Collection titles
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    gap: 0;
+    min-height: 13vh;
+    justify-content: space-between;
+  }
 `;
 
 export const BaseInfo: FC<BaseInfoProps> = ({
