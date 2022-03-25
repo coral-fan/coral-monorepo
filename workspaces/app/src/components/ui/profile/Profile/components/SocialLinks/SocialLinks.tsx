3d89606e68d389edebbfd6a6701cebf63017df0e
maks-p <mpazuniak@gmail.com>
2022-03-25 12:12:27 -0400

    Center SocialLinks on mobile

--- workspaces/app/src/components/ui/profile/Profile/components/SocialLinks/SocialLinks.tsx
+++ workspaces/app/src/components/ui/profile/Profile/components/SocialLinks/SocialLinks.tsx
@@ -1,12 +1,18 @@
 import styled from '@emotion/styled';
 import { SocialLink, SocialType } from 'components/ui';
 import { SocialHandles } from 'libraries/models';
+import { DESKTOP_BREAKPOINT } from 'styles';
 
 export const SOCIAL_TYPES: SocialType[] = ['twitter', 'instagram', 'soundcloud'];
 
 const SocialLinkContainer = styled.div`
   display: flex;
   gap: 25px;
+  justify-content: center;
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    justify-content: start;
+  }
 `;
 
 export interface SocialLinksProp {
