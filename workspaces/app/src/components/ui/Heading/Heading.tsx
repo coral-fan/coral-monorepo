da9c64decb3fea99d0cb47a60e5716c6ee2caf7c
iknowhtml <iknowsmtp@gmail.com>
2022-03-24 21:03:18 -0400

    Update Heading component - Refactor style logic - Add color variant logic for Heading component

--- workspaces/app/src/components/ui/Heading/Heading.tsx
+++ workspaces/app/src/components/ui/Heading/Heading.tsx
@@ -1,37 +1,29 @@
 import { css, jsx } from '@emotion/react';
 import { FC } from 'react';
 import tokens from 'styles/tokens';
+import { Variant } from '../Card';
 
 const { color, weight, size, line_height, letter_spacing } = tokens.font;
 
-const headingStyle = css`
-  font-weight: ${weight.bold};
-  color: ${color.primary};
-`;
-
 const h1Style = css`
-  ${headingStyle};
   font-size: ${size.xxl};
   line-height: ${line_height.xxl};
   letter-spacing: ${letter_spacing.xxl};
 `;
 
 const h2Style = css`
-  ${headingStyle};
   font-size: ${size.xl};
   line-height: ${line_height.xl};
   letter-spacing: ${letter_spacing.xl};
 `;
 
 const h3Style = css`
-  ${headingStyle};
   font-size: ${size.lg};
   line-height: ${line_height.lg};
   letter-spacing: ${letter_spacing.lg};
 `;
 
 const h4Style = css`
-  ${headingStyle};
   font-size: ${size.md};
   line-height: ${line_height.md};
   letter-spacing: ${letter_spacing.md};
@@ -48,10 +40,24 @@ const styleDictionary = {
 export type HeadingLevel = 1 | 2 | 3 | 4;
 export type HeadingStyleVariant = keyof typeof styleDictionary;
 
+const getHeadingStyle = (
+  styleVariant: HeadingStyleVariant,
+  colorVariant: Variant = 'primary'
+) => css`
+  font-weight: ${weight.bold};
+  color: ${color[colorVariant]};
+  ${styleDictionary[styleVariant]}
+`;
 export interface HeadingProps {
   level: HeadingLevel;
   styleVariant: HeadingStyleVariant;
+  colorVariant?: Variant;
 }
 
-export const Heading: FC<HeadingProps> = ({ children, level, styleVariant, ...props }) =>
-  jsx(`h${level}`, { css: styleDictionary[styleVariant], ...props }, children);
+export const Heading: FC<HeadingProps> = ({
+  children,
+  level,
+  styleVariant,
+  colorVariant,
+  ...props
+}) => jsx(`h${level}`, { css: getHeadingStyle(styleVariant, colorVariant), ...props }, children);
