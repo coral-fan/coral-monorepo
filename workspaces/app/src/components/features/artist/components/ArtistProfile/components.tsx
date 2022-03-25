6ad283c7bb2d67db9d34e2c59f4aee5b92687360
maks-p <mpazuniak@gmail.com>
2022-03-25 12:14:20 -0400

    Tag component for artists

---
+++ workspaces/app/src/components/features/artist/components/ArtistProfile/components.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
+
+export const Tag = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  background: ${tokens.background.color.brand};
+  border-radius: ${tokens.border.radius.xxl};
+  text-transform: uppercase;
+  color: ${tokens.font.color.contrast};
+  font-size: ${tokens.font.size.xs};
+  line-height: ${tokens.font.line_height.xs};
+  letter-spacing: ${tokens.font.letter_spacing.xs};
+  padding: 8px;
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    font-size: ${tokens.font.size.sm};
+    line-height: {$tokens.font.line_height.sm};
+    letter-spacing: {$tokens.font.letter_spacing.sm};
+    padding: 6px;
+  }
+`;
