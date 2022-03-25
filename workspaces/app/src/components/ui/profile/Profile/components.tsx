a023504ec70d2dd6ee1c89dd1d47e187467fc64c
maks-p <mpazuniak@gmail.com>
2022-03-25 12:19:36 -0400

    Refactor spacing in Profile, misc edits

--- workspaces/app/src/components/ui/profile/Profile/components.tsx
+++ workspaces/app/src/components/ui/profile/Profile/components.tsx
@@ -1,14 +1,40 @@
 import styled from '@emotion/styled';
 import tokens, { DESKTOP_BREAKPOINT } from 'styles/tokens';
 
-const { size, line_height, letter_spacing, weight } = tokens.font;
+const { size, line_height, letter_spacing, weight, color } = tokens.font;
+const { mobile, desktop } = tokens.spacing;
+
+const verticalSpacerDesktop = desktop.lg;
+const verticalSpacerMobile = mobile.lg;
+
+export const PageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: ${verticalSpacerMobile};
+  align-items: center;
+  /* border: solid 1px red; */
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    align-items: flex-start;
+    flex-direction: row;
+    gap: ${verticalSpacerDesktop};
+  }
+`;
 
 export const ProfileContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 30px;
-  padding: 0 16px;
+  gap: ${verticalSpacerMobile};
+  flex: 1 0 42.5%;
+  /* border: solid 1px blue; */
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    gap: ${verticalSpacerDesktop};
+    position: sticky;
+    top: 100px;
+  }
 `;
 
 export const MainProfileContainer = styled.div`
@@ -16,11 +42,12 @@ export const MainProfileContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 4px;
+  gap: ${verticalSpacerMobile};
+  /* border: solid 1px yellow; */
 
   @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    gap: ${desktop.md};
     flex-direction: row;
-    gap: 30px;
   }
 `;
 
@@ -52,10 +79,26 @@ export const Bio = styled.p`
   letter-spacing: ${letter_spacing.sm};
   line-height: ${line_height.sm};
   font-weight: ${weight.normal};
+  /* border: solid 1px white; */
+`;
+
+export const Quote = styled.q`
+  color: ${color.secondary};
+  font-size: ${size.lg};
+  letter-spacing: ${letter_spacing.lg};
+  line-height: ${line_height.lg};
+  font-weight: ${weight.bold};
+  /* border: solid 1px white; */
 `;
 
 export const UserContentContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: ${tokens.spacing.mobile.md};
+  text-align: left;
+  /* border: solid 1px purple; */
+
+  @media (min-width: ${DESKTOP_BREAKPOINT}) {
+    gap: ${tokens.spacing.desktop.md};
+  }
 `;
