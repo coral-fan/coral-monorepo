6e358e4a6c8233a7738af44e04cdf5926a60aa83
maks-p <mpazuniak@gmail.com>
2022-03-25 18:45:41 -0400

    Delete Quote

--- workspaces/app/src/components/ui/profile/Profile/Profile.tsx
+++ workspaces/app/src/components/ui/profile/Profile/Profile.tsx
@@ -7,7 +7,6 @@ import {
   Username,
   UserContentContainer,
   Bio,
-  Quote,
   PageContainer,
 } from './components';
 import { useIsDesktop } from 'libraries/window';
@@ -18,26 +17,22 @@ interface ProfileProps {
   username: string;
   profilePhoto: Photo;
   bio: NullableString;
-  quote?: NullableString;
   socialHandles: SocialHandles;
   editAvatar?: JSX.Element | false;
   editProfileInfo?: JSX.Element | false;
   artistTag?: JSX.Element;
-  collections?: JSX.Element;
-  assets?: JSX.Element;
+  items?: JSX.Element;
 }
 
 export const Profile = ({
   username,
   profilePhoto,
   bio,
-  quote,
   socialHandles,
   editAvatar,
   editProfileInfo,
   artistTag,
-  collections,
-  assets,
+  items,
 }: ProfileProps) => {
   const isDesktop = useIsDesktop();
   const avatarSize = isDesktop ? 200 : 125;
@@ -58,12 +53,10 @@ export const Profile = ({
         </MainProfileContainer>
         <UserContentContainer>
           <Bio>{bio}</Bio>
-          {quote && isDesktop && <Quote>{quote}</Quote>}
           <SocialLinks socialHandles={socialHandles} />
         </UserContentContainer>
       </ProfileContainer>
-      {collections}
-      {assets}
+      {items}
     </PageContainer>
   );
 };
