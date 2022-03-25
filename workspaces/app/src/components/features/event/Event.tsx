45169004675df0aa090d5542b132b9734590be41
iknowhtml <iknowsmtp@gmail.com>
2022-03-24 21:06:43 -0400

    Add content gating logic & flow to Event page

--- workspaces/app/src/components/features/event/Event.tsx
+++ workspaces/app/src/components/features/event/Event.tsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
 
 import { useIsAuthenticated } from 'libraries/authentication';
+import { getWalletNfts$ } from 'libraries/blockchain/wallet/observables';
 import { GetServerSideProps } from 'next';
-import { useState } from 'react';
-import { WebPlayer, PrivateEventModal } from './components';
+import { useEffect, useState } from 'react';
+import { interval, mergeMapTo, skipUntil, timer } from 'rxjs';
+import { WebPlayer, PrivateEventModal, AccessGrantedModal } from './components';
+import { CheckingNftModal } from './components/CheckingNFtModal';
 
 const Container = styled.div`
   display: flex;
@@ -14,18 +17,46 @@ interface EventPageProps {
   mediaId: string;
 }
 
+const allowedNftCollections = ['0x71a517b09a62e3ddbdfab02d13bf237ad602f21b'];
+
 export const EventPage = ({ mediaId }: EventPageProps) => {
   const isAuthenticated = useIsAuthenticated();
-  const [doesUserHaveAccess, setDoesUserHaveAccess] = useState(true);
+
+  const [doesUserHaveAccess, setDoesUserHaveAccess] = useState(false);
+  const [isCheckingWallet, setIsCheckingWallet] = useState(true);
+
+  const [showIsAccessGrantedModal, setIsAccessGrantedModal] = useState(true);
+
+  useEffect(() => {
+    const walletNftsMap$ = getWalletNfts$('0xd9C03cE871ed2EBd809f7c86Ff22ec62a7b02644');
+
+    const checkNftMap$ = interval(2500).pipe(skipUntil(walletNftsMap$), mergeMapTo(walletNftsMap$));
+
+    checkNftMap$.subscribe((nftsMap) => {
+      setDoesUserHaveAccess(
+        allowedNftCollections.some((address) => nftsMap[address] !== undefined)
+      );
+      setIsCheckingWallet(false);
+    });
+
+    checkNftMap$.pipe(mergeMapTo(timer(2500))).subscribe(() => setIsAccessGrantedModal(false));
+  }, []);
+
+  if (isCheckingWallet) {
+    return <CheckingNftModal />;
+  }
 
   if (!isAuthenticated || !doesUserHaveAccess) {
     return <PrivateEventModal collectionId={'1'} />;
   }
 
   return (
-    <Container>
-      <WebPlayer mediaId={mediaId} />
-    </Container>
+    <>
+      {showIsAccessGrantedModal && <AccessGrantedModal />}
+      <Container>
+        <WebPlayer mediaId={mediaId} />
+      </Container>
+    </>
   );
 };
 
