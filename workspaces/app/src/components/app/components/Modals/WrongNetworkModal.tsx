64bd6cc80d9f7854f2579f95c2eb09341d79a210
iknowhtml <iknowsmtp@gmail.com>
2022-03-25 11:50:05 -0400

    Add title, message content & layout styling

--- workspaces/app/src/components/app/components/Modals/WrongNetworkModal.tsx
+++ workspaces/app/src/components/app/components/Modals/WrongNetworkModal.tsx
@@ -1,7 +1,8 @@
-import { Modal, Button } from 'components/ui';
+import { Modal, Button, Message } from 'components/ui';
 
 import { useWallet, useIsNetworkSupported } from 'libraries/blockchain';
 import { AVALANCHE } from 'consts';
+import { css } from '@emotion/react';
 
 const AVALANCHE_NETWORK_PARAMS = {
   chainId: AVALANCHE.CHAIN_ID.HEX, // A 0x-prefixed hexadecimal chainId
@@ -14,6 +15,10 @@ const AVALANCHE_NETWORK_PARAMS = {
   rpcUrls: [AVALANCHE.RPC_URL],
   blockExplorerUrls: [AVALANCHE.BLOCK_EXPLORER_URL],
 };
+
+const mainContainerStyle = css`
+  gap: 16px;
+`;
 
 export const WrongNetworkModal = () => {
   const { connector } = useWallet();
@@ -32,7 +37,8 @@ export const WrongNetworkModal = () => {
   }
 
   return (
-    <Modal>
+    <Modal title="Wrong Network" mainContainerStyle={mainContainerStyle}>
+      <Message>You are on the wrong network. Please connect to the Avalanche network.</Message>
       <Button onClick={addAvalancheNetwork}>Switch To Avalanche</Button>
     </Modal>
   );
