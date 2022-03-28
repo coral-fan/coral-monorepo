65d3cd1e60987a302a64f45c593d9d5b7f3bb51d
maks-p <mpazuniak@gmail.com>
2022-03-28 14:51:33 -0400

    Make utility function implicit return

--- workspaces/app/src/components/ui/profile/Profile/utils.ts
+++ workspaces/app/src/components/ui/profile/Profile/utils.ts
@@ -1,5 +1,4 @@
 import { Collection } from 'libraries/models';
 
-export const sortCollectionByDropDateDesc = (arr: Collection[]) => {
-  return arr.sort((a, b) => new Date(a.dropDate).getTime() - new Date(b.dropDate).getTime());
-};
+export const sortCollectionByDropDateDesc = (arr: Collection[]) =>
+  arr.sort((a, b) => new Date(a.dropDate).getTime() - new Date(b.dropDate).getTime());
