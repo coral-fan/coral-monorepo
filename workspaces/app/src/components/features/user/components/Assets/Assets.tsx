39703f29ab5b5390af06b56ea92b3d1e8a8292f1
maks-p <mpazuniak@gmail.com>
2022-03-28 12:05:13 -0400

    Add placeholder for zero placeholder items

--- workspaces/app/src/components/features/user/components/Assets/Assets.tsx
+++ workspaces/app/src/components/features/user/components/Assets/Assets.tsx
@@ -1,7 +1,7 @@
 import { getBadge } from 'components/ui/badges/utils';
 import { Asset } from 'libraries/models';
 import { ProfileItems } from 'components/ui';
-import { ProfileItemWrapper } from 'components/ui/profile';
+import { ItemsPlaceholder, ProfileItemWrapper } from 'components/ui/profile';
 import { AssetCard } from '../AssetCard';
 
 interface AssetProps {
@@ -10,14 +10,18 @@ interface AssetProps {
 
 export const Assets = ({ assets }: AssetProps) => (
   <ProfileItems>
-    {assets.map((asset) => {
-      const { collectionName, type, id } = asset;
-      const Badge = getBadge(type);
-      return (
-        <ProfileItemWrapper key={`${collectionName}-${id}`}>
-          <AssetCard title={collectionName} Badge={Badge} {...asset} />
-        </ProfileItemWrapper>
-      );
-    })}
+    {assets.length > 0 ? (
+      assets.map((asset) => {
+        const { collectionName, type, id } = asset;
+        const Badge = getBadge(type);
+        return (
+          <ProfileItemWrapper key={`${collectionName}-${id}`}>
+            <AssetCard title={collectionName} Badge={Badge} {...asset} />
+          </ProfileItemWrapper>
+        );
+      })
+    ) : (
+      <ItemsPlaceholder>Nothing collected yet.</ItemsPlaceholder>
+    )}
   </ProfileItems>
 );
--- workspaces/app/src/components/ui/profile/ProfileItems/components.tsx
+++ workspaces/app/src/components/ui/profile/ProfileItems/components.tsx
@@ -1,6 +1,14 @@
 import styled from '@emotion/styled';
+import tokens from 'styles/tokens';
 
 export const ProfileItemWrapper = styled.div`
   min-width: 280px;
   flex: 0 0 48%;
+`;
+
+export const ItemsPlaceholder = styled.div`
+  font-style: italic;
+  font-size: ${tokens.font.size.md};
+  letter-spacing: ${tokens.font.letter_spacing.md};
+  line-height: ${tokens.font.line_height.md};
 `;
