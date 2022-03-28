c1b785b271bb909922cfafc9115f2b1dbce80d58
maks-p <mpazuniak@gmail.com>
2022-03-28 12:05:13 -0400

    Add placeholder for zero placeholder items

--- workspaces/app/src/components/features/artist/components/Collections/Collections.tsx
+++ workspaces/app/src/components/features/artist/components/Collections/Collections.tsx
@@ -1,7 +1,7 @@
 import { getBadge } from 'components/ui/badges/utils';
 import { DropCard } from 'components/ui';
 import { Collection } from 'libraries/models';
-import { sortCollectionByDropDateDesc } from 'components/ui/profile';
+import { ItemsPlaceholder, sortCollectionByDropDateDesc } from 'components/ui/profile';
 import { ProfileItems } from 'components/ui';
 import { ProfileItemWrapper } from 'components/ui/profile';
 
@@ -11,20 +11,24 @@ interface CollectionProps {
 
 export const Collections = ({ collections }: CollectionProps) => (
   <ProfileItems>
-    {sortCollectionByDropDateDesc(collections).map((collection) => {
-      const { name, type, dropDate } = collection;
-      const Badge = getBadge(type);
-      return (
-        <ProfileItemWrapper key={name}>
-          <DropCard
-            title={name}
-            Badge={Badge}
-            dropDateTimestamp={dropDate}
-            {...collection}
-            description={''}
-          />
-        </ProfileItemWrapper>
-      );
-    })}
+    {collections.length > 0 ? (
+      sortCollectionByDropDateDesc(collections).map((collection) => {
+        const { name, type, dropDate } = collection;
+        const Badge = getBadge(type);
+        return (
+          <ProfileItemWrapper key={name}>
+            <DropCard
+              title={name}
+              Badge={Badge}
+              dropDateTimestamp={dropDate}
+              {...collection}
+              description={''}
+            />
+          </ProfileItemWrapper>
+        );
+      })
+    ) : (
+      <ItemsPlaceholder>First drop coming soon.</ItemsPlaceholder>
+    )}
   </ProfileItems>
 );
