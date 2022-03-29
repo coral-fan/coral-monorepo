b2a39f5fa4ac47a7a85236bd24dcf9aeb60785c1
iknowhtml <iknowsmtp@gmail.com>
2022-03-29 16:55:40 -0400

    Update Navigation Bar - Add Install MetaMask Modal - Add open/close Modal functions - Refactor rendering logic for Modals

--- workspaces/app/src/components/app/components/NavigationBar/NavigationBar.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-import { HamburgerMenuButton, Menu, LogoHomeLink } from './components';
+import { HamburgerMenuButton, Menu, LogoHomeLink, InstallMetaMaskModal } from './components';
 import { useCallback, useState } from 'react';
 import { User } from 'libraries/models';
 import { useObservable } from 'libraries/utils';
@@ -30,13 +30,30 @@ export const NavigationBar = () => {
 
   const userProfile = useObservable(getUserProfile$, undefined);
 
-  const openMenuModal = useCallback(() => setIsMenuOpen(true), [setIsMenuOpen]);
+  const openMenuModal = useCallback(() => setIsMenuOpen(true), []);
+  const closeMenuModal = useCallback(() => setIsMenuOpen(false), []);
+
+  const [isInstallMetaMaskModalOpen, setIsInstallMetaMaskModalOpen] = useState(false);
+
+  const openInstallMetaMaskModal = useCallback(() => setIsInstallMetaMaskModalOpen(true), []);
+  const closeInstallMetaMaskModal = useCallback(() => setIsInstallMetaMaskModalOpen(false), []);
 
   return (
-    <Container>
-      <LogoHomeLink />
-      <HamburgerMenuButton onClick={openMenuModal} />
-      <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} userProfile={userProfile} />
-    </Container>
+    <>
+      {isInstallMetaMaskModalOpen && (
+        <InstallMetaMaskModal closeModal={closeInstallMetaMaskModal} />
+      )}
+      {isMenuOpen && (
+        <Menu
+          closeMenuModal={closeMenuModal}
+          openInstallMetaMaskModal={openInstallMetaMaskModal}
+          userProfile={userProfile}
+        />
+      )}
+      <Container>
+        <LogoHomeLink />
+        <HamburgerMenuButton onClick={openMenuModal} />
+      </Container>
+    </>
   );
 };
