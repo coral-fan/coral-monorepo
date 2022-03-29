bdf65a066e993897cf8277c78f74492d586279e5
iknowhtml <iknowsmtp@gmail.com>
2022-03-29 17:22:36 -0400

    Configure link to open download page in new tab

--- workspaces/app/src/components/app/components/NavigationBar/components/InstallMetaMaskModal.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/InstallMetaMaskModal.tsx
@@ -7,6 +7,8 @@ interface InstallMetaMaskModalProps {
 export const InstallMetaMaskModal = ({ closeModal }: InstallMetaMaskModalProps) => (
   <Modal title="MetaMask Is Not Installed" onClick={closeModal}>
     <Message>Please install MetaMask Wallet Browser Extension.</Message>
-    <ButtonLink href="https://metamask.io/download/">Install MetaMask</ButtonLink>
+    <ButtonLink href="https://metamask.io/download/" target="_blank">
+      Install MetaMask
+    </ButtonLink>
   </Modal>
 );
