0bfd6d5a35295c6b7fdea281b8b95581694b32fe
iknowhtml <iknowsmtp@gmail.com>
2022-03-30 13:12:41 -0400

    Add Event data models

---
+++ workspaces/app/src/libraries/models/event/types.ts
@@ -0,0 +1,19 @@
+import { Artist } from '../artist';
+import { CollectionData } from '../collection';
+
+export interface EventData {
+  id: string;
+  streamId: string;
+  name: string;
+  date: string;
+  description: string;
+  artistId: Artist['id'];
+  allowedCollections: CollectionData['id'][];
+  exclusiveCollections: CollectionData['id'][] | null;
+}
+
+export interface Event extends Omit<EventData, 'artistId'> {
+  artistName: Artist['name'];
+  artistProfilePhoto: Artist['profilePhoto'];
+  artistSocialHandles: Artist['socialHandles'];
+}
