db6bfd649be46b60336e101b30555b13dbf63148
iknowhtml <iknowsmtp@gmail.com>
2022-03-31 11:16:25 -0400

    Fix Modal components render issue - Use refactor logic for conditionally rendering Modal or Component - Rename Modal component to ModalOrComponent component

--- workspaces/app/src/components/app/components/index.ts
+++ workspaces/app/src/components/app/components/index.ts
@@ -2,5 +2,5 @@ export * from './Footer';
 export * from './NavigationBar';
 export * from './Notification';
 export * from './Managers';
-export * from './Modals';
+export * from './ModalOrComponent';
 export * from './Layout';
