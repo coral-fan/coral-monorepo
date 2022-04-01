3f9e2e1df4fdf6c859bfbbf16735846244a6db82
maks-p <mpazuniak@gmail.com>
2022-04-01 16:56:32 -0400

    Refactor time, timer observables

---
+++ workspaces/app/src/libraries/observables/timer.ts
@@ -0,0 +1,29 @@
+import { getMilliSecsDiff } from 'libraries/utils/time';
+import { concat, interval, map, take, takeLast, takeUntil, timer } from 'rxjs';
+
+type TimeRemainingCallback = (milliSecsDiff: number) => any;
+
+/*
+  Solve for timer over 24 days found here:
+  https://github.com/ReactiveX/rxjs/issues/3015
+*/
+export const bigTimer$ = (timestamp: string) => {
+  const milliSecsDiff = getMilliSecsDiff(timestamp);
+
+  const MAX = 2147483647;
+
+  if (milliSecsDiff > MAX) {
+    const numIntervals = Math.floor(milliSecsDiff / MAX);
+    const remainder = milliSecsDiff % MAX;
+    return concat(timer(MAX, MAX).pipe(take(numIntervals)), timer(remainder)).pipe(takeLast(1));
+  }
+
+  return timer(milliSecsDiff);
+};
+
+export const getTimeRemaining$ = (timestamp: string, callback: TimeRemainingCallback) =>
+  interval(1000).pipe(
+    takeUntil(bigTimer$(timestamp)),
+    map(() => getMilliSecsDiff(timestamp)),
+    map(callback)
+  );
---
+++ workspaces/app/src/libraries/utils/time.ts
@@ -0,0 +1,41 @@
+export const getMilliSecsDiff = (timestamp: string) => {
+  const referenceTime = new Date(timestamp).getTime();
+  const currentTime = new Date().getTime();
+
+  return referenceTime - currentTime;
+};
+
+export const getTimeParts = (milliSecsDiff: number) => {
+  if (milliSecsDiff < 0) {
+    return { daysDiff: 0, hoursDiff: 0, minutesDiff: 0, secondsDiff: 0 };
+  }
+
+  const dateDiff = new Date(milliSecsDiff);
+
+  const daysDiff = Math.floor(milliSecsDiff / 1000 / 60 / (60 * 24));
+  const hoursDiff = dateDiff.getUTCHours();
+  const minutesDiff = dateDiff.getUTCMinutes();
+  const secondsDiff = dateDiff.getUTCSeconds();
+
+  return { daysDiff, hoursDiff, minutesDiff, secondsDiff };
+};
+
+export const getDateString = (timestamp: string) => {
+  const date = new Date(timestamp);
+
+  const month = date.toLocaleString('en-US', { month: 'short' });
+  const day = date.getDate();
+
+  return `${month} ${day}`;
+};
+
+export const getTimeString = (timestamp: string) => {
+  const date = new Date(timestamp);
+
+  return date
+    .toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      timeZoneName: 'short',
+    })
+    .replace(' ', '');
+};
