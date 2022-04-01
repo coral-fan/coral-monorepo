24c96a40f805c68db0be264d13e28051ab03df12
iknowhtml <iknowsmtp@gmail.com>
2022-04-01 19:10:02 -0400

    Add rotation on entry animation

--- workspaces/app/src/components/features/event/components/modals/CheckingNFtModal.tsx
+++ workspaces/app/src/components/features/event/components/modals/CheckingNFtModal.tsx
@@ -1,4 +1,5 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
 import { LogoSpinner } from 'components/app/components/ModalOrComponent/SignInModal/LogoSpinner';
 import { Modal } from 'components/ui';
 import { useIconSize } from './hooks';
@@ -7,11 +8,30 @@ const mainContainerStyle = css`
   align-items: center;
 `;
 
+const rotate = keyframes`
+  0% {
+    transform: rotateY(-90deg);
+  }
+  100% {
+    transform: rotateY(0deg);
+}
+`;
+
+const rotatingEntryStyle = css`
+  transform: rotateY(-90deg);
+  transform-style: preserve-3d;
+  animation: ${rotate} 1s 2s ease-in-out forwards;
+`;
+
 export const CheckingNftModal = () => {
   const iconSize = useIconSize();
 
   return (
-    <Modal title="Checking Your Wallet" mainContainerStyle={mainContainerStyle}>
+    <Modal
+      title="Checking Your Wallet"
+      contentStyle={rotatingEntryStyle}
+      mainContainerStyle={mainContainerStyle}
+    >
       <LogoSpinner size={iconSize} />
     </Modal>
   );
