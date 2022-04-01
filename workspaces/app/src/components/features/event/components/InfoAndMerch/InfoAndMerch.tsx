661bf710a518bf1cdca4f2d95f6839e9cf8b7cec
iknowhtml <iknowsmtp@gmail.com>
2022-04-01 10:00:24 -0400

    Add Event Name & Description and layout

--- workspaces/app/src/components/features/event/components/InfoAndMerch/InfoAndMerch.tsx
+++ workspaces/app/src/components/features/event/components/InfoAndMerch/InfoAndMerch.tsx
@@ -2,8 +2,8 @@ import styled from '@emotion/styled';
 import { Info, Merch } from './components';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 `;
 
 export const InfoAndMerch = () => (
