9769993db34d9b8f3f39dd50bf50c26658255027
maks-p <mpazuniak@gmail.com>
2022-04-05 17:03:13 -0400

    Extract Social Icons to separate component

---
+++ workspaces/app/src/components/ui/social/SocialIcon/SocialIcon.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import twitterSVG from './assets/twitter.svg';
+import instagramSVG from './assets/instagram.svg';
+import facebookSVG from './assets/facebook.svg';
+import soundcloudSVG from './assets/soundcloud.svg';
+import tiktokSVG from './assets/tiktok.svg';
+import spotifySVG from './assets/spotify.svg';
+import discogsSVG from './assets/discogs.svg';
+import { SocialHandles } from 'libraries/models';
+import { getIconComponent } from 'components/ui/icons/utils';
+
+type SocialType = keyof SocialHandles;
+
+export interface SocialIconProp {
+  socialType: SocialType;
+}
+
+const socialSVGs: Record<SocialType, string> = {
+  twitter: twitterSVG,
+  instagram: instagramSVG,
+  facebook: facebookSVG,
+  soundcloud: soundcloudSVG,
+  tiktok: tiktokSVG,
+  spotify: spotifySVG,
+  discogs: discogsSVG,
+};
+
+export const SocialIcon = ({ socialType }: SocialIconProp) => {
+  const svg = socialSVGs[socialType];
+  const Icon = getIconComponent(`${socialType}Avatar`, svg);
+  return <Icon />;
+};
---
+++ workspaces/app/src/components/ui/social/SocialLink/SocialLink.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+import { Link as LinkComponent } from 'components/ui';
+import { SocialHandles } from 'libraries/models';
+import styled from '@emotion/styled';
+import { SocialIcon } from '../SocialIcon';
+
+export type SocialType = keyof SocialHandles;
+
+export interface SocialLinkProps {
+  socialType: SocialType;
+  username: string;
+}
+
+const socialLinksMap: Record<SocialType, string> = {
+  twitter: 'https://twitter.com/',
+  instagram: 'https://instagram.com/',
+  facebook: 'https://facebook.com/',
+  soundcloud: 'https://soundcloud.com/',
+  tiktok: 'https://tiktok.com/@',
+  spotify: 'https://open.spotify.com/user/',
+  discogs: 'https://discogs.com/user/',
+};
+
+const Link = styled(LinkComponent)`
+  display: flex;
+  width: 30px;
+  height: 30px;
+`;
+
+export const SocialLink = ({ socialType, username }: SocialLinkProps) => {
+  const url = socialLinksMap[socialType];
+  return (
+    <Link href={`${url}${username}`} openInNewTab>
+      <SocialIcon socialType={socialType} />
+    </Link>
+  );
+};
