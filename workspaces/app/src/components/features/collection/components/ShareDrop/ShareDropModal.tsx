5e2a64dc262d3fd31339fb08758e66c4f7b0a867
maks-p <mpazuniak@gmail.com>
2022-04-06 14:05:52 -0400

    Remove unnecessary fragment

--- workspaces/app/src/components/features/collection/components/ShareDrop/ShareDropModal.tsx
+++ workspaces/app/src/components/features/collection/components/ShareDrop/ShareDropModal.tsx
@@ -1,4 +1,4 @@
-import { ShareModal } from 'components/app/components';
+import { ShareModal } from 'components/ui';
 import { ShareCard } from 'components/ui/nft';
 import { Artist } from 'libraries/models';
 
@@ -22,24 +22,22 @@ export const ShareDropModal = ({
   Badge,
   id,
 }: ShareDropProps) => (
-  <>
-    <ShareModal
-      title={'Share Drop'}
-      url={`https://coral.fan/collection/${id}`}
-      postTitle={name}
-      closeShareModal={closeShareModal}
-    >
-      <ShareCard
-        imageUrl={imageUrl}
-        artistName={artistName}
-        artistProfilePhoto={artistProfilePhoto}
-        isCard={true}
-        title={name}
-        titleHeadingLevel={2}
-        titleStyleVariant={'h3'}
-        description={description}
-        Badge={Badge}
-      />
-    </ShareModal>
-  </>
+  <ShareModal
+    title={'Share Drop'}
+    url={`https://coral.fan/collection/${id}`}
+    postTitle={name}
+    closeShareModal={closeShareModal}
+  >
+    <ShareCard
+      imageUrl={imageUrl}
+      artistName={artistName}
+      artistProfilePhoto={artistProfilePhoto}
+      isCard={true}
+      title={name}
+      titleHeadingLevel={2}
+      titleStyleVariant={'h3'}
+      description={description}
+      Badge={Badge}
+    />
+  </ShareModal>
 );
