74c02c5957f9b331660590637fda32845f6434e1
iknowhtml <iknowsmtp@gmail.com>
2022-04-06 12:53:59 -0400

    Update Event model

--- workspaces/app/src/libraries/models/event/types.ts
+++ workspaces/app/src/libraries/models/event/types.ts
@@ -1,20 +1,24 @@
-import { Artist } from '../artist';
+import { DocumentReference } from 'firebase/firestore';
+import { Artist, ArtistData } from '../artist';
 import { CollectionData } from '../collection';
 
 export interface EventData {
   id: string;
+  // sprout media id
   streamId: string;
+  // chatango id
   chatId: string;
   name: string;
   date: string;
   description: string;
-  artistId: Artist['id'];
+  artist: DocumentReference<ArtistData>;
   allowedCollections: CollectionData['id'][];
   exclusiveCollections: CollectionData['id'][] | null;
 }
 
-export interface Event extends Omit<EventData, 'artistId'> {
+export interface Event extends Omit<EventData, 'artistId' | 'allowedCollections'> {
   artistName: Artist['name'];
   artistProfilePhoto: Artist['profilePhoto'];
   artistSocialHandles: Artist['socialHandles'];
+  allowedCollections: Set<CollectionData['id']>;
 }
