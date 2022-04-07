be24b12677702cf522088b5065a19cda2cd9497f
maks-p <mpazuniak@gmail.com>
2022-04-07 11:27:52 -0400

    Add wallet balance to menu profile info

--- workspaces/app/src/components/app/components/NavigationBar/components/MenuProfileInfo/MenuProfileInfo.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/MenuProfileInfo/MenuProfileInfo.tsx
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 import { Photo } from 'libraries/models';
 import { ProfileInfo, ProfileInfoProps } from 'components/ui';
+import { useProvider, useWallet } from 'libraries/blockchain';
+import { getWalletBalance } from 'libraries/blockchain/wallet/utils';
+import { useState } from 'react';
 
 export interface MenuProfileProps extends Omit<ProfileInfoProps, 'secondaryInfo'> {
-  walletBalance: number;
   profilePhoto: Photo;
 }
 
@@ -11,16 +13,24 @@ const Wrapper = styled.div`
   padding: 16px 20px 16px 0px;
 `;
 
-export const MenuProfileInfo = ({
-  username,
-  profilePhoto,
-  walletBalance,
-}: Omit<MenuProfileProps, 'size'>) => (
-  <Wrapper>
-    <ProfileInfo
-      profilePhoto={profilePhoto}
-      username={username}
-      secondaryInfo={`${walletBalance} AVAX`}
-    />
-  </Wrapper>
-);
+export const MenuProfileInfo = ({ username, profilePhoto }: Omit<MenuProfileProps, 'size'>) => {
+  const [walletBalance, setWalletBalance] = useState(0);
+  const provider = useProvider();
+  const address = useWallet().address;
+
+  getWalletBalance(address, provider).then((balance) => {
+    if (balance) {
+      setWalletBalance(balance);
+    }
+  });
+
+  return (
+    <Wrapper>
+      <ProfileInfo
+        profilePhoto={profilePhoto}
+        username={username}
+        secondaryInfo={`${walletBalance} AVAX`}
+      />
+    </Wrapper>
+  );
+};
