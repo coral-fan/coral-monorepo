ca026fcebb2bd3220d180e5c6cb56648cad78586
iknowhtml <iknowsmtp@gmail.com>
2022-04-07 12:47:02 -0400

    Add balance to wallet hook

--- workspaces/app/src/libraries/blockchain/wallet/hooks.ts
+++ workspaces/app/src/libraries/blockchain/wallet/hooks.ts
@@ -2,6 +2,10 @@ import { initializeConnector } from '@web3-react/core';
 import { AVALANCHE } from 'consts';
 import { MetaMask } from '@web3-react/metamask';
 import { Actions } from '@web3-react/types';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { from, map } from 'rxjs';
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
 
 const getMetaMaskConnector = (actions: Actions) => new MetaMask(actions);
 
@@ -10,13 +14,41 @@ export const [connector, { useProvider, useWeb3React }] = initializeConnector<Me
   [AVALANCHE.CHAIN_ID.INT]
 );
 
+const avalancheRpcProvider = new JsonRpcProvider(AVALANCHE.RPC_URL);
+
+const safeRound = (value: number, decimalPlace: number) => {
+  if (!Number.isInteger(decimalPlace)) {
+    throw Error(`Number ${decimalPlace} is not a integer.`);
+  }
+
+  const decimalFactor = 10 ** decimalPlace;
+
+  return Math.round((value + Number.EPSILON) * decimalFactor) / decimalFactor;
+};
+
+export const getWalletBalance$ = (address: string) =>
+  from(avalancheRpcProvider.getBalance(address)).pipe(
+    map((balanceBigInt) => ethers.utils.formatEther(balanceBigInt)),
+    map((balanceString) => parseFloat(balanceString)),
+    map((balance) => safeRound(balance, 2))
+  );
+
 export const useWallet = () => {
   const { account: address, active: isActive, chainId, error } = useWeb3React(useProvider());
+
+  const [balance, setBalance] = useState<number>();
+
+  useEffect(() => {
+    if (address) {
+      getWalletBalance$(address).subscribe(setBalance);
+    }
+  }, [address]);
 
   return {
     connector,
     chainId,
     address,
+    balance,
     isActive,
     error,
   };
