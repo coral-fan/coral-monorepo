43153b8e2820aba41f6f233b3c68a5704837f283
maks-p <mpazuniak@gmail.com>
2022-04-08 08:34:34 -0400

    Update safeRound to always show correct places

--- workspaces/app/src/libraries/utils/math.ts
+++ workspaces/app/src/libraries/utils/math.ts
@@ -4,6 +4,7 @@ export const safeRound = (value: number, decimalPlace: number) => {
   }
 
   const decimalFactor = 10 ** decimalPlace;
+  const numRounded = Math.round((value + Number.EPSILON) * decimalFactor) / decimalFactor;
 
-  return Math.round((value + Number.EPSILON) * decimalFactor) / decimalFactor;
+  return numRounded.toFixed(decimalPlace);
 };
