e092aa08011bd824ef142147c26b977fccb0f70c
maks-p <mpazuniak@gmail.com>
2022-04-08 14:58:48 -0400

    Move gated content links to GatedContent directory

--- workspaces/app/src/components/ui/nft/GatedContent/components/index.ts
+++ workspaces/app/src/components/ui/nft/GatedContent/components/index.ts
@@ -1,4 +1,6 @@
 export * from './AccessDeniedModal';
 export * from './AccessGrantedModal';
+export * from './ContentLink';
+export * from './EventLink';
 export * from './CheckingNftModal';
 export * from './LoginButton';
