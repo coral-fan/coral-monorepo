b11550add7e62b6ef517777ae013f8654fef7e48
maks-p <mpazuniak@gmail.com>
2022-04-11 10:06:11 -0400

    Misc export updates

--- workspaces/app/src/components/features/collection/components/index.ts
+++ workspaces/app/src/components/features/collection/components/index.ts
@@ -2,4 +2,3 @@ export * from '../../../ui/nft/Layout/components';
 export * from './ProgressBar';
 export * from './SimilarCollections';
 export * from './DropOrAvailable';
-export * from '../../../ui/nft/components/ShareDrop';
