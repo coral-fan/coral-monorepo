c2da50b05ad1c7e0b56a12b4c0ebdcf9fcac9a2e
iknowhtml <iknowsmtp@gmail.com>
2022-04-11 13:25:15 -0400

    Add add collection utiity script

---
+++ workspaces/app/scripts/collection/add.ts
@@ -0,0 +1,53 @@
+import { getDocumentReferenceServerSide } from 'libraries/firebase';
+import { ArtistData, CollectionData } from 'libraries/models';
+
+const COLLECTION: Omit<CollectionData, 'artistRef'> & { artistId: string } = {
+  id: '0xf6a817e0af31d1f8accf9e301c2a9bb08962c160',
+  name: 'Test NFT',
+  artistId: '0xabcdefghijklmnopqrstuvwxyz01234567891011',
+  imageUrl:
+    'https://firebasestorage.googleapis.com/v0/b/coral-c373f.appspot.com/o/collections%2F0xf6a817e0af31d1f8accf9e301c2a9bb08962c160%2Fimage.png?alt=media&token=c08b4275-06bc-41a9-bbe7-69d907239c05',
+  maxMintable: 10,
+  type: 'event',
+  price: 0.01,
+  dropDate: '2022-04-11',
+  description: 'Test NFT Edition v1',
+  details: ['This is a test NFT.'],
+  gatedContent: {
+    type: 'event',
+    id: '1',
+  },
+};
+
+const addCollection = async () => {
+  console.log('adding collection...');
+  const { artistId, ...partialCollection } = COLLECTION;
+  const artistRef = await getDocumentReferenceServerSide<ArtistData>('artists', artistId);
+  const artistDocSnapshot = await artistRef.get();
+
+  if (!artistDocSnapshot.exists) {
+    throw Error(`artist with id ${artistId} doesn't exist.`);
+  }
+
+  const collection: CollectionData = {
+    ...partialCollection,
+    artistRef,
+  };
+
+  const collectionRef = await getDocumentReferenceServerSide<CollectionData>(
+    'collections',
+    collection.id
+  );
+
+  const collectionDocSnapshot = await collectionRef.get();
+
+  if (collectionDocSnapshot.exists) {
+    throw Error(`collection with id ${collection.id} already exists.`);
+  }
+
+  await collectionRef.set(collection);
+
+  console.log('collection added!');
+};
+
+addCollection();
