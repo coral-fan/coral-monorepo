63c4d369462b392fbd3962d794d167d472de9426
iknowhtml <iknowsmtp@gmail.com>
2022-04-14 08:21:47 -0400

    Add error throwing logic to isMetaMaskInjected

--- workspaces/app/src/libraries/blockchain/metamask/utils.ts
+++ workspaces/app/src/libraries/blockchain/metamask/utils.ts
@@ -1,10 +1,17 @@
 import { MetaMaskInpageProvider } from '@metamask/providers';
+import { isServerSide } from 'libraries/utils';
 
-export const isMetaMaskInjected = () =>
-  window.ethereum !== undefined &&
-  /* 
+export const isMetaMaskInjected = () => {
+  if (isServerSide()) {
+    throw 'isMetaMaskInjected should only be called client-side.';
+  }
+  return (
+    window.ethereum !== undefined &&
+    /* 
   casting due windowing typing collision from @metamask/detect-provider
   unable to define custom global declaration of window for some reason
   see node_modules/@metamask/detect-provider/dist/index.d.ts
   */
-  (window.ethereum as MetaMaskInpageProvider).addListener !== undefined;
+    (window.ethereum as MetaMaskInpageProvider).addListener !== undefined
+  );
+};
