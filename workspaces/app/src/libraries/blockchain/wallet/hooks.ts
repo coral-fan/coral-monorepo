1374092b3954afcdd55309ea888aabc8ea09bd47
iknowhtml <iknowsmtp@gmail.com>
2022-04-14 13:35:50 -0400

    Refactor wallet hook to use UID hook for address

--- workspaces/app/src/libraries/blockchain/wallet/hooks.ts
+++ workspaces/app/src/libraries/blockchain/wallet/hooks.ts
@@ -6,6 +6,7 @@ import { initializeConnector } from '@web3-react/core';
 import { AVALANCHE } from 'consts';
 import { getWalletBalance$ } from './observables';
 import { MetaMask as MetaMaskConnector } from '@web3-react/metamask';
+import { useUserUid } from 'libraries/models';
 
 const getCustomAuthConnector = (actions: Actions) => new CustomAuthConnector(actions);
 
@@ -25,7 +26,10 @@ export const useWallet = () => {
 
   const provider = useProvider();
 
-  const { account: address, active: isActive, chainId, error } = useWeb3React(provider);
+  const { active: isActive, chainId, error } = useWeb3React(provider);
+
+  // user uid, aka, id in Firebase is their wallet address
+  const address = useUserUid();
 
   const [balance, setBalance] = useState<number>();
 
