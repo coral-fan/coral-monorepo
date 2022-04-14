4a31a69150460171c7fc060c6766394ff9c8e1b5
iknowhtml <iknowsmtp@gmail.com>
2022-04-14 13:08:07 -0400

    Add get client side only error message util

--- workspaces/app/src/libraries/utils/environment.ts
+++ workspaces/app/src/libraries/utils/environment.ts
@@ -1,1 +1,4 @@
 export const isServerSide = () => typeof window === 'undefined';
+
+export const getClientSideOnlyErrorMessage = (functionName: string) =>
+  `${functionName} should only be called client-side.`;
