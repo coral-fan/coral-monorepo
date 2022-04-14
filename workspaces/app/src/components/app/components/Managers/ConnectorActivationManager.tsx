1e76c75437881476a2fd38cd0b426ef7689a356b
iknowhtml <iknowsmtp@gmail.com>
2022-04-13 20:27:25 -0400

    Add logic to eager connect only if logged in

--- workspaces/app/src/components/app/components/Managers/ConnectorActivationManager.tsx
+++ workspaces/app/src/components/app/components/Managers/ConnectorActivationManager.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
 import { useWallet } from 'libraries/blockchain';
+import { useIdToken } from 'libraries/authentication';
 
 export const ConnectorActivationManager = () => {
   const { connector } = useWallet();
 
+  const idToken = useIdToken();
+
   useEffect(() => {
-    connector.connectEagerly();
-  }, [connector]);
+    if (idToken && connector.connectEagerly) {
+      connector.connectEagerly();
+    }
+  }, [idToken, connector]);
 
   return <></>;
 };
