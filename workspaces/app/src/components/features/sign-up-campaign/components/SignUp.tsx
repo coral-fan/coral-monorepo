a77c87f3a229c8fb841630187134941ff102ee61
iknowhtml <iknowsmtp@gmail.com>
2022-04-18 12:55:34 -0400

    Add Sign Up view

---
+++ workspaces/app/src/components/features/sign-up-campaign/components/SignUp.tsx
@@ -0,0 +1,80 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Button, Link } from 'components/ui';
+import { getIconComponent } from 'components/ui/icons/utils';
+import { useLogin } from 'libraries/authentication';
+import { QUERY } from 'styles/tokens';
+import avalancheLogoSVG from './avalanche-logo.svg';
+import { CtaLayout } from './components';
+
+const Heading = styled.h1`
+  --font-size: 34px;
+  @media ${QUERY.TABLET} {
+    --font-size: 85px;
+  }
+  font-size: var(--font-size);
+
+  max-width: 1000px;
+
+  text-align: center;
+`;
+
+const rewardSpotsStyle = css`
+  text-transform: uppercase;
+  font-size: 12px;
+`;
+interface RewardSpotsProps {
+  remaining: number;
+}
+const RewardSpots = ({ remaining }: RewardSpotsProps) => (
+  <span css={rewardSpotsStyle}>{remaining}/250 Early Reward Spots</span>
+);
+
+const poweredByAvalancheStyle = css`
+  display: flex;
+  justify-self: flex-end;
+  font-size: 18px;
+  gap: 10px;
+  align-items: center;
+`;
+
+const AvalancheLogo = getIconComponent('AvalancheLogo', avalancheLogoSVG);
+const poweredByStyle = css`
+  width: 100%;
+`;
+const PoweredByAvalanche = () => (
+  <div css={poweredByAvalancheStyle}>
+    <span css={poweredByStyle}>Powered By</span>
+    <Link href="https://www.avax.network/">
+      <AvalancheLogo />
+    </Link>
+  </div>
+);
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+  max-width: 250px;
+`;
+
+export const SignUp = () => {
+  const { login, isLoggingIn } = useLogin();
+
+  return (
+    <>
+      <Heading>A modern marketplace for music, collectibles, events and experiences</Heading>
+      <CtaLayout>
+        <Container>
+          <RewardSpots remaining={198} />
+          <Button onClick={login} loading={isLoggingIn}>
+            Sign Up
+          </Button>
+        </Container>
+      </CtaLayout>
+      <PoweredByAvalanche />
+    </>
+  );
+};
