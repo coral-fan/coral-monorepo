74821ed7c22c814b08e82e547582ea99876c44af
iknowhtml <iknowsmtp@gmail.com>
2022-04-18 13:32:51 -0400

    Set assets to empty for campaign in production

--- workspaces/app/src/components/features/user/User.tsx
+++ workspaces/app/src/components/features/user/User.tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps } from 'next';
 import { getDocumentData } from 'libraries/firebase';
 import { getIdToken } from 'libraries/authentication';
 import { destroyCookie } from 'nookies';
-import { ID_TOKEN_KEY } from 'consts';
+import { ID_TOKEN_KEY, SERVER_ENVIRONMENT } from 'consts';
 import { getAuthenticationServerSide } from 'libraries/firebase/authentication';
 import { UserPageProvider } from './provider';
 import { UserProfile } from './components/UserProfile';
@@ -22,123 +22,128 @@ export const UserPage = ({ userData }: UserPageProps) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps<UserPageProps, { userProfileId: string }> =
-  async (context) => {
-    const { params } = context;
+export const getServerSideProps: GetServerSideProps<
+  UserPageProps,
+  { userProfileId: string }
+> = async (context) => {
+  const { params } = context;
 
-    if (params === undefined) {
-      return {
-        notFound: true,
-      };
-    }
-
-    const { userProfileId } = params;
-    const publicUserData = await getDocumentData<PublicUserData>('users', userProfileId);
-
-    if (publicUserData === undefined) {
-      return {
-        notFound: true,
-      };
-    }
-
-    const auth = await getAuthenticationServerSide();
-
-    const token = getIdToken(context);
-
-    const authenticatedUserUid =
-      token === null
-        ? undefined
-        : (
-            await auth.verifyIdToken(token).catch(() => {
-              destroyCookie(context, ID_TOKEN_KEY);
-            })
-          )?.uid;
-
-    const privateUserData =
-      authenticatedUserUid === userProfileId
-        ? await getDocumentData<PrivateUserData>('users', userProfileId, 'private', 'data')
-        : undefined;
-
-    const assets: Asset[] = [
-      {
-        id: 3671,
-        contractAddress: '1',
-        collectionName: 'Bonobo',
-        ownerUsername: publicUserData.username,
-        ownerAddress: userProfileId,
-        ownerType: 'fan',
-        ownerProfilePhoto: publicUserData.profilePhoto,
-        gatedContent: {
-          type: 'url',
-          url: '/',
-        },
-        type: 'music',
-        collectionDescription:
-          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-        collectionDetails: null,
-        ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-      },
-      {
-        id: 9,
-        contractAddress: '1',
-        collectionName: 'Behind the Scenes Studio Tour',
-        ownerUsername: publicUserData.username,
-        ownerAddress: userProfileId,
-        ownerType: 'fan',
-        ownerProfilePhoto: publicUserData.profilePhoto,
-        gatedContent: {
-          type: 'url',
-          url: '/',
-        },
-        type: 'music',
-        collectionDescription:
-          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-        collectionDetails: null,
-        ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-      },
-      {
-        id: 9732,
-        contractAddress: '2',
-        collectionName: 'Access Tour: This is an Extra Long Title',
-        ownerUsername: publicUserData.username,
-        ownerAddress: userProfileId,
-        ownerType: 'fan',
-        ownerProfilePhoto: publicUserData.profilePhoto,
-        gatedContent: {
-          type: 'url',
-          url: '/',
-        },
-        type: 'merch',
-        collectionDescription:
-          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-        collectionDetails: null,
-        ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-      },
-      {
-        id: 234,
-        contractAddress: '3',
-        collectionName: 'This Is An Event',
-        ownerUsername: publicUserData.username,
-        ownerAddress: userProfileId,
-        ownerType: 'fan',
-        ownerProfilePhoto: publicUserData.profilePhoto,
-        gatedContent: {
-          type: 'url',
-          url: '/',
-        },
-        type: 'event',
-        collectionDescription:
-          'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-        collectionDetails: null,
-        ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-      },
-    ];
-
-    const userData: User = { ...publicUserData, ...privateUserData, assets, following: [] };
-
+  if (params === undefined) {
     return {
-      props: {
-        userData,
-      },
+      notFound: true,
     };
+  }
+
+  const { userProfileId } = params;
+  const publicUserData = await getDocumentData<PublicUserData>('users', userProfileId);
+
+  if (publicUserData === undefined) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const auth = await getAuthenticationServerSide();
+
+  const token = getIdToken(context);
+
+  const authenticatedUserUid =
+    token === null
+      ? undefined
+      : (
+          await auth.verifyIdToken(token).catch(() => {
+            destroyCookie(context, ID_TOKEN_KEY);
+          })
+        )?.uid;
+
+  const privateUserData =
+    authenticatedUserUid === userProfileId
+      ? await getDocumentData<PrivateUserData>('users', userProfileId, 'private', 'data')
+      : undefined;
+
+  const assets: Asset[] =
+    SERVER_ENVIRONMENT === 'production'
+      ? []
+      : [
+          {
+            id: 3671,
+            contractAddress: '1',
+            collectionName: 'Bonobo',
+            ownerUsername: publicUserData.username,
+            ownerAddress: userProfileId,
+            ownerType: 'fan',
+            ownerProfilePhoto: publicUserData.profilePhoto,
+            gatedContent: {
+              type: 'url',
+              url: '/',
+            },
+            type: 'music',
+            collectionDescription:
+              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+            collectionDetails: null,
+            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+          },
+          {
+            id: 9,
+            contractAddress: '1',
+            collectionName: 'Behind the Scenes Studio Tour',
+            ownerUsername: publicUserData.username,
+            ownerAddress: userProfileId,
+            ownerType: 'fan',
+            ownerProfilePhoto: publicUserData.profilePhoto,
+            gatedContent: {
+              type: 'url',
+              url: '/',
+            },
+            type: 'music',
+            collectionDescription:
+              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+            collectionDetails: null,
+            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+          },
+          {
+            id: 9732,
+            contractAddress: '2',
+            collectionName: 'Access Tour: This is an Extra Long Title',
+            ownerUsername: publicUserData.username,
+            ownerAddress: userProfileId,
+            ownerType: 'fan',
+            ownerProfilePhoto: publicUserData.profilePhoto,
+            gatedContent: {
+              type: 'url',
+              url: '/',
+            },
+            type: 'merch',
+            collectionDescription:
+              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+            collectionDetails: null,
+            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+          },
+          {
+            id: 234,
+            contractAddress: '3',
+            collectionName: 'This Is An Event',
+            ownerUsername: publicUserData.username,
+            ownerAddress: userProfileId,
+            ownerType: 'fan',
+            ownerProfilePhoto: publicUserData.profilePhoto,
+            gatedContent: {
+              type: 'url',
+              url: '/',
+            },
+            type: 'event',
+            collectionDescription:
+              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+            collectionDetails: null,
+            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+          },
+        ];
+
+  const userData: User = { ...publicUserData, ...privateUserData, assets, following: [] };
+
+  return {
+    props: {
+      userData,
+    },
   };
+};
