e15d4b90b7f8496037a94cb0d91a178c21b58a2b
maks-p <mpazuniak@gmail.com>
2022-04-19 13:28:19 -0400

    Add Avax icons

---
+++ workspaces/app/src/components/features/collection/components/PaymentModal/components/icons/AvaxAltIcon/AvaxAltIcon.tsx
@@ -0,0 +1,4 @@
+import avaxAltIconSVG from '../assets/avaxAlt.svg';
+import { getIconComponent } from 'components/ui/icons/utils';
+
+export const AvaxAltIcon = getIconComponent('AvaxAltIcon', avaxAltIconSVG);
---
+++ workspaces/app/src/components/features/collection/components/PaymentModal/components/icons/AvaxIcon/AvaxIcon.tsx
@@ -0,0 +1,4 @@
+import avaxIconSVG from '../assets/avax.svg';
+import { getIconComponent } from 'components/ui/icons/utils';
+
+export const AvaxIcon = getIconComponent('AvaxIcon', avaxIconSVG);
