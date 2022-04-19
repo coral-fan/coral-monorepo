19cc9de86560146da5fb7078bf837d5a2f87cd8a
maks-p <mpazuniak@gmail.com>
2022-04-19 13:30:27 -0400

    Add color prop to spinner

--- workspaces/app/src/components/ui/Spinner/ConditionalSpinner.tsx
+++ workspaces/app/src/components/ui/Spinner/ConditionalSpinner.tsx
@@ -4,10 +4,12 @@ import { Spinner } from './Spinner';
 interface ConditionalSpinnerProps {
   loading?: boolean;
   size?: string;
+  color?: string;
 }
 
 export const ConditionalSpinner: FC<ConditionalSpinnerProps> = ({
   loading = false,
   size,
+  color,
   children,
-}) => (loading ? <Spinner size={size} /> : <>{children}</>);
+}) => (loading ? <Spinner size={size} color={color} /> : <>{children}</>);
