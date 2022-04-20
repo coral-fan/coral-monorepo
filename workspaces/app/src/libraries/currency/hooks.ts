e4a3a5ed469bf9ecfd272dc838e6aea8e8e996d1
maks-p <mpazuniak@gmail.com>
2022-04-20 14:06:55 -0400

    Refactor currency hook, observable

--- workspaces/app/src/libraries/currency/hooks.ts
+++ workspaces/app/src/libraries/currency/hooks.ts
@@ -1,16 +1,20 @@
 import { getCurrencyPairPrice$ } from './observables';
 import { useEffect, useState } from 'react';
+import { delay, mergeMapTo, timer } from 'rxjs';
 
 export const useAvaxUsdPrice = () => {
-  const [data, setData] = useState({
-    exchangeRate: 0,
-    loading: true,
-  });
+  const [exchangeRate, setExchangeRate] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const subject = getCurrencyPairPrice$('avax_usd').subscribe(setData);
+    const subject = getCurrencyPairPrice$('avax_usd')
+      .pipe(delay(1000))
+      .subscribe((exchangeRate) => {
+        setExchangeRate(exchangeRate);
+        setIsLoading(false);
+      });
     return () => subject.unsubscribe();
   }, []);
 
-  return data;
+  return { exchangeRate, isLoading };
 };
