e350c7b8ec928a5152d7845a7737f4fb61888b7c
iknowhtml <iknowsmtp@gmail.com>
2022-04-27 15:11:39 -0400

    Remove unnecessary global window type declaration

--- workspaces/app/src/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-import type { JWPlayer } from '@types/jwplayer';
-// prevent type error when accessing the ethereum property on the window object
-declare global {
-  interface Window {
-    jwplayer: JWPlayer;
-  }
-}
