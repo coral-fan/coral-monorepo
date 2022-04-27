cf6f3df2e828cbb901416fcbce56d8775ddc6c02
iknowhtml <iknowsmtp@gmail.com>
2022-04-27 18:25:39 -0400

    Refactor get user profile observable utility Return RxFire observable instead of promise in mergeMap callback in pipe

--- workspaces/app/src/components/app/components/NavigationBar/observables.ts
+++ workspaces/app/src/components/app/components/NavigationBar/observables.ts
@@ -1,11 +1,15 @@
-import { getDocumentData } from 'libraries/firebase';
+import { getDocumentReferenceClientSide } from 'libraries/firebase';
 import { getUserUid$, User } from 'libraries/models';
+import { docData } from 'rxfire/firestore';
 import { filter, map, mergeMap } from 'rxjs/operators';
 
 export const getUserProfile$ = () =>
   getUserUid$().pipe(
     filter((uid): uid is string => uid !== undefined),
-    mergeMap((uid) => getDocumentData<User>('users', uid)),
+    mergeMap((uid) => {
+      const documentRef = getDocumentReferenceClientSide('users', uid);
+      return docData(documentRef);
+    }),
     filter((user): user is User => user !== undefined),
     map(({ username, profilePhoto }) => ({ username, profilePhoto }))
   );
