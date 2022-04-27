51f8c634723d06274fb0de51c3638a726c398b5d
iknowhtml <iknowsmtp@gmail.com>
2022-04-27 15:11:30 -0400

    Remove unnecesary casting for window.ethereum

--- workspaces/app/src/libraries/blockchain/metamask/utils.ts
+++ workspaces/app/src/libraries/blockchain/metamask/utils.ts
@@ -1,4 +1,3 @@
-import { MetaMaskInpageProvider } from '@metamask/providers';
 import { getClientSideOnlyErrorMessage, isServerSide } from 'libraries/utils';
 
 const CLIENT_SIDE_ONLY_ERROR_MESSAGE = getClientSideOnlyErrorMessage('isMetaMaskInjected');
@@ -7,13 +6,5 @@ export const isMetaMaskInjected = () => {
   if (isServerSide()) {
     throw CLIENT_SIDE_ONLY_ERROR_MESSAGE;
   }
-  return (
-    window.ethereum !== undefined &&
-    /* 
-  casting due windowing typing collision from @metamask/detect-provider
-  unable to define custom global declaration of window for some reason
-  see node_modules/@metamask/detect-provider/dist/index.d.ts
-  */
-    (window.ethereum as MetaMaskInpageProvider).addListener !== undefined
-  );
+  return window.ethereum !== undefined && window.ethereum.addListener !== undefined;
 };
