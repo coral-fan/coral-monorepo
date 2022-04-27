19dccfec18538ad5372aee41596dfc99ab147a69
iknowhtml <iknowsmtp@gmail.com>
2022-04-27 15:59:33 -0400

    Add conditional return for sign up campaign

--- workspaces/app/src/components/features/user/User.tsx
+++ workspaces/app/src/components/features/user/User.tsx
@@ -25,6 +25,13 @@ export const UserPage = ({ userData }: UserPageProps) => {
 export const getServerSideProps: GetServerSideProps<UserPageProps, { id: string }> = async (
   context
 ) => {
+  //  TODO: remove conditional return for sign up campaign
+  if (SERVER_ENVIRONMENT === 'production') {
+    return {
+      notFound: true,
+    };
+  }
+
   const { params } = context;
 
   if (params === undefined) {
@@ -60,83 +67,80 @@ export const getServerSideProps: GetServerSideProps<UserPageProps, { id: string 
       ? await getDocumentData<PrivateUserData>('users', id, 'private', 'data')
       : undefined;
 
-  const assets: Asset[] =
-    SERVER_ENVIRONMENT === 'production'
-      ? []
-      : [
-          {
-            id: 3671,
-            contractAddress: '1',
-            collectionName: 'Bonobo',
-            ownerUsername: publicUserData.username,
-            ownerAddress: id,
-            ownerType: 'fan',
-            ownerProfilePhoto: publicUserData.profilePhoto,
-            gatedContent: {
-              type: 'url',
-              url: '/',
-            },
-            type: 'music',
-            collectionDescription:
-              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-            collectionDetails: null,
-            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-          },
-          {
-            id: 9,
-            contractAddress: '1',
-            collectionName: 'Behind the Scenes Studio Tour',
-            ownerUsername: publicUserData.username,
-            ownerAddress: id,
-            ownerType: 'fan',
-            ownerProfilePhoto: publicUserData.profilePhoto,
-            gatedContent: {
-              type: 'url',
-              url: '/',
-            },
-            type: 'music',
-            collectionDescription:
-              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-            collectionDetails: null,
-            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-          },
-          {
-            id: 9732,
-            contractAddress: '2',
-            collectionName: 'Access Tour: This is an Extra Long Title',
-            ownerUsername: publicUserData.username,
-            ownerAddress: id,
-            ownerType: 'fan',
-            ownerProfilePhoto: publicUserData.profilePhoto,
-            gatedContent: {
-              type: 'url',
-              url: '/',
-            },
-            type: 'merch',
-            collectionDescription:
-              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-            collectionDetails: null,
-            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-          },
-          {
-            id: 234,
-            contractAddress: '3',
-            collectionName: 'This Is An Event',
-            ownerUsername: publicUserData.username,
-            ownerAddress: id,
-            ownerType: 'fan',
-            ownerProfilePhoto: publicUserData.profilePhoto,
-            gatedContent: {
-              type: 'url',
-              url: '/',
-            },
-            type: 'event',
-            collectionDescription:
-              'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
-            collectionDetails: null,
-            ...IMAGE_WITH_INFO_DEFAULT_ARGS,
-          },
-        ];
+  const assets: Asset[] = [
+    {
+      id: 3671,
+      contractAddress: '1',
+      collectionName: 'Bonobo',
+      ownerUsername: publicUserData.username,
+      ownerAddress: id,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'music',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+    {
+      id: 9,
+      contractAddress: '1',
+      collectionName: 'Behind the Scenes Studio Tour',
+      ownerUsername: publicUserData.username,
+      ownerAddress: id,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'music',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+    {
+      id: 9732,
+      contractAddress: '2',
+      collectionName: 'Access Tour: This is an Extra Long Title',
+      ownerUsername: publicUserData.username,
+      ownerAddress: id,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'merch',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+    {
+      id: 234,
+      contractAddress: '3',
+      collectionName: 'This Is An Event',
+      ownerUsername: publicUserData.username,
+      ownerAddress: id,
+      ownerType: 'fan',
+      ownerProfilePhoto: publicUserData.profilePhoto,
+      gatedContent: {
+        type: 'url',
+        url: '/',
+      },
+      type: 'event',
+      collectionDescription:
+        'Exclusive access to a one on one call with me between recording sessions on my next album. With this token you’ll get 30 minutes of solo time with me and the band.',
+      collectionDetails: null,
+      ...IMAGE_WITH_INFO_DEFAULT_ARGS,
+    },
+  ];
 
   const userData: User = { ...publicUserData, ...privateUserData, assets, following: [] };
 
