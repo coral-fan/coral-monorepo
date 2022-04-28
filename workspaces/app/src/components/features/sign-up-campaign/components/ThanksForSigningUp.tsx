d2e24ecf375f3eee4d205d9f62add3f170ad1dfd
iknowhtml <iknowsmtp@gmail.com>
2022-04-28 12:13:16 -0400

    Add conditional logic for thank you message

--- workspaces/app/src/components/features/sign-up-campaign/components/ThanksForSigningUp.tsx
+++ workspaces/app/src/components/features/sign-up-campaign/components/ThanksForSigningUp.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Button } from 'components/ui';
 import { QUERY } from 'styles';
+import { SignUpCampaignProps as ThanksForSigningUpProps } from '../SignUpCampaign';
 import { CtaLayout } from './components';
 
 const Heading = styled.h1`
@@ -26,15 +27,23 @@ const shareButtonStyle = css`
   margin-top: 30px;
 `;
 
-export const ThanksForSigningUp = () => (
-  <>
-    <Heading>Thanks For Signing Up</Heading>
-    <CtaLayout>
-      <SecondaryInfo>
-        <span>As an early supporter,</span>
-        <span>you will receive a $10 credit when Coral is live.</span>
-      </SecondaryInfo>
-      <Button css={shareButtonStyle}>Share</Button>
-    </CtaLayout>
-  </>
-);
+export const ThanksForSigningUp = ({ isUserEarlySupporter }: ThanksForSigningUpProps) => {
+  return (
+    <>
+      <Heading>Thanks For Signing Up</Heading>
+      <CtaLayout>
+        <SecondaryInfo>
+          {isUserEarlySupporter ? (
+            <>
+              <span>As an early supporter,</span>
+              <span>you will receive a $10 credit when Coral is live.</span>
+            </>
+          ) : (
+            <span>Stay tuned for updates</span>
+          )}
+        </SecondaryInfo>
+        <Button css={shareButtonStyle}>Share</Button>
+      </CtaLayout>
+    </>
+  );
+};
