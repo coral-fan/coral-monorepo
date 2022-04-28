454125a3cc5e396e603182f8b665dfbdfba1e9ea
iknowhtml <iknowsmtp@gmail.com>
2022-04-28 08:39:31 -0400

    Update SIgn Up Campaign feature component Add logic to retrieve campaign data server-side & pass it client-side

--- workspaces/app/src/components/features/sign-up-campaign/SignUpCampaign.tsx
+++ workspaces/app/src/components/features/sign-up-campaign/SignUpCampaign.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 import { useIsAuthenticated } from 'libraries/authentication';
+import { getDocumentData } from 'libraries/firebase';
+import { GetServerSideProps } from 'next';
 import { SignUp, ThanksForSigningUp } from './components';
+import { PrelaunchSignUpCampaignData } from './types';
 
 const Container = styled.div`
   flex: 1;
@@ -11,8 +14,29 @@ const Container = styled.div`
   gap: 50px;
 `;
 
-export const SignUpCampaign = () => {
+export interface SignUpCampaignProps {
+  prelaunchSignUpUsers: PrelaunchSignUpCampaignData['users'];
+}
+
+export const SignUpCampaign = (props: SignUpCampaignProps) => {
   const isAuthenticated = useIsAuthenticated();
 
-  return <Container>{isAuthenticated ? <ThanksForSigningUp /> : <SignUp />}</Container>;
+  return <Container>{isAuthenticated ? <ThanksForSigningUp /> : <SignUp {...props} />}</Container>;
+};
+
+export const getServerSideProps: GetServerSideProps<SignUpCampaignProps> = async () => {
+  const signUpCampaignData = await getDocumentData<PrelaunchSignUpCampaignData>(
+    'app',
+    'prelaunch-sign-up-campaign'
+  );
+
+  if (signUpCampaignData === undefined) {
+    throw 'signUpCampaignData cannot be undefined.';
+  }
+
+  return {
+    props: {
+      prelaunchSignUpUsers: signUpCampaignData.users,
+    },
+  };
 };
