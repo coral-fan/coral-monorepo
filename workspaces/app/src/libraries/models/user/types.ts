89500c0ccf0dab69b9fbc4b16a0ec2ce97cdd784
iknowhtml <iknowsmtp@gmail.com>
2022-04-28 08:35:07 -0400

    Update incoming user data type

--- workspaces/app/src/libraries/models/user/types.ts
+++ workspaces/app/src/libraries/models/user/types.ts
@@ -54,7 +54,7 @@ https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-r
 */
 type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
 
-export type IncomingUserData = AtLeastOne<User>;
+export type IncomingUserData = Omit<AtLeastOne<User>, 'id'>;
 
 export type IsUserSigningUpData = {
   isSigningUp: boolean;
