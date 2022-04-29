7a7f1a88b908f43a05a3c3650a35795ab705a6d8
iknowhtml <iknowsmtp@gmail.com>
2022-04-29 15:12:51 -0400

    Remove legal pages logic  for Sign Up Campaign

--- workspaces/app/src/components/app/components/ModalOrComponent/ModalOrComponent.tsx
+++ workspaces/app/src/components/app/components/ModalOrComponent/ModalOrComponent.tsx
@@ -2,9 +2,9 @@ import { useIsNetworkSupported } from 'libraries/blockchain';
 import { useIsSigningUp } from 'libraries/authentication';
 import { WrongNetworkModal } from './WrongNetworkModal';
 import { SignUpModal } from './SignUpModal';
-import { useRouter } from 'next/router';
-import { SITE_LINKS } from 'consts';
-
+// TODO: uncomment out post sign up campaign
+// import { useRouter } from 'next/router';
+// import { SITE_LINKS } from 'consts';
 interface ModalOrComponentProps {
   component: JSX.Element;
 }
@@ -12,12 +12,13 @@ export const ModalOrComponent = ({ component }: ModalOrComponentProps) => {
   const isNetworkSupported = useIsNetworkSupported();
   const [isSigningUp] = useIsSigningUp();
 
-  const { asPath } = useRouter();
+  // TODO: uncomment out post sign up campgain launch
+  // const { asPath } = useRouter();
 
-  // TODO: revisit to see if there's a better way to do this
-  if (asPath === SITE_LINKS.PRIVACY_POLICY || asPath === SITE_LINKS.TERMS_OF_SERVICE) {
-    return component;
-  }
+  // // TODO: revisit to see if there's a better way to do this
+  // if (asPath === SITE_LINKS.PRIVACY_POLICY || asPath === SITE_LINKS.TERMS_OF_SERVICE) {
+  //   return component;
+  // }
 
   if (!isNetworkSupported) {
     return <WrongNetworkModal />;
