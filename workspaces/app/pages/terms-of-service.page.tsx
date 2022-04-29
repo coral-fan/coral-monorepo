46dbaaea0b5d1f4b95c9de75e58a2864bb2af5ec
iknowhtml <iknowsmtp@gmail.com>
2022-04-29 12:24:55 -0400

    Remove Terms of Service from Sign Up Campaign

--- workspaces/app/pages/terms-of-service.page.tsx
+++ workspaces/app/pages/terms-of-service.page.tsx
@@ -1,1 +1,12 @@
+import { SERVER_ENVIRONMENT } from 'consts';
+
 export { TermsOfServices as default } from 'components/features/legal';
+
+export const getServerSideProps = async () => {
+  //  TODO: remove conditional return for sign up campaign
+  if (SERVER_ENVIRONMENT === 'production') {
+    return {
+      notFound: true,
+    };
+  }
+};
