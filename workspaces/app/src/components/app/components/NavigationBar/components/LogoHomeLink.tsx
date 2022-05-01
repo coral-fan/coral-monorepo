eab3d3cbd593fb036ab8e6bd9bb81006f2b0f56e
iknowhtml <iknowsmtp@gmail.com>
2022-05-01 16:33:32 -0400

    Update Logo Home Link component style Use custom style & remove hover styling from component

--- workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
@@ -1,13 +1,27 @@
+import { css } from '@emotion/react';
 import { LogoIcon, Link } from 'components/ui';
-import { getNavigationButtonStyle } from 'components/ui/buttons/variants/NavigationButton/utils';
 import { getIconComponent } from 'components/ui/icons/utils';
 import { CLIENT_ENVIRONMENT } from 'consts';
+import tokens, { QUERY } from 'styles/tokens';
 import logoWordmarkSVG from './logo-wordmark.svg';
 
 const LogoWordmarkIcon = getIconComponent('LogoWordmarkIcon', logoWordmarkSVG);
 
+const logoHomeLinkStyle = css`
+  --button-size: ${tokens.buttons.size.mobile};
+
+  @media ${QUERY.LAPTOP} {
+    --button-size: ${tokens.buttons.size.desktop};
+  }
+
+  width: var(--button-size);
+  height: var(--button-size);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 export const LogoHomeLink = () => (
-  <Link css={getNavigationButtonStyle(true)} href="/">
+  <Link css={logoHomeLinkStyle} href="/">
     {CLIENT_ENVIRONMENT === 'production' ? <LogoWordmarkIcon /> : <LogoIcon />}
   </Link>
 );
