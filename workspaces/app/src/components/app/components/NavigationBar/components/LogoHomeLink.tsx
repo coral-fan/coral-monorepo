8a37c2409d651ebe66d682d0c0ead2e77980a54e
iknowhtml <iknowsmtp@gmail.com>
2022-05-01 16:04:31 -0400

    Add Logo Wordmark component for Sign Up Campaign

--- workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
@@ -1,8 +1,13 @@
 import { LogoIcon, Link } from 'components/ui';
 import { getNavigationButtonStyle } from 'components/ui/buttons/variants/NavigationButton/utils';
+import { getIconComponent } from 'components/ui/icons/utils';
+import { CLIENT_ENVIRONMENT } from 'consts';
+import logoWordmarkSVG from './logo-wordmark.svg';
+
+const LogoWordmarkIcon = getIconComponent('LogoWordmarkIcon', logoWordmarkSVG);
 
 export const LogoHomeLink = () => (
   <Link css={getNavigationButtonStyle(true)} href="/">
-    <LogoIcon />
+    {CLIENT_ENVIRONMENT === 'production' ? <LogoWordmarkIcon /> : <LogoIcon />}
   </Link>
 );
