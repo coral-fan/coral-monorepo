5ace0bbc7c32a8a52ad875f0a8c656ed3bc0df3b
iknowhtml <iknowsmtp@gmail.com>
2022-05-01 12:32:42 -0400

    Add mutex lock logic for eager connection

--- workspaces/app/src/components/app/components/Managers/ConnectorActivationManager.tsx
+++ workspaces/app/src/components/app/components/Managers/ConnectorActivationManager.tsx
@@ -1,17 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useWallet } from 'libraries/blockchain';
 import { useIdToken } from 'libraries/authentication';
 
 export const ConnectorActivationManager = () => {
-  const { connector } = useWallet();
+  const { isActive, connector } = useWallet();
 
   const idToken = useIdToken();
 
+  // TODO: put is connecting state inside of wallet hook?
+  // is connecting acts as a mutex lock to prevent multiple calls to connect eagerly before one resolves
+  const [isConnecting, setIsConnecting] = useState(false);
+
   useEffect(() => {
-    if (idToken && connector.connectEagerly) {
-      connector.connectEagerly();
+    if (idToken && !isActive && !isConnecting && connector.connectEagerly) {
+      setIsConnecting(true);
+      const maybePromise = connector.connectEagerly();
+      // maybePromised needs to be checked for what type it is since it can return void OR Promise<void>
+      if (maybePromise instanceof Promise) {
+        maybePromise.then(() => setIsConnecting(false));
+      } else {
+        setIsConnecting(false);
+      }
     }
-  }, [idToken, connector]);
+  }, [idToken, isActive, isConnecting, connector]);
 
   return <></>;
 };
