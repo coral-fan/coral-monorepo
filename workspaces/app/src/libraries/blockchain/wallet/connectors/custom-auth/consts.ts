0f3fe088df7798eb5d22c796ac9f7954be0a8d3c
iknowhtml <iknowsmtp@gmail.com>
2022-05-01 08:44:12 -0400

    Remove CustomAuth connector

--- workspaces/app/src/libraries/blockchain/wallet/connectors/custom-auth/consts.ts
+++
@@ -1,3 +0,0 @@
-export const CUSTOM_AUTH_GOOGLE_VERIFIER = 'coral-test-google-verifier';
-export const GOOGLE_O_AUTH_CLIENT_ID =
-  '805666237729-1g4llv1pd3fd3s647kq229l1fjj3b02a.apps.googleusercontent.com';
