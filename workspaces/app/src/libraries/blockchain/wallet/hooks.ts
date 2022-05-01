20e5fde1e700c581b2d0af3362cf0b7b432c53ea
iknowhtml <iknowsmtp@gmail.com>
2022-05-01 10:12:09 -0400

    Update wallet hook - Refactor to accomodate Web3Auth & remove CustomAuth logic - Remove auto-detect wallet logic - Use Zustand to add logic to accomodate user wallet/connector selection - Use  account value from web3-react for wallet address - Expose set connector type function

--- workspaces/app/src/libraries/blockchain/wallet/hooks.ts
+++ workspaces/app/src/libraries/blockchain/wallet/hooks.ts
@@ -1,35 +1,63 @@
 import { useEffect, useMemo, useState } from 'react';
 import { Actions, Connector } from '@web3-react/types';
 import { initializeConnector } from '@web3-react/core';
-import { MetaMask as MetaMaskConnector } from '@web3-react/metamask';
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
 import { AVALANCHE } from 'consts';
-import { useUserUid } from 'libraries/models';
-import { CustomAuthConnector } from './connectors';
+import { Web3AuthConnector, MetaMaskConnector } from './connectors';
 import { useIsMetaMaskInjected } from '../metamask';
 import { getWalletBalance$ } from './observables';
 
-const getCustomAuthConnector = (actions: Actions) => new CustomAuthConnector(actions);
-
-const customAuth = initializeConnector<Connector>(getCustomAuthConnector, [AVALANCHE.CHAIN_ID.INT]);
-
+const getWeb3AuthConnector = (actions: Actions) => new Web3AuthConnector(actions);
 const getMetaMaskConnector = (actions: Actions) => new MetaMaskConnector(actions);
 
-const metaMask = initializeConnector<Connector>(getMetaMaskConnector);
+export const CONNECTOR_MAP = {
+  WEB3AUTH: initializeConnector<Connector>(getWeb3AuthConnector, [AVALANCHE.CHAIN_ID.INT]),
+  METAMASK: initializeConnector<Connector>(getMetaMaskConnector),
+};
 
+export type ConnectorType = keyof typeof CONNECTOR_MAP;
+
+interface ConnectorState {
+  defaultConnectorType: ConnectorType;
+  setDefaultConnectorType: (defaultConnectorType: ConnectorType) => void;
+  connectorType?: ConnectorType;
+  setConnectorType: (connectorType?: ConnectorType) => void;
+}
+
+const useConnectorState = create<ConnectorState>()(
+  persist(
+    (set) => ({
+      defaultConnectorType: 'WEB3AUTH',
+      setDefaultConnectorType: (defaultConnectorType) =>
+        set((state) => ({ ...state, defaultConnectorType })),
+      connectorType: undefined,
+      setConnectorType: (connectorType) => set((state) => ({ ...state, connectorType })),
+    }),
+    {
+      name: 'coral_connector_store',
+    }
+  )
+);
+// TODO: refactor to stop exposing connector direcly through hook?
 export const useWallet = () => {
   const isMetaMaskInjected = useIsMetaMaskInjected();
 
+  const { defaultConnectorType, setDefaultConnectorType, connectorType, setConnectorType } =
+    useConnectorState();
+
+  useEffect(() => {
+    setDefaultConnectorType(isMetaMaskInjected ? 'METAMASK' : 'WEB3AUTH');
+  }, [setDefaultConnectorType, isMetaMaskInjected]);
+
   const [connector, { useWeb3React, useProvider }] = useMemo(
-    () => (isMetaMaskInjected ? metaMask : customAuth),
-    [isMetaMaskInjected]
+    () => CONNECTOR_MAP[connectorType ?? defaultConnectorType],
+    [connectorType, defaultConnectorType]
   );
 
   const provider = useProvider();
 
-  const { active: isActive, chainId, error } = useWeb3React(provider);
-
-  // user uid, aka, id in Firebase is their wallet address
-  const address = useUserUid();
+  const { active: isActive, chainId, error, account: address } = useWeb3React(provider);
 
   const [balance, setBalance] = useState<number>();
 
@@ -40,11 +68,13 @@ export const useWallet = () => {
   }, [address]);
 
   return {
-    connector,
-    chainId,
-    address,
-    balance,
     isActive,
+    connector,
+    provider,
+    address,
+    chainId,
+    balance,
     error,
+    setConnectorType,
   };
 };
