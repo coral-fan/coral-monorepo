d08e536cc265fdc47cdf3163afd90d155c043fdb
maks-p <mpazuniak@gmail.com>
2022-05-03 11:29:39 -0400

    Abstract out animation library

--- workspaces/app/src/components/features/collection/components/DropOrAvailable/DropOrAvailable.tsx
+++ workspaces/app/src/components/features/collection/components/DropOrAvailable/DropOrAvailable.tsx
@@ -1,37 +1,20 @@
-import styled from '@emotion/styled';
 import { CtaButton, DropTimer } from 'components/ui';
 import { getMilliSecsDiff, getTimeRemaining$ } from 'libraries/time';
 import { useObservable } from 'libraries/utils';
-import { useCallback, useRef, useState } from 'react';
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import { useCallback, useState } from 'react';
+
 import { AvailableContainer } from 'components/ui/nft';
 import { Price, PriceProp } from '../Price';
 import { ProgressBar } from '../ProgressBar';
 import { AssetInfoProps } from '../PaymentModal/components/AssetInfo';
 import { PaymentModal } from '../PaymentModal';
+import { FadeOutInSwitchAnimation } from 'libraries/animation';
 
 interface DropOrAvailableProps extends PriceProp, AssetInfoProps {
   numMinted?: number;
   maxMintable: number;
   dropDate: string;
 }
-
-const TRANSITION_NAME = 'fade';
-
-const FadeContainer = styled.div`
-  &.${TRANSITION_NAME}-enter {
-    opacity: 0;
-  }
-  &.${TRANSITION_NAME}-enter-active, &.${TRANSITION_NAME}-exit {
-    opacity: 1;
-    transition: opacity 500ms ease-in;
-  }
-  &.${TRANSITION_NAME}-exit-active {
-    opacity: 0;
-    transition: opacity 750ms ease-in-out;
-    transition-delay: 100ms;
-  }
-`;
 
 export const DropOrAvailable = ({
   usdPrice,
@@ -53,7 +36,6 @@ export const DropOrAvailable = ({
     SwitchTransition implementation from:
     https://stackoverflow.com/questions/64126226/how-do-i-get-switchtranition-to-work-with-csstransition-with-typescript
   */
-  const nodeRef = useRef<HTMLDivElement>(null);
 
   const getMilliSecondsRemaining = useCallback(
     () => getTimeRemaining$(dropDate, (milliSecsDiff) => milliSecsDiff),
@@ -71,39 +53,28 @@ export const DropOrAvailable = ({
   const isAvailable = millisecondsRemaining > 0 ? 'false' : 'true';
 
   return (
-    <SwitchTransition mode="out-in">
-      <CSSTransition
-        key={isAvailable}
-        nodeRef={nodeRef}
-        addEndListener={(done: () => void) => {
-          nodeRef.current?.addEventListener('transitionend', done, false);
-        }}
-        classNames={TRANSITION_NAME}
-      >
-        <FadeContainer ref={nodeRef}>
-          {isAvailable === 'true' ? (
-            <AvailableContainer>
-              <Price usdPrice={usdPrice} />
-              <CtaButton onClick={handleBuyButtonClick}>Buy Now</CtaButton>
-              {numMinted && <ProgressBar maxMintable={maxMintable} numMinted={numMinted} />}
-            </AvailableContainer>
-          ) : (
-            <DropTimer tokenSupply={maxMintable} timestamp={dropDate} />
-          )}
-          {isPaymentModalOpen && (
-            <PaymentModal
-              title="Checkout"
-              closeShareModal={closePaymentModal}
-              imageUrl={imageUrl}
-              collectionName={collectionName}
-              artistName={artistName}
-              artistProfilePhoto={artistProfilePhoto}
-              type={type}
-              usdPrice={usdPrice}
-            />
-          )}
-        </FadeContainer>
-      </CSSTransition>
-    </SwitchTransition>
+    <FadeOutInSwitchAnimation isAvailable={isAvailable}>
+      {isAvailable === 'true' ? (
+        <AvailableContainer>
+          <Price usdPrice={usdPrice} />
+          <CtaButton onClick={handleBuyButtonClick}>Buy Now</CtaButton>
+          {numMinted && <ProgressBar maxMintable={maxMintable} numMinted={numMinted} />}
+        </AvailableContainer>
+      ) : (
+        <DropTimer tokenSupply={maxMintable} timestamp={dropDate} />
+      )}
+      {isPaymentModalOpen && (
+        <PaymentModal
+          title="Checkout"
+          closeShareModal={closePaymentModal}
+          imageUrl={imageUrl}
+          collectionName={collectionName}
+          artistName={artistName}
+          artistProfilePhoto={artistProfilePhoto}
+          type={type}
+          usdPrice={usdPrice}
+        />
+      )}
+    </FadeOutInSwitchAnimation>
   );
 };
---
+++ workspaces/app/src/libraries/animation/FadeOutInSwitchAnimation.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import { FC, Key, useRef } from 'react';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+
+const TRANSITION_NAME = 'fade';
+
+const FadeContainer = styled.div`
+  &.${TRANSITION_NAME}-enter {
+    opacity: 0;
+  }
+  &.${TRANSITION_NAME}-enter-active, &.${TRANSITION_NAME}-exit {
+    opacity: 1;
+    transition: opacity 500ms ease-in;
+  }
+  &.${TRANSITION_NAME}-exit-active {
+    opacity: 0;
+    transition: opacity 750ms ease-in-out;
+    transition-delay: 100ms;
+  }
+`;
+
+interface FadeAnimationProp {
+  isAvailable: Key;
+}
+
+export const FadeOutInSwitchAnimation: FC<FadeAnimationProp> = ({ isAvailable, children }) => {
+  const nodeRef = useRef<HTMLDivElement>(null);
+  return (
+    <SwitchTransition mode="out-in">
+      <CSSTransition
+        key={isAvailable}
+        nodeRef={nodeRef}
+        addEndListener={(done: () => void) => {
+          nodeRef.current?.addEventListener('transitionend', done, false);
+        }}
+        classNames={TRANSITION_NAME}
+      >
+        <FadeContainer ref={nodeRef}>{children}</FadeContainer>
+      </CSSTransition>
+    </SwitchTransition>
+  );
+};
