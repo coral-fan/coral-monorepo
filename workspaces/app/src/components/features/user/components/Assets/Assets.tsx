566c60a56a094e121c6d0a381930fca623987e3d
maks-p <mpazuniak@gmail.com>
2022-05-03 11:29:58 -0400

    Add assets animation

--- workspaces/app/src/components/features/user/components/Assets/Assets.tsx
+++ workspaces/app/src/components/features/user/components/Assets/Assets.tsx
@@ -1,27 +1,26 @@
 import { getBadge } from 'components/ui/badges/utils';
 import { Asset } from 'libraries/models';
-import { ProfileItems } from 'components/ui';
+import { ConditionalSpinner, ProfileItems } from 'components/ui';
 import { ItemsPlaceholder, ProfileItemWrapper } from 'components/ui/profile';
 import { AssetCard } from '../AssetCard';
 
 interface AssetProps {
+  isLoading: boolean;
   assets: Asset[];
 }
 
-export const Assets = ({ assets }: AssetProps) => (
-  <ProfileItems>
-    {assets.length > 0 ? (
-      assets.map((asset) => {
-        const { collectionName, type, id } = asset;
-        const Badge = getBadge(type);
-        return (
-          <ProfileItemWrapper key={`${collectionName}-${id}`}>
-            <AssetCard title={collectionName} Badge={Badge} {...asset} isCard={true} />
-          </ProfileItemWrapper>
-        );
-      })
-    ) : (
-      <ItemsPlaceholder>Nothing collected yet.</ItemsPlaceholder>
-    )}
+export const Assets = ({ assets, isLoading }: AssetProps) => (
+  <ProfileItems isLoading={isLoading}>
+    {assets.length > 0
+      ? assets.map((asset) => {
+          const { collectionName, type, id } = asset;
+          const Badge = getBadge(type);
+          return (
+            <ProfileItemWrapper key={`${collectionName}-${id}`}>
+              <AssetCard title={collectionName} Badge={Badge} {...asset} isCard={true} />
+            </ProfileItemWrapper>
+          );
+        })
+      : !isLoading && <ItemsPlaceholder>Nothing collected yet.</ItemsPlaceholder>}
   </ProfileItems>
 );
