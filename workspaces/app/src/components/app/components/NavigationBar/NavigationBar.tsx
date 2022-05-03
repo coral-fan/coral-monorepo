34415e1d95e2bd9f72734af9afbe48811ded91b2
iknowhtml <iknowsmtp@gmail.com>
2022-05-03 10:23:17 -0400

    Update Navigation Bar for Sign Up Campaign - Left align logo & center align workmark - Add standalone workmark SVG - Remove logo & workmark SVG

--- workspaces/app/src/components/app/components/NavigationBar/NavigationBar.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/NavigationBar.tsx
@@ -7,14 +7,14 @@ import { useObservable } from 'libraries/utils';
 import { getUserProfile$ } from './observables';
 import tokens, { QUERY } from 'styles/tokens';
 import { CLIENT_ENVIRONMENT } from 'consts';
+import { getIconComponent } from 'components/ui/icons/utils';
+import wordmarkSVG from './wordmark.svg';
 
 const { mobile, desktop } = tokens.layout.padding;
 
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  /* TODO: post sign up campaign launch */
-  justify-content: ${CLIENT_ENVIRONMENT === 'production' ? 'center' : 'space-between'};
   width: 100%;
   position: sticky;
   top: 0;
@@ -24,6 +24,18 @@ const Container = styled.div`
   @media ${QUERY.LAPTOP} {
     padding: ${desktop.vertical} 0;
   }
+`;
+
+// TODO: remove components & conditional rendering logic post sign up campaign
+const WordmarkIcon = styled(getIconComponent('LogoWordmarkIcon', wordmarkSVG))`
+  --button-size: ${tokens.buttons.size.mobile};
+  @media ${QUERY.LAPTOP} {
+    --button-size: ${tokens.buttons.size.desktop};
+  }
+  height: var(--button-size);
+  width: 100%;
+  position: absolute;
+  z-index: -1;
 `;
 
 export type UserProfile = Pick<User, 'username' | 'profilePhoto'>;
@@ -42,7 +54,11 @@ export const NavigationBar = () => {
       <Container>
         <LogoHomeLink />
         {/* TODO: remove conditional logic post sign up campaign */}
-        {CLIENT_ENVIRONMENT !== 'production' && <HamburgerMenuButton onClick={openMenuModal} />}
+        {CLIENT_ENVIRONMENT === 'production' ? (
+          <WordmarkIcon />
+        ) : (
+          <HamburgerMenuButton onClick={openMenuModal} />
+        )}
       </Container>
     </>
   );
--- workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
+++ workspaces/app/src/components/app/components/NavigationBar/components/LogoHomeLink.tsx
@@ -1,11 +1,6 @@
 import { css } from '@emotion/react';
 import { LogoIcon, Link } from 'components/ui';
-import { getIconComponent } from 'components/ui/icons/utils';
-import { CLIENT_ENVIRONMENT } from 'consts';
 import tokens, { QUERY } from 'styles/tokens';
-import logoWordmarkSVG from './logo-wordmark.svg';
-
-const LogoWordmarkIcon = getIconComponent('LogoWordmarkIcon', logoWordmarkSVG);
 
 const logoHomeLinkStyle = css`
   --button-size: ${tokens.buttons.size.mobile};
@@ -14,14 +9,16 @@ const logoHomeLinkStyle = css`
     --button-size: ${tokens.buttons.size.desktop};
   }
 
-  width: var(--button-size);
   height: var(--button-size);
+  width: var(--button-size);
+
   display: flex;
   align-items: center;
   justify-content: center;
 `;
+
 export const LogoHomeLink = () => (
   <Link css={logoHomeLinkStyle} href="/">
-    {CLIENT_ENVIRONMENT === 'production' ? <LogoWordmarkIcon /> : <LogoIcon />}
+    <LogoIcon />
   </Link>
 );
