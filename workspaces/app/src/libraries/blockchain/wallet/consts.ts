e2ff892fecccbecd8115d5e22d861d78544af386
iknowhtml <iknowsmtp@gmail.com>
2022-05-03 20:05:58 -0400

    Remove Covalent constant

--- workspaces/app/src/libraries/blockchain/wallet/consts.ts
+++
@@ -1,7 +0,0 @@
-import { getEnvironmentVariableErrorMessage } from 'libraries/utils';
-
-if (!process.env.NEXT_PUBLIC_COVALENT_API_KEY) {
-  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_COVALENT_API_KEY'));
-}
-
-export const COVALENT_API_KEY = process.env.NEXT_PUBLIC_COVALENT_API_KEY;
