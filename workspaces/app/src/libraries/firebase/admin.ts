1edb7eb8df3ebda3bb9a1c37a897a08c1818b44a
iknowhtml <iknowsmtp@gmail.com>
2022-05-04 07:00:39 -0400

    Use only JSON for admin credentials

--- workspaces/app/src/libraries/firebase/admin.ts
+++ workspaces/app/src/libraries/firebase/admin.ts
@@ -1,26 +1,15 @@
-import { SERVER_ENVIRONMENT } from 'consts';
 import { getEnvironmentVariableErrorMessage } from 'libraries/utils/errors';
-
-const getCredential = (): string | Record<string, string> => {
-  if (SERVER_ENVIRONMENT === 'production') {
-    if (!process.env.FIREBASE_ADMIN_CREDENTIAL_JSON) {
-      throw Error(getEnvironmentVariableErrorMessage('FIREBASE_ADMIN_CREDENTIAL_JSON'));
-    }
-    return JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIAL_JSON);
-  } else {
-    if (!process.env.FIREBASE_ADMIN_CREDENTIAL_PATH) {
-      throw Error(getEnvironmentVariableErrorMessage('FIREBASE_ADMIN_CREDENTIAL_PATH'));
-    }
-    return process.env.FIREBASE_ADMIN_CREDENTIAL_PATH;
-  }
-};
 
 export const initializeFirebaseAdmin = async () => {
   const { getApps } = await import('firebase-admin/app');
   // checks if admin has been initialized already
   if (getApps().length < 1) {
+    if (process.env.FIREBASE_ADMIN_CREDENTIAL_JSON === undefined) {
+      throw getEnvironmentVariableErrorMessage('FIREBASE_ADMIN_CREDENTIAL_JSON');
+    }
+
     const { initializeApp, cert } = await import('firebase-admin/app');
-    const serviceAccount = getCredential();
+    const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIAL_JSON);
     initializeApp({
       credential: cert(serviceAccount),
     });
