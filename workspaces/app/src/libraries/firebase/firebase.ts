1f4f404bcb391df77f45e0dfcd6db576783fe13d
iknowhtml <iknowsmtp@gmail.com>
2022-05-04 06:59:59 -0400

    Use Firebase configuration from env variable

--- workspaces/app/src/libraries/firebase/firebase.ts
+++ workspaces/app/src/libraries/firebase/firebase.ts
@@ -1,4 +1,11 @@
 import { initializeApp, getApps } from '@firebase/app';
+import { getEnvironmentVariableErrorMessage } from 'libraries/utils';
+
+if (!process.env.NEXT_PUBLIC_FIREBASE_CONFIG) {
+  throw getEnvironmentVariableErrorMessage('NEXT_PUBLIC_FIREBASE_CONFIG');
+}
+
+const FIREBASE_CONFIG = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 export const initializeFirebaseApp = () => {
   // getApps() checks if any apps have been initialized and prevents re-initialization of Firebase SDK during hot reloads
@@ -7,15 +14,6 @@ export const initializeFirebaseApp = () => {
     initialize Firebase using Web version 9 (modular), so authorization is handled by library
     https://firebase.google.com/docs/firestore/quickstart#set_up_your_development_environment
     */
-    const firebaseConfig = {
-      apiKey: 'AIzaSyAjdS0N93SLZUkiYgdwxE_7sNjX0gXZc-E',
-      authDomain: 'coral-c373f.firebaseapp.com',
-      projectId: 'coral-c373f',
-      storageBucket: 'coral-c373f.appspot.com',
-      messagingSenderId: '805666237729',
-      appId: '1:805666237729:web:ddd1fb95078d202d15a260',
-      measurementId: 'G-91HP8670JW',
-    };
-    initializeApp(firebaseConfig);
+    initializeApp(FIREBASE_CONFIG);
   }
 };
