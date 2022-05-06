7236180c61b8ec904c4a4f6d427b498532a5fb50
iknowhtml <iknowsmtp@gmail.com>
2022-05-06 15:29:48 -0400

    Fix font loading issue

--- workspaces/app/src/styles/global.tsx
+++ workspaces/app/src/styles/global.tsx
@@ -4,34 +4,10 @@ import tokens, { globalTokens } from './tokens';
 export const globalStyles = css`
   @font-face {
     font-family: 'Kunst Grotesk';
-    font-style: 'normal';
-    font-weight: 400;
-    font-display: swap;
-    src: './fonts/FTKunstGrotesk-Regular.woff2' format('woff2');
-  }
-
-  @font-face {
-    font-family: 'Kunst Grotesk';
-    font-style: 'normal';
-    font-weight: 500;
-    font-display: swap;
-    src: './fonts/FTKunstGrotesk-Medium.woff2' format('woff2');
-  }
-
-  @font-face {
-    font-family: 'Kunst Grotesk';
-    font-style: 'normal';
-    font-weight: 700;
-    font-display: swap;
-    src: './fonts/FTKunstGrotesk-Bold.woff2' format('woff2');
-  }
-
-  @font-face {
-    font-family: 'Kunst Grotesk';
     font-style: 'italic';
     font-weight: 400;
     font-display: swap;
-    src: './fonts/FTKunstGrotesk-RegularItalic.woff2' format('woff2');
+    src: url('/fonts/FTKunstGrotesk-RegularItalic.woff2') format('woff2');
   }
 
   @font-face {
@@ -39,7 +15,7 @@ export const globalStyles = css`
     font-style: 'italic';
     font-weight: 500;
     font-display: swap;
-    src: './fonts/FTKunstGrotesk-MediumItalic.woff2' format('woff2');
+    src: url('/fonts/FTKunstGrotesk-MediumItalic.woff2') format('woff2');
   }
 
   @font-face {
@@ -47,7 +23,31 @@ export const globalStyles = css`
     font-style: 'italic';
     font-weight: 700;
     font-display: swap;
-    src: './fonts/FTKunstGrotesk-BoldItalic.woff2' format('woff2');
+    src: url('/fonts/FTKunstGrotesk-BoldItalic.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'normal';
+    font-weight: 400;
+    font-display: swap;
+    src: url('./fonts/FTKunstGrotesk-Regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'normal';
+    font-weight: 500;
+    font-display: swap;
+    src: url('/fonts/FTKunstGrotesk-Medium.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Kunst Grotesk';
+    font-style: 'normal';
+    font-weight: 700;
+    font-display: swap;
+    src: url('/fonts/FTKunstGrotesk-Bold.woff2') format('woff2');
   }
 
   * {
