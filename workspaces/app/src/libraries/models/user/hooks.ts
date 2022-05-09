1442d8e50cf0253f1094234a738d387e608bea45
maks-p <mpazuniak@gmail.com>
2022-05-09 12:15:26 -0400

    Add stripe customer id observable and hook

--- workspaces/app/src/libraries/models/user/hooks.ts
+++ workspaces/app/src/libraries/models/user/hooks.ts
@@ -1,6 +1,8 @@
 import { getAuth } from 'firebase/auth';
+import { getCoralAPIAxios } from 'libraries/utils';
 import { useObservable } from 'libraries/utils/hooks';
 import { useMemo } from 'react';
+import { getStripeCustomerId$, IncomingUserData } from '.';
 import { getUsernames$, getUserUid$ } from './observables';
 
 const getUserUid = () => getAuth().currentUser?.uid;
@@ -11,3 +13,15 @@ export const useUsernames = () => {
   const initialUsernames = useMemo(() => new Set<string>(), []);
   return useObservable(getUsernames$, initialUsernames);
 };
+
+const axios = getCoralAPIAxios();
+
+const upsertUser = (uid: string, incomingUserData: IncomingUserData) =>
+  axios.post('user', {
+    uid,
+    incomingUserData,
+  });
+
+export const useUpsertUser = () => upsertUser;
+
+export const useStripeCustomerId = () => useObservable(getStripeCustomerId$, null);
