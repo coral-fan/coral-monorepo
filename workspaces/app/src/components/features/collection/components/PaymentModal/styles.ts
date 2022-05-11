70dce3182e77789d24242d48febbda3dea13c43e
maks-p <mpazuniak@gmail.com>
2022-05-11 10:10:38 -0400

    Add card element styling

---
+++ workspaces/app/src/components/features/collection/components/PaymentModal/styles.ts
@@ -0,0 +1,14 @@
+export const cardElementOptions = {
+  style: {
+    base: {
+      fontSize: '12px',
+      color: '#F0F0F0',
+      '::placeholder': {
+        color: '#929292',
+      },
+    },
+    invalid: {
+      color: '#E13214',
+    },
+  },
+};
