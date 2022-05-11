6638bdbe739844b79dd667fa2a7183ef979121cd
maks-p <mpazuniak@gmail.com>
2022-05-11 08:21:13 -0400

    Payment utility functions

---
+++ workspaces/app/src/components/features/collection/components/PaymentModal/utils.ts
@@ -0,0 +1,44 @@
+import { NullableString } from 'libraries/models';
+import axios from 'axios';
+import { Stripe, StripeCardElement, StripeCardNumberElement } from '@stripe/stripe-js';
+
+interface PaymentIntentProps {
+  amount: number;
+  savePaymentInfo: boolean;
+  paymentMethodId: string;
+  collectionId: string;
+  stripeCustomerId?: NullableString;
+}
+
+export const createPaymentIntent = async ({
+  amount,
+  savePaymentInfo,
+  stripeCustomerId,
+  paymentMethodId,
+  collectionId,
+}: PaymentIntentProps) => {
+  const {
+    data: { clientSecret, customerId },
+  } = await axios.post('/api/payment/pay', {
+    amount: Math.ceil(amount * 100),
+    savePaymentInfo,
+    paymentMethodId,
+    stripeCustomerId,
+    collectionId,
+  });
+
+  return { clientSecret, customerId };
+};
+
+export const createPaymentMethod = async (cardElement: StripeCardElement, stripe: Stripe) => {
+  if (!stripe) {
+    throw Error('Stripe not loaded');
+  }
+
+  const { paymentMethod, error } = await stripe.createPaymentMethod({
+    type: 'card',
+    card: cardElement,
+  });
+
+  return { paymentMethod, paymentMethodError: error };
+};
