00dcdaf1dc15143fb04d5f8c91ab6ebf81b99560
maks-p <mpazuniak@gmail.com>
2022-05-11 15:03:03 -0400

    Rename stripe key

--- workspaces/app/src/libraries/stripe/consts.ts
+++ workspaces/app/src/libraries/stripe/consts.ts
@@ -1,7 +1,7 @@
 import { getEnvironmentVariableErrorMessage } from 'libraries/utils';
 
-if (!process.env.NEXT_PUBLIC_PUBLISHABLE_KEY) {
-  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_PUBLISHABLE_KEY'));
+if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+  throw Error(getEnvironmentVariableErrorMessage('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY'));
 }
 
-export const NEXT_PUBLIC_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_PUBLISHABLE_KEY;
+export const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
