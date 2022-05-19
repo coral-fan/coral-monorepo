fdf64a3365b08751aa93f8c958a23592f233b763
iknowhtml <iknowsmtp@gmail.com>
2022-05-19 12:37:52 -0400

    Rename hash field & reorder fields

--- workspaces/app/src/libraries/models/transaction/types.ts
+++ workspaces/app/src/libraries/models/transaction/types.ts
@@ -3,7 +3,7 @@ import { User } from '../user';
 type Status = 'pending' | 'completed' | 'rejected';
 
 export interface TransactionData {
-  transactionHash: string | null;
   userId: User['id'];
   status: Status;
+  hash: string | null;
 }
