ba96f3f3609fc2d4a6d4c52441a39350c7002dbe
iknowhtml <iknowsmtp@gmail.com>
2022-05-19 12:38:37 -0400

    Add logic to update price on every block

--- workspaces/app/src/libraries/blockchain/currency/observables.ts
+++ workspaces/app/src/libraries/blockchain/currency/observables.ts
@@ -1,8 +1,20 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { AVALANCHE, SERVER_ENVIRONMENT } from 'consts';
 import { ethers } from 'ethers';
-import { forkJoin, from, map } from 'rxjs';
-import { ChainLinkOracleAggregatorV3__factory } from '@coral/contracts';
+import {
+  distinctUntilChanged,
+  forkJoin,
+  from,
+  fromEvent,
+  map,
+  Observable,
+  startWith,
+  switchMap,
+} from 'rxjs';
+import {
+  ChainLinkOracleAggregatorV3__factory,
+  ChainLinkOracleAggregatorV3,
+} from '@coral/contracts';
 import { AVAX_USD_PAIR_ADDRESS } from './consts';
 
 const avalancheRpcProvider = new JsonRpcProvider(AVALANCHE.RPC_URL);
@@ -18,15 +30,23 @@ const currencyPairDictionary = {
 
 type CurrencyPair = keyof typeof currencyPairDictionary;
 
+const newBlock$ = fromEvent(avalancheRpcProvider, 'block') as Observable<number>;
+
+const getPriceFeedData$ = (priceFeed: ChainLinkOracleAggregatorV3) =>
+  forkJoin({
+    roundData: from(priceFeed.latestRoundData()),
+    decimals: from(priceFeed.decimals()),
+  });
+
 export const getCurrencyPairPrice$ = (pair: CurrencyPair) => {
   const address = currencyPairDictionary[pair];
   const priceFeed = ChainLinkOracleAggregatorV3__factory.connect(address, avalancheRpcProvider);
 
-  return forkJoin({
-    roundData: from(priceFeed.latestRoundData()),
-    decimals: from(priceFeed.decimals()),
-  }).pipe(
+  return newBlock$.pipe(
+    startWith(getPriceFeedData$(priceFeed)),
+    switchMap(() => getPriceFeedData$(priceFeed)),
     map(({ roundData, decimals }) => ethers.utils.formatUnits(roundData.answer, decimals)),
-    map((priceString) => parseFloat(priceString))
+    map((priceString) => parseFloat(priceString)),
+    distinctUntilChanged()
   );
 };
