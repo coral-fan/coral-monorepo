e7a890c994fa0c7eff5da8883eb0dd34f1c452e3
iknowhtml <iknowsmtp@gmail.com>
2022-05-19 13:32:49 -0400

    Fix variable name & add logic to set loading state

--- workspaces/app/src/libraries/blockchain/currency/hooks.ts
+++ workspaces/app/src/libraries/blockchain/currency/hooks.ts
@@ -1,19 +1,22 @@
 import { getCurrencyPairPrice$ } from './observables';
 import { useEffect, useState } from 'react';
-import { delay, mergeMapTo, timer } from 'rxjs';
+import { delay, tap } from 'rxjs';
 
 export const useAvaxUsdPrice = () => {
   const [exchangeRate, setExchangeRate] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const subject = getCurrencyPairPrice$('avax_usd')
-      .pipe(delay(1000))
+    const subscription = getCurrencyPairPrice$('avax_usd')
+      .pipe(
+        tap(() => setIsLoading(true)),
+        delay(1000)
+      )
       .subscribe((exchangeRate) => {
         setExchangeRate(exchangeRate);
         setIsLoading(false);
       });
-    return () => subject.unsubscribe();
+    return () => subscription.unsubscribe();
   }, []);
 
   return { exchangeRate, isLoading };
