a1c0b2168420a4b92c879ec2e660ada245a98b4c
maks-p <mpazuniak@gmail.com>
2022-05-21 16:57:57 -0400

    Add dependencies

---
+++ workspaces/contracts/assets/coral-test-v2/metadata/coral-test-v2-metadata.mjs
@@ -0,0 +1,7 @@
+export const metadata = {
+  name: 'Coral Test 0523',
+  description: 'Well, all information looks like noise until you break the code.',
+  properties: {
+    type: 'All Access',
+  },
+};
