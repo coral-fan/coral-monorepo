fe0e0695aef26ea9d55642d2a30b93eacdc8d7e9
iknowhtml <iknowsmtp@gmail.com>
2022-05-21 18:24:31 -0400

    Fix infinite re-render issue - isConnecting is modified inside use effect & was in dependency array

--- workspaces/app/src/components/app/components/Managers/ConnectorActivationManager.tsx
+++ workspaces/app/src/components/app/components/Managers/ConnectorActivationManager.tsx
@@ -1,28 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useWallet } from 'libraries/blockchain';
-import { useIdToken } from 'libraries/authentication';
+import { useIsAuthenticated } from 'libraries/authentication';
 
 export const ConnectorActivationManager = () => {
-  const { isActive, connector } = useWallet();
-
-  const idToken = useIdToken();
-
-  // TODO: put is connecting state inside of wallet hook?
-  // is connecting acts as a mutex lock to prevent multiple calls to connect eagerly before one resolves
-  const [isConnecting, setIsConnecting] = useState(false);
+  const { connector, isActive } = useWallet();
+  const isAuthenticated = useIsAuthenticated();
 
   useEffect(() => {
-    if (idToken && !isActive && !isConnecting && connector.connectEagerly) {
-      setIsConnecting(true);
-      const maybePromise = connector.connectEagerly();
-      // maybePromised needs to be checked for what type it is since it can return void OR Promise<void>
-      if (maybePromise instanceof Promise) {
-        maybePromise.then(() => setIsConnecting(false));
-      } else {
-        setIsConnecting(false);
-      }
+    if (isAuthenticated && !isActive && connector.connectEagerly) {
+      connector.connectEagerly();
     }
-  }, [idToken, isActive, isConnecting, connector]);
+  }, [isAuthenticated, isActive, connector]);
 
   return <></>;
 };
