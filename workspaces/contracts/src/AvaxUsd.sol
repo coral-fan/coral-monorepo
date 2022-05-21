55dfb7463323fd2e0f384b7a10a3ada8cc9253d7
maks-p <mpazuniak@gmail.com>
2022-05-21 16:55:58 -0400

    Update Avax logic, minting contract

---
+++ workspaces/contracts/src/AvaxUsd.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.14;
+// import 'hardhat/console.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+
+contract AvaxUsd {
+  AggregatorV3Interface internal _priceFeed;
+
+  constructor() {
+    // Avax-Usd on FUJI Testnet
+    _priceFeed = AggregatorV3Interface(0x5498BB86BC934c8D34FDA08E81D444153d0D06aD);
+  }
+
+  function _getLatestPrice() private view returns (int256) {
+    (, int256 price, , , ) = _priceFeed.latestRoundData();
+    return price;
+  }
+
+  function getAvaxPrice(uint256 tokenUsdPrice) internal view returns (uint256) {
+    // Convert USD price to same unit as Chainlink
+    uint256 usdTokenPriceWithDecimals = tokenUsdPrice * (10**8);
+
+    // Get current AvaxUsd price
+    uint256 avaxUsdPriceAtWithDecimals = uint256(_getLatestPrice());
+
+    // Avax Price in Wei
+    uint256 avaxTokenPrice = (usdTokenPriceWithDecimals * (10**18)) / avaxUsdPriceAtWithDecimals;
+
+    return avaxTokenPrice;
+  }
+}
--- workspaces/contracts/src/NFTCollectible.sol
+++ workspaces/contracts/src/NFTCollectible.sol
@@ -1,91 +1,81 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4;
+pragma solidity >=0.8.14;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import './AvaxUsd.sol';
 
-import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
-
-contract NFTCollectible is ERC721, Ownable {
+contract NFTCollectible is ERC721, Ownable, AvaxUsd {
   using Counters for Counters.Counter;
-
   Counters.Counter private _tokenIds;
 
-  uint8 public constant MAX_SUPPLY = 50;
+  uint256 public constant MAX_SUPPLY = 50;
+  uint256 public constant USD_PRICE_PER_TOKEN = 100;
+  uint256 public constant MAX_TOKENS_PER_WALLET = 10;
 
-  uint256 public usdPrice;
-  uint256 public platformFeeNumerator;
-  uint256 public totalUsdPrice;
+  string private _tokenURI =
+    'ipfs://bafyreibbhcuoijlbwmxbuq6neafvmodzbgqoxdday62cdmks6rek35yuna/metadata.json';
 
-  string public baseTokenURI;
+  mapping(address => bool) private _relayList;
 
-  AggregatorV3Interface internal priceFeed;
-
-  constructor(
-    string memory _baseURI,
-    uint256 _usdPrice,
-    uint256 _platformFeeNumerator
-  ) ERC721('Coral Test v1', 'CTV1') {
-    usdPrice = _usdPrice;
-    platformFeeNumerator = _platformFeeNumerator;
-    totalUsdPrice = usdPrice + ((usdPrice * (platformFeeNumerator * 10)) / 1000);
-
-    setBaseURI(_baseURI);
+  constructor() ERC721('Coral Test v1', 'CTV1') {
+    // Start token count at 1;
     _tokenIds.increment();
-
-    // Avax-Usd on FUJI Testnet
-    priceFeed = AggregatorV3Interface(0x5498BB86BC934c8D34FDA08E81D444153d0D06aD);
   }
 
-  function totalSupply() external view returns (uint256) {
+  function totalSupply() public view returns (uint256) {
+    // Subtract 1 since Token count starts at 1;
     return _tokenIds.current() - 1;
   }
 
-  // TODO: Set to Private
-  function getLatestPrice() public view returns (int256) {
-    (, int256 price, , , ) = priceFeed.latestRoundData();
-    return price;
-  }
-
-  function getAvaxTotalPrice() internal view returns (uint256) {
-    uint256 usdPriceAt108 = totalUsdPrice * (10**8);
-    uint256 avaxPriceAt108 = uint256(getLatestPrice());
-
-    uint256 avaxPrice = usdPriceAt108 / avaxPriceAt108;
-
-    return avaxPrice;
-  }
-
-  function getAvaxPriceInWei() public view returns (uint256) {
-    return getAvaxTotalPrice() * (10**18);
-  }
-
-  function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-    baseTokenURI = _baseTokenURI;
+  function setTokenURI(string memory _newTokenURI) external onlyOwner {
+    _tokenURI = _newTokenURI;
   }
 
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
     require(_exists(tokenId), 'ERC721Metadata: URI query for nonexistent token');
-    return baseTokenURI;
+    return _tokenURI;
   }
 
-  function mintNFT() public payable onlyOwner {
-    uint256 avaxPriceInWei = getAvaxTotalPrice() * (10**18);
-    require(msg.value >= avaxPriceInWei, 'Not enough ether to purchase');
+  function relayMint(address to) external {
+    require(_relayList[msg.sender] == true, 'Not on relay list');
 
     uint256 newTokenID = _tokenIds.current();
     require(newTokenID <= MAX_SUPPLY, 'Already Sold Out');
 
-    _safeMint(msg.sender, newTokenID);
+    // require(balanceOf(to) < MAX_TOKENS_PER_WALLET, 'Wallet already owns maximum amount');
+
+    _mint(to, newTokenID);
     _tokenIds.increment();
   }
 
-  function withdraw() public onlyOwner {
+  function publicMint() external payable {
+    uint256 avaxTokenPrice = getAvaxPrice(USD_PRICE_PER_TOKEN);
+    require(msg.value >= avaxTokenPrice, 'Not enough ether to purchase');
+
+    uint256 newTokenID = _tokenIds.current();
+    require(newTokenID <= MAX_SUPPLY, 'Already Sold Out');
+
+    // require(balanceOf(msg.sender) < MAX_TOKENS_PER_WALLET, 'Wallet already owns maximum amount');
+
+    _mint(msg.sender, newTokenID);
+    _tokenIds.increment();
+  }
+
+  function withdraw() external onlyOwner {
     uint256 balance = address(this).balance;
     require(balance > 0, 'Nothing to withdraw');
 
-    (bool success, ) = (msg.sender).call{ value: balance }('');
+    (bool success, ) = payable(msg.sender).call{ value: balance }('');
     require(success, 'Transfer failed');
+  }
+
+  function addRelayAddr(address _newRelayAddr) external onlyOwner {
+    _relayList[_newRelayAddr] = true;
+  }
+
+  function revokeRelayAddrPrivilege(address _relayAddr) external onlyOwner {
+    _relayList[_relayAddr] = false;
   }
 }
