708bcabf72d8366bc8d3e9e4141d2244372bcab1
maks-p <mpazuniak@gmail.com>
2022-05-24 14:22:23 -0400

    Add setter to AvaxUsd

--- workspaces/contracts/src/AvaxUsd.sol
+++ workspaces/contracts/src/AvaxUsd.sol
@@ -3,30 +3,35 @@
 pragma solidity >=0.8.14;
 // import 'hardhat/console.sol';
 import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract AvaxUsd {
-  AggregatorV3Interface internal _priceFeed;
+contract AvaxUsd is Ownable {
+  AggregatorV3Interface internal priceFeed;
 
   constructor() {
     // Avax-Usd on FUJI Testnet
-    _priceFeed = AggregatorV3Interface(0x5498BB86BC934c8D34FDA08E81D444153d0D06aD);
+    priceFeed = AggregatorV3Interface(0x5498BB86BC934c8D34FDA08E81D444153d0D06aD);
   }
 
   function _getLatestPrice() private view returns (int256) {
-    (, int256 price, , , ) = _priceFeed.latestRoundData();
+    (, int256 price, , , ) = priceFeed.latestRoundData();
     return price;
   }
 
-  function getAvaxPrice(uint256 tokenUsdPrice) internal view returns (uint256) {
+  function _getAvaxPrice(uint256 tokenUsdPrice) internal view returns (uint256) {
     // Convert USD price to same unit as Chainlink
     uint256 usdTokenPriceWithDecimals = tokenUsdPrice * (10**8);
 
     // Get current AvaxUsd price
-    uint256 avaxUsdPriceAtWithDecimals = uint256(_getLatestPrice());
+    uint256 avaxUsdPriceWithDecimals = uint256(_getLatestPrice());
 
     // Avax Price in Wei
-    uint256 avaxTokenPrice = (usdTokenPriceWithDecimals * (10**18)) / avaxUsdPriceAtWithDecimals;
+    uint256 avaxTokenPrice = (usdTokenPriceWithDecimals * (10**18)) / avaxUsdPriceWithDecimals;
 
     return avaxTokenPrice;
+  }
+
+  function setPriceFeedAddress(address _newAddress) public onlyOwner {
+    priceFeed = AggregatorV3Interface(_newAddress);
   }
 }
