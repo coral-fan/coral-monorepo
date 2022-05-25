734aea4dd926b1a6a58955b71122eb6a42f1b2d2
maks-p <maks@coral.fan>
2022-05-25 17:09:51 -0400

    Misc

--- workspaces/contracts/assets/coral-test-v2/metadata/coral-test-v2-metadata.mjs
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  name: 'Coral Test 0523',
-  description: 'Well, all information looks like noise until you break the code.',
-  properties: {
-    type: 'All Access',
-  },
-};
--- workspaces/contracts/assets/coral-test-v3/metadata/coral-test-v3-metadata.mjs
+++
@@ -1,6 +0,0 @@
-export const metadata = {
-  name: 'Coral Test 0524',
-  description:
-    '“Besides, interesting things happen along borders—transitions—not in the middle where everything is the same.”',
-  attributes: [{ type: 'All Access' }],
-};
