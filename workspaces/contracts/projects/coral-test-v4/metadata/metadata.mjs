d7da4ea95d8f75d95c7fcf2a1b30020f004fb0ae
maks-p <maks@coral.fan>
2022-05-26 08:33:36 -0400

    Update metadata template

--- workspaces/contracts/projects/coral-test-v4/metadata/metadata.mjs
+++ workspaces/contracts/projects/coral-test-v4/metadata/metadata.mjs
@@ -1,10 +1,7 @@
 import constructorArgs from '../config.json' assert { type: 'json' };
 
-console.log(constructorArgs);
-
 export const metadata = {
   name: constructorArgs.name,
-  description:
-    '"Computers rely on the one and the zero to represent all things. This distinction between something and nothing—this pivotal separation between being and nonbeing—is quite fundamental and underlies many Creation myths."',
-  attributes: [{ trait_type: 'Type', value: 'All Access', key: 'type' }],
+  description: 'Updated metadata test',
+  attributes: [{ trait_type: 'Type', value: 'All Access' }],
 };
--- workspaces/contracts/projects/template/metadata/metadata.mjs
+++ workspaces/contracts/projects/template/metadata/metadata.mjs
@@ -3,5 +3,5 @@ import constructorArgs from '../config.json' assert { type: 'json' };
 export const metadata = {
   name: constructorArgs.name,
   description: 'Description',
-  attributes: [{ trait_type: 'Type', value: 'Value', key: 'type' }],
+  attributes: [{ trait_type: 'Type', value: 'Value' }],
 };
