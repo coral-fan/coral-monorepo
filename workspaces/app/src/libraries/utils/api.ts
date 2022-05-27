9ff81223ce53c35eb05d73703715c5e689a607ef
iknowhtml <aki@coral.fan>
2022-05-27 15:06:58 -0400

    Add logic for exponential API request retries

--- workspaces/app/src/libraries/utils/api.ts
+++ workspaces/app/src/libraries/utils/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import { CORAL_API_ENDPOINT } from 'consts';
 
 const coralAPIAxios = axios.create({
   baseURL: CORAL_API_ENDPOINT,
 });
+
+axiosRetry(coralAPIAxios, { retryDelay: axiosRetry.exponentialDelay });
 
 export const getCoralAPIAxios = () => coralAPIAxios;
