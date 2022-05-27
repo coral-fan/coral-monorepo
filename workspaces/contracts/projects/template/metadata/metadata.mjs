8a0e680e7a02b14da6677e305153195744b15afb
maks-p <maks@coral.fan>
2022-05-27 19:19:38 -0400

    Delete template file

--- workspaces/contracts/projects/template/metadata/metadata.mjs
+++
@@ -1,7 +0,0 @@
-import constructorArgs from '../config.json' assert { type: 'json' };
-
-export const metadata = {
-  name: constructorArgs.name,
-  description: 'Description',
-  attributes: [{ trait_type: 'Type', value: 'Value' }],
-};
