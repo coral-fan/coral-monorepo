ba306d15368dad9e8a554cd92f512c2a746345ce
iknowhtml <aki@coral.fan>
2022-05-28 07:45:28 -0400

    Add Purchase API endpoint

---
+++ workspaces/app/pages/api/purchase.api.ts
@@ -0,0 +1,37 @@
+import { getCollectionReferenceServerSide } from 'libraries/firebase';
+import { PurchaseData } from 'libraries/models';
+import { ERROR_RESPONSE } from './consts';
+import { Handler } from './types';
+import { getHandler } from './utils';
+
+type DefaultPurchaseData = Omit<PurchaseData, 'userId' | 'collectionId' | 'metadata'>;
+
+const DEFAULT_PURCHASE_DATA: DefaultPurchaseData = {
+  status: 'pending',
+  transactionHash: null,
+  assetId: null,
+};
+
+const post: Handler = async (req, res) => {
+  try {
+    // TODO: add validation!!!
+    const { userId, collectionId, metadata } = req.body;
+    const purchaseCollectionRef = await getCollectionReferenceServerSide('purchases');
+
+    const purchaseData: PurchaseData = {
+      ...DEFAULT_PURCHASE_DATA,
+      userId,
+      collectionId,
+      metadata: metadata ?? null,
+    };
+
+    const { id } = await purchaseCollectionRef.add(purchaseData);
+
+    return res.status(200).json({ id });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json(ERROR_RESPONSE);
+  }
+};
+
+export default getHandler({ post });
