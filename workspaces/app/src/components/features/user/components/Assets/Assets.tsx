7f9f1eb18573f8c471a894a2755dc1ee5cbe4e2b
iknowhtml <aki@coral.fan>
2022-05-29 09:04:12 -0400

    Update Asset component - Remove unnecessary loading logic - Update component key derivation logic

--- workspaces/app/src/components/features/user/components/Assets/Assets.tsx
+++ workspaces/app/src/components/features/user/components/Assets/Assets.tsx
@@ -5,22 +5,23 @@ import { ItemsPlaceholder, ProfileItemWrapper } from 'components/ui/profile';
 import { AssetCard } from '../AssetCard';
 
 interface AssetProps {
-  isLoading: boolean;
   assets: Asset[];
 }
 
-export const Assets = ({ assets, isLoading }: AssetProps) => (
-  <ProfileItems isLoading={isLoading}>
-    {assets.length > 0
-      ? assets.map((asset) => {
-          const { collectionName, type, id } = asset;
-          const Badge = getBadge(type);
-          return (
-            <ProfileItemWrapper key={`${collectionName}-${id}`}>
-              <AssetCard title={collectionName} Badge={Badge} {...asset} isCard={true} />
-            </ProfileItemWrapper>
-          );
-        })
-      : !isLoading && <ItemsPlaceholder>Nothing collected yet.</ItemsPlaceholder>}
+export const Assets = ({ assets }: AssetProps) => (
+  <ProfileItems>
+    {assets.length > 0 ? (
+      assets.map((asset) => {
+        const { type, id, contractAddress, collectionName } = asset;
+        const Badge = getBadge(type);
+        return (
+          <ProfileItemWrapper key={`${contractAddress}-${id}`}>
+            <AssetCard title={collectionName} Badge={Badge} {...asset} isCard={true} />
+          </ProfileItemWrapper>
+        );
+      })
+    ) : (
+      <ItemsPlaceholder>Nothing collected yet.</ItemsPlaceholder>
+    )}
   </ProfileItems>
 );
