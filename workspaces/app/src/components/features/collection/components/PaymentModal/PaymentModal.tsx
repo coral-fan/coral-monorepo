c0da6d0580b68ee582248e8dff81b5da1cad73f6
Aki Gao <aki@coral.fan>
2022-05-30 13:45:56 -0400

    Remove setPurchaseId prop setPurchaseId doesn't exist AvaxPayment prop interface

--- workspaces/app/src/components/features/collection/components/PaymentModal/PaymentModal.tsx
+++ workspaces/app/src/components/features/collection/components/PaymentModal/PaymentModal.tsx
@@ -170,7 +170,6 @@ export const PaymentModal = ({
                 <AvaxPayment
                   total={total}
                   handleSwitchPaymentClick={handleSwitchPaymentMethodClick}
-                  setPurchaseId={setPurchaseId}
                 />
               ) : shouldUseExistingCard && stripeCustomerId ? (
                 <ExistingCardPayment
