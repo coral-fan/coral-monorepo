1a1ec7a25ef55f463cb604a8f7cd1dd81d4230ee
Aki Gao <aki@coral.fan>
2022-06-01 13:01:22 -0400

    Function overload sort collection uility function

--- workspaces/app/src/components/ui/profile/Profile/utils.ts
+++ workspaces/app/src/components/ui/profile/Profile/utils.ts
@@ -1,5 +1,13 @@
 import { DocumentDataWithId } from 'libraries/firebase';
-import { CollectionData } from 'libraries/models';
+import { Collection, CollectionData } from 'libraries/models';
 
-export const sortCollectionByDropDateDesc = (arr: DocumentDataWithId<CollectionData>[]) =>
-  arr.sort((a, b) => new Date(a.dropDate).getTime() - new Date(b.dropDate).getTime());
+// TODO: hacked this together to fix typing errors using function overloading. revisit to see if this is the best approach
+export function sortCollectionByDropDateDesc(arr: Collection[]): typeof arr;
+
+export function sortCollectionByDropDateDesc(arr: DocumentDataWithId<CollectionData>[]): typeof arr;
+
+export function sortCollectionByDropDateDesc(
+  arr: Collection[] | DocumentDataWithId<CollectionData>[]
+) {
+  return arr.sort((a, b) => new Date(a.dropDate).getTime() - new Date(b.dropDate).getTime());
+}
