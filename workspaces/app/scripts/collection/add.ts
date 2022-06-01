a9b81e2bf7c2f1bac20de974f6029e8cffbf1c8f
maks-p <maks@coral.fan>
2022-06-01 17:18:47 -0400

    Update add collection script

--- workspaces/app/scripts/collection/add.ts
+++ workspaces/app/scripts/collection/add.ts
@@ -1,45 +1,113 @@
-import { getDocumentReferenceServerSide } from 'libraries/firebase';
-import { ArtistData, CollectionData } from 'libraries/models';
+import { getDocumentReferenceServerSide, initializeFirebaseAdmin } from 'libraries/firebase';
+import { ArtistData, CollectionData, CollectionType, GatedEvent } from 'libraries/models';
+import { getStorage } from 'firebase-admin/storage';
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import { readFile } from 'fs/promises';
 
-const id = '0xf6a817e0af31d1f8accf9e301c2a9bb08962c160';
-const collection: CollectionData = {
-  name: 'Test NFT',
-  artistId: '0xabcdefghijklmnopqrstuvwxyz01234567891011',
-  imageUrl:
-    'https://firebasestorage.googleapis.com/v0/b/coral-c373f.appspot.com/o/collections%2F0xf6a817e0af31d1f8accf9e301c2a9bb08962c160%2Fimage.png?alt=media&token=c08b4275-06bc-41a9-bbe7-69d907239c05',
-  maxMintable: 10,
-  type: 'event',
-  price: 0.01,
-  dropDate: '2022-04-11',
-  description: 'Test NFT Edition v1',
-  details: ['This is a test NFT.'],
-  gatedContent: {
-    type: 'event',
-    id: '1',
-  },
+/*
+Utility Functions
+*/
+// TODO: Create shared library for file / path functions for app and contract workspaces
+const parseCollectionName = (collectionName: string) =>
+  collectionName
+    .replace(/^\.*\/|\/?[^\/]+\.[a-z]+|\/$/g, '')
+    .replaceAll(' ', '-')
+    .toLowerCase();
+
+const getImagePath = (projectDir: string) => {
+  const __dirname = path.resolve();
+  return path.resolve(__dirname, '..', 'contracts', 'projects', projectDir, 'image', 'image.png');
 };
 
-const addCollection = async () => {
-  console.log('adding collection...');
+const getConfigFilePath = (projectDir: string) => {
+  const __dirname = path.resolve();
+  return path.resolve(__dirname, '..', 'contracts', 'projects', projectDir, 'config.json');
+};
 
-  const { artistId } = collection;
+/*
+Script pulls collection name in from arguments
+*/
+const collectionName = process.argv[2];
+
+const addCollection = async (collectionName: string) => {
+  console.log('Adding collection...');
+
+  // Get config file
+  const projectDir = parseCollectionName(collectionName);
+  const configFilePath = getConfigFilePath(projectDir);
+  const projectData = await readFile(configFilePath, 'utf8');
+  const configFile = JSON.parse(projectData);
+
+  const { contract, collectionData } = configFile;
+  const { name, address, description, maxSupply, usdPricePerToken } = contract;
+  const { artistId, type, dropDate, details, gatedContent } = collectionData;
+
   const artistRef = await getDocumentReferenceServerSide<ArtistData>('artists', artistId);
+
   const artistDocSnapshot = await artistRef.get();
 
   if (!artistDocSnapshot.exists) {
     throw Error(`artist with id ${artistId} doesn't exist.`);
   }
 
-  const collectionRef = await getDocumentReferenceServerSide<CollectionData>('collections', id);
+  /*
+  Handle Image
+  */
+  await initializeFirebaseAdmin();
+  const { getApp } = await import('firebase-admin/app');
+  const app = getApp();
+  const imagePath = getImagePath(projectDir);
+  const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+
+  const uuid = uuidv4();
+
+  const uploadResponse = await getStorage(app)
+    .bucket(storageBucket)
+    .upload(imagePath, {
+      destination: `collections/${address}/image.png`,
+      gzip: true,
+      contentType: 'image/png',
+      metadata: {
+        contentType: 'image/png',
+        metadata: {
+          firebaseStorageDownloadTokens: uuid,
+        },
+      },
+    });
+
+  const fileName = uploadResponse[0].name;
+  const fileUrl = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${encodeURIComponent(
+    fileName
+  )}?alt=media&token=${uuid}`;
+
+  const collectionRef = await getDocumentReferenceServerSide<CollectionData>(
+    'collections',
+    address
+  );
+
   const collectionDocSnapshot = await collectionRef.get();
 
   if (collectionDocSnapshot.exists) {
-    throw Error(`collection with id ${id} already exists.`);
+    throw Error(`collection with id ${address} already exists.`);
   }
+
+  const collection: CollectionData = {
+    name: name,
+    artistId: artistId,
+    imageUrl: fileUrl,
+    maxMintable: maxSupply,
+    type: type as CollectionType,
+    price: usdPricePerToken,
+    dropDate: dropDate,
+    description: description,
+    details: details,
+    gatedContent: gatedContent as GatedEvent,
+  };
 
   await collectionRef.set(collection);
 
-  console.log('collection added!');
+  console.log('Collection added!');
 };
 
-addCollection();
+addCollection(collectionName);
