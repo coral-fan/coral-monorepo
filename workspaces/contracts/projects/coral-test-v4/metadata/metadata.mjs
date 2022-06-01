ac17ebe557ec682acfe3df8d235feb504db04d89
maks-p <maks@coral.fan>
2022-06-01 17:33:03 -0400

    Misc project files

--- workspaces/contracts/projects/coral-test-v4/metadata/metadata.mjs
+++
@@ -1,7 +0,0 @@
-import constructorArgs from '../config.json' assert { type: 'json' };
-
-export const metadata = {
-  name: constructorArgs.name,
-  description: 'Updated metadata test',
-  attributes: [{ trait_type: 'Type', value: 'All Access' }],
-};
