3f205eb1bb856eec5d11b709269fdbf1ef3d1a71
Aki Gao <aki@coral.fan>
2022-06-01 13:01:00 -0400

    Update todo comment to match convention

--- workspaces/app/src/components/features/artist/components/Collections/Collections.tsx
+++ workspaces/app/src/components/features/artist/components/Collections/Collections.tsx
@@ -22,7 +22,7 @@ export const Collections = ({ collections }: CollectionProps) => (
               Badge={Badge}
               dropDateTimestamp={dropDate}
               {...collection}
-              //To Do: Remove description prop after data fetching added
+              //TODO: Remove description prop after data fetching added
               description={''}
               isCard
             />
