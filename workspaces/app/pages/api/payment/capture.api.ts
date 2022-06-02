0e5e865406ce353c86b6faffaac2f711021f9c75
Aki Gao <aki@coral.fan>
2022-06-02 14:25:45 -0400

    Update error handling for consistency

--- workspaces/app/pages/api/payment/capture.api.ts
+++ workspaces/app/pages/api/payment/capture.api.ts
@@ -1,6 +1,7 @@
 import { getEnvironmentVariableErrorMessage } from 'libraries/utils/errors';
 import Stripe from 'stripe';
 import { object, string } from 'yup';
+import { ERROR_RESPONSE } from '../consts';
 import { Handler } from '../types';
 import { getHandler, getStripe } from '../utils';
 
@@ -15,25 +16,10 @@ const post: Handler = async (req, res) => {
     const { paymentIntentId } = await captureAPISchema.validate(req.body);
     const paymentIntent = await stripe.paymentIntents.capture(paymentIntentId);
 
-    console.log(paymentIntent);
-
     res.status(200).send(`PaymentIntent status: ${paymentIntent.status}`);
-
-    // TODO: Type error properly
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
-    switch (e.type) {
-      case 'StripeCardError':
-        console.log(`A payment error occurred: ${e.message}`);
-        break;
-      case 'StripeInvalidRequestError':
-        console.log(`An invalid request occurred: ${e.message}`);
-        break;
-      default:
-        console.log(`Another problem occurred, maybe unrelated to Stripe: ${e.message}.`);
-        break;
-    }
-    res.status(500).json({ statusCode: 500, message: e.message });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json(ERROR_RESPONSE);
   }
 };
 
