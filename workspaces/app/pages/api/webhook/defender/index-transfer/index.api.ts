48a4c7c3ac7f858ef283f4871857347ce5280433
Aki Gao <aki@coral.fan>
2022-06-02 12:12:59 -0400

    Update Transfer Indexing Defender webhook - Add console log statement for testing purposes - Update logic to derive NFT smart contract address from webhook data

--- workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
+++ workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
@@ -11,6 +11,7 @@ import { number, object, string } from 'yup';
 import { Handler } from '../../../types';
 import { getHandler } from '../../../utils';
 import { addNFTOwnership, removeNFTOwnership } from './utils';
+import { getNftSmartContractAddress } from '../utils';
 
 const transferParamsSchema = object({
   from: string().required(),
@@ -25,13 +26,14 @@ const isNotZeroAddress = (address: string) => address !== ethers.constants.Addre
 export const post: Handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { events } = req.body as SentinelConditionRequest;
   if (events) {
+    // TODO: remove!
+    console.log(JSON.stringify(events));
     for (const sentinelTriggerEvent of events) {
       if (sentinelTriggerEvent.type === SubscriberType.BLOCK) {
         const {
           matchReasons,
-          sentinel: {
-            addresses: [contractAddress],
-          },
+          matchedAddresses,
+          sentinel: { addresses },
         } = sentinelTriggerEvent as BlockTriggerEvent;
         for (const reason of matchReasons) {
           if (reason.type === 'event') {
@@ -39,11 +41,12 @@ export const post: Handler = async (req: NextApiRequest, res: NextApiResponse) =
             if (signature === 'Transfer(address,address,uint256)') {
               try {
                 const { from, to, tokenId } = await transferParamsSchema.validate(params);
+                const nftContractAddress = getNftSmartContractAddress(addresses, matchedAddresses);
                 if (isNotZeroAddress(from)) {
-                  await removeNFTOwnership(from, contractAddress, tokenId);
+                  await removeNFTOwnership(from, nftContractAddress, tokenId);
                 }
                 if (isNotZeroAddress(to)) {
-                  await addNFTOwnership(to, contractAddress, tokenId);
+                  await addNFTOwnership(to, nftContractAddress, tokenId);
                 }
               } catch (e) {
                 console.error(e);
