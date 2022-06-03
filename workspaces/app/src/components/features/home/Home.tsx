3be1fc0ea141d8849bfe682b991a3af88ae7e305
Aki Gao <aki@coral.fan>
2022-06-03 17:42:11 -0400

    Add sample code for Cristi

--- workspaces/app/src/components/features/home/Home.tsx
+++ workspaces/app/src/components/features/home/Home.tsx
@@ -1,6 +1,25 @@
-import { useIsAuthenticated } from 'libraries/authentication';
+import { useSignInModalState } from 'components/app';
 
 export const Home = () => {
-  const isAuthenticated = useIsAuthenticated();
-  return <div>You are {`${isAuthenticated ? 'authenticated' : 'not authenticated'}.`}</div>;
+  const { openModal } = useSignInModalState();
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Sample HTML Using Tailwind</h1>
+      {/* Example Link */}
+      <a
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        href="#"
+      >
+        Link
+      </a>
+      <br />
+      {/* Example Button */}
+      <button
+        onClick={openModal}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Sign Up
+      </button>
+    </div>
+  );
 };
