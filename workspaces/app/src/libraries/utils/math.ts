f0534b7b73f52bc123668434ab2142453d26ca9f
maks-p <maks@coral.fan>
2022-06-05 12:49:05 -0400

    Add safeRoundUp logic, remove closePaymentModal

--- workspaces/app/src/libraries/utils/math.ts
+++ workspaces/app/src/libraries/utils/math.ts
@@ -8,3 +8,14 @@ export const safeRound = (value: number, decimalPlace: number) => {
 
   return roundedValue;
 };
+
+export const safeRoundUp = (value: number, decimalPlace: number) => {
+  if (!Number.isInteger(decimalPlace)) {
+    throw Error(`Number ${decimalPlace} is not a integer.`);
+  }
+
+  const decimalFactor = 10 ** decimalPlace;
+  const roundedValue = Math.ceil((value + Number.EPSILON) * decimalFactor) / decimalFactor;
+
+  return roundedValue;
+};
