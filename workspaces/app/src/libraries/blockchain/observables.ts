611d4836c79203f2fec445fc98a723bb8cf980e7
maks-p <maks@coral.fan>
2022-06-06 18:10:39 -0400

    Create observables that update on new blocks

--- workspaces/app/src/libraries/blockchain/observables.ts
+++ workspaces/app/src/libraries/blockchain/observables.ts
@@ -1,7 +1,28 @@
+import { CoralNftV1__factory } from '@coral/contracts/dist';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { AVALANCHE } from 'consts';
-import { fromEvent, Observable } from 'rxjs';
+import { fromEvent, startWith, Observable, switchMap, map } from 'rxjs';
 
 const avalancheRpcProvider = new JsonRpcProvider(AVALANCHE.RPC_URL);
 
 export const newBlock$ = fromEvent(avalancheRpcProvider, 'block') as Observable<number>;
+
+export const getUserTokenBalance$ = (collectionAddress: string, userAddress: string) => {
+  const nftContract = CoralNftV1__factory.connect(collectionAddress, avalancheRpcProvider);
+
+  return newBlock$.pipe(
+    startWith(nftContract.balanceOf(userAddress)),
+    switchMap(() => nftContract.balanceOf(userAddress)),
+    map((tokenBalance) => tokenBalance.toNumber())
+  );
+};
+
+export const getTokenTotalSupply$ = (address: string) => {
+  const nftContract = CoralNftV1__factory.connect(address, avalancheRpcProvider);
+
+  return newBlock$.pipe(
+    startWith(nftContract.totalSupply()),
+    switchMap(() => nftContract.totalSupply()),
+    map((tokenBalance) => tokenBalance.toNumber())
+  );
+};
