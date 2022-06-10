1e518d794da11f34d2249d50ce58268550ea6fb3
maks-p <maks@coral.fan>
2022-06-09 23:06:54 -0400

    Add comments to getDeploymentConsts

--- workspaces/contracts/tasks/utils/getDeploymentConsts.ts
+++ workspaces/contracts/tasks/utils/getDeploymentConsts.ts
@@ -1,14 +1,36 @@
 import { config } from 'dotenv';
 config();
 
+/*
+API Keys for the Deployer Relay. This Relay deploys our NFT contracts and is the owner
+of the contract until ownership is transferred to the Multi-Sig.
+*/
 const DEPLOYER_RELAY_API_KEY_FUJI = process.env.DEPLOYER_RELAY_API_KEY_FUJI;
 const DEPLOYER_RELAY_SECRET_KEY_FUJI = process.env.DEPLOYER_RELAY_SECRET_KEY_FUJI;
+
+//TODO: Create Deployer Relays on Mainnet and add API keys to .env
 const DEPLOYER_RELAY_API_KEY_MAINNET = process.env.DEPLOYER_RELAY_API_KEY_MAINNET;
 const DEPLOYER_RELAY_SECRET_KEY_MAINNET = process.env.DEPLOYER_RELAY_SECRET_KEY_MAINNET;
+
+/*
+OpenZeppelin Defender Team API keys manage the Sentinels. We use them to add new contract
+addresses to the Sentinels listening to mint and transfer events.
+*/
 const DEFENDER_TEAM_API_KEY = process.env.DEFENDER_TEAM_API_KEY;
 const DEFENDER_TEAM_SECRET_KEY = process.env.DEFENDER_TEAM_SECRET_KEY;
+
 const CONTRACT_NAME = process.env.CONTRACT_NAME;
+
+/*
+Images and metadata are uploaded via an nft.storage API.
+*/
 const NFT_STORAGE_KEY = process.env.NFT_STORAGE_API_KEY;
+
+/*
+The Payment Relay Address manages credit card mints. This Relay signs the relayMint
+transaction called by our frontend. Only this address can call relayMint. The deploy script 
+adds this address to the list of allowed relay mint addresses that can make this call.
+*/
 const PAYMENT_RELAY_ADDRESSES_FUJI = process.env.PAYMENT_RELAY_ADDRESSES_FUJI;
 const PAYMENT_RELAY_ADDRESSES_MAINNET = process.env.PAYMENT_RELAY_ADDRESSES_MAINNET;
 
@@ -37,6 +59,10 @@ interface DeploymentConstNetworkDependentObject {
   sentinelIds: SentinelObject;
 }
 
+/*
+Sentinels listen to relayMint and transfer events, sending events to our webhooks
+that manage indexing and payment captures.
+*/
 const SENTINEL_IDS_FUJI: SentinelObject = {
   transferInPerson: '7b0b2398-31d7-4c93-a560-efb85e541ce4',
   relayMint: 'b89eefbf-5f44-466c-9c7f-fe9251db0f6a',
@@ -49,11 +75,14 @@ const SENTINEL_IDS_MAINNET: SentinelObject = {
   transfer: 'tbd',
 };
 
-export const getConsts = (network: Network) => {
+export const getDeploymentConsts = (network: Network) => {
   if (!CONTRACT_NAME || !NFT_STORAGE_KEY || !DEFENDER_TEAM_API_KEY || !DEFENDER_TEAM_SECRET_KEY) {
     throw MISSING_ENV_VARIABLE;
   }
 
+  /*
+  These environment variables are the same regardless of network.
+  */
   const baseObject: DeploymentConstBaseObject = {
     contractName: CONTRACT_NAME,
     nftStorageKey: NFT_STORAGE_KEY,
