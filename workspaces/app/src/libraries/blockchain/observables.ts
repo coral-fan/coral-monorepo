eae64ce9cf4a57df4703e53cf055f28c9b5b694e
Aki Gao <aki@coral.fan>
2022-06-12 12:00:46 -0400

    Update get token total supply observable util Move query logic to separate util function

--- workspaces/app/src/libraries/blockchain/observables.ts
+++ workspaces/app/src/libraries/blockchain/observables.ts
@@ -1,13 +1,29 @@
-import { CoralNftV1__factory } from '@coral/contracts/dist';
+import { CoralNftV1__factory } from '@coral/contracts';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { AVALANCHE } from 'consts';
-import { fromEvent, startWith, Observable, switchMap, map, retry } from 'rxjs';
+import {
+  fromEvent,
+  startWith,
+  Observable,
+  switchMap,
+  map,
+  retry,
+  distinctUntilChanged,
+} from 'rxjs';
+import { getTokenOwner, getTokenTotalSupply } from './utils';
 
 const avalancheRpcProvider = new JsonRpcProvider(AVALANCHE.RPC_URL);
 
 export const newBlock$ = (fromEvent(avalancheRpcProvider, 'block') as Observable<number>).pipe(
   retry()
 );
+
+export const getTokenOwner$ = (collectionAddress: string, assetId: number) =>
+  newBlock$.pipe(
+    startWith(getTokenOwner(collectionAddress, assetId)),
+    switchMap(() => getTokenOwner(collectionAddress, assetId)),
+    retry()
+  );
 
 export const getUserTokenBalance$ = (collectionAddress: string, userAddress: string) => {
   const nftContract = CoralNftV1__factory.connect(collectionAddress, avalancheRpcProvider);
@@ -21,12 +37,8 @@ export const getUserTokenBalance$ = (collectionAddress: string, userAddress: str
 };
 
 export const getTokenTotalSupply$ = (address: string) => {
-  const nftContract = CoralNftV1__factory.connect(address, avalancheRpcProvider);
-
   return newBlock$.pipe(
-    startWith(nftContract.totalSupply()),
-    switchMap(() => nftContract.totalSupply()),
-    map((tokenBalance) => tokenBalance.toNumber()),
+    switchMap(() => getTokenTotalSupply(address)),
     retry()
   );
 };
