8f86e409ef97d91d4bd9594337e4257caa9396e2
maks-p <maks@coral.fan>
2022-06-14 18:27:35 -0400

    Update naming, use parseLogs

--- workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
+++ workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
@@ -10,18 +10,18 @@ import { CoralNftV1__factory } from '@coral/contracts/dist';
 const isNotZeroAddress = (address: string) => address !== ethers.constants.AddressZero;
 
 export const post: Handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const iface = new ethers.utils.Interface(CoralNftV1__factory.abi);
-
   const { events } = req.body as SentinelConditionRequest;
   if (events) {
     for (const sentinelTriggerEvent of events) {
       if (sentinelTriggerEvent.type === SubscriberType.BLOCK) {
+        const contractInterface = new ethers.utils.Interface(CoralNftV1__factory.abi);
         try {
           const logs = sentinelTriggerEvent.transaction.logs;
           const nftContractAddress = ethers.utils.getAddress(sentinelTriggerEvent.transaction.to);
           for (const log of logs) {
-            if (iface.parseLog(log).name === 'Transfer') {
-              const { from, to, tokenId } = iface.decodeEventLog('Transfer', log.data, log.topics);
+            const { name, args: result } = contractInterface.parseLog(log);
+            if (name === 'Transfer') {
+              const { from, to, tokenId } = result;
               const tokenIdInt = tokenId.toNumber();
               if (isNotZeroAddress(from)) {
                 await removeNFTOwnership(from, nftContractAddress, tokenIdInt);
