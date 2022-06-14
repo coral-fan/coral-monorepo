14c9f0d415a8a4dc60c2330efc4419ba6a289334
maks-p <maks@coral.fan>
2022-06-14 17:47:51 -0400

    Update indexer to parse logs

--- workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
+++ workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
@@ -1,56 +1,38 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import {
-  BlockTriggerEvent,
-  EventConditionSummary,
-  SentinelConditionRequest,
-  SubscriberType,
-} from 'defender-autotask-utils';
+import { SentinelConditionRequest, SubscriberType } from 'defender-autotask-utils';
 import { ethers } from 'ethers';
-import { number, object, string } from 'yup';
 
 import { Handler } from '../../../types';
 import { getHandler } from '../../../utils';
 import { addNFTOwnership, removeNFTOwnership } from './utils';
-import { getNftSmartContractAddress } from '../utils';
-
-const transferParamsSchema = object({
-  from: string().required(),
-  to: string().required(),
-  tokenId: number()
-    .required()
-    .transform((tokenId) => parseInt(tokenId)),
-});
+import { CoralNftV1__factory } from '@coral/contracts/dist';
 
 const isNotZeroAddress = (address: string) => address !== ethers.constants.AddressZero;
 
 export const post: Handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const iface = new ethers.utils.Interface(CoralNftV1__factory.abi);
+
   const { events } = req.body as SentinelConditionRequest;
   if (events) {
     for (const sentinelTriggerEvent of events) {
       if (sentinelTriggerEvent.type === SubscriberType.BLOCK) {
-        const {
-          matchReasons,
-          matchedAddresses,
-          sentinel: { addresses },
-        } = sentinelTriggerEvent as BlockTriggerEvent;
-        for (const reason of matchReasons) {
-          if (reason.type === 'event') {
-            const { params, signature } = reason as EventConditionSummary;
-            if (signature === 'Transfer(address,address,uint256)') {
-              try {
-                const { from, to, tokenId } = await transferParamsSchema.validate(params);
-                const nftContractAddress = getNftSmartContractAddress(addresses, matchedAddresses);
-                if (isNotZeroAddress(from)) {
-                  await removeNFTOwnership(from, nftContractAddress, tokenId);
-                }
-                if (isNotZeroAddress(to)) {
-                  await addNFTOwnership(to, nftContractAddress, tokenId);
-                }
-              } catch (e) {
-                console.error(e);
+        try {
+          const logs = sentinelTriggerEvent.transaction.logs;
+          const nftContractAddress = ethers.utils.getAddress(sentinelTriggerEvent.transaction.to);
+          for (const log of logs) {
+            if (iface.parseLog(log).name === 'Transfer') {
+              const { from, to, tokenId } = iface.decodeEventLog('Transfer', log.data, log.topics);
+              const tokenIdInt = tokenId.toNumber();
+              if (isNotZeroAddress(from)) {
+                await removeNFTOwnership(from, nftContractAddress, tokenIdInt);
+              }
+              if (isNotZeroAddress(to)) {
+                await addNFTOwnership(to, nftContractAddress, tokenIdInt);
               }
             }
           }
+        } catch (e) {
+          console.error(e);
         }
       }
     }
