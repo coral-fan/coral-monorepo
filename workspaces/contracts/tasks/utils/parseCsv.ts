f13900ac2a056769b29d2334228972adc3375acd
maks-p <maks@coral.fan>
2022-06-14 19:07:37 -0400

    Create parseCSV utility function

---
+++ workspaces/contracts/tasks/utils/parseCsv.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
+export const parseCsv = (file: string) => {
+  return new Promise((resolve, reject) => {
+    let readStream = fs.createReadStream(file);
+
+    let fileRows: string[][] = [];
+    const parser = parse({
+      delimiter: ',',
+    });
+
+    parser.on('readable', () => {
+      let record;
+      while ((record = parser.read())) {
+        if (record) {
+          fileRows.push(record[1].trim());
+        }
+      }
+    });
+
+    parser.on('error', (err) => {
+      console.error(err.message);
+    });
+
+    parser.on('end', () => {
+      const { lines } = parser.info;
+      console.log(`${lines} addresses processed`);
+      resolve(fileRows);
+    });
+
+    readStream.on('open', () => {
+      readStream.pipe(parser);
+    });
+
+    readStream.on('error', (err) => {
+      resolve({ status: null, error: 'readStream error' + err });
+    });
+  });
+};
