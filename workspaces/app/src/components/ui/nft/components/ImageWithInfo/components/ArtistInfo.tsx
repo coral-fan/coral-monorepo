09baee301bb1303262cc79bb377bda762084b98a
maks-p <maks@coral.fan>
2022-06-15 11:26:19 -0400

    Make artist info smaller on mobile

--- workspaces/app/src/components/ui/nft/components/ImageWithInfo/components/ArtistInfo.tsx
+++ workspaces/app/src/components/ui/nft/components/ImageWithInfo/components/ArtistInfo.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { Avatar } from 'components/ui';
-import tokens from 'styles/tokens';
+import tokens, { QUERY } from 'styles/tokens';
 import { Photo } from 'libraries/models';
 import { ComponentPropsWithRef, forwardRef } from 'react';
 
@@ -10,17 +10,28 @@ const Container = styled.div`
   background: rgba(0, 0, 0, 0.5);
   border-radius: ${tokens.border.radius.md};
   align-items: center;
-  padding: 3px 12px 3px 3px;
-  gap: 8px;
+  padding: 3px 6px 3px 4px;
+  gap: 4px;
+
+  @media ${QUERY.TABLET} {
+    gap: 8px;
+    padding: 3px 9px 3px 6px;
+  }
 `;
 
 const Info = styled.div`
-  font-size: ${tokens.font.size.sm};
-  line-height: ${tokens.font.line_height.sm};
-  letter-spacing: ${tokens.font.letter_spacing.sm};
+  font-size: ${tokens.font.size.xs};
+  line-height: ${tokens.font.line_height.xs};
+  letter-spacing: ${tokens.font.letter_spacing.xs};
   color: ${tokens.font.color.primary};
   font-weight: ${tokens.font.weight.bold};
   text-transform: capitalize;
+
+  @media ${QUERY.TABLET} {
+    font-size: ${tokens.font.size.sm};
+    line-height: ${tokens.font.line_height.sm};
+    letter-spacing: ${tokens.font.letter_spacing.sm};
+  }
 `;
 
 export interface ArtistInfoProps extends ComponentPropsWithRef<'div'> {
