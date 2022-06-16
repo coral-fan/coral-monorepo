01e62c92ca4867ec01ed156de3baf0bf09905b67
Aki Gao <aki@coral.fan>
2022-06-16 13:29:10 -0400

    Fix issue with indexing Transfers Event logs from non-Coral contracts can exist, causing an error to be thrown, because parsing logic assumes a CoralNftV1 interface

--- workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
+++ workspaces/app/pages/api/webhook/defender/index-transfer/index.api.ts
@@ -17,27 +17,41 @@ export const post: Handler = async (req: NextApiRequest, res: NextApiResponse) =
         const contractInterface = new ethers.utils.Interface(CoralNftV1__factory.abi);
         try {
           const logs = sentinelTriggerEvent.transaction.logs;
-          const nftContractAddress = ethers.utils.getAddress(sentinelTriggerEvent.transaction.to);
           for (const log of logs) {
-            const { name, args: result } = contractInterface.parseLog(log);
-            if (name === 'Transfer') {
-              const { from, to, tokenId } = result;
-              if (typeof from !== 'string') {
-                throw 'from is not of type string';
-              }
-              if (typeof to !== 'string') {
-                throw 'to is not of type string';
-              }
-              if (!(tokenId instanceof BigNumber)) {
-                throw 'tokenId is not of type BigNumber';
+            const { address } = log;
+            if (address === undefined) {
+              throw new Error('address is undefined.');
+            }
+
+            const nftContractAddress = ethers.utils.getAddress(address);
+
+            try {
+              const { name, args: result } = contractInterface.parseLog(log);
+              if (name === 'Transfer') {
+                const { from, to, tokenId } = result;
+                if (typeof from !== 'string') {
+                  throw 'from is not of type string';
+                }
+                if (typeof to !== 'string') {
+                  throw 'to is not of type string';
+                }
+                if (!(tokenId instanceof BigNumber)) {
+                  throw 'tokenId is not of type BigNumber';
+                }
+
+                const tokenIdInt = tokenId.toNumber();
+                if (isNotZeroAddress(from)) {
+                  await removeNFTOwnership(from, nftContractAddress, tokenIdInt);
+                }
+                if (isNotZeroAddress(to)) {
+                  await addNFTOwnership(to, nftContractAddress, tokenIdInt);
+                }
               }
 
-              const tokenIdInt = tokenId.toNumber();
-              if (isNotZeroAddress(from)) {
-                await removeNFTOwnership(from, nftContractAddress, tokenIdInt);
-              }
-              if (isNotZeroAddress(to)) {
-                await addNFTOwnership(to, nftContractAddress, tokenIdInt);
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any -- usual ignore comment for error handling
+            } catch (e: any) {
+              if (e.reason !== 'no matching event') {
+                throw e;
               }
             }
           }
