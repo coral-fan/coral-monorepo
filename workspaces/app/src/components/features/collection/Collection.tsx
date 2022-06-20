d2b6a6b5a4056ec2aa0bb4cf9d67412afd89a36b
Aki Gao <aki@coral.fan>
2022-06-20 10:38:38 -0400

    Add token gating logic to Collection page

--- workspaces/app/src/components/features/collection/Collection.tsx
+++ workspaces/app/src/components/features/collection/Collection.tsx
@@ -4,7 +4,7 @@ import { NextParsedUrlQuery } from 'next/dist/server/request-meta';
 
 import { Collection, getCollection, getSimilarCollections } from 'libraries/models/collection';
 import { DropOrAvailable, SimilarCollections, PartialCollection } from './components';
-import { Layout as CollectionLayout } from 'components/ui/nft';
+import { GatedContent, Layout as CollectionLayout } from 'components/ui/nft';
 import { useEffect, useMemo, useState } from 'react';
 
 // stripe
@@ -16,6 +16,49 @@ import { getTokenTotalSupply } from 'libraries/blockchain/utils';
 import { getDocumentReferenceServerSide } from 'libraries/firebase';
 import { RedeemCode } from 'libraries/models/redeemCode';
 import { NullableString } from 'libraries/models';
+import { ConditionalWrap, Link } from 'components/ui';
+import { css } from '@emotion/react';
+
+interface CollectionLinkProps {
+  id: string;
+}
+const CollectionLink = ({ id }: CollectionLinkProps) => (
+  <Link
+    css={css`
+      text-decoration: underline;
+    `}
+    href={`/collection/${id}`}
+  >
+    {id}
+  </Link>
+);
+
+const getPunctuation = (index: number, length: number) => {
+  if (index === length - 1) {
+    return '';
+  }
+
+  if (index === length - 2) {
+    return ' and ';
+  }
+
+  return ', ';
+};
+
+const getCollectionLinks = (collectionIds: string[]) => {
+  if (collectionIds.length === 0) {
+    throw "collectionIds can't be an empty array";
+  }
+  if (collectionIds.length === 1) {
+    return <CollectionLink id={collectionIds[0]} />;
+  }
+  return collectionIds.map((id, index) => (
+    <>
+      <CollectionLink key={id} id={id} />
+      {getPunctuation(index, collectionIds.length)}
+    </>
+  ));
+};
 
 // similarCollections optional so failure to fetch
 // doesn't block page from loading
@@ -45,6 +88,7 @@ export const CollectionPage = ({
   maxMintablePerWallet,
   redeemCode,
   merchOptionTypes,
+  accessGrantingTokenAddresses,
 }: CollectionPageProps) => {
   const [numMinted, setNumMinted] = useState(tokenTotalSupply);
   const [isSoldOut, setIsSoldOut] = useState(tokenTotalSupply >= maxSupply);
@@ -54,7 +98,6 @@ export const CollectionPage = ({
       setNumMinted(totalSupply);
       setIsSoldOut(totalSupply >= maxSupply);
     });
-
     return () => subscription.unsubscribe();
   }, [id, maxSupply]);
 
@@ -103,22 +146,48 @@ export const CollectionPage = ({
   );
 
   return (
-    <Elements stripe={stripePromise}>
-      <CollectionLayout
-        isAsset={false}
-        type={type}
-        imageUrl={imageUrl}
-        artistName={artistName}
-        artistProfilePhoto={artistProfilePhoto}
-        artistId={artistId}
-        name={name}
-        description={description}
-        details={details}
-        collectionId={id}
-        dropOrAvailable={dropOrAvailable}
-        similarCollections={similarCollectionsSection}
-      />
-    </Elements>
+    <ConditionalWrap
+      shouldWrap={accessGrantingTokenAddresses !== null}
+      wrap={(children) => {
+        if (accessGrantingTokenAddresses === null) {
+          throw "accessGrantingTokenAddresses can't be null";
+        }
+        return (
+          <GatedContent
+            accessGrantingTokenAddresses={accessGrantingTokenAddresses}
+            accessDeniedModalProps={{
+              title: 'This is an exclusive collection',
+              actionElement: (
+                <div>
+                  {'Only holders of '}
+                  {getCollectionLinks(accessGrantingTokenAddresses)}
+                  {' have access to this collection.'}
+                </div>
+              ),
+            }}
+          >
+            {children}
+          </GatedContent>
+        );
+      }}
+    >
+      <Elements stripe={stripePromise}>
+        <CollectionLayout
+          isAsset={false}
+          type={type}
+          imageUrl={imageUrl}
+          artistName={artistName}
+          artistProfilePhoto={artistProfilePhoto}
+          artistId={artistId}
+          name={name}
+          description={description}
+          details={details}
+          collectionId={id}
+          dropOrAvailable={dropOrAvailable}
+          similarCollections={similarCollectionsSection}
+        />
+      </Elements>
+    </ConditionalWrap>
   );
 };
 
