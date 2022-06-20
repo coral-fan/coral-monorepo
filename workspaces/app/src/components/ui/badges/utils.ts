4336210da92045df837aad3c588eb4ebd33f359b
maks-p <maks@coral.fan>
2022-06-20 10:27:58 -0400

    Add ticket badge type

--- workspaces/app/src/components/ui/badges/utils.ts
+++ workspaces/app/src/components/ui/badges/utils.ts
@@ -1,7 +1,13 @@
 import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
 import { CollectionType } from 'libraries/models';
-import { VideoBadge, MusicBadge, MerchBadge, StreamBadge } from './variants';
-import { AllAccessBadge } from './variants/AllAccessBadge';
+import {
+  VideoBadge,
+  MusicBadge,
+  MerchBadge,
+  StreamBadge,
+  TicketBadge,
+  AllAccessBadge,
+} from './variants';
 
 type BadgesType = Record<CollectionType, () => EmotionJSX.Element>;
 
@@ -10,8 +16,9 @@ const badgeDictionary: BadgesType = {
   music: MusicBadge,
   merch: MerchBadge,
   stream: StreamBadge,
-  // TODO: add all_access pass badfe
+  // TODO: add all_access pass badge
   all_access: AllAccessBadge,
+  ticket: TicketBadge,
 };
 
 export const getBadge = (type: CollectionType) => badgeDictionary[type];
---
+++ workspaces/app/src/components/ui/badges/variants/TicketBadge/TicketBadge.tsx
@@ -0,0 +1,4 @@
+import { BaseBadge } from '../../BaseBadge';
+import ticketSVG from './ticket.svg';
+
+export const TicketBadge = () => <BaseBadge svg={ticketSVG}>Ticket</BaseBadge>;
