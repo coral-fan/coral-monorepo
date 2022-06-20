9fb67247bc4d8d0a1bc42b9461381a1346a7a096
maks-p <maks@coral.fan>
2022-06-20 19:18:01 -0400

    Update airdrop script

--- workspaces/contracts/tasks/airdrop.ts
+++ workspaces/contracts/tasks/airdrop.ts
@@ -60,36 +60,60 @@ task('execute-airdrop', 'Execute airdrop mint')
 
     const __dirname = path.resolve();
     const airdropFile = path.resolve(__dirname, 'airdrops', projectDir, recipient, 'airdrop.csv');
-    console.log(airdropFile);
+    console.log('Airdrop file identified: ');
+    console.log(airdropFile, '\n');
 
-    console.log('Parsing CSV...');
-    console.log('');
+    console.log('Parsing CSV...\n');
     const addresses = await parseCsv(airdropFile);
 
     if (!Array.isArray(addresses)) {
       throw 'CSV Parser did not return array';
     }
 
+    console.log('Total addresses: ', addresses.length, '\n');
+    console.log('Filtering to unique addresses only...');
+
+    const uniqueAddresses = [...new Set(addresses)].slice(1);
+
+    console.log('Unique addresses: ', uniqueAddresses.length);
+
     try {
       const provider = new DefenderRelayProvider(relayerCredentials);
-      const signer = new DefenderRelaySigner(relayerCredentials, provider, { speed: 'fast' });
+      const signer = new DefenderRelaySigner(relayerCredentials, provider, { speed: 'fastest' });
 
       const contractFactory: ContractFactory = await ethers.getContractFactory(contractName);
       const contract = new Contract(contractAddress, contractFactory.interface, signer);
+      const contractInterface = contractFactory.interface;
 
-      console.log('Starting airdrop...');
-      console.log('');
+      console.log(`Starting airdrop to ${uniqueAddresses.length} addresses...\n`);
 
-      const addressGroups = getAirdropRounds(addresses, 4);
+      const addressGroups = getAirdropRounds(uniqueAddresses, 10);
 
       for (let i = 0; i < addressGroups.length; i++) {
         console.log(`Airdropping group ${i + 1} of ${addressGroups.length}...`);
         console.log('');
         const txn = await contract.airdropMint(addressGroups[i]);
         const receipt = await txn.wait();
+
         if (receipt.status === 1) {
           console.log(`Airdrop Round ${i + 1} of ${addressGroups.length} successful`);
+          console.log('NFTs airdropped to the following addresses (by tokenId)...\n');
+          const logs = receipt.logs;
+          for (const log of logs) {
+            try {
+              const { name, args: result } = contractInterface.parseLog(log);
+              if (name === 'Transfer') {
+                const { from, to, tokenId } = result;
+                const tokenIdInt = tokenId.toNumber();
+                console.log(tokenIdInt, ',', to);
+              }
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any -- usual ignore comment for error handling
+            } catch (e: any) {
+              console.log('Error parsing logs: ', e);
+            }
+          }
         }
+        console.log('\n\n');
       }
     } catch (e: any) {
       console.error(`ERROR: ${e}`);
