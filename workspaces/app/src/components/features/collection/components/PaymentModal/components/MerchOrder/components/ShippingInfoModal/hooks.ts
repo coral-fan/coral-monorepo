1d262cbb4ff487e2c6ff16fee6d04d8ab4e99471
maks-p <maks@coral.fan>
2022-06-22 11:15:05 -0400

    Clean up handleSubmitShippingInfo hook (TODO)

--- workspaces/app/src/components/features/collection/components/PaymentModal/components/MerchOrder/components/ShippingInfoModal/hooks.ts
+++ workspaces/app/src/components/features/collection/components/PaymentModal/components/MerchOrder/components/ShippingInfoModal/hooks.ts
@@ -2,9 +2,8 @@ import { useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { getShippingInfoSchema, ShippingInfoSchema } from './schemas';
-import { useUpsertUser, useUsernames, useUserUid } from 'libraries/models';
+import { useUserUid } from 'libraries/models';
 import { useRefetchPageData } from 'libraries/utils/hooks';
-import { useIsUpdateProfileInfoModalOpen, useUser } from 'components/features/user/hooks';
 
 export const useUpdateShippingForm = () => {
   const shippingInfoSchema = getShippingInfoSchema();
@@ -23,45 +22,13 @@ export const useUpdateShippingForm = () => {
 
   const refetchPageData = useRefetchPageData();
 
-  const upsertUser = useUpsertUser();
-
   const handleSubmitShippingInfo = useMemo(
     () =>
       handleSubmit(async () => {
+        // TODO: Handle submit Shipping Info
         console.log('Submitted Form');
       }),
     [handleSubmit]
-    //     setIsUpdateProfileInfoSubmitting(true);
-    //     if (
-    //       uid !== undefined &&
-    //       username !== undefined &&
-    //       email !== undefined &&
-    //       bio !== undefined
-    //     ) {
-    //       await upsertUser(uid, {
-    //         username,
-    //         email,
-    //         bio,
-    //         socialHandles: {
-    //           ...socialHandlesFromDb,
-    //           ...socialHandlesFromForm,
-    //         },
-    //       });
-    //       setUser((user) => ({
-    //         ...user,
-    //         username,
-    //         email,
-    //         bio,
-    //         socialHandles: {
-    //           ...socialHandlesFromDb,
-    //           ...socialHandlesFromForm,
-    //         },
-    //       }));
-    //       await refetchPageData();
-    //       setIsModalOpen(false);
-    //     }
-    //   }),
-    // [handleSubmit, uid, socialHandlesFromDb, setUser, refetchPageData, setIsModalOpen, upsertUser]
   );
 
   return {
