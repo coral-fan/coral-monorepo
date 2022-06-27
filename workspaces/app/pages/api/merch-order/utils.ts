e6ef561ce2176b61c229abfb2ca971ccbc2cc845
Aki Gao <aki@coral.fan>
2022-06-27 09:56:49 -0400

    Update merch order utils - Extract get merch order document reference to reusable utility function - Add update merch order status utility function

--- workspaces/app/pages/api/merch-order/utils.ts
+++ workspaces/app/pages/api/merch-order/utils.ts
@@ -1,13 +1,26 @@
 import { getDocumentReferenceServerSide } from 'libraries/firebase';
+import { MerchOrder } from 'libraries/models';
 
-export const updateMerchOrderTransactionHash = async (id: string, transactionHash: string) => {
-  const merchOrderDocumentRef = await getDocumentReferenceServerSide('merch-order', id);
+const getMerchOrderDocRef = async (id: string) => {
+  const merchOrderDocumentRef = await getDocumentReferenceServerSide<MerchOrder>('merch-order', id);
   const merchOrderSnapshot = await merchOrderDocumentRef.get();
 
   if (!merchOrderSnapshot.exists) {
     throw new Error(`Merch order ${id} doesn't exist in database.`);
   }
 
+  return merchOrderDocumentRef;
+};
+
+export const updateMerchOrderStatus = async (id: string, status: MerchOrder['status']) => {
+  const merchOrderDocumentRef = await getMerchOrderDocRef(id);
+  const result = await merchOrderDocumentRef.set({ status }, { merge: true });
+
+  return result.writeTime;
+};
+
+export const updateMerchOrderTransactionHash = async (id: string, transactionHash: string) => {
+  const merchOrderDocumentRef = await getMerchOrderDocRef(id);
   const result = await merchOrderDocumentRef.set({ transactionHash }, { merge: true });
 
   return result.writeTime;
