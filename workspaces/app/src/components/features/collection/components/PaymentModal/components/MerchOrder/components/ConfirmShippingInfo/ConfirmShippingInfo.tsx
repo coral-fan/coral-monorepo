a6d15181402e2428f4180b92e5f35c9bf1ff3014
maks-p <maks@coral.fan>
2022-06-29 13:05:13 -0400

    Move styled components, add address data fetching

--- workspaces/app/src/components/features/collection/components/PaymentModal/components/MerchOrder/components/ConfirmShippingInfo/ConfirmShippingInfo.tsx
+++ workspaces/app/src/components/features/collection/components/PaymentModal/components/MerchOrder/components/ConfirmShippingInfo/ConfirmShippingInfo.tsx
@@ -1,48 +1,14 @@
-import styled from '@emotion/styled';
-import { LinkButton } from 'components/ui';
-import tokens from 'styles/tokens';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-const AddressContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const AddressLineItem = styled.span`
-  font-size: ${tokens.font.size.sm};
-  line-height: ${tokens.font.line_height.sm};
-  letter-spacing: ${tokens.font.letter_spacing.sm};
-  color: ${tokens.font.color.secondary};
-`;
-
-const DifferentAddressLinkButton = styled(LinkButton)`
-  text-transform: uppercase;
-  text-decoration: underline;
-  font-size: ${tokens.font.size.xs};
-  line-height: ${tokens.font.line_height.xs};
-  letter-spacing: ${tokens.font.letter_spacing.xs};
-`;
-
-const AddShippingInfoLinkButton = styled(LinkButton)`
-  text-transform: uppercase;
-  text-decoration: underline;
-  font-size: ${tokens.font.size.sm};
-  line-height: ${tokens.font.line_height.sm};
-  letter-spacing: ${tokens.font.letter_spacing.sm};
-`;
-
-const NoShippingInfo = styled.p`
-  font-style: italic;
-  font-size: ${tokens.font.size.sm};
-  line-height: ${tokens.font.line_height.sm};
-  letter-spacing: ${tokens.font.letter_spacing.sm};
-  color: ${tokens.font.color.secondary};
-`;
+import { ShippingInfo } from 'libraries/models';
+import { getShippingAddressData$ } from 'libraries/models/shippingInfo/observables';
+import { useEffect, useState } from 'react';
+import {
+  AddressContainer,
+  AddressLineItem,
+  AddShippingInfoLinkButton,
+  Container,
+  DifferentAddressLinkButton,
+  NoShippingInfo,
+} from './components/components';
 
 interface ConfirmShippingInfoProps {
   shippingInfoId: string | null;
@@ -52,17 +18,22 @@ export const ConfirmShippingInfo = ({
   shippingInfoId,
   addOrUpdateAddress,
 }: ConfirmShippingInfoProps) => {
-  // TODO: Get Shipping Info from database
+  const [addressData, setAddressData] = useState<ShippingInfo>();
 
-  const { firstName, lastName, addressLineOne, addressLineTwo, city, state, postalCode } = {
-    firstName: 'Maks',
-    lastName: 'Pazuniak',
-    addressLineOne: '510 Willoughby Avenue',
-    addressLineTwo: '#4',
-    city: 'Brooklyn',
-    state: 'NY',
-    postalCode: '11206',
-  };
+  useEffect(() => {
+    if (!shippingInfoId) {
+      return;
+    }
+
+    const subscription = getShippingAddressData$(shippingInfoId).subscribe((addressData) => {
+      setAddressData(addressData);
+    });
+
+    return () => subscription.unsubscribe();
+  }, [shippingInfoId]);
+
+  const { firstName, lastName, addressLineOne, addressLineTwo, city, state, zipCode } =
+    addressData || {};
 
   return shippingInfoId ? (
     <Container>
@@ -73,7 +44,7 @@ export const ConfirmShippingInfo = ({
         <AddressLineItem>{addressLineOne}</AddressLineItem>
         <AddressLineItem>{addressLineTwo}</AddressLineItem>
         <AddressLineItem>
-          {city}, {state} {postalCode}
+          {city}, {state} {zipCode}
         </AddressLineItem>
       </AddressContainer>
       <DifferentAddressLinkButton onClick={addOrUpdateAddress}>
