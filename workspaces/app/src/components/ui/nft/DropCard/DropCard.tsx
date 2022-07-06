8dd2dcecad1ffa4d0026619d7735045e7981500b
maks-p <maks@coral.fan>
2022-07-06 13:17:33 -0400

    Add price conditional to DropCard

--- workspaces/app/src/components/ui/nft/DropCard/DropCard.tsx
+++ workspaces/app/src/components/ui/nft/DropCard/DropCard.tsx
@@ -2,6 +2,7 @@ import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
 import styled from '@emotion/styled';
 import { Link } from 'components/ui';
 import { Card } from 'components/ui/Card';
+import { useCallback } from 'react';
 import tokens from 'styles/tokens';
 import { BaseInfo, BaseInfoProps } from '../components';
 
@@ -10,6 +11,7 @@ export interface DropCardProps
   Badge: () => EmotionJSX.Element;
   id: string;
   dropTimestamp: string;
+  price: number;
 }
 
 const DropCardContainer = styled(Card)`
@@ -24,15 +26,24 @@ const BadgeAndDropDateContainer = styled.div`
   justify-content: space-between;
 `;
 
-const DropDateContainer = styled.div`
+const PriceDateBaseContainer = styled.div`
   display: flex;
   flex-direction: column;
   text-transform: uppercase;
-  font-size: ${tokens.font.size.xs};
-  letter-spacing: ${tokens.font.letter_spacing};
-  line-height: ${tokens.font.line_height};
   font-weight: ${tokens.font.weight.bold};
   flex-shrink: 0;
+`;
+
+const DropDateContainer = styled(PriceDateBaseContainer)`
+  font-size: ${tokens.font.size.xs};
+  letter-spacing: ${tokens.font.letter_spacing.xs};
+  line-height: ${tokens.font.line_height.xs};
+`;
+
+const PriceContainer = styled(PriceDateBaseContainer)`
+  font-size: ${tokens.font.size.md};
+  letter-spacing: ${tokens.font.letter_spacing.md};
+  line-height: ${tokens.font.line_height.md};
 `;
 
 const formatDropCardDate = (timestamp: string) => {
@@ -48,19 +59,23 @@ const formatDropCardDate = (timestamp: string) => {
   return formattedDate;
 };
 
-export const DropCard = ({ Badge, dropTimestamp, id, ...baseInfoProps }: DropCardProps) => (
+const getIsDropped = (dropTimestamp: string) =>
+  new Date(dropTimestamp).getTime() < new Date().getTime();
+
+export const DropCard = ({ Badge, dropTimestamp, id, price, ...baseInfoProps }: DropCardProps) => (
   <Link href={`/collection/${id}`}>
     <DropCardContainer>
       <BaseInfo titleHeadingLevel={3} titleStyleVariant={'h3'} {...baseInfoProps}>
         <BadgeAndDropDateContainer>
           <Badge />
-          <DropDateContainer>
-            <span>
-              {new Date(dropTimestamp).getTime() < new Date().getTime() ? 'Dropped ' : 'Drops '}
-              on
-            </span>
-            <span>{formatDropCardDate(dropTimestamp)}</span>
-          </DropDateContainer>
+          {getIsDropped(dropTimestamp) ? (
+            <PriceContainer>${price.toFixed(2)}</PriceContainer>
+          ) : (
+            <DropDateContainer>
+              <span>Drops on</span>
+              <span>{formatDropCardDate(dropTimestamp)}</span>
+            </DropDateContainer>
+          )}
         </BadgeAndDropDateContainer>
       </BaseInfo>
     </DropCardContainer>
