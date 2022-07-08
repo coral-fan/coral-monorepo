ab17071f46a445234406d348be3a7456242eb10e
Aki Gao <aki@coral.fan>
2022-07-08 13:17:31 -0400

    Update icon size hook - Use is mobile hook instead of is desktop hook - Reduce icon size for mobile viewport

--- workspaces/app/src/components/ui/nft/GatedContent/components/hooks.ts
+++ workspaces/app/src/components/ui/nft/GatedContent/components/hooks.ts
@@ -1,9 +1,9 @@
-import { useIsDesktop } from 'libraries/window';
+import { useIsMobile } from 'libraries/window';
 import { useMemo } from 'react';
 
 export const useIconSize = () => {
-  const isDesktop = useIsDesktop();
-  const iconSize = useMemo(() => (isDesktop ? 275 : 175), [isDesktop]);
+  const isMobile = useIsMobile();
+  const iconSize = useMemo(() => (isMobile ? 135 : 275), [isMobile]);
 
   return iconSize;
 };
