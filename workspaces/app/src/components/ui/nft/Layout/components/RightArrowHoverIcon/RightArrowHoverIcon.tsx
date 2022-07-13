c6ab4f6374c00269d74525056ae6f7f8c4e8cd72
maks-p <maks@coral.fan>
2022-07-13 17:52:19 -0400

    Create RightArrowHoverIcon component

---
+++ workspaces/app/src/components/ui/nft/Layout/components/RightArrowHoverIcon/RightArrowHoverIcon.tsx
@@ -0,0 +1,11 @@
+import SVG, { Props as SVGProps } from 'react-inlinesvg';
+import leftArrowIcon from 'components/ui/icons/LeftArrowIcon/leftArrow.svg';
+import styled from '@emotion/styled';
+
+const StyledSVG = styled(SVG)<SVGProps>`
+  transform: rotate(180deg);
+  width: 20px;
+  height: 20px;
+`;
+
+export const RightArrowHoverIcon = () => <StyledSVG src={leftArrowIcon} title="RightArrowIcon" />;
