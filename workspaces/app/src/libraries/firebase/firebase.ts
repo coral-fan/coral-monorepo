056927341d2a8d1cfd6c05e0ed7a81080e802bed
Aki Gao <aki@coral.fan>
2022-07-22 17:23:37 -0400

    Move env check to allow usage out of app workspace

--- workspaces/app/src/libraries/firebase/firebase.ts
+++ workspaces/app/src/libraries/firebase/firebase.ts
@@ -1,19 +1,18 @@
 import { initializeApp, getApps } from '@firebase/app';
 import { getEnvironmentVariableErrorMessage } from 'libraries/utils';
 
-if (!process.env.NEXT_PUBLIC_FIREBASE_CONFIG) {
-  throw getEnvironmentVariableErrorMessage('NEXT_PUBLIC_FIREBASE_CONFIG');
-}
-
-const FIREBASE_CONFIG = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
-
 export const initializeFirebaseApp = () => {
   // getApps() checks if any apps have been initialized and prevents re-initialization of Firebase SDK during hot reloads
   if (getApps().length < 1) {
+    if (!process.env.NEXT_PUBLIC_FIREBASE_CONFIG) {
+      throw getEnvironmentVariableErrorMessage('NEXT_PUBLIC_FIREBASE_CONFIG');
+    }
+
+    const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
     /* 
     initialize Firebase using Web version 9 (modular), so authorization is handled by library
     https://firebase.google.com/docs/firestore/quickstart#set_up_your_development_environment
     */
-    initializeApp(FIREBASE_CONFIG);
+    initializeApp(firebaseConfig);
   }
 };
