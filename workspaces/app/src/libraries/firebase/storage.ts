c6b4b7f936a51e97b4eedf8c150d267d12c3a1ba
Aki Gao <aki@coral.fan>
2022-07-28 15:16:53 -0400

    Update Firebase storage util functions - Move environment variable retrieval inside functions to allow code sharing - Throw error if storage bucket url isn't defined in env

--- workspaces/app/src/libraries/firebase/storage.ts
+++ workspaces/app/src/libraries/firebase/storage.ts
@@ -1,14 +1,24 @@
 import { getStorage } from 'firebase-admin/storage';
+import { getEnvironmentVariableErrorMessage } from 'libraries/utils';
 import { initializeFirebaseAdmin } from './admin';
 
-const storageBucketName = process.env.FIREBASE_STORAGE_BUCKET;
-
-export const getStorageBucket = async () => {
-  await initializeFirebaseAdmin();
-  return getStorage().bucket(storageBucketName);
+const getStorageBucketUrl = () => {
+  const STORAGE_BUCKET_URL = process.env.FIREBASE_STORAGE_BUCKET_URL;
+  if (STORAGE_BUCKET_URL === undefined) {
+    throw new Error(getEnvironmentVariableErrorMessage('FIREBASE_STORAGE_BUCKET_URL'));
+  }
+  return STORAGE_BUCKET_URL;
 };
 
-export const getPublicFileUrl = (destinationPath: string, accessToken: string) =>
-  `https://firebasestorage.googleapis.com/v0/b/${storageBucketName}/o/${encodeURIComponent(
+export const getStorageBucket = async () => {
+  const storageBucketUrl = getStorageBucketUrl();
+  await initializeFirebaseAdmin();
+  return getStorage().bucket(storageBucketUrl);
+};
+
+export const getPublicFileUrl = (destinationPath: string, accessToken: string) => {
+  const storageBucketUrl = getStorageBucketUrl();
+  `https://firebasestorage.googleapis.com/v0/b/${storageBucketUrl}/o/${encodeURIComponent(
     destinationPath
   )}?alt=media&token=${accessToken}`;
+};
