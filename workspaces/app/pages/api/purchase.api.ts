6ec6ffa19cb620ccd56f791ba2c897ad0fcc3a5b
Aki Gao <aki@coral.fan>
2022-08-17 16:22:59 -0400

    Add fingerprint to purchase if collection matches

--- workspaces/app/pages/api/purchase.api.ts
+++ workspaces/app/pages/api/purchase.api.ts
@@ -1,5 +1,5 @@
 import { getCollectionReferenceServerSide, getDocumentData } from 'libraries/firebase';
-import { PurchaseData } from 'libraries/models';
+import { PurchaseData, ReferralData } from 'libraries/models';
 import { ERROR_RESPONSE } from './consts';
 import { Handler } from './types';
 import { getHandler } from './utils';
@@ -17,7 +17,23 @@ const post: Handler = async (req, res) => {
     const { userId, collectionId, transactionHash, fingerprint } = req.body;
     const purchaseCollectionRef = await getCollectionReferenceServerSide('purchases');
 
-    const fingerprintData = await getDocumentData('fingerprints', fingerprint);
+    const fingerprintData = await getDocumentData<{ referralCode: string }>(
+      'fingerprints',
+      fingerprint
+    );
+
+    let metadata = null;
+
+    // check to ensure referral code is for correct collection
+    if (fingerprintData) {
+      const { referralCode } = fingerprintData;
+
+      const referral = await getDocumentData<ReferralData>('referrals', referralCode);
+
+      if (referral && referral.collectionId === collectionId) {
+        metadata = { fingerprint };
+      }
+    }
 
     const purchaseData: PurchaseData = {
       ...DEFAULT_PURCHASE_DATA,
@@ -25,7 +41,7 @@ const post: Handler = async (req, res) => {
       userId,
       collectionId,
       transactionHash: transactionHash ?? null,
-      metadata: fingerprintData ? { fingerprint } : null,
+      metadata,
     };
 
     const { id } = await purchaseCollectionRef.add(purchaseData);
