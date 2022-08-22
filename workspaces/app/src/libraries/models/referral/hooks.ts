387f60b17d3dfb942ec5098978b27ae2e4213b2e
maks-p <maks@coral.fan>
2022-08-22 15:53:25 -0400

    Update observables + hooks to display user points

--- workspaces/app/src/libraries/models/referral/hooks.ts
+++ workspaces/app/src/libraries/models/referral/hooks.ts
@@ -1,4 +1,6 @@
 import { useObservable } from 'libraries/utils';
-import { getIsReferralUser$ } from './observables';
+import { getIsReferralUser$, getUserReferralAccount$ } from './observables';
 
 export const useIsReferralUser = () => useObservable(getIsReferralUser$, false);
+
+export const useReferralUserData = () => useObservable(getUserReferralAccount$, null);
