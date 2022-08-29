43c07730df023e91a04a09500623025af91e84ae
maks-p <maks@coral.fan>
2022-08-29 16:11:14 -0400

    Add isRedeeming logic, misc clean up

--- workspaces/app/src/components/features/user/components/PointsRedemptionModal/PointsRedemptionModal.tsx
+++ workspaces/app/src/components/features/user/components/PointsRedemptionModal/PointsRedemptionModal.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
-import { Button, Input, Modal } from 'components/ui';
+import { Button, ConditionalSpinner, Input, Modal } from 'components/ui';
 import { useIsAuthenticated } from 'libraries/authentication';
-import { useUserUid } from 'libraries/models';
 import tokens from 'styles/tokens';
 import { getUseRedeemPointsForm } from './hooks';
 import { useMemo } from 'react';
@@ -24,6 +23,13 @@ const Form = styled.form`
   gap: 30px;
 `;
 
+const SpinnerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 260px;
+`;
+
 const REDEMPTION_CONTENT =
   'Enter a wallet address you would like to send your $AVAX to. Please be careful you enter the address in accurately. Once you hit claim, this action can not be done and we cannot retrieve assets once sent. ';
 
@@ -31,31 +37,45 @@ export const PointsRedemptionModal = ({
   redemptionAmount,
   closeModal,
 }: PointsRedemptionModalProps) => {
-  const uid = useUserUid();
   const isAuthenticated = useIsAuthenticated();
 
-  const useRedeemPointsForm = useMemo(
-    () => getUseRedeemPointsForm(uid, redemptionAmount, closeModal),
-    [uid, redemptionAmount, closeModal]
-  );
-  const { register, errors, handleSubmitAddress } = useRedeemPointsForm();
+  const useRedeemPointsForm = useMemo(() => getUseRedeemPointsForm(closeModal), [closeModal]);
+  const { register, errors, isRedeemingPoints, isDirty, isValid, handleSubmitAddress } =
+    useRedeemPointsForm();
 
   if (!isAuthenticated) {
     return null;
   }
 
   return (
-    <Modal onClick={closeModal} title="Redeem My Points" fullHeight>
-      <Form onSubmit={handleSubmitAddress}>
-        <Input
-          placeholder={'0x...'}
-          label={'Enter Wallet Address'}
-          {...register('address')}
-          error={errors?.address?.message}
-        />
-        <TextContent>{REDEMPTION_CONTENT}</TextContent>
-        <Button type="submit">Claim {redemptionAmount / POINTS_AVAX_VALUE} AVAX</Button>
-      </Form>
+    <Modal
+      onClick={closeModal}
+      title={isRedeemingPoints ? 'Redeeming Points...' : 'Redeem My Points'}
+      fullHeight
+    >
+      <SpinnerWrapper>
+        <Form onSubmit={handleSubmitAddress}>
+          <ConditionalSpinner
+            size={'80px'}
+            center
+            color={tokens.background.color.brand}
+            loading={isRedeemingPoints}
+          >
+            <Input
+              placeholder={'0x...'}
+              label={'Enter Wallet Address'}
+              {...register('address')}
+              error={errors?.address?.message}
+            />
+            <TextContent>{REDEMPTION_CONTENT}</TextContent>
+            <Button disabled={!isDirty || !isValid || redemptionAmount <= 0} type="submit">
+              {redemptionAmount > 0
+                ? `Claim ${redemptionAmount / POINTS_AVAX_VALUE} AVAX`
+                : `No Points to Redeem`}
+            </Button>
+          </ConditionalSpinner>
+        </Form>
+      </SpinnerWrapper>
     </Modal>
   );
 };
