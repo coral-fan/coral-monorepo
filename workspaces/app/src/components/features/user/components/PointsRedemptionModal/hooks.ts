6066bc90aeba61e41df5081543d1cf1cb087e267
maks-p <maks@coral.fan>
2022-08-29 16:11:14 -0400

    Add isRedeeming logic, misc clean up

--- workspaces/app/src/components/features/user/components/PointsRedemptionModal/hooks.ts
+++ workspaces/app/src/components/features/user/components/PointsRedemptionModal/hooks.ts
@@ -1,52 +1,60 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useMemo } from 'react';
-import axios from 'axios';
-import { validateAddress } from 'libraries/utils';
+import { useMemo, useState } from 'react';
+import { getCoralAPIAxios, validateAddress } from 'libraries/utils';
 import { z } from 'zod';
 import { getDocumentData } from 'libraries/firebase';
-import { UserReferralAccount } from 'libraries/models';
+import { UserReferralAccount, useUserUid } from 'libraries/models';
 
 const ADDRESS_INPUT_SCHEMA = z.object({
   address: z.string().refine((addr) => validateAddress(addr)),
 });
 
+const axios = getCoralAPIAxios();
+
 type AddressInputSchema = z.infer<typeof ADDRESS_INPUT_SCHEMA>;
 
-export const getUseRedeemPointsForm =
-  (uid: string | undefined, points: number, closeModal: () => void) => () => {
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<AddressInputSchema>({
-      resolver: zodResolver(ADDRESS_INPUT_SCHEMA),
-      mode: 'all',
-    });
+export const getUseRedeemPointsForm = (closeModal: () => void) => () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid },
+  } = useForm<AddressInputSchema>({
+    resolver: zodResolver(ADDRESS_INPUT_SCHEMA),
+    mode: 'all',
+  });
 
-    const handleSubmitAddress = useMemo(
-      () =>
-        handleSubmit(async ({ address }) => {
-          // Check that redemption is not already in process
-          const referralUserDocument =
-            uid && (await getDocumentData<UserReferralAccount>('user-referral-accounts', uid));
-          if (!referralUserDocument || referralUserDocument.isRedeeming) {
-            throw new Error(`User ${uid} has already requested a redemption that is in process`);
-          }
+  const [isRedeemingPoints, setIsRedeemingPoints] = useState(false);
+  const uid = useUserUid();
 
-          await axios.post('../api/referral-redemption', {
-            uid,
-            address,
-            points,
-          });
-          closeModal();
-        }),
-      [handleSubmit]
-    );
+  const handleSubmitAddress = useMemo(
+    () =>
+      handleSubmit(async ({ address }) => {
+        setIsRedeemingPoints(true);
 
-    return {
-      register,
-      errors,
-      handleSubmitAddress,
-    };
+        // Check that redemption is not already in process
+        const referralUserDocument =
+          uid && (await getDocumentData<UserReferralAccount>('user-referral-accounts', uid));
+
+        if (!referralUserDocument || referralUserDocument.isRedeeming) {
+          throw new Error(`User ${uid} has already requested a redemption that is in process`);
+        }
+
+        await axios.post('referral-redemption', {
+          address,
+        });
+        setIsRedeemingPoints(false);
+        closeModal();
+      }),
+    [uid, handleSubmit]
+  );
+
+  return {
+    register,
+    errors,
+    isRedeemingPoints,
+    isDirty,
+    isValid,
+    handleSubmitAddress,
   };
+};
