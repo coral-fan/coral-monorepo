8f31023375bb1f42c591e5cc328bd12267a6b6f1
Aki Gao <aki@coral.fan>
2022-09-16 17:59:41 -0400

    Add logic to update referral metrics

--- workspaces/app/pages/api/record-fingerprint.api.ts
+++ workspaces/app/pages/api/record-fingerprint.api.ts
@@ -1,9 +1,10 @@
 import { Handler } from './types';
 import { z } from 'zod';
 import { ERROR_RESPONSE } from './consts';
-import { getDocumentData, getDocumentReferenceServerSide } from 'libraries/firebase';
+import { getBatch, getDocumentReferenceServerSide } from 'libraries/firebase';
 import { getHandler } from './utils';
 import { ReferralData } from 'libraries/models';
+import { FieldValue } from 'firebase-admin/firestore';
 
 const RecordFingerprintRequestBody = z.object({
   referralCode: z.string(),
@@ -14,9 +15,14 @@ const RecordFingerprintRequestBody = z.object({
 const post: Handler = async (req, res) => {
   try {
     const { referralCode, fingerprint, referrer } = RecordFingerprintRequestBody.parse(req.body);
-    const referralCodeDocumentData = await getDocumentData<ReferralData>('referrals', referralCode);
+    const referralCodeDocRef = await getDocumentReferenceServerSide<ReferralData>(
+      'referrals',
+      referralCode
+    );
+    const referralCodeSnapshot = await referralCodeDocRef.get();
+    const referralCodeData = referralCodeSnapshot.data();
 
-    if (!referralCodeDocumentData) {
+    if (referralCodeData === undefined) {
       throw new Error(`Referral code ${referralCode} isn't in database.`);
     }
 
@@ -29,6 +35,23 @@ const post: Handler = async (req, res) => {
       referralCode,
       referrer,
     });
+
+    const { seenFingerprints } = referralCodeData;
+
+    const batch = await getBatch();
+
+    if (seenFingerprints.indexOf(fingerprint) < 0) {
+      batch.update(referralCodeDocRef, {
+        seenFingerprints: FieldValue.arrayUnion(fingerprint),
+        uniqueVisits: FieldValue.increment(1),
+      });
+    }
+
+    batch.update(referralCodeDocRef, {
+      visits: FieldValue.increment(1),
+    });
+
+    await batch.commit();
   } catch (e) {
     console.error(e);
     return res.status(500).json(ERROR_RESPONSE);
