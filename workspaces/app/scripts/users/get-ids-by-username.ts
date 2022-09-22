dab69a352b42796306f789959c771b3d1692c933
Aki Gao <aki@coral.fan>
2022-09-22 18:28:16 -0400

    Add script to generate ids by username CSV

---
+++ workspaces/app/scripts/users/get-ids-by-username.ts
@@ -0,0 +1,31 @@
+import { stringify } from 'csv-stringify';
+import fs from 'fs';
+import { getCollectionReferenceServerSide } from 'libraries/firebase';
+import { User } from 'libraries/models';
+import path from 'path';
+
+const usernames = ['bchcash', 'okes', '147258', 'cool'];
+
+const columns = ['Username', 'User ID'];
+
+const stringifier = stringify({ header: true, columns });
+
+const generateCSV = async () => {
+  const userCollectionRef = await getCollectionReferenceServerSide<User>('users');
+
+  const userQuerySnapshots = await userCollectionRef.where('username', 'in', usernames).get();
+
+  for (const userSnapshot of userQuerySnapshots.docs) {
+    const { id } = userSnapshot;
+    const { username } = userSnapshot.data();
+
+    stringifier.write([username, id]);
+  }
+
+  const writableStream = fs.createWriteStream(path.join(__dirname, 'ids-by-username.csv'));
+
+  stringifier.pipe(writableStream);
+};
+
+console.log('writing CSV...');
+generateCSV().catch(console.error);
