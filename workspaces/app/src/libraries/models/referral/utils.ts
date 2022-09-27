7a7bc506bf383393112af5e2f8decbd70754e612
maks-p <maks@coral.fan>
2022-09-27 10:59:17 -0400

    Update getReferralCode

--- workspaces/app/src/libraries/models/referral/utils.ts
+++ workspaces/app/src/libraries/models/referral/utils.ts
@@ -1,4 +1,4 @@
 import md5 from 'md5';
 
-export const getReferralCode = (userId: string, collectionId: string) =>
-  md5(`${userId}${collectionId}`);
+export const getReferralCode = (userId: string, campaignId: string) =>
+  md5(`${userId}${campaignId}`);
