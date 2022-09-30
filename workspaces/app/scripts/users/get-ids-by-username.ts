dff6812989df003ff3c21b7c68879d0950d8f66d
Aki Gao <aki@coral.fan>
2022-09-30 15:38:18 -0400

    Update get ID by username script to accept CSV

--- workspaces/app/scripts/users/get-ids-by-username.ts
+++ workspaces/app/scripts/users/get-ids-by-username.ts
@@ -3,22 +3,45 @@ import fs from 'fs';
 import { getCollectionReferenceServerSide } from 'libraries/firebase';
 import { User } from 'libraries/models';
 import path from 'path';
+import { parse } from 'csv-parse';
 
-const usernames = ['Abigail', 'paomian'];
+const getUsernames = () =>
+  new Promise<string[]>((resolve) => {
+    const usernames: string[] = [];
+    fs.createReadStream(path.join(__dirname, `usernames_9-28-2022.csv`))
+      .pipe(
+        parse({
+          from_line: 2,
+        }).on('data', async ([, username]) => {
+          usernames.push(username);
+        })
+      )
+      .on('end', () => resolve(usernames));
+  });
+
+// const usernames = ['Abigail', 'paomian'];
 
 const columns = ['Username', 'User ID'];
 
 const stringifier = stringify({ header: true, columns });
 
 const generateCSV = async () => {
+  const usernames = await getUsernames();
+
   const userCollectionRef = await getCollectionReferenceServerSide<User>('users');
 
-  const userQuerySnapshots = await userCollectionRef.where('username', 'in', usernames).get();
+  const userQuerySnapshots = await userCollectionRef.get();
+
+  const usernamesToIds = new Map();
 
   for (const userSnapshot of userQuerySnapshots.docs) {
-    const { id } = userSnapshot;
     const { username } = userSnapshot.data();
+    const { id } = userSnapshot;
+    usernamesToIds.set(username, id);
+  }
 
+  for (const username of usernames) {
+    const id = usernamesToIds.get(username);
     stringifier.write([username, id]);
   }
 
