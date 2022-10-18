3cbdd2de8bf4d707eaa5b54188a41180a2279290
maks-p <maks@coral.fan>
2022-10-18 15:32:14 -0400

    Create withdraw modal

---
+++ workspaces/app/src/components/app/components/NavigationBar/components/WithdrawAvaxModal/WithdrawAvaxModal.tsx
@@ -0,0 +1,91 @@
+import styled from '@emotion/styled';
+import { Button, ConditionalSpinner, Input, Modal, TransactionSuccessModal } from 'components/ui';
+import { useIsAuthenticated } from 'libraries/authentication';
+import { useWallet } from 'libraries/blockchain';
+import tokens from 'styles/tokens';
+import { useWithdrawAvaxForm } from './hooks';
+// import { SuccessContent } from './components';
+
+interface WithdrawAvaxModalProps {
+  closeWithdrawModal: () => void;
+}
+
+const TextContent = styled.div`
+  font-size: ${tokens.font.size.sm};
+  letter-spacing: ${tokens.font.letter_spacing.sm};
+  line-height: ${tokens.font.line_height.sm};
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+export const SpinnerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 260px;
+`;
+
+const SEND_AVAX_CONTENT =
+  'Enter a wallet address you would like to send your $AVAX to. Please be careful you enter the address in accurately. Once you hit claim, this action can not be undone and we cannot retrieve assets once sent. Amount sent will be your total balance net of gas costs.';
+
+export const WithdrawAvaxModal = ({ closeWithdrawModal }: WithdrawAvaxModalProps) => {
+  const { balance } = useWallet();
+  const isAuthenticated = useIsAuthenticated();
+
+  const {
+    register,
+    errors,
+    isWithdrawingAvax,
+    isDirty,
+    isValid,
+    withdrawTxnReceipt,
+    handleSubmitAddress,
+  } = useWithdrawAvaxForm();
+
+  if (!isAuthenticated || !balance) {
+    return null;
+  }
+
+  return withdrawTxnReceipt && withdrawTxnReceipt.status === 1 && withdrawTxnReceipt ? (
+    <TransactionSuccessModal
+      transactionType="withdraw"
+      transactionHash={withdrawTxnReceipt.transactionHash}
+      closeModal={closeWithdrawModal}
+    />
+  ) : (
+    <Modal
+      onClick={closeWithdrawModal}
+      title={isWithdrawingAvax ? 'Withdrawing AVAX...' : 'Withdraw AVAX'}
+      fullHeight
+    >
+      <SpinnerWrapper>
+        <Form onSubmit={handleSubmitAddress}>
+          <ConditionalSpinner
+            size={'80px'}
+            center
+            color={tokens.background.color.brand}
+            loading={isWithdrawingAvax}
+          >
+            <Input
+              placeholder={'0x...'}
+              label={'Enter Wallet Address'}
+              {...register('to')}
+              error={errors?.to?.message}
+            />
+            <TextContent>{SEND_AVAX_CONTENT}</TextContent>
+            <Button
+              disabled={isWithdrawingAvax || !isDirty || !isValid || balance <= 0}
+              type="submit"
+            >
+              Withdraw your AVAX
+            </Button>
+          </ConditionalSpinner>
+        </Form>
+      </SpinnerWrapper>
+    </Modal>
+  );
+};
