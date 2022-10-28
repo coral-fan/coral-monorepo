bbc2e7c982f77aac0005450c9a84ddd7a0bf7353
Aki Gao <aki@coral.fan>
2022-10-28 12:07:24 -0400

    Correct Web3Auth logout redirect url key value

--- workspaces/app/public/scripts/redirect.js
+++ workspaces/app/public/scripts/redirect.js
@@ -1,5 +1,6 @@
-if (localStorage.getItem('logout_redirect_url')) {
-  var redirectUrl = localStorage.getItem('logout_redirect_url');
-  localStorage.removeItem('logout_redirect_url');
+var WEB3AUTH_LOGOUT_REDIRECT_URL = 'web3auth_logout_redirect_url';
+if (localStorage.getItem(WEB3AUTH_LOGOUT_REDIRECT_URL)) {
+  var redirectUrl = localStorage.getItem(WEB3AUTH_LOGOUT_REDIRECT_URL);
+  localStorage.removeItem(WEB3AUTH_LOGOUT_REDIRECT_URL);
   window.location.replace(redirectUrl);
 }
