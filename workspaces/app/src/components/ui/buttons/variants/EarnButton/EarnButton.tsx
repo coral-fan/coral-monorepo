93d3d4189807383fe67523f35d0ab3f7a36aa8e7
maks-p <maks@coral.fan>
2022-11-17 14:26:30 -0500

    Add Earn Button

---
+++ workspaces/app/src/components/ui/buttons/variants/EarnButton/EarnButton.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { EarnModal } from 'components/ui';
+import { useUserUid } from 'libraries/models';
+import { useState } from 'react';
+import { CtaButton } from '../CtaButton';
+
+const EarnButtonWrapper = styled(CtaButton)`
+  padding: 20px;
+`;
+
+interface EarnButtonProps {
+  campaignId: string;
+}
+
+export const EarnButton = ({ campaignId }: EarnButtonProps) => {
+  const [showEarnModal, setShowEarnModal] = useState(false);
+
+  const uid = useUserUid();
+
+  return (
+    <>
+      {uid && showEarnModal && (
+        <EarnModal
+          closeEarnModal={() => setShowEarnModal(false)}
+          campaignId={campaignId}
+        ></EarnModal>
+      )}
+      <EarnButtonWrapper disabled={!uid} onClick={() => setShowEarnModal(true)}>
+        Earn Now
+      </EarnButtonWrapper>
+    </>
+  );
+};
