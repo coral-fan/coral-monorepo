6cb9bfad833dd2be1faf139ea915e0836d7c2399
maks-p <maks@coral.fan>
2022-11-28 14:49:00 -0500

    Modify getDoesOwnToken to handle Eth NFTs

--- workspaces/app/src/libraries/blockchain/utils.ts
+++ workspaces/app/src/libraries/blockchain/utils.ts
@@ -1,21 +1,33 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
-import { AVALANCHE } from 'consts';
+import { AVALANCHE, ETHEREUM } from 'consts';
 import { TotalSuppliable__factory, ERC721__factory } from '@coral/contracts';
 
 const avalancheRpcProvider = new JsonRpcProvider(AVALANCHE.RPC_URL);
+const ethereumRpcProvider = new JsonRpcProvider(ETHEREUM.RPC_URL);
 
 export const getTokenOwner = (address: string, assetId: number) => {
   const contract = ERC721__factory.connect(address, avalancheRpcProvider);
   return contract.ownerOf(assetId);
 };
 
-export const getDoesOwnToken = async (collectionAddress: string, userAddress: string) => {
-  const contract = ERC721__factory.connect(collectionAddress, avalancheRpcProvider);
+// if ethereum, set isAvalanche to false
+export const getDoesOwnToken = async (
+  collectionAddress: string,
+  userAddress: string,
+  isAvalanche = true
+) => {
+  try {
+    const rpcProvider = isAvalanche ? avalancheRpcProvider : ethereumRpcProvider;
+    const contract = ERC721__factory.connect(collectionAddress, rpcProvider);
 
-  const bigNumTokenBalance = await contract.balanceOf(userAddress);
-  const tokenBalance = bigNumTokenBalance.toNumber();
+    const bigNumTokenBalance = await contract.balanceOf(userAddress);
+    const tokenBalance = bigNumTokenBalance.toNumber();
 
-  return tokenBalance > 0;
+    return tokenBalance > 0;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 };
 
 export const getOwnedTokensByCollection = async (
