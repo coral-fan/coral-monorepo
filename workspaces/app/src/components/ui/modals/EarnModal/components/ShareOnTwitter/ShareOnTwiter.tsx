99442f78ac52b5bc605022c4a0cb29d0c36171e4
Aki Gao <aki@coral.fan>
2022-11-30 14:11:30 -0500

    Update Share On Twitter UI/logic - Add form validation for url input - Wire up form to Tweet verification API - Update Verify Button UI

--- workspaces/app/src/components/ui/modals/EarnModal/components/ShareOnTwitter/ShareOnTwiter.tsx
+++ workspaces/app/src/components/ui/modals/EarnModal/components/ShareOnTwitter/ShareOnTwiter.tsx
@@ -1,30 +1,42 @@
 import styled from '@emotion/styled';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, buttonStyle } from 'components/ui/buttons';
 import { buttonBaseStyle } from 'components/ui/buttons/styles';
 import { Input } from 'components/ui/inputs';
-import { useMemo } from 'react';
+import { getCoralAPIAxios } from 'libraries/utils';
+import { useErrorToast } from 'libraries/utils/toasts';
+import { FormEvent, useCallback, useMemo, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { TwitterShareButton } from 'react-share';
-import tokens from 'styles/tokens';
+import tokens, { QUERY } from 'styles/tokens';
+import { z } from 'zod';
 import { BrandColor, ContentContainer, Heading, PrimaryContainer } from '../components';
 
 interface ShareOnTwitterProps {
   socialShareCode: string;
   defaultContent: string;
   points: number;
+  campaignId: string;
+  setHasVerified: (hasVerified: boolean) => void;
 }
 
 const ShareButton = styled.div`
   ${buttonBaseStyle}
   ${buttonStyle}
+  height: 45px;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 175px;
 `;
 
 const ShareButtonWrapper = styled.div`
-  height: fit-content;
-  width: fit-content;
+  .react-share__ShareButton {
+    width: 100%;
+
+    @media ${QUERY.TABLET} {
+      width: 175px;
+    }
+  }
 `;
 
 const ModalText = styled.div`
@@ -33,32 +45,83 @@ const ModalText = styled.div`
   line-height: ${tokens.font.line_height.md};
 `;
 
-const VerifyInputContainer = styled.div`
+interface FormProps {
+  inputHasError: boolean;
+}
+const Form = styled.form<FormProps>`
   display: flex;
-  align-items: end;
+  flex-direction: column;
   gap: 10px;
+
+  @media ${QUERY.TABLET} {
+    flex-direction: row;
+    align-items: ${({ inputHasError }) => (inputHasError ? 'center' : 'end')};
+  }
 `;
 
 const VerifyButton = styled(Button)`
-  width: 125px;
-  height: 44px;
+  width: 100%;
+
+  @media ${QUERY.TABLET} {
+    width: 125px;
+  }
+
+  height: 45px;
 `;
+
+const verifyTweetSchema = z.object({
+  url: z.string().url(),
+});
+
+type VerifyTweetSchema = z.infer<typeof verifyTweetSchema>;
+
+const coralAPI = getCoralAPIAxios();
 
 export const ShareOnTwitter = ({
   socialShareCode,
   defaultContent,
   points,
+  campaignId,
+  setHasVerified,
 }: ShareOnTwitterProps) => {
   const postContent = useMemo(() => defaultContent.replaceAll('/n', '\n'), [defaultContent]);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isDirty },
+  } = useForm<VerifyTweetSchema>({ resolver: zodResolver(verifyTweetSchema), mode: 'all' });
+
+  const [isVerifyingTweet, setIsVerifyingTweet] = useState(false);
+  const errorToast = useErrorToast();
+
+  const handleSubmitUrl = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      await handleSubmit(async ({ url }) => {
+        setIsVerifyingTweet(true);
+        try {
+          await coralAPI.post('share-to-earn/verify/tweet', { tweetUrl: url, campaignId });
+          setHasVerified(true);
+        } catch (e) {
+          errorToast();
+        }
+        setIsVerifyingTweet(false);
+      })(e);
+    },
+    [handleSubmit, setIsVerifyingTweet, campaignId, setHasVerified, errorToast]
+  );
   return (
     <PrimaryContainer>
       <ContentContainer>
         <Heading>
-          1. Share this page on Twitter and <BrandColor>earn {points} Coral points</BrandColor>
+          1. Share This On Twitter And <BrandColor>Earn {points} Coral points</BrandColor>
         </Heading>
-        <ModalText>Post a public tweet containing your unique identifier:</ModalText>
-        <ModalText>{socialShareCode}</ModalText>
+        <ModalText>
+          {`If you change the Tweet, be sure to include all urls, mentions (@), tags (#) and unique code (${socialShareCode}) from the pre-populated content to earn your
+          points.`}
+        </ModalText>
         <ShareButtonWrapper>
           <TwitterShareButton key={'twitter'} title={postContent} url={socialShareCode}>
             <ShareButton>Post On Twitter</ShareButton>
@@ -67,10 +130,19 @@ export const ShareOnTwitter = ({
       </ContentContainer>
       <ContentContainer>
         <Heading>2. Verify your Tweet</Heading>
-        <VerifyInputContainer>
-          <Input label="paste the url of the tweet you just posted"></Input>
-          <VerifyButton>Verify</VerifyButton>
-        </VerifyInputContainer>
+        <Form inputHasError={errors?.url !== undefined} onSubmit={handleSubmitUrl}>
+          <Input
+            label="paste the url of the tweet you just posted"
+            error={errors?.url?.message}
+            {...register('url')}
+          />
+          <VerifyButton
+            loading={isVerifyingTweet}
+            disabled={isVerifyingTweet || !isValid || !isDirty}
+          >
+            Verify
+          </VerifyButton>
+        </Form>
       </ContentContainer>
     </PrimaryContainer>
   );
