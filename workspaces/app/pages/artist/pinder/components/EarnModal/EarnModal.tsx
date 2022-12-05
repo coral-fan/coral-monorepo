289768a22d29a65e69bbddf8e84e2b2038564785
Aki Gao <aki@coral.fan>
2022-12-05 16:25:12 -0500

    Fix bug with social share code generation

--- workspaces/app/pages/artist/pinder/components/EarnModal/EarnModal.tsx
+++ workspaces/app/pages/artist/pinder/components/EarnModal/EarnModal.tsx
@@ -3,7 +3,7 @@ import { getDocumentData } from 'libraries/firebase';
 import { getEarnCode, SocialShareCampaignData, SocialShareData } from 'libraries/models';
 import { getCoralAPIAxios } from 'libraries/utils';
 import { useErrorToast } from 'libraries/utils/toasts';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import tokens from 'styles/tokens';
 import { CampaignExpired, CampaignNotActive, HasVerified } from './components';
 import { ShareOnTwitter } from './components/ShareOnTwitter/ShareOnTwitter';
@@ -31,6 +31,16 @@ export const EarnModal = ({
   const potentialEarnCode = useMemo(() => getEarnCode(uid, campaignId), [uid, campaignId]);
 
   const errorToast = useErrorToast();
+
+  /**
+   * below ref is necessary to prevent calling the generate code api twice because this component renders twice back to back (not 100% why)
+   * because of cold starts in Vercel serverless functions, it takes up to 3 - 4 seconds for the first generate code call to resolve when deployed
+   * as a result, on the 2nd render, social share code doesn't exist and the generate code api is called again
+   * however, this double call results in "Error: 4 DEADLINE_EXCEEDED: Deadline Exceeded"
+   * https://stackoverflow.com/questions/66865783/google-cloud-firestore-error-4-deadline-exceeded-deadline-exceeded-while-crea
+   * showing the user a could not generate code toast (even though the code was generated)
+   */
+  const isGeneratingCode = useRef(false);
 
   const isCampaignExpired = useMemo(
     () =>
@@ -60,36 +70,41 @@ export const EarnModal = ({
     };
 
     const generateSocialShareCode = async () => {
-      try {
-        const {
-          data: { socialShareCode },
-        } = await coralAPI.post<{ socialShareCode: string }>('generate-social-share-code', {
-          campaignId,
-        });
-        setSocialShareCode(socialShareCode);
-      } catch (e) {
-        errorToast('Could not generate code');
-        console.error(e);
+      if (!isGeneratingCode.current) {
+        isGeneratingCode.current = true;
+        try {
+          const {
+            data: { socialShareCode },
+          } = await coralAPI.post<{ socialShareCode: string }>('generate-social-share-code', {
+            campaignId,
+          });
+          setSocialShareCode(socialShareCode);
+        } catch (e) {
+          errorToast('Could not generate code');
+          console.error(e);
+        } finally {
+          isGeneratingCode.current = false;
+        }
       }
     };
 
     setIsCheckingSocialShareCode(true);
 
-    getSocialShareCampaignData().then((data) => {
-      if (data && !isCampaignExpired) {
-        getShareCodeData().then((data) => {
-          if (data) {
-            data.sharedSocials.twitter
-              ? setHasVerified(true)
-              : setSocialShareCode(potentialEarnCode);
-          } else {
-            generateSocialShareCode();
-          }
-        });
-      }
-    });
-
-    setIsCheckingSocialShareCode(false);
+    getSocialShareCampaignData()
+      .then((data) => {
+        if (data && !isCampaignExpired) {
+          getShareCodeData().then((data) => {
+            if (data) {
+              data.sharedSocials.twitter
+                ? setHasVerified(true)
+                : setSocialShareCode(potentialEarnCode);
+            } else {
+              generateSocialShareCode();
+            }
+          });
+        }
+      })
+      .finally(() => setIsCheckingSocialShareCode(false));
   }, [campaignId, potentialEarnCode, errorToast, isCampaignExpired]);
 
   return (
