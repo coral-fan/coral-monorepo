030eb70d8b85d7d2f7625145d87e7da2813c1ea1
Aki Gao <aki@coral.fan>
2023-01-17 15:26:19 -0500

    Remove ESLint rule

--- workspaces/app/.eslintrc.js
+++ workspaces/app/.eslintrc.js
@@ -2,7 +2,4 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
 };
