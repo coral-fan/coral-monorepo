047a3b01c23a677bb49df0493c83079cc04e0c5c
Aki Gao <aki@coral.fan>
2023-02-02 17:17:17 -0500

    Update add artist script for Tayla Parx

--- workspaces/app/scripts/artist/add.ts
+++ workspaces/app/scripts/artist/add.ts
@@ -1,21 +1,21 @@
 import { getDocumentReferenceServerSide } from 'libraries/firebase';
 import { ArtistData } from 'libraries/models';
 
-const id = '0xCa86C97A5f5D8906DAeF4Bc83Ad9665D5298d35B';
+const id = 'tayla-parx';
 
 const artist: ArtistData = {
-  name: 'Van Buren Records',
+  name: 'Tayla Parx',
   profilePhoto: {
-    src: 'https://firebasestorage.googleapis.com/v0/b/coral-fan.appspot.com/o/artists%2F0xCa86C97A5f5D8906DAeF4Bc83Ad9665D5298d35B%2FVanBuren_ProfilePhoto.jpeg?alt=media&token=66199a10-a4d9-4e81-b2f8-8afbd5f82d06',
+    src: 'https://firebasestorage.googleapis.com/v0/b/coral-fan.appspot.com/o/artists%2Ftayla-parx%2FTaylaParx-Profile%20Image.jpg?alt=media&token=450a57f5-ed1c-4409-a5eb-cc1ab66636c0',
     offsetPercentages: [0, 0],
     scale: 1,
   },
-  bio: ' Community is all about creating a feeling of togetherness that fosters a sense of belonging—elevating each other’s value and leaving a bigger impact than any one individual ever could. Van Buren Records is a community, but also so much more.',
-  quote: '',
+  bio: 'Hey there. I make beats you all love to listen to.</br>Sometimes I do other things like drop cool NFTs.',
+  quote: null,
   socialHandles: {
-    twitter: 'VanBurenRecords',
-    spotify: '2T6EOVQ5lAQc64poyLnXmj',
-    instagram: 'vanburenrecords',
+    instagram: 'taylaparx',
+    spotify: '1LzWWI9v4UKdbBgz8fqi15',
+    twitter: 'TAYLAPARX',
   },
   collectionIds: [],
 };
@@ -23,11 +23,11 @@ const artist: ArtistData = {
 const addArtist = async () => {
   console.log('adding artist...');
   const artistRef = await getDocumentReferenceServerSide('artists', id);
-  const artistDocSnapshot = await artistRef.get();
+  // const artistDocSnapshot = await artistRef.get();
 
-  if (artistDocSnapshot.exists) {
-    throw `artist with id ${id} already exists.`;
-  }
+  // if (artistDocSnapshot.exists) {
+  //   throw `artist with id ${id} already exists.`;
+  // }
 
   await artistRef.set(artist);
 
