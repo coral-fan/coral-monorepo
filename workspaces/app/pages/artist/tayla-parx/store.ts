d6c76b860461c77357ff47c16a0e7d3c021b8096
Aki Gao <aki@coral.fan>
2023-02-07 15:48:25 -0500

    Add utility to get initial Tayla Parx store state

--- workspaces/app/pages/artist/tayla-parx/store.ts
+++ workspaces/app/pages/artist/tayla-parx/store.ts
@@ -1,5 +1,7 @@
-import { createStore } from 'libraries/store';
-import { TaylaParxData } from './types';
+import { getDoesOwnToken } from 'libraries/blockchain/utils';
+import { getArtist } from 'libraries/models';
+import { createStore } from 'libraries/store/store';
+import { TaylaParxData, TaylaParxMetadata } from './types';
 
 export const {
   Provider: TaylaParxProvider,
@@ -7,3 +9,25 @@ export const {
   initializeStore: initializeTaylaParxStore,
   useCreateStore: useCreateTaylaParxStore,
 } = createStore<TaylaParxData>();
+
+export const getInitialTaylaParxStoreState = async (
+  address: string | undefined
+): Promise<TaylaParxData> => {
+  const taylaParxData = await getArtist<TaylaParxMetadata>('tayla-parx');
+
+  if (!taylaParxData) {
+    throw new Error('Tayla Parx data is undefined.');
+  }
+
+  const initialDoesUserHaveTaylaParxAllAccessPass =
+    address === undefined
+      ? false
+      : await getDoesOwnToken(taylaParxData.metadata.id.allAccessPass, address);
+
+  const taylaParxStore = initializeTaylaParxStore({
+    ...taylaParxData,
+    initialDoesUserHaveAccessPass: initialDoesUserHaveTaylaParxAllAccessPass,
+  });
+
+  return JSON.parse(JSON.stringify(taylaParxStore.getState()));
+};
