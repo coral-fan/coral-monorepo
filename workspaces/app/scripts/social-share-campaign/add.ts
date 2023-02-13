85fe68d961d55f432fae9a986a3a81b52a421142
Aki Gao <aki@coral.fan>
2023-02-12 19:39:40 -0500

    Update add social share campaign script

--- workspaces/app/scripts/social-share-campaign/add.ts
+++ workspaces/app/scripts/social-share-campaign/add.ts
@@ -12,11 +12,11 @@ const socialShareCampaigns: SocialShareCampaignData[] = [
     name: 'Tayla Parx All Access Pass NFT Social Share Campaign',
     description: 'Tayla Parx All Access Pass NFT social share campaign',
     shareUrl: 'N/A',
-    pointsValue: 150,
-    totalPointsPool: 3000,
+    pointsValue: 20,
+    totalPointsPool: 3500,
     totalPointsEarned: 0,
-    startDate: '2023-02-09T00:00:00.000Z',
-    endDate: '2024-02-09T00:00:00.000Z',
+    startDate: '2023-02-12T00:00:00.000Z',
+    endDate: '2024-02-12T00:00:00.000Z',
     isActive: true,
     createdBy: 'tayla-parx',
     socials: ['twitter'],
@@ -30,15 +30,40 @@ const socialShareCampaigns: SocialShareCampaignData[] = [
     ]),
     whitelistId: 'test-whitelist',
   },
+  {
+    name: "Tayla Parx For What It's Worth On Spotify Social Share Campaign",
+    description: "Tayla Parx For What It's Worth on Spotify social share campaign",
+    shareUrl: 'N/A',
+    pointsValue: 20,
+    totalPointsPool: 3500,
+    totalPointsEarned: 0,
+    startDate: '2023-02-12T00:00:00.000Z',
+    endDate: '2024-02-12T00:00:00.000Z',
+    isActive: true,
+    createdBy: 'tayla-parx',
+    socials: ['twitter'],
+    requiredContent: {
+      urls: [],
+      usernames: ['TAYLAPARX', 'coral__fan'],
+      topics: [],
+    },
+    defaultContent: getDefaultContentString([
+      `Go stream @TAYLAPARX new single “For What It's Worth” now on Spotify.`,
+      "I'm supporting Tayla via @coral__fan.",
+    ]),
+    whitelistId: 'test-whitelist',
+  },
 ];
 
-const addSocialShareCampaign = async (socialShareCampaign: SocialShareCampaignData) => {
-  console.log('Adding social share campaign...');
-  const socialShareCollection = await getCollectionReferenceServerSide('social-share-campaigns');
+const addSocialShareCampaign = async () => {
+  for (const data of socialShareCampaigns) {
+    console.log('Adding social share campaign...');
+    const socialShareCollection = await getCollectionReferenceServerSide('social-share-campaigns');
 
-  const res = await socialShareCollection.add(socialShareCampaign);
+    const res = await socialShareCollection.add(data);
 
-  console.log('Added social-share-campaign with ID: ', res.id);
+    console.log('Added social-share-campaign with ID: ', res.id);
+  }
 };
 
-socialShareCampaigns.forEach(addSocialShareCampaign);
+addSocialShareCampaign();
