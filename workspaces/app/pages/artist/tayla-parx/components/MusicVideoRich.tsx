937f9f5c565d789ffe8ed43d5f8c1444f431d325
Aki Gao <aki@coral.fan>
2023-02-12 19:08:28 -0500

    Remove share and earn section

--- workspaces/app/pages/artist/tayla-parx/components/MusicVideoRich.tsx
+++ workspaces/app/pages/artist/tayla-parx/components/MusicVideoRich.tsx
@@ -1,10 +1,7 @@
 import styled from '@emotion/styled';
 import { WebPlayer } from 'components/features/stream/components/StreamPlayer/components';
 import { QUERY } from 'styles';
-import { useTaylaParxStore } from '../store';
-import { Earn } from './Earn';
 import { LargeHeader } from './LargeHeader';
-import { ShareAndEarnButton } from './pills';
 
 const Container = styled.div`
   display: flex;
@@ -17,24 +14,9 @@ const Container = styled.div`
   }
 `;
 
-const CTAContainer = styled.div`
-  display: flex;
-  gap: 10px;
-`;
-
-export const MusicVideoRich = () => {
-  const {
-    metadata: { id },
-  } = useTaylaParxStore();
-  return (
-    <Container>
-      <LargeHeader>Watch Tayla Parx’s new music video “Rich”</LargeHeader>
-      <CTAContainer>
-        <Earn campaignId={id.shareToEarnCampaign.musicVideoRich}>
-          <ShareAndEarnButton />
-        </Earn>
-      </CTAContainer>
-      <WebPlayer mediaId="ac9fdfb31a11efc125/0de95e08f5cfb66f" />
-    </Container>
-  );
-};
+export const MusicVideoRich = () => (
+  <Container>
+    <LargeHeader>Watch Tayla Parx’s new music video “Rich”</LargeHeader>
+    <WebPlayer mediaId="ac9fdfb31a11efc125/0de95e08f5cfb66f" />
+  </Container>
+);
